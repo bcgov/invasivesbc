bd1688525fc2be5ea766c3ed1f45a11203e86495
Roland Stens <roland.stens@gmail.com>
2020-09-23 17:03:27 -0700

    First kick at the APP pipeline

---
+++ .github/ISSUE_TEMPLATE/task.md
@@ -0,0 +1,20 @@
+---
+name: Task
+about: work that cannot be written as a User Story
+title: ''
+labels: Task
+assignees: ''
+
+---
+
+**Describe the task**
+A clear and concise description of what the task is.
+
+**Acceptance Criteria**
+- [ ] first
+- [ ] second
+- [ ] third
+
+**Additional context**
+- Add any other context about the task here.
+- Or here
---
+++ app/.pipeline/lib/build.js
@@ -0,0 +1,29 @@
+'use strict';
+const {OpenShiftClientX} = require('pipeline-cli')
+const path = require('path');
+
+module.exports = (settings)=>{
+  const phases = settings.phases
+  const options = settings.options
+  const oc=new OpenShiftClientX(Object.assign({'namespace':phases.build.namespace}, options));
+  const phase='build'
+  let objects = []
+  const templatesLocalBaseUrl =oc.toFileUrl(path.resolve(__dirname, 'app/openshift'))
+  const staticBranches = settings.staticBranches;
+  const changeId = phases[phase].changeId;
+
+  // The building of your cool app goes here ▼▼▼
+  objects.push(...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/app.bc.yaml`, {
+    'param':{
+      'NAME': phases[phase].name,
+      'SUFFIX': phases[phase].suffix,
+      'VERSION': phases[phase].tag,
+      'SOURCE_REPOSITORY_URL': oc.git.http_url,
+      'SOURCE_REPOSITORY_REF': phases[phase].branch || oc.git.ref
+    }
+  }));
+
+  oc.applyRecommendedLabels(objects, phases[phase].name, phase, phases[phase].changeId, phases[phase].instance)
+  console.log(`${JSON.stringify(objects, null, 2)}`);
+  oc.applyAndBuild(objects)
+}
