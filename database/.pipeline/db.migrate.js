61833f00591446800b5f8fe9b408d2a367282a56
Nick Phura <nickphura@gmail.com>
2020-10-08 18:06:39 -0700

    928: wip 12

--- database/.pipeline/db.migrate.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const config = require('./lib/config.js');
-const migrateDatabaseTask = require('./lib/db.migrate.js');
-
-const settings = { ...config, phase: settings.options.env };
-
-// apply database migrations
-migrateDatabaseTask(settings);
