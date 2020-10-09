a5e92c4477194d41f9e0a4f16671168c08fb8f7c
Anissa Agahchen <anissa.agahchen@akimilabs.com>
2020-10-09 11:39:08 -0700

    Photo on device (#75)
    * Photos associated to activity in DB
    
    * Photo functionality works on web and on mobile devices
    
    * Adding .DS_Store to .gitignore
    
    * remove unused imports
    
    * minor code clean up
    
    * refactoring to use the dataUrl to store and retrieve photos
    
    * minor clean up

--- app/src/components/photo/PhotoContainer.tsx
+++ app/src/components/photo/PhotoContainer.tsx
@@ -1,7 +1,20 @@
 import { CardMedia, Fab, Grid, Paper, SvgIcon } from '@material-ui/core';
 import { AddAPhoto, DeleteForever } from '@material-ui/icons';
-import { usePhotoGallery } from 'hooks/usePhotoGallery';
-import React from 'react';
+import { useCamera } from '@ionic/react-hooks/camera';
+import { CameraResultType, CameraSource } from '@capacitor/core';
+import { DatabaseContext } from 'contexts/DatabaseContext';
+import React, { useState, useContext, useEffect } from 'react';
+
+import {
+  ActivityStatus,
+} from 'constants/activities'
+
+interface Photo {
+  filepath: string;
+  webviewPath?: string;
+  base64?: string;
+  dataUrl?: string;
+}
 
 interface IPhotoContainerProps {
   classes?: any;
@@ -9,7 +22,65 @@ interface IPhotoContainerProps {
 }
 
 const PhotoContainer: React.FC<IPhotoContainerProps> = (props) => {
-  const { photos, takePhoto, deletePhotos } = usePhotoGallery();
+  const { getPhoto } = useCamera();
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const databaseContext = useContext(DatabaseContext);
+
+  const initPhotos = async function (activityDoc: any) {
+    const dbDoc = await databaseContext.database.get(activityDoc._id)
+    const dbPhotos = dbDoc.photos || [];
+    setPhotos(dbPhotos);
+  }
+
+  const updateDB = async function (activityDoc: any) {
+    await databaseContext.database.upsert(activityDoc._id, (doc) => {
+      return { ...doc, photos: photos, status: ActivityStatus.EDITED, dateUpdated: new Date() };
+    });
+  }
+
+  const takePhoto = async (doc: any) => {
+    const cameraPhoto = await getPhoto({
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+
+    console.log("cameraPhoto object");
+    console.dir(cameraPhoto);
+
+    const fileName = new Date().getTime() + "." + cameraPhoto.format;
+    const photo = {
+      filepath: fileName,
+      dataUrl: cameraPhoto.dataUrl
+    };
+
+    const newPhotos = [photo, ...photos];
+    setPhotos(newPhotos);
+  };
+
+  const deletePhotos = async (doc: any) => {
+    setPhotos([]);
+  };
+
+  const deletePhoto = async (filepath: any) => {
+
+    const reducedPhotos = photos.filter((photo) => photo.filepath != filepath);
+    setPhotos(reducedPhotos);
+  };
+
+  useEffect(() => {
+    if (props) {
+      initPhotos(props.activity);
+    }
+  }, [props]);
+
+  useEffect(() => {
+    if (props) {
+      updateDB(props.activity);
+    }
+  }, [photos]);
+
+
 
   // Grid with overlays: https://material-ui.com/components/grid-list/
   return (
@@ -19,21 +90,23 @@ const PhotoContainer: React.FC<IPhotoContainerProps> = (props) => {
           {photos.map((photo, index) => (
             <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
               <Paper>
-                <CardMedia src={photo.base64 ?? photo.webviewPath} component="img" />
+                <CardMedia src={photo.dataUrl} component="img" />
+                <div style={{ cursor: 'pointer' }} onClick={() => deletePhoto(photo.filepath)}><SvgIcon component={DeleteForever} /> {photo.filepath}</div>
               </Paper>
             </Grid>
-          ))}
+            ))
+          }
         </Grid>
       </div>
       <div>
         <Grid container spacing={3} justify="center">
           <Grid item>
-            <Fab color="primary" onClick={() => takePhoto()}>
+            <Fab color="primary" onClick={() => takePhoto(props.activity)}>
               <SvgIcon component={AddAPhoto} />
             </Fab>
           </Grid>
           <Grid item>
-            <Fab onClick={() => deletePhotos()}>
+            <Fab onClick={() => deletePhotos(props.activity)}>
               <SvgIcon component={DeleteForever} />
             </Fab>
           </Grid>
