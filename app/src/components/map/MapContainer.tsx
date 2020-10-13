866e28796f8a968ad0461b83dfc983251598c940
Jamie Popkin <popkinj@littleearth.ca>
2020-10-13 16:14:08 -0700

    A little cleanup

--- app/src/components/map/MapContainer.tsx
+++ app/src/components/map/MapContainer.tsx
@@ -119,13 +119,12 @@ const MapContainer: React.FC<IMapContainerProps> = (props) => {
     });
 
 
-    // const esriBase = L.tileLayer(
-    //   'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-    //   {
-    //     maxZoom: 24,
-    //     maxNativeZoom: 17
-    //   }
-    // ).addTo(map);
+    esriBase.on('saveend', (e) => {
+      console.log(`Saved ${e.lengthSaved} tiles`)
+    })
+    esriBase.on('tilesremoved', () => {
+      console.log('Removed all tiles')
+    });
 
     const bcBase = L.tileLayer(
       'https://maps.gov.bc.ca/arcgis/rest/services/province/roads_wm/MapServer/tile/{z}/{y}/{x}',
