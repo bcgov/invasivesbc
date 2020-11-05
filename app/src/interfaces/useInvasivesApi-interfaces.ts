f6e559f620c51ea5065bed83ffae07ebe3398bf7
Micheal W. Wells <micheal.w.wells@gov.bc.ca>
2020-11-05 15:39:13 -0800

    Wire front end to point of interest API (#149)
    * add point of interest api call logic ot useinvasives api and trip data controls

--- app/src/interfaces/useInvasivesApi-interfaces.ts
+++ app/src/interfaces/useInvasivesApi-interfaces.ts
@@ -1,4 +1,5 @@
 import { ActivityType, ActivitySubtype } from 'constants/activities';
+import { PointOfInterestSubtype, PointOfInterestType } from 'constants/pointsOfInterest';
 import { Feature } from 'geojson';
 
 /**
@@ -84,4 +85,76 @@ export interface IMedia {
   description?: string;
   file_name: string;
   encoded_file: string;
+}
+
+/**
+ * PointOfInterest search filter criteria.
+ *
+ * @export
+ * @interface IPointOfInterestSearchCriteria
+ */
+export interface IPointOfInterestSearchCriteria {
+  /**
+   * The page of results to return. Starts at 0.
+   *
+   * @type {number}
+   * @memberof IPointOfInterestSearchCriteria
+   */
+  page?: number;
+  /**
+   * The number of results to return.
+   *@
+   * @type {number}
+   * @memberof IPointOfInterestSearchCriteria
+   */
+  limit?: number;
+  /**
+   * PointOfInterest type filter.
+   *
+   * @type {string}
+   * @memberof IPointOfInterestSearchCriteria
+   */
+  point_of_interest_type?: string;
+  /**
+   * PointOfInterest sub type filter.
+   *
+   * @type {string}
+   * @memberof IPointOfInterestSearchCriteria
+   */
+  point_of_interest_subtype?: string;
+  /**
+   * Date start filter. Defaults time to start of day.
+   *
+   * @type {Date}
+   * @memberof IPointOfInterestSearchCriteria
+   */
+  date_range_start?: Date;
+  /**
+   * Date end filter. Defaults time to end of day.
+   *
+   * @type {Date}
+   * @memberof IPointOfInterestSearchCriteria
+   */
+  date_range_end?: Date;
+  /**
+   * GeoJSON feature (of type polygon) to search in.
+   *
+   * @type {Feature}
+   * @memberof IPointOfInterestSearchCriteria
+   */
+  search_feature?: Feature;
+}
+
+/**
+ * Create point_of_interest endpoint post body.
+ *
+ * @export
+ * @interface ICreatePointOfInterest
+ */
+export interface ICreatePointOfInterest {
+  point_of_interest_type: PointOfInterestType;
+  point_of_interest_subtype: PointOfInterestSubtype;
+  geometry: Feature[];
+  media: IMedia[];
+  form_data: any;
 }
