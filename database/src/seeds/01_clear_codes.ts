4fe05f36e007dbd0027714cb1703fa9b71c240dc
Nick Phura <Nick.Phura@quartech.com>
2020-11-12 16:28:06 -0800

    62: single activity edit (#154)
    * 62: Update activity_incoming_data table migration.
    - Add new date columns
    - Add index on activity_id
    - Remove version column
    - Tweak other index names
    - Merge media_keys migration into activity_incoming_data migration.
    - Run linter on database folder
    - Add DB_SCHEMA variable where applicable
    - Remove some old migrations

--- database/src/seeds/01_clear_codes.ts
+++ database/src/seeds/01_clear_codes.ts
@@ -1,17 +1,13 @@
-import * as Knex from "knex";
-//import csv from "csv-parse";
-import parse from "csv-parse";
-import { fileURLToPath } from "url";
-const fs = require('fs');
+import Knex from 'knex';
 
-const results = [];
+const DB_SCHEMA = process.env.DB_SCHEMA || 'invasivesbc';
 
 export async function seed(knex: Knex): Promise<void> {
-  await knex("code").withSchema('invasivesbc').del();
-  await knex("code_header").withSchema('invasivesbc').del();
-  await knex("code_category").withSchema('invasivesbc').del();
+  await knex('code').withSchema(DB_SCHEMA).del();
+  await knex('code_header').withSchema(DB_SCHEMA).del();
+  await knex('code_category').withSchema(DB_SCHEMA).del();
 
-  await knex.raw('alter sequence invasivesbc.code_code_id_seq restart with 1');
-  await knex.raw('alter sequence invasivesbc.code_header_code_header_id_seq restart with 1');
-  await knex.raw('alter sequence invasivesbc.code_category_code_category_id_seq restart with 1');
-};
+  await knex.raw(`alter sequence ${DB_SCHEMA}.code_code_id_seq restart with 1`);
+  await knex.raw(`alter sequence ${DB_SCHEMA}.code_header_code_header_id_seq restart with 1`);
+  await knex.raw(`alter sequence ${DB_SCHEMA}.code_category_code_category_id_seq restart with 1`);
+}
--- database/src/seeds/03_business_codes.ts
+++ database/src/seeds/03_business_codes.ts
@@ -1,109 +1,116 @@
-import * as Knex from "knex";
-import parse from "csv-parse";
-const fs = require('fs');
+import Knex from 'knex';
+import parse from 'csv-parse';
+import * as fs from 'fs';
+
+const DB_SCHEMA = process.env.DB_SCHEMA || 'invasivesbc';
 
 export async function seed(knex: Knex): Promise<void> {
-  let category_id = await _load_category(knex);
-  let header_name_map = await _load_headers(knex, category_id);
+  const category_id = await _load_category(knex);
+  const header_name_map = await _load_headers(knex, category_id);
   await _load_codes(knex, header_name_map);
-};
-
-async function _load_category(knex: Knex): Promise<number> {
-    // Insert code_category entry
-    let category_id = (await knex("code_category").withSchema('invasivesbc').returning('code_category_id').insert([
-        { code_category_name: 'invasives', code_category_title: 'Invasives Codes', 
-            code_category_description: 'The category for invasives codes', created_by_user_id: 1, updated_by_user_id: 1},
-    ]))[0];
-
-    // Insert code_header entry
-    // const header_id = (await knex("code_header").withSchema('invasivesbc').returning('code_header_id').insert(
-    //     { code_category_id: category_id, code_header_name: 'app_role_code', code_header_title: 'Application Access Role Codes', code_header_description: 'This category holds definition of different user roles of the system along with cross domain code', 
-    //         valid_from: knex.fn.now(), created_by_user_id: 1, updated_by_user_id: 1}
-    // ))[0];
-
-    return category_id;
 }
 
-async function _load_headers(knex: Knex, category_id: number): Promise<Map<string,number>> {
+async function _load_category(knex: Knex): Promise<number> {
+  // Insert code_category entry
+  const category_id = (
+    await knex('code_category')
+      .withSchema(DB_SCHEMA)
+      .returning('code_category_id')
+      .insert([
+        {
+          code_category_name: 'invasives',
+          code_category_title: 'Invasives Codes',
+          code_category_description: 'The category for invasives codes',
+          created_by_user_id: 1,
+          updated_by_user_id: 1
+        }
+      ])
+  )[0];
 
-  let file = __dirname + '/data/invasives/business_codes.csv';
+  return category_id;
+}
 
-  let header_name_map = new Map<string,number>();
+async function _load_headers(knex: Knex, category_id: number): Promise<Map<string, number>> {
+  const file = __dirname + '/data/invasives/business_codes.csv';
 
-  let headerRows = [];
+  const header_name_map = new Map<string, number>();
+
+  const headerRows = [];
 
   await new Promise((resolve, reject) => {
-    fs.createReadStream(file)  
-    .pipe(parse({
-      columns: true
-    }))        
-    .on('data', (dataRow) => {
-      let header_id = 0;
+    fs.createReadStream(file)
+      .pipe(
+        parse({
+          columns: true
+        })
+      )
+      .on('data', (dataRow) => {
+        const header_id = 0;
 
-      if (!header_name_map.has(dataRow.code_header_name)) {
-        // record the new header name
-        header_name_map.set(dataRow.code_header_name,header_id);
+        if (!header_name_map.has(dataRow.code_header_name)) {
+          // record the new header name
+          header_name_map.set(dataRow.code_header_name, header_id);
 
-        // construct the header row and add it to the header rows array
-        let extendedRow = {code_category_id: category_id, code_header_name: dataRow.code_header_name, code_header_title: dataRow.code_header_name, 
-          code_header_description: dataRow.code_header_name, valid_from: knex.fn.now(), created_by_user_id: 1, updated_by_user_id: 1}
-        headerRows.push(extendedRow);
-      }
-    })          
-    .on('error', (error) => reject(error))
-    .on('end', () => { 
-      resolve(); 
-    });
+          // construct the header row and add it to the header rows array
+          const extendedRow = {
+            code_category_id: category_id,
+            code_header_name: dataRow.code_header_name,
+            code_header_title: dataRow.code_header_name,
+            code_header_description: dataRow.code_header_name,
+            valid_from: knex.fn.now(),
+            created_by_user_id: 1,
+            updated_by_user_id: 1
+          };
+          headerRows.push(extendedRow);
+        }
+      })
+      .on('error', (error) => reject(error))
+      .on('end', () => {
+        resolve();
+      });
   });
-  
+
   // insert headers
-  let headerResults = (await knex("code_header").withSchema('invasivesbc').returning('*').insert(
-      headerRows)) as Object[];
+  const headerResults = (await knex('code_header').withSchema(DB_SCHEMA).returning('*').insert(headerRows)) as object[];
 
   // update the header name map
-  headerResults.forEach(result => {
-    header_name_map.set(result["code_header_name"],result["code_header_id"]);
+  headerResults.forEach((result) => {
+    header_name_map.set(result['code_header_name'], result['code_header_id']);
   });
 
   return header_name_map;
-};
+}
 
-async function _load_codes(knex: Knex, header_name_map: Map<string,number>): Promise<void> {
-
+async function _load_codes(knex: Knex, header_name_map: Map<string, number>): Promise<void> {
   const file = __dirname + '/data/invasives/business_codes.csv';
 
-  let results = [];
+  const results = [];
 
   await new Promise((resolve, reject) => {
-    fs.createReadStream(file)  
-    .pipe(parse({
-      columns: true
-    }))        
-    .on('data', (dataRow) => {
-      const extendedRow = {code_header_id: header_name_map.get(dataRow.code_header_name), code_name: dataRow.code_name, code_description: dataRow.code_description, code_sort_order: 0, valid_from: knex.fn.now(), created_by_user_id: 1, updated_by_user_id: 1}
-      results.push(extendedRow);
-    })          
-    .on('error', (error) => reject(error))
-    .on('end', () => { 
-      resolve(); 
-    });   
+    fs.createReadStream(file)
+      .pipe(
+        parse({
+          columns: true
+        })
+      )
+      .on('data', (dataRow) => {
+        const extendedRow = {
+          code_header_id: header_name_map.get(dataRow.code_header_name),
+          code_name: dataRow.code_name,
+          code_description: dataRow.code_description,
+          code_sort_order: 0,
+          valid_from: knex.fn.now(),
+          created_by_user_id: 1,
+          updated_by_user_id: 1
+        };
+        results.push(extendedRow);
+      })
+      .on('error', (error) => reject(error))
+      .on('end', () => {
+        resolve();
+      });
   });
 
   // Insert code entries
-  await knex("code").withSchema('invasivesbc').insert(results);
-
-  // detect duplicate codes
-  // let codeMap = new Map<string,object>();
-  // console.log("codeRows:");
-  // results.forEach(row => {
-  //   let key = row["code_header_id"] + row["code_name"];
-  //   if (!codeMap.has(key)) {
-  //     // new code
-  //     codeMap.set(key,row);
-  //   } else {
-  //     // existing code
-  //     console.log("existing code found.  new row: ", row);
-  //     console.log("original row: ", codeMap.get(key));
-  //   }
-  // });
-};
+  await knex('code').withSchema(DB_SCHEMA).insert(results);
+}
