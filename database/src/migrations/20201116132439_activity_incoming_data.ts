03da9350446a70d5cecd3b7b1eeba30240b019a7
Jamie Popkin <popkinj@littleearth.ca>
2020-11-17 15:41:23 -0800

    Ready for next migration

--- database/src/migrations/20201116132439_activity_incoming_data.ts
+++ database/src/migrations/20201116132439_activity_incoming_data.ts
@@ -22,16 +22,52 @@ export async function up(knex: Knex): Promise<void> {
     COMMENT ON COLUMN ${DB_SCHEMA}.application_user.ownership IS 'The information is sourced from the Crown Land Registry which is the primary government record of lands transferred out of Crown Provincial ownership (as defined under the Land Act).';
 
 
-    alter table activity_incoming_data
+    alter table ${DB_SCHEMA}.activity_incoming_data
     add column elevation integer;
 
     COMMENT ON COLUMN ${DB_SCHEMA}.application_user.elevation IS 'Metres above sea level. Abstracted from the Federal Government API for the Canadian Digital Elevation Model (CDEM).';
 
 
-    alter table activity_incoming_data
+    alter table ${DB_SCHEMA}.activity_incoming_data
     add column biogeoclimatic_zones varchar(30);
 
     COMMENT ON COLUMN ${DB_SCHEMA}.application_user.elevation IS 'Corporate provincial digital Biogeoclimatic Ecosystem Classification (BEC) Zone/Subzone/Variant/Phase map';
+
+
+    alter table ${DB_SCHEMA}.activity_incoming_data
+    add column well_proximity integer;
+
+    COMMENT ON COLUMN ${DB_SCHEMA}.application_user.well_proximity IS 'Distance to the closest well in metres.';
+
+
+    alter table ${DB_SCHEMA}.activity_incoming_data
+    add column utm_zone integer;
+
+    COMMENT ON COLUMN ${DB_SCHEMA}.application_user.utm_zone IS 'Northern hemisphere UTM zone number.';
+
+
+    alter table ${DB_SCHEMA}.activity_incoming_data
+    add column utm_northing real;
+
+    COMMENT ON COLUMN ${DB_SCHEMA}.application_user.utm_northing IS 'Northern hemisphere UTM Y coordinate in metres.';
+
+
+    alter table ${DB_SCHEMA}.activity_incoming_data
+    add column utm_easting real;
+
+    COMMENT ON COLUMN ${DB_SCHEMA}.application_user.utm_easting IS 'Northern hemisphere UTM X coordinate in metres.';
+
+
+    alter table ${DB_SCHEMA}.activity_incoming_data
+    add column albers_northing real;
+
+    COMMENT ON COLUMN ${DB_SCHEMA}.application_user.albers_northing IS 'Albers Y coordinate in metres.';
+
+
+    alter table ${DB_SCHEMA}.activity_incoming_data
+    add column albers_easting real;
+
+    COMMENT ON COLUMN ${DB_SCHEMA}.application_user.albers_easting IS 'Albers X coordinate in metres.';
   `);
 }
 
@@ -41,13 +77,26 @@ export async function down(knex: Knex): Promise<void> {
     set schema '${DB_SCHEMA}';
     set search_path = ${DB_SCHEMA},public;
 
-    alter table activity_incoming_data
+    alter table ${DB_SCHEMA}.activity_incoming_data
     rename column ownership to forest_cover_ownership;
 
     COMMENT ON COLUMN ${DB_SCHEMA}.application_user.forest_cover_ownership IS 'This data product is a generalized description of the primary ownership of forest lands for use in strategic decision making such as Timber Supply Analysis. It is based upon the structure used in the Forest Inventory Planning (FIP/FC1) format. It is created and revised using information from Min of Agriculture and Lands Registries Branch.';
 
-    alter table activity_incoming_data
-    drop column elevation;
+    alter table ${DB_SCHEMA}.activity_incoming_data drop column elevation;
+
+    alter table ${DB_SCHEMA}.activity_incoming_data drop column biogeoclimatic_zones;
+
+    alter table ${DB_SCHEMA}.activity_incoming_data drop column well_proximity;
+
+    alter table ${DB_SCHEMA}.activity_incoming_data drop column utm_zone;
+
+    alter table ${DB_SCHEMA}.activity_incoming_data drop column utm_northing;
+
+    alter table ${DB_SCHEMA}.activity_incoming_data drop column utm_easting;
+
+    alter table ${DB_SCHEMA}.activity_incoming_data drop column albers_northing;
+
+    alter table ${DB_SCHEMA}.activity_incoming_data drop column albers_easting;
 
   `);
 }
