78ce9834e49eeecedb613fb0e4faa2721c136151
Jamie Popkin <popkinj@littleearth.ca>
2020-11-27 15:45:14 -0800

    New migration

---
+++ database/src/migrations/20201127153245_activity_incoming_data.ts
@@ -0,0 +1,21 @@
+import * as Knex from "knex";
+
+const DB_SCHEMA = process.env.DB_SCHEMA || 'invasivesbc';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(`
+    -- ### Increasing column lengths for ${DB_SCHEMA}.activity_incoming_data ### --
+
+    set schema '${DB_SCHEMA}';
+    set search_path = ${DB_SCHEMA},public;
+  `);
+}
+
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw(`
+    set schema '${DB_SCHEMA}';
+    set search_path = ${DB_SCHEMA},public;
+  `);
+}
+
