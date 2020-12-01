9365907ded1200b9ca0e12aef830795e7657591f
Nick Phura <nickphura@gmail.com>
2020-11-30 17:57:01 -0800

    158: Custom RJSF templates to support multi-column forms. - Re-organized `/rjsf` folder and updated imports as needed. - Moved material-ui theme objects to separate files - Wrap form in rjsf theme provider

---
+++ app/src/rjsf/README.md
@@ -0,0 +1,30 @@
+# bcgov/invasivesbc/app/src/rjsf
+
+Contains config and components used by rjsf.
+
+## Documentation
+
+- [rjsf GitHub](https://github.com/rjsf-team/react-jsonschema-form)
+- [rjsf docs](https://react-jsonschema-form.readthedocs.io/en/latest/)
+- [rjsf demos](https://rjsf-team.github.io/react-jsonschema-form/)
+- [rjsf material-ui theme](https://www.npmjs.com/package/@rjsf/material-ui)
+
+## Folders
+
+### templates
+
+The 3 base [rjsf templates](https://react-jsonschema-form.readthedocs.io/en/latest/advanced-customization/custom-templates/) used by rjsf when rendering form elements.
+
+These are copied from the [@rjsf/material-ui](https://www.npmjs.com/package/@rjsf/material-ui) package as a starting point.
+
+Why? InvasivesBC needs some custom styling for their forms that is not currently possible using the stock rfsf themes. It may be possible to inject some custom components, which rjsf has some privisions to allow, but the code needed to achieve that would likely be similar in size/complexity to the stock template components themselves. In this case, stealing and modifying them locally is simpler and tantamount to re-creating them ourselves.
+
+### components
+
+Rjsf specific react components used by custom templates and widgets.
+
+### uiSchema
+
+[rjsf uiSchema](https://react-jsonschema-form.readthedocs.io/en/latest/api-reference/uiSchema/) config objects.
+
+These are used by rjsf to determine what widgets a given fields uses, as well as display order, and visibility, etc.
--- app/src/rjsf/uiSchema/RootUISchemas.ts
+++ app/src/rjsf/uiSchema/RootUISchemas.ts
@@ -4,209 +4,362 @@
  * These should mirror the schemas of the same name in the api-doc.json.
  */
 
-import UISchemaComponents from 'rjsf/UISchemaComponents';
-import BaseUISchemaComponents from 'rjsf/BaseUISchemaComponents';
+import UISchemaComponents from 'rjsf/uiSchema/UISchemaComponents';
+import BaseUISchemaComponents from 'rjsf/uiSchema/BaseUISchemaComponents';
 
 const Activity_Observation_PlantTerrestial = {
   activity_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...UISchemaComponents.Activity
   },
   activity_type_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Observation
   },
   activity_subtype_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Observation_PlantTerrestial
   }
 };
 
 const Activity_Observation_PlantAquatic = {
   activity_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...UISchemaComponents.Activity
   },
   activity_type_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Observation
   },
   activity_subtype_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Observation_PlantAquatic
   }
 };
 
 const Activity_Observation_AnimalTerrestrial = {
   activity_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...UISchemaComponents.Activity
   },
   activity_type_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Observation
   },
   activity_subtype_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Observation_AnimalTerrestrial
   }
 };
 
 const Activity_Observation_AnimalAquatic = {
   activity_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...UISchemaComponents.Activity
   },
   activity_type_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Observation
   },
   activity_subtype_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Observation_AnimalAquatic
   }
 };
 
 const Activity_Treatment_ChemicalPlant = {
   activity_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...UISchemaComponents.Activity
   },
   activity_type_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Treatment
   },
   activity_subtype_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...UISchemaComponents.Treatment_ChemicalPlant
   }
 };
 
 const Activity_Treatment_MechanicalPlant = {
   activity_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...UISchemaComponents.Activity
   },
   activity_type_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Treatment
   },
   activity_subtype_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Treatment_MechanicalPlant
   }
 };
 
 const Activity_Treatment_BiologicalPlant = {
   activity_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...UISchemaComponents.Activity
   },
   activity_type_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Treatment
   },
   activity_subtype_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Treatment_BiologicalPlant
   }
 };
 
 const Activity_Treatment_BiologicalDispersalPlant = {
   activity_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...UISchemaComponents.Activity
   },
   activity_type_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Treatment
   },
   activity_subtype_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Treatment_BiologicalDispersalPlant
   }
 };
 
 const Activity_Treatment_MechanicalTerrestrialAnimal = {
   activity_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...UISchemaComponents.Activity
   },
   activity_type_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Treatment
   },
   activity_subtype_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Treatment_MechanicalTerrestrialAnimal
   }
 };
 
 const Activity_Treatment_ChemicalTerrestrialAnimal = {
   activity_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...UISchemaComponents.Activity
   },
   activity_type_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Treatment
   },
   activity_subtype_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Treatment_ChemicalTerrestrialAnimal
   }
 };
 
 const Activity_Treatment_BiologicalTerrestrialAnimal = {
   activity_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...UISchemaComponents.Activity
   },
   activity_type_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Treatment
   },
   activity_subtype_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Treatment_BiologicalTerrestrialAnimal
   }
 };
 
 const Activity_Monitoring_ChemicalTerrestrialAquaticPlant = {
   activity_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...UISchemaComponents.Activity
   },
   activity_type_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Monitoring
   },
   activity_subtype_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Monitoring_ChemicalTerrestrialAquaticPlant
   }
 };
 
 const Activity_Monitoring_MechanicalTerrestrialAquaticPlant = {
   activity_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...UISchemaComponents.Activity
   },
   activity_type_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Monitoring
   },
   activity_subtype_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Monitoring_MechanicalTerrestrialAquaticPlant
   }
 };
 
 const Activity_Monitoring_BiologicalTerrestrialPlant = {
   activity_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...UISchemaComponents.Activity
   },
   activity_type_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Monitoring
   },
   activity_subtype_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Monitoring_BiologicalTerrestrialPlant
   }
 };
 
 const Activity_Monitoring_MechanicalTerrestrialAnimal = {
   activity_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...UISchemaComponents.Activity
   },
   activity_type_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Monitoring
   },
   activity_subtype_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Monitoring_MechanicalTerrestrialAnimal
   }
 };
 
 const Activity_Monitoring_ChemicalTerrestrialAnimal = {
   activity_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...UISchemaComponents.Activity
   },
   activity_type_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Monitoring
   },
   activity_subtype_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Monitoring_ChemicalTerrestrialAnimal
   }
 };
 
 const Activity_Monitoring_BiologicalTerrestrialAnimal = {
   activity_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...UISchemaComponents.Activity
   },
   activity_type_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Monitoring
   },
   activity_subtype_data: {
+    'ui:column-xs': 12,
+    'ui:column-md': 6,
+    'ui:column-lg': 4,
     ...BaseUISchemaComponents.Monitoring_BiologicalTerrestrialAnimal
   }
 };
