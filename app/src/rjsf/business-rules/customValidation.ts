3eb5ce3086def25d82561d2da2d43a94b559fce5
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-12-03 17:15:31 -0800

    fix issue with persistent errors

--- app/src/rjsf/business-rules/customValidation.ts
+++ app/src/rjsf/business-rules/customValidation.ts
@@ -1,19 +1,23 @@
 import { FormValidation } from '@rjsf/core';
 
-let areaLimit = Number.POSITIVE_INFINITY;
+let areaLimit = 0;
 
 // keep track of all business rules for custom form validation logic
 export function customValidation(activitySubtype: string) {
   if (activitySubtype === 'Activity_Observation_PlantTerrestial') {
     areaLimit = 10000;
+  } else {
+    areaLimit = Number.POSITIVE_INFINITY;
   }
 
   return validate;
 }
 
-export function validate(formData: any, errors: FormValidation) : FormValidation {
+function validate(formData: any, errors: FormValidation) : FormValidation {
+  // validate reported area limit
+  errors.activity_data['reported_area'].__errors = [];
   if (formData.activity_data['reported_area'] > areaLimit) { 
-    errors.activity_data['reported_area'].addError("Area is too large for given activity type.");
+    errors.activity_data['reported_area'].addError("Area is too large for given activity type");
   }
 
   return errors;
