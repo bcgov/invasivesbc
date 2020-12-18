12ac879c9a90f51b1651e2a86dd25eb7ca9a45f0
Nick Phura <nickphura@gmail.com>
2020-12-18 13:24:31 -0800

    Testing

--- database/.pipeline/lib/db.deploy.js
+++ database/.pipeline/lib/db.deploy.js
@@ -9,36 +9,48 @@ const path = require('path');
  * @returns
  */
 module.exports = (settings) => {
-  const phases = settings.phases;
-  const options = settings.options;
-  const phase = options.env;
+  try {
+    const phases = settings.phases;
+    const options = settings.options;
+    const phase = options.env;
 
-  const oc = new OpenShiftClientX(Object.assign({ namespace: phases[phase].namespace }, options));
+    console.log('Test 1');
+    const oc = new OpenShiftClientX(Object.assign({ namespace: phases[phase].namespace }, options));
+    console.log('Test 2');
+    const templatesLocalBaseUrl = oc.toFileUrl(path.resolve(__dirname, '../../openshift'));
+    console.log('Test 3');
+    const name = `${phases[phase].name}`;
+    const instance = `${phases[phase].instance}`;
+    const changeId = `${phases[phase].changeId}`;
 
-  const templatesLocalBaseUrl = oc.toFileUrl(path.resolve(__dirname, '../../openshift'));
-
-  const name = `${phases[phase].name}`;
-  const instance = `${phases[phase].instance}`;
-  const changeId = `${phases[phase].changeId}`;
-
-  const objects = [];
-
-  objects.push(
-    ...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/db.dc.yaml`, {
-      param: {
-        NAME: name,
-        DATABASE_SERVICE_NAME: `${name}-postgresql${phases[phase].suffix}`,
-        IMAGE_STREAM_NAME: name,
-        IMAGE_STREAM_VERSION: phases.build.tag,
-        POSTGRESQL_DATABASE: 'InvasivesBC',
-        IMAGE_STREAM_NAMESPACE: phases.build.namespace,
-        VOLUME_CAPACITY:
-          `${name}-postgresql${phases[phase].suffix}` === `${name}-postgresql-dev-deploy` ? '20Gi' : '3Gi'
-      }
-    })
-  );
-
-  oc.applyRecommendedLabels(objects, name, phase, changeId, instance);
-  oc.importImageStreams(objects, phases[phase].tag, phases.build.namespace, phases.build.tag);
-  oc.applyAndDeploy(objects, instance);
+    const objects = [];
+    console.log('Test 4');
+    objects.push(
+      ...oc.processDeploymentTemplate(`${templatesLocalBaseUrl}/db.dc.yaml`, {
+        param: {
+          NAME: name,
+          DATABASE_SERVICE_NAME: `${name}-postgresql${phases[phase].suffix}`,
+          IMAGE_STREAM_NAME: name,
+          IMAGE_STREAM_VERSION: phases.build.tag,
+          POSTGRESQL_DATABASE: 'InvasivesBC',
+          IMAGE_STREAM_NAMESPACE: phases.build.namespace,
+          VOLUME_CAPACITY:
+            `${name}-postgresql${phases[phase].suffix}` === `${name}-postgresql-dev-deploy` ? '20Gi' : '3Gi'
+        }
+      })
+    );
+    console.log(JSON.stringify(objects));
+    console.log('Test 5');
+    oc.applyRecommendedLabels(objects, name, phase, changeId, instance);
+    console.log(JSON.stringify(objects));
+    console.log('Test 6');
+    oc.importImageStreams(objects, phases[phase].tag, phases.build.namespace, phases.build.tag);
+    console.log(JSON.stringify(objects));
+    console.log('Test 7');
+    oc.applyAndDeploy(objects, instance);
+    console.log('Test 8');
+  } catch (error) {
+    console.log(error);
+    console.log('Test 9');
+  }
 };
