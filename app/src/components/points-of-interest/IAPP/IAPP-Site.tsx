ff77ff6a8409c15883d4ecfa2c1b34af46068f5f
dogcomplex <warrenkoch@gmail.com>
2020-12-21 23:31:52 -0800

    Prettier fixes IAPPTable

--- app/src/components/points-of-interest/IAPP/IAPP-Site.tsx
+++ app/src/components/points-of-interest/IAPP/IAPP-Site.tsx
@@ -52,7 +52,7 @@ const useStyles = makeStyles((theme) => ({
     color: '#777'
   },
   header: {
-    backgroundColor: 'rgba(0, 0, 0, 0.06)'  // TODO pull from theme
+    backgroundColor: 'rgba(0, 0, 0, 0.06)' // TODO pull from theme
   },
   dropdown: {
     paddingBottom: 0,
@@ -68,10 +68,10 @@ export interface IAPPSitePropType {
   record: any;
 }
 
-
 export const IAPPSite: React.FC<IAPPSitePropType> = (props) => {
   const classes = useStyles();
-  const ifApplicable = (value) => (value && String(value).trim() != '') ? value : <div className={classes.missingValue}>N/A</div>;
+  const ifApplicable = (value) =>
+    value && String(value).trim() != '' ? value : <div className={classes.missingValue}>N/A</div>;
 
   const {
     aspect,
@@ -214,27 +214,27 @@ export const IAPPSite: React.FC<IAPPSitePropType> = (props) => {
         <AccordionDetails className={classes.surveyContainer}>
           <IAPPTable
             headers={[
-              "Survey ID",
-              "Common Name",
-              "Species",
-              "Genus",
-              "Survey Date",
-              "Agency",
-              "Hectares",
+              'Survey ID',
+              'Common Name',
+              'Species',
+              'Genus',
+              'Survey Date',
+              'Agency',
+              'Hectares',
               {
                 align: 'center',
-                children: "Density"
+                children: 'Density'
               },
               {
                 align: 'center',
-                children: "Distribution"
+                children: 'Distribution'
               },
               {
                 className: classes.wideCell,
-                children: "Comments"
-              },
+                children: 'Comments'
+              }
             ]}
-            rows={surveys.map((row) => ([
+            rows={surveys.map((row) => [
               row.SurveyID,
               row.CommonName,
               row.Species,
@@ -254,7 +254,7 @@ export const IAPPSite: React.FC<IAPPSitePropType> = (props) => {
                 className: classes.wideCell,
                 children: row.Comment
               }
-            ]))}
+            ])}
             pagination={true}
           />
         </AccordionDetails>
@@ -267,61 +267,66 @@ export const IAPPSite: React.FC<IAPPSitePropType> = (props) => {
         <AccordionDetails className={classes.treatmentContainer}>
           <IAPPTable
             headers={[
-              "Treatment ID",
-              "Mechanical ID",
-              "Common Name",
-              "Treatment Date",
-              "Agency",
-              "Hectares",
-              "Mech Method",
-              "PaperFile ID",
+              'Treatment ID',
+              'Mechanical ID',
+              'Common Name',
+              'Treatment Date',
+              'Agency',
+              'Hectares',
+              'Mech Method',
+              'PaperFile ID',
               {
                 className: classes.wideCell,
-                children: "Comments"
+                children: 'Comments'
               }
             ]}
-            rows={mechanical_treatments ? mechanical_treatments.map((row) => ([
-              row.TreatmentID,
-              row.MechanicalID,
-              row.CommonName,
-              row.TreatmentDate,
-              row.TreatmentAgency,
-              parseFloat(row.AreaTreated).toFixed(4),
-              row.MechanicalMethod,
-              row.PaperFileID,
-              {
-                className: classes.wideCell,
-                children: row.Comment
-              }
-            ])) : []}
+            rows={
+              mechanical_treatments
+                ? mechanical_treatments.map((row) => [
+                    row.TreatmentID,
+                    row.MechanicalID,
+                    row.CommonName,
+                    row.TreatmentDate,
+                    row.TreatmentAgency,
+                    parseFloat(row.AreaTreated).toFixed(4),
+                    row.MechanicalMethod,
+                    row.PaperFileID,
+                    {
+                      className: classes.wideCell,
+                      children: row.Comment
+                    }
+                  ])
+                : []
+            }
             dropdown={(i) =>
-              mechanical_treatments[i].monitoring === undefined || mechanical_treatments[i].monitoring.length === 0
-                ? null
-                : <IAPPTable
-                    key={'dropdown_' + i}
-                    headers={[
-                      'Monitoring ID',
-                      'Monitoring Date',
-                      'Agency',
-                      'Efficacy',
-                      'PaperFile ID',
-                      {
-                        className: classes.wideCell,
-                        children: 'Comments'
-                      }
-                    ]}
-                    rows={mechanical_treatments[i].monitoring.map((row, j) => ([
-                      row.monitoring_id,
-                      row.monitoring_date,
-                      row.agency_code,
-                      row.efficacy_percent,
-                      row.paper_file_id,
-                      {
-                        className: classes.wideCell,
-                        children: row.comment
-                      }
-                    ]))}
-                  />
+              mechanical_treatments[i].monitoring === undefined ||
+              mechanical_treatments[i].monitoring.length === 0 ? null : (
+                <IAPPTable
+                  key={'dropdown_' + i}
+                  headers={[
+                    'Monitoring ID',
+                    'Monitoring Date',
+                    'Agency',
+                    'Efficacy',
+                    'PaperFile ID',
+                    {
+                      className: classes.wideCell,
+                      children: 'Comments'
+                    }
+                  ]}
+                  rows={mechanical_treatments[i].monitoring.map((row, j) => [
+                    row.monitoring_id,
+                    row.monitoring_date,
+                    row.agency_code,
+                    row.efficacy_percent,
+                    row.paper_file_id,
+                    {
+                      className: classes.wideCell,
+                      children: row.comment
+                    }
+                  ])}
+                />
+              )
             }
             pagination={true}
           />
@@ -335,98 +340,113 @@ export const IAPPSite: React.FC<IAPPSitePropType> = (props) => {
         <AccordionDetails className={classes.treatmentContainer}>
           <IAPPTable
             headers={[
-              "Treatment ID",
-              "Common Name",
-              "Treatment Date",
-              "Agency",
-              "Hectares",
-              "Chem Method",
-              "PaperFile ID",
+              'Treatment ID',
+              'Common Name',
+              'Treatment Date',
+              'Agency',
+              'Hectares',
+              'Chem Method',
+              'PaperFile ID',
               {
                 className: classes.wideCell,
-                children: "Comments"
+                children: 'Comments'
               }
             ]}
-            rows={chemical_treatments ? chemical_treatments.map((row) => ([
-              row.TreatmentID,
-              row.MapCommon,
-              row.TreatmentDate,
-              row.TreatmentAgency,
-              parseFloat(row.AreaTreated).toFixed(4),
-              row.ChemicalMethod,
-              row.PaperFileID,
-              {
-                className: classes.wideCell,
-                children: row.Comment
-              }
-            ])) : []}
-            dropdown={(i) =>
+            rows={
+              chemical_treatments
+                ? chemical_treatments.map((row) => [
+                    row.TreatmentID,
+                    row.MapCommon,
+                    row.TreatmentDate,
+                    row.TreatmentAgency,
+                    parseFloat(row.AreaTreated).toFixed(4),
+                    row.ChemicalMethod,
+                    row.PaperFileID,
+                    {
+                      className: classes.wideCell,
+                      children: row.Comment
+                    }
+                  ])
+                : []
+            }
+            dropdown={(i) => (
               <React.Fragment key={'dropdown_' + i}>
                 <IAPPTable
                   headers={[
-                    'PMP Confirmation #', 'Description', 'PMRA Reg #', 'Temperature', 'Humidity',
-                    'Wind Velocity', 'Wind Direction', 'Application Rate', 'Amount Used', 'Dilution Rate'
+                    'PMP Confirmation #',
+                    'Description',
+                    'PMRA Reg #',
+                    'Temperature',
+                    'Humidity',
+                    'Wind Velocity',
+                    'Wind Direction',
+                    'Application Rate',
+                    'Amount Used',
+                    'Dilution Rate'
                   ]}
-                  rows={[[
-                    chemical_treatments[i].Pmp_Confirmation_Number,
-                    chemical_treatments[i].Description,
-                    chemical_treatments[i].Pmra_Reg_Number,
-                    chemical_treatments[i].Temperature,
-                    chemical_treatments[i].Humidity,
-                    chemical_treatments[i].Wind_Velocity,
-                    chemical_treatments[i].Wind_Direction,
-                    chemical_treatments[i].Application_Rate,
-                    chemical_treatments[i].Amount_Used,
-                    chemical_treatments[i].Dilution_Rate
-                  ]]}
+                  rows={[
+                    [
+                      chemical_treatments[i].Pmp_Confirmation_Number,
+                      chemical_treatments[i].Description,
+                      chemical_treatments[i].Pmra_Reg_Number,
+                      chemical_treatments[i].Temperature,
+                      chemical_treatments[i].Humidity,
+                      chemical_treatments[i].Wind_Velocity,
+                      chemical_treatments[i].Wind_Direction,
+                      chemical_treatments[i].Application_Rate,
+                      chemical_treatments[i].Amount_Used,
+                      chemical_treatments[i].Dilution_Rate
+                    ]
+                  ]}
                 />
-                <br/>
-                {chemical_treatments[i].monitoring === undefined || chemical_treatments[i].monitoring.length === 0
-                  ? null
-                  : <IAPPTable
-                      headers={[
-                        'Monitoring ID',
-                        'Monitoring Date',
-                        'Agency',
-                        'Efficacy',
-                        'PaperFile ID',
-                        {
-                          className: classes.wideCell,
-                          children: 'Comments'
-                        }
-                      ]}
-                      rows={chemical_treatments[i].monitoring.map((row, j) => ([
-                        row.monitoring_id,
-                        row.inspection_date,
-                        row.invasive_plant_agency_code,
-                        row.EFFICACY_RATING_CODE,
-                        row.PAPER_FILE_ID,
-                        {
-                          className: classes.wideCell,
-                          children: row.comments
-                        }
-                      ]))}
-                    />
-                }
+                <br />
+                {chemical_treatments[i].monitoring === undefined ||
+                chemical_treatments[i].monitoring.length === 0 ? null : (
+                  <IAPPTable
+                    headers={[
+                      'Monitoring ID',
+                      'Monitoring Date',
+                      'Agency',
+                      'Efficacy',
+                      'PaperFile ID',
+                      {
+                        className: classes.wideCell,
+                        children: 'Comments'
+                      }
+                    ]}
+                    rows={chemical_treatments[i].monitoring.map((row, j) => [
+                      row.monitoring_id,
+                      row.inspection_date,
+                      row.invasive_plant_agency_code,
+                      row.EFFICACY_RATING_CODE,
+                      row.PAPER_FILE_ID,
+                      {
+                        className: classes.wideCell,
+                        children: row.comments
+                      }
+                    ])}
+                  />
+                )}
               </React.Fragment>
-            }
+            )}
             pagination={true}
           />
         </AccordionDetails>
       </Accordion>
 
-      <br/><br/><br/><br/>
-
+      <br />
+      <br />
+      <br />
+      <br />
     </Container>
   );
 };
 
-
 export interface IAPPTablePropType {
-  headers: Array<any|object>;
-  rows: Array<Array<string|object>|object>;
+  headers: Array<any | object>;
+  rows: Array<Array<string | object> | object>;
   dropdown?: (index: number) => any;
-  pagination?: boolean|object;
+  pagination?: boolean | object;
   startsOpen?: boolean;
 }
 
@@ -439,20 +459,15 @@ export interface IAPPTablePropType {
 */
 // general table with pagination
 const IAPPTable: React.FC<IAPPTablePropType> = (props) => {
-  const {
-    headers,
-    rows,
-    dropdown = undefined,
-    pagination = undefined,
-    startsOpen = undefined
-  } = props;
+  const { headers, rows, dropdown = undefined, pagination = undefined, startsOpen = undefined } = props;
 
   const classes = useStyles();
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
   const startingRow = page * rowsPerPage;
 
-  const ifApplicable = (value) => (value && String(value).trim() != '') ? value : <div className={classes.missingValue}>N/A</div>;
+  const ifApplicable = (value) =>
+    value && String(value).trim() != '' ? value : <div className={classes.missingValue}>N/A</div>;
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -463,56 +478,54 @@ const IAPPTable: React.FC<IAPPTablePropType> = (props) => {
     setPage(0);
   };
 
-  const IAPPBodyRow = ({row, index}) => {
+  const IAPPBodyRow = ({ row, index }) => {
     const [open, setOpen] = React.useState(startsOpen);
 
     const renderedDropdown = dropdown ? dropdown(index) : null;
     // allow the row to override standard rendering if it is a string or element
-    const renderedCells = typeof row === 'string' || React.isValidElement(row)
-      ? row
-      : row.map((cell, j) => renderCell(cell, j));
+    const renderedCells =
+      typeof row === 'string' || React.isValidElement(row) ? row : row.map((cell, j) => renderCell(cell, j));
 
     return (
       <React.Fragment key={index}>
         <TableRow onClick={() => setOpen(!open)}>
-          {dropdown &&
+          {dropdown && (
             <TableCell className={classes.dropdownCol}>
-              {renderedDropdown !== null &&
+              {renderedDropdown !== null && (
                 <IconButton aria-label="expand row" size="small">
                   {open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
                 </IconButton>
-              }
+              )}
             </TableCell>
-          }
+          )}
           {renderedCells}
         </TableRow>
         {dropdown && renderedDropdown !== null && (
           <TableRow>
             <TableCell className={classes.dropdown} colSpan={9}>
               <Collapse in={open} timeout="auto">
-                <Box margin={2}>
-                  {renderedDropdown}
-                </Box>
+                <Box margin={2}>{renderedDropdown}</Box>
               </Collapse>
             </TableCell>
           </TableRow>
         )}
       </React.Fragment>
     );
-  }
+  };
 
   const renderCell = (cell, i) => {
     if (typeof cell === 'string')
-      return <TableCell key={i} className={classes.cell}>{ifApplicable(cell)}</TableCell>;
-    if (typeof cell === 'object') {
-      return React.createElement(
-        TableCell,
-        {
-          key: i,
-          className: classes.cell,
-          ...cell
-        }
+      return (
+        <TableCell key={i} className={classes.cell}>
+          {ifApplicable(cell)}
+        </TableCell>
       );
+    if (typeof cell === 'object') {
+      return React.createElement(TableCell, {
+        key: i,
+        className: classes.cell,
+        ...cell
+      });
     }
   };
 
@@ -521,34 +534,32 @@ const IAPPTable: React.FC<IAPPTablePropType> = (props) => {
     .slice(startingRow, startingRow + rowsPerPage)
     .map((row, i) => <IAPPBodyRow row={row} index={startingRow + i} key={startingRow + i} />);
 
-  return (
-    (!rows || rows.length === 0)
-      ? <div>No Data</div>
-      : (
-        <React.Fragment>
-          <TableContainer component={Paper} className={classes.tableContainer}>
-            <Table className={classes.table} aria-label="mechanical treatments">
-              <TableHead className={classes.header}>
-                <TableRow>
-                  {dropdown && <TableCell className={classes.dropdownCol}/>}
-                  {renderedHeaders}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {renderedRows}
-              </TableBody>
-            </Table>
-            {pagination && rows && rows.length > rowsPerPage && <TablePagination
-              rowsPerPageOptions={[rowsPerPage]}
-              component="div"
-              count={rows.length}
-              rowsPerPage={rowsPerPage}
-              page={page}
-              onChangePage={handleChangePage}
-              onChangeRowsPerPage={handleChangeRowsPerPage}
-            />}
-          </TableContainer>
-        </React.Fragment>
-    )
+  return !rows || rows.length === 0 ? (
+    <div>No Data</div>
+  ) : (
+    <React.Fragment>
+      <TableContainer component={Paper} className={classes.tableContainer}>
+        <Table className={classes.table} aria-label="mechanical treatments">
+          <TableHead className={classes.header}>
+            <TableRow>
+              {dropdown && <TableCell className={classes.dropdownCol} />}
+              {renderedHeaders}
+            </TableRow>
+          </TableHead>
+          <TableBody>{renderedRows}</TableBody>
+        </Table>
+        {pagination && rows && rows.length > rowsPerPage && (
+          <TablePagination
+            rowsPerPageOptions={[rowsPerPage]}
+            component="div"
+            count={rows.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onChangePage={handleChangePage}
+            onChangeRowsPerPage={handleChangeRowsPerPage}
+          />
+        )}
+      </TableContainer>
+    </React.Fragment>
   );
-}
+};
