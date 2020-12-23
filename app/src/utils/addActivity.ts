eb5e00cf6373db02622cca89d3683f4039604d75
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-12-23 13:42:51 -0800

    lint

--- app/src/utils/addActivity.ts
+++ app/src/utils/addActivity.ts
@@ -45,7 +45,7 @@ export async function addActivityToDB(
     } else {
       formData.activity_type_data = {
         activity_id: linkedRecord._id
-      }
+      };
     }
   } else {
     formData = {
