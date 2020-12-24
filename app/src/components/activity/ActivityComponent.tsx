2ef3497605522e7cbccc37ccf34ff77461740480
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2020-12-24 12:33:59 -0800

    fix linting

--- app/src/components/activity/ActivityComponent.tsx
+++ app/src/components/activity/ActivityComponent.tsx
@@ -37,7 +37,7 @@ const ActivityComponent: React.FC<IActivityComponentProps> = (props) => {
     if (props.activity.activityType === 'Monitoring') {
       getActivityData();
     }
-  }, [databaseContext]);
+  }, [databaseContext, props]);
 
   return (
     <>
