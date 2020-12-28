41bd768f3887172ab48ac48fb417afd36a6c59c9
Micheal Wells <micheal.w.wells@gov.bc.ca>
2020-12-28 15:02:23 -0800

    Filled out a few more of them and tested.  Need to track down some that no longer seem present in the data.

--- database/src/migrations/20201228114906_observation_view.ts
+++ database/src/migrations/20201228114906_observation_view.ts
@@ -8,14 +8,15 @@ export async function up(knex: Knex): Promise<void> {
     activity_sub_type as observation_type,
     activity_payload::json->'form_data'->'activity_type_data'->'negative_obs_ind' as negative_observation_ind from activity_incoming_data,
     activity_payload::json->'aquatic_observation_ind' as aquatic_observation_ind__NEEDS_VERIFY,
-    activity_payload::json->'form_data'->'activity_type_data'->'observer_last_name' as primary_user_last_name__NEEDS_VERIFY,
-    activity_payload::json->'species' as species__NEEDS_VERIFY,
-    activity_payload::json->'primary_file_id' as primary_file_id,
-    activity_payload::json->'secondary_file_id' as secondary_file_id,
-    activity_payload::json->'location_comment' as location_comment,
-    activity_payload::json->'general_observation_comment' as general_observation_comment,
-    activity_payload::json->'sample_taken_ind' as sample_taken_ind,
-    activity_payload::json-> 'sample_label_number' as sample_label_number
+    activity_payload::json->'form_data'->'activity_type_data'->'observer_last_name' as primary_user_last_name,
+    activity_payload::json->'form_data'->'activity_type_data'->'observer_first_name' as primary_user_first_name,
+    activity_payload::json->'form_data'->'acitivity_subtype_data'->invasive_plant_code' as invasive_plant_code,
+    activity_payload::json->'primary_file_id' as primary_file_id__NEEDS_VERIFY,
+    activity_payload::json->'secondary_file_id' as secondary_file_id__NEEDS_VERIFY,
+    activity_payload::json->'location_comment' as location_comment__NEEDS_VERIFY,
+    activity_payload::json->'form_data'->'activity_data'->'general_comment as general_observation_comment__NEEDS_VERIFY,
+    activity_payload::json->'sample_taken_ind' as sample_taken_ind_NEEDS_VERIFY,
+    activity_payload::json-> 'sample_label_number' as sample_label_number__NEEDS_VERIFY
 
     from activity_incoming_data
     where activity_incoming_data.activity_type = 'Observation'
