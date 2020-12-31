b9b43caabd1cfdba0efc40857acdb852674d77ec
Micheal Wells <micheal.w.wells@gov.bc.ca>
2020-12-31 11:25:52 -0800

    hit save

--- database/src/migrations/20201230162913_Activity_Observation_TerrestrialPlant_codes_resolved.ts
+++ database/src/migrations/20201230162913_Activity_Observation_TerrestrialPlant_codes_resolved.ts
@@ -1,10 +1,75 @@
-import * as Knex from "knex";
-
+import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-}
+  knex.raw(`
+  set search_path=invasivesbc;
+  drop view if exists invasivesbc.Activity_Observation_TerrestrialPlant;
+  CREATE OR REPLACE VIEW Activity_Observation_TerrestrialPlant as (
+      select
+        activity_id as activity_id,
+	    invasive_plant_codes.code_description as invasive_plant,
+		invasive_plant_density_codes.code_description as invasive_plant_density,
+		invasive_plant_distribution_codes.code_description as invasive_plant_distribution,
+		soil_texture_codes.code_description as soil_texture,
+		specific_use_codes.code_description as specific_use,
+		slope_codes.code_description as slope,
+		aspect_codes.code_description as aspect,
+		proposed_treatment_codes.code_description as proposed_treatment,
+		range_unit_number,
+		plant_life_stage_codes.code_description as plant_life_stage,
+		plant_health_codes.code_description as plant_health,
+		plant_seed_stage_codes.code_description as plant_seed_stage,
+		flowering,
+		legacy_site_ind,
+		early_detection_rapid_resp_ind,
+		research_detection_ind,
+		well_ind,
+		special_care_ind,
+		biological_ind
 
+from invasivesbc.Activity_Observation_TerrestrialPlant_with_codes
+
+--technically this should be used in all the subsequent joins, but we can get away without for now:
+--join code_category on inv_code_category on inv_code_category.code_category_name = 'invasives'
+
+join code_header invasive_plant_code_header on invasive_plant_code_header.code_header_title = 'invasive_plant_code' and invasive_plant_code_header.valid_to is null
+join code invasive_plant_codes on invasive_plant_codes.code_header_id = invasive_plant_code_header.code_header_id
+
+join code_header invasive_plant_density_code_header on invasive_plant_density_code_header.code_header_title = 'invasive_plant_density_code' and invasive_plant_density_code_header.valid_to is null
+join code invasive_plant_density_codes on invasive_plant_density_codes.code_header_id = invasive_plant_density_code_header.code_header_id
+
+join code_header invasive_plant_distribution_code_header on invasive_plant_distribution_code_header.code_header_title = 'invasive_plant_distribution_code' and invasive_plant_distribution_code_header.valid_to is null
+join code invasive_plant_distribution_codes on invasive_plant_distribution_codes.code_header_id = invasive_plant_distribution_code_header.code_header_id
+
+join code_header soil_texture_code_header on soil_texture_code_header.code_header_title = 'soil_texture_code' and soil_texture_code_header.valid_to is null
+join code soil_texture_codes on soil_texture_codes.code_header_id = soil_texture_code_header.code_header_id
+
+join code_header specific_use_code_header on specific_use_code_header.code_header_title = 'specific_use_code' and specific_use_code_header.valid_to is null
+join code specific_use_codes on specific_use_codes.code_header_id = specific_use_code_header.code_header_id
+
+join code_header slope_code_header on slope_code_header.code_header_title = 'slope_code' and slope_code_header.valid_to is null
+join code slope_codes on slope_codes.code_header_id = slope_code_header.code_header_id
+
+join code_header aspect_code_header on aspect_code_header.code_header_title = 'aspect_code' and aspect_code_header.valid_to is null
+join code aspect_codes on aspect_codes.code_header_id = aspect_code_header.code_header_id
+
+join code_header proposed_treatment_code_header on proposed_treatment_code_header.code_header_title = 'proposed_treatment_code' and proposed_treatment_code_header.valid_to is null
+join code proposed_treatment_codes on proposed_treatment_codes.code_header_id = proposed_treatment_code_header.code_header_id
+
+join code_header plant_life_stage_code_header on plant_life_stage_code_header.code_header_title = 'plant_life_stage_code' and plant_life_stage_code_header.valid_to is null
+join code plant_life_stage_codes on plant_life_stage_codes.code_header_id = plant_life_stage_code_header.code_header_id
+
+join code_header plant_health_code_header on plant_health_code_header.code_header_title = 'plant_health_code' and plant_health_code_header.valid_to is null
+join code plant_health_codes on plant_health_codes.code_header_id = plant_health_code_header.code_header_id
+
+join code_header plant_seed_stage_code_header on plant_seed_stage_code_header.code_header_title = 'plant_seed_stage_code' and plant_seed_stage_code_header.valid_to is null
+join code plant_seed_stage_codes on plant_seed_stage_codes.code_header_id = plant_seed_stage_code_header.code_header_id
+)
+
+    COMMENT ON VIEW Activity_Observation_TerrestrialPlant IS 'View on terrestrial plant observation specific fields, with code table values resolved';
+  `);
+}
 
 export async function down(knex: Knex): Promise<void> {
+  knex.raw(`drop view if exists invasivesbc.Activity_Observation_TerrestrialPlant`);
 }
-
