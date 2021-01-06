4a52f65b1e43ebfe2889b9e9505f3f3a0e8b0a47
Micheal Wells <micheal.w.wells@gov.bc.ca>
2021-01-06 15:55:14 -0800

    change code resolving to left joins in aqua plant obs table also

--- database/src/migrations/20201230162857_Activity_Observation_AquaticPlant_codes_resolved.ts
+++ database/src/migrations/20201230162857_Activity_Observation_AquaticPlant_codes_resolved.ts
@@ -33,28 +33,28 @@ export async function up(knex: Knex): Promise<void> {
         record.voucher_submission_detail
         from invasivesbc.Activity_Observation_AquaticPlant_with_codes record
 
-  join code_header invasive_plant_code_header on invasive_plant_code_header.code_header_title = 'invasive_plant_code' and invasive_plant_code_header.valid_to is null
-  join code invasive_plant_codes on invasive_plant_codes.code_header_id = invasive_plant_code_header.code_header_id
+  left join code_header invasive_plant_code_header on invasive_plant_code_header.code_header_title = 'invasive_plant_code' and invasive_plant_code_header.valid_to is null
+  left join code invasive_plant_codes on invasive_plant_codes.code_header_id = invasive_plant_code_header.code_header_id
   and record.invasive_plant_code = invasive_plant_codes.code_name
 
-  join code_header proposed_treatment_code_header on proposed_treatment_code_header.code_header_title = 'proposed_treatment_code' and proposed_treatment_code_header.valid_to is null
-  join code proposed_treatment_codes on proposed_treatment_codes.code_header_id = proposed_treatment_code_header.code_header_id
+  left join code_header proposed_treatment_code_header on proposed_treatment_code_header.code_header_title = 'proposed_treatment_code' and proposed_treatment_code_header.valid_to is null
+  left join code proposed_treatment_codes on proposed_treatment_codes.code_header_id = proposed_treatment_code_header.code_header_id
   and record.proposed_treatment_code = proposed_treatment_codes.code_name
 
-  join code_header specific_use_code_header on specific_use_code_header.code_header_title = 'specific_use_code' and specific_use_code_header.valid_to is null
-  join code specific_use_codes on specific_use_codes.code_header_id = specific_use_code_header.code_header_id
+  left join code_header specific_use_code_header on specific_use_code_header.code_header_title = 'specific_use_code' and specific_use_code_header.valid_to is null
+  left join code specific_use_codes on specific_use_codes.code_header_id = specific_use_code_header.code_header_id
   and record.specific_use_code = specific_use_codes.code_name
 
-  join code_header plant_life_stage_code_header on plant_life_stage_code_header.code_header_title = 'plant_life_stage_code' and plant_life_stage_code_header.valid_to is null
-  join code plant_life_stage_codes on plant_life_stage_codes.code_header_id = plant_life_stage_code_header.code_header_id
+  left join code_header plant_life_stage_code_header on plant_life_stage_code_header.code_header_title = 'plant_life_stage_code' and plant_life_stage_code_header.valid_to is null
+  left join code plant_life_stage_codes on plant_life_stage_codes.code_header_id = plant_life_stage_code_header.code_header_id
   and record.plant_life_stage_code = plant_life_stage_codes.code_name
 
-  join code_header plant_health_code_header on plant_health_code_header.code_header_title = 'plant_health_code' and plant_health_code_header.valid_to is null
-  join code plant_health_codes on plant_health_codes.code_header_id = plant_health_code_header.code_header_id
+  left join code_header plant_health_code_header on plant_health_code_header.code_header_title = 'plant_health_code' and plant_health_code_header.valid_to is null
+  left join code plant_health_codes on plant_health_codes.code_header_id = plant_health_code_header.code_header_id
   and record.plant_health_code = plant_health_codes.code_name
 
-  join code_header plant_seed_stage_code_header on plant_seed_stage_code_header.code_header_title = 'plant_seed_stage_code' and plant_seed_stage_code_header.valid_to is null
-  join code plant_seed_stage_codes on plant_seed_stage_codes.code_header_id = plant_seed_stage_code_header.code_header_id
+  left join code_header plant_seed_stage_code_header on plant_seed_stage_code_header.code_header_title = 'plant_seed_stage_code' and plant_seed_stage_code_header.valid_to is null
+  left join code plant_seed_stage_codes on plant_seed_stage_codes.code_header_id = plant_seed_stage_code_header.code_header_id
   and record.plant_seed_stage_code = plant_seed_stage_codes.code_name)    `);
 }
 
