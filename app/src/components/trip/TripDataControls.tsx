e0562e3cf759eac056f128f055260804a6727443
Warren Koch <warrenkoch@gmail.com>
2021-02-07 18:58:25 -0800

    Update app/src/components/trip/TripDataControls.tsx
    Co-authored-by: Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>

--- app/src/components/trip/TripDataControls.tsx
+++ app/src/components/trip/TripDataControls.tsx
@@ -235,7 +235,7 @@ export const TripDataControls: React.FC = (props) => {
                 pointOfInterestType: row.point_of_interest_type,
                 pointOfInterestSubtype: row.point_of_interest_subtype,
                 geometry: [...row.point_of_interest_payload.geometry],
-                photos: photos
+                photos
               };
             });
             countPois++;
