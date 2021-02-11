9404d1177d4c93cd08e2627fe1c78253bdd74fa9
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-02-11 12:40:37 -0800

    fix lint

--- app/src/features/home/activity/ActivityPage.tsx
+++ app/src/features/home/activity/ActivityPage.tsx
@@ -391,8 +391,8 @@ const ActivityPage: React.FC<IActivityPageProps> = (props) => {
             <Typography variant="h4">Current Activity</Typography>
           </Box>
           <Typography>
-            There is no current activity. When you start creating an activity,
-            it will become your current activity and show up in this tab.
+            There is no current activity. When you start creating an activity, it will become your current activity and
+            show up in this tab.
           </Typography>
         </>
       )}
