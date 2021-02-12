5be0f009b4e32edfc29c7a6dd96190591d2bcfdd
Shreyas Devalapurkar <shreyasdevalapurkar@gmail.com>
2021-02-12 14:08:58 -0800

    revert

--- app/src/contexts/DatabaseContext.tsx
+++ app/src/contexts/DatabaseContext.tsx
@@ -93,11 +93,7 @@ export const DatabaseContextProvider: React.FC = (props) => {
   }, []);
 
   useEffect(() => {
-    const callSetupDatabase = async () => {
-      setupDatabase();
-    };
-
-    callSetupDatabase();
+    setupDatabase();
 
     return async () => {
       await cleanupDatabase();
