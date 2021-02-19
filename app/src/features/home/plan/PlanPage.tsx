7a9568b054907f4f3e458049b6c7436f92c1359e
Micheal Wells <micheal.w.wells@gov.bc.ca>
2021-02-19 14:50:21 -0800

    prettier

--- app/src/features/home/plan/PlanPage.tsx
+++ app/src/features/home/plan/PlanPage.tsx
@@ -142,21 +142,20 @@ const PlanPage: React.FC<IPlanPageProps> = (props) => {
       <Grid container spacing={3} className={classes.tripGrid}>
         <Grid item md={6}>
           <Paper className={classes.paper}>
-              <Typography variant="h5">How to get the data you need:</Typography>
-              <br></br>
-              <br></br>
-              <Typography variant="body1">
-                This is where you'll load data on to your device before you go out into the field.  The general flow is to use the filters in the accoridans below,
-                and once you've added some filters hit the 'Fetch' button to pull data onto your device.
-              </Typography>
-              <br></br>
-              <Typography variant="body1">
-                You can perform as many 'Fetches' as you like.  If you are starting a new trip it is a good idea to wipe the
-                data off your device prio
-
-              </Typography>
-
-           </Paper>
+            <Typography variant="h5">How to get the data you need:</Typography>
+            <br></br>
+            <br></br>
+            <Typography variant="body1">
+              This is where you'll load data on to your device before you go out into the field. The general flow is to
+              use the filters in the accoridans below, and once you've added some filters hit the 'Fetch' button to pull
+              data onto your device.
+            </Typography>
+            <br></br>
+            <Typography variant="body1">
+              You can perform as many 'Fetches' as you like. If you are starting a new trip it is a good idea to wipe
+              the data off your device prio
+            </Typography>
+          </Paper>
           <Accordion defaultExpanded={false}>
             <AccordionSummary
               expandIcon={<ExpandMore />}
@@ -228,12 +227,12 @@ const PlanPage: React.FC<IPlanPageProps> = (props) => {
               <MetabaseSearch />
             </AccordionDetails>
           </Accordion>
-            <ManageDatabaseComponent />
-            <TripDataControls />
+          <ManageDatabaseComponent />
+          <TripDataControls />
         </Grid>
         <Grid item md={6}>
           <Paper className={classes.paper}>
-              <Typography variant="h5">Search boundary</Typography>
+            <Typography variant="h5">Search boundary</Typography>
             <Tooltip
               title="Use this map along with the controls on the left to cache data.  Draw a shape to search by, or search without a spatial filter by using the trash can to delete the shape.  Hit the save icon on the map to save map tiles up to the zoom level you are in for a given area."
               arrow>
