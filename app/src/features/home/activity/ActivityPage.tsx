bdb63e97f273abe151efd3e6422342e7ae174fdd
Micheal Wells <micheal.w.wells@gov.bc.ca>
2021-02-26 08:37:02 -0800

    experiment - geo location

--- app/src/features/home/activity/ActivityPage.tsx
+++ app/src/features/home/activity/ActivityPage.tsx
@@ -67,6 +67,24 @@ const ActivityPage: React.FC<IActivityPageProps> = (props) => {
   const initialContextMenuState: MapContextMenuData = { isOpen: false, lat: 0, lng: 0 };
   const [contextMenuState, setContextMenuState] = useState(initialContextMenuState);
 
+const { Geolocation } = Plugins;
+
+/*
+  const getCurrentPosition = async () => {
+    const coordinates = await Geolocation.getCurrentPosition();
+    console.log('Current', coordinates);
+  };
+  */
+
+  const watchPosition= () => {
+    const wait = Geolocation.watchPosition({}, (position, err) => {
+      console.log(position)
+    })
+  };
+
+  watchPosition();
+
+
   /* commented out for sonar cloud, but this will be needed to close the context menu for this page:
     const handleContextMenuClose = () => {
       setContextMenuState({ ...contextMenuState, isOpen: false });
