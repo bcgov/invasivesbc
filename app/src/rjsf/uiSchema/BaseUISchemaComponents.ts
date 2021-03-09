c41ff544510d2864feb2c633494e872162009239
dogcomplex <warrenkoch@gmail.com>
2021-03-09 02:30:17 -0800

    Phen transect fixes

--- app/src/rjsf/uiSchema/BaseUISchemaComponents.ts
+++ app/src/rjsf/uiSchema/BaseUISchemaComponents.ts
@@ -637,29 +637,6 @@ const BiocontrolEfficacyTransectPoints = {
   veg_transect_litter: {},
   veg_total_percentage: {
     'ui:readonly': true
-  },
-  phen_transect_sampler: {},
-  phen_transect_recorder: {},
-  phen_transect_seedlings: {},
-  phen_transect_rosettes: {},
-  phen_transect_bolting: {},
-  phen_transect_flowering: {},
-  phen_transect_seeds: {},
-  phen_transect_senescent: {},
-  phen_total_plants: {},
-  phen_number_stems: {},
-  phen_tallest_1: {},
-  phen_tallest_2: {},
-  phen_tallest_3: {},
-  phen_tallest_4: {},
-  phen_level_se: {},
-  phen_level_ro: {},
-  phen_level_bo: {},
-  phen_level_fl: {},
-  phen_level_sf: {},
-  phen_level_sc: {},
-  phen_total_percentage: {
-    'ui:readonly': true
   }
 };
 
@@ -889,7 +866,30 @@ const Dispersal_BiologicalDispersal = {
   applicator1_name: {},
   applicator2_name: {},
   treatment_organization: {},
-  invasive_plant_code: {}
+  invasive_plant_code: {},
+  phen_transect_sampler: {},
+  phen_transect_recorder: {},
+  phen_transect_seedlings: {},
+  phen_transect_rosettes: {},
+  phen_transect_bolting: {},
+  phen_transect_flowering: {},
+  phen_transect_seeds: {},
+  phen_transect_senescent: {},
+  phen_total_plants: {},
+  phen_number_stems: {},
+  phen_tallest_1: {},
+  phen_tallest_2: {},
+  phen_tallest_3: {},
+  phen_tallest_4: {},
+  phen_level_se: {},
+  phen_level_ro: {},
+  phen_level_bo: {},
+  phen_level_fl: {},
+  phen_level_sf: {},
+  phen_level_sc: {},
+  phen_total_percentage: {
+    'ui:readonly': true
+  }
 };
 
 /*
