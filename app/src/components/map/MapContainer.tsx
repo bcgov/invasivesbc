396119248ae7354ca494c61186684edcf9965ddf
Micheal Wells <micheal.w.wells@gov.bc.ca>
2021-03-11 15:24:29 -0800

    make zoom use state var - will return current zoom even if user hasnt zoomed yet, also placeholder hook

--- app/src/components/map/MapContainer.tsx
+++ app/src/components/map/MapContainer.tsx
@@ -289,7 +289,7 @@ const MapContainer: React.FC<IMapContainerProps> = (props) => {
 
   const addASaveTilesControl = (layerSaveControl: any) => {
     layerSaveControl.remove(mapRef.current);
-    if (mapRef.current.getZoom() > 16) {
+    if (mapRef.current.getZoom() > 13) {
       layerSaveControl.addTo(mapRef.current);
     }
   };
@@ -313,9 +313,15 @@ const MapContainer: React.FC<IMapContainerProps> = (props) => {
     );
   };
 
+  const [currentZoom, setCurrentZoom] = useState(null);
+  useEffect(()=> {
+    //custom on-zoom stuff
+  },[currentZoom])
+
   const initMap = () => {
     mapRef.current = L.map(props.mapId, { zoomControl: false }).setView([55, -128], 10);
 
+    setCurrentZoom(mapRef.current.getZoom());
     addContextMenuClickListener();
 
     addZoomControls();
@@ -385,6 +391,7 @@ const MapContainer: React.FC<IMapContainerProps> = (props) => {
 
     mapRef.current.on('zoomend', () => {
       props.extentState.setExtent(mapRef.current.getBounds());
+      setCurrentZoom(mapRef.current.getZoom());
       addASaveTilesControl(esriSaveTilesControl);
     });
 
