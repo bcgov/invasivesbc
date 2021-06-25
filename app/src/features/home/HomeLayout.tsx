7397e3159cb013c928ac4afbb7ca0925d404af78
Mike Wells <micheal.w.wells@gmail.com>
2021-06-24 22:10:34 -0700

    rebase better_maps ontop of dev, resolve a few conflicts from warren's last big pr a number of mobile acompile fixes, and move to node 14.17.1

--- app/src/features/home/HomeLayout.tsx
+++ app/src/features/home/HomeLayout.tsx
@@ -21,38 +21,7 @@ const HomeLayout: React.FC<IHomeLayoutProps> = (props: any) => {
   const [notification, setNotification] = useState(null);
   const [notificationCount, setNotificationCount] = useState(0);
 
-  // const addNotificationsToPage = useCallback(async () => {
-  //   let notifications = await databaseContext.database.find({
-  //     selector: {
-  //       docType: DocType.NOTIFICATION,
-  //       acknowledged: false
-  //     },
-  //     fields: ['dateCreated', '_id', 'notificationType', 'text', 'docType', 'acknowledged'],
-  //     use_index: 'notificationsIndex'
-  //   });
-
-  //   notifications.docs = notifications.docs.filter((note) => note._id && note.notificationType && note.text);
-  //   notifications.docs.sort((a, b) => {
-  //     if (a.dateCreated < b.dateCreated) return 1;
-  //     if (a.dateCreated > b.dateCreated) return -1;
-  //     return 0;
-  //   });
-
-  //   setNotificationCount(notifications.docs.length);
-
-  //   if (notifications.docs.length > 0) {
-  //     setNotification(notifications.docs[0]);
-  //     setIsOpen(true);
-  //   }
-  // }, [databaseContext.database]);
-
-  // useEffect(() => {
-  //   const updateComponent = () => {
-  //     addNotificationsToPage();
-  //   };
-
-  //   updateComponent();
-  // }, [databaseChangesContext, addNotificationsToPage]);
+  const addNotificationsToPage = null;
 
   const acknowledgeNotification = (docId: string) => {
     databaseContext.database.upsert(docId, (doc) => {
