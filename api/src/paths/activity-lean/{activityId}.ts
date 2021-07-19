d279300801b3406236dd00f0427de2e2c125e2dc
dogcomplex <warrenkoch@gmail.com>
2021-07-19 10:26:02 -0700

    Prettier changes remove imports

--- api/src/paths/activity-lean/{activityId}.ts
+++ api/src/paths/activity-lean/{activityId}.ts
@@ -3,10 +3,8 @@
 import { GetObjectOutput } from 'aws-sdk/clients/s3';
 import { RequestHandler } from 'express';
 import { Operation } from 'express-openapi';
-import { SQLStatement } from 'sql-template-strings';
 import { ALL_ROLES } from '../../constants/misc';
 import { getDBConnection } from './../../database/db';
-import { getActivitySQL } from './../../queries/activity-queries';
 import { getFileFromS3 } from './../../utils/file-utils';
 import { getLogger } from './../../utils/logger';
 import { getMediaItemsList } from './../media';
