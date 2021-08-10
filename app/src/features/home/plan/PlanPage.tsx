799029f7258204ba72a83ab39cb58a87b6aed3d9
Mike Wells <micheal.w.wells@gmail.com>
2021-08-10 11:14:23 -0700

    lint and fix sonar flag

--- app/src/features/home/plan/PlanPage.tsx
+++ app/src/features/home/plan/PlanPage.tsx
@@ -525,8 +525,8 @@ const PlanPage: React.FC<IPlanPageProps> = (props) => {
     // await databaseContext.database.get(trip_ID.toString()).then((doc) => {
     //   return databaseContext.database.remove(doc);
     // });
-    // NOSONAR
-    setNewTripID(Math.random());
+
+    setNewTripID(Math.random()); //NOSONAR
   };
 
   return (
