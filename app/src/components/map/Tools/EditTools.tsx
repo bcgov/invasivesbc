a5d5ce9724b7ed9fff23c3412e815f90ed9e4cb4
bburciaga <burciagab16@gmail.com>
2021-08-12 11:17:02 -0700

    added back files for single or multi mode

--- app/src/components/map/Tools/EditTools.tsx
+++ app/src/components/map/Tools/EditTools.tsx
@@ -5,6 +5,8 @@ import { MapContainer, useMapEvent } from 'react-leaflet';
 import * as turf from '@turf/turf';
 import L from 'leaflet';
 import React from 'react';
+import single from '../Icons/square.png';
+import multi from '../Icons/trim.png';
 
 const useStyles = makeStyles((theme) => ({
     image: {
@@ -34,7 +36,7 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
-const EditTools = (props) => 
+const EditTools = (props) => {
     const classes = useStyles();
     // This should get the 'FeatureGroup' connected to the tools
     const [multiMode, setMultiMode] = useState(false);
@@ -59,7 +61,7 @@ const EditTools = (props) =>
     // Put new feature into the FeatureGroup
     const onDrawCreate = (e: any) => {
         var newLayer = e.layer;
-        context.layerContainer.addLayer(e.layer);
+        context.layerContainer.addLayer(newLayer);
 
         let aGeo = newLayer.toGeoJSON();
         if (e.layerType === 'circle') {
@@ -69,10 +71,10 @@ const EditTools = (props) =>
         aGeo = convertLineStringToPoly(aGeo);
         // Drawing one geo wipes all others
         if (multiMode) {
-          let newState = [];
-          newState = props.geometryState.geometry ? [...props.geometryState.geometry] : newState;
-          newState = aGeo ? [...newState, aGeo] : newState;
-          props.geometryState.setGeometry([...newState])
+            let newState = [];
+            newState = props.geometryState.geometry ? [...props.geometryState.geometry] : newState;
+            newState = aGeo ? [...newState, aGeo] : newState;
+            props.geometryState.setGeometry([...newState])
         } else {
             props.geometryState.setGeometry([aGeo])
         }
@@ -94,190 +96,190 @@ const EditTools = (props) =>
         });
     }
 
-  // Grab the map object
-  let map = useMapEvent('draw:created' as any, onDrawCreate);
+    // Grab the map object
+    let map = useMapEvent('draw:created' as any, onDrawCreate);
 
-  let mapDrawStart = useMapEvent('draw:drawstart' as any, () => {
-    (context.layerContainer as any).clearLayers();
-  });
-  let mapDrawDeleted = useMapEvent('draw:deleted' as any, () => {
-    props.geometryState.setGeometry([]);
-  });
-  let mapEditSave = useMapEvent('draw:edited' as any, onEditStop);
+    let mapDrawStart = useMapEvent('draw:drawstart' as any, () => {
+        (context.layerContainer as any).clearLayers();
+    });
+    let mapDrawDeleted = useMapEvent('draw:deleted' as any, () => {
+        props.geometryState.setGeometry([]);
+    });
+    let mapEditSave = useMapEvent('draw:edited' as any, onEditStop);
 
-  const convertLineStringToPoly = (aGeo: any) => {
-    if (aGeo.geometry.type === 'LineString') {
-      const buffer = prompt('Enter buffer width (total) in meters', '1');
-      const buffered = turf.buffer(aGeo.geometry, parseInt(buffer, 10) / 1000, { units: 'kilometers', steps: 1 });
-      const result = turf.featureCollection([buffered, aGeo.geometry]);
+    const convertLineStringToPoly = (aGeo: any) => {
+        if (aGeo.geometry.type === 'LineString') {
+            const buffer = prompt('Enter buffer width (total) in meters', '1');
+            const buffered = turf.buffer(aGeo.geometry, parseInt(buffer, 10) / 1000, { units: 'kilometers', steps: 1 });
+            const result = turf.featureCollection([buffered, aGeo.geometry]);
 
-      return { ...aGeo, geometry: result.features[0].geometry };
-    }
-
-    return aGeo;
-  };
-
-  const setGeometryMapBounds = () => {
-    if (
-      props.geometryState?.geometry?.length &&
-      !(props.geometryState?.geometry[0].geometry.type === 'Point' && !props.geometryState?.geometry[0].radius)
-    ) {
-      const allGeosFeatureCollection = {
-        type: 'FeatureCollection',
-        features: [...props.geometryState.geometry]
-      };
-      const bboxCoords = turf.bbox(allGeosFeatureCollection);
-
-      map.fitBounds([
-        [bboxCoords[1], bboxCoords[0]],
-        [bboxCoords[3], bboxCoords[2]]
-      ]);
-    }
-  };
-
-  const updateMapOnGeometryChange = () => {
-    // upload from geometrystate props
-    // updates drawnItems with the latest geo changes, attempting to only draw new geos and delete no-longer-present ones
-    const newGeoKeys = { ...geoKeys };
-    //console.dir(props.geometryState);
-
-    if (props.geometryState) {
-      // For each geometry, add a new layer to the drawn features
-      props.geometryState.geometry.forEach((collection) => {
-        const style = {
-          weight: 4,
-          opacity: 0.65
-        };
-
-        const markerStyle = {
-          radius: 10,
-          weight: 4,
-          stroke: true
-        };
-
-        L.geoJSON(collection, {
-          style,
-          pointToLayer: (feature: any, latLng: any) => {
-            if (feature.properties.radius) {
-              return L.circle(latLng, { radius: feature.properties.radius });
-            } else {
-              return L.circleMarker(latLng, markerStyle);
-            }
-          },
-          onEachFeature: (feature: any, layer: any) => {
-            context.layerContainer.addLayer(layer);
-          }
-        });
-      });
-    }
-    if (props.interactiveGeometryState?.interactiveGeometry) {
-      props.interactiveGeometryState.interactiveGeometry.forEach((interactObj) => {
-        const key = interactObj.recordDocID || interactObj._id;
-        if (newGeoKeys[key] && newGeoKeys[key].hash === JSON.stringify(interactObj) && newGeoKeys[key] !== true) {
-          // old unchanged geo, no need to redraw
-          newGeoKeys[key] = {
-            ...newGeoKeys[key],
-            updated: false
-          };
-          return;
+            return { ...aGeo, geometry: result.features[0].geometry };
         }
 
-        // else prepare new Geo for drawing:
-        const style = {
-          color: interactObj.color,
-          weight: 4,
-          opacity: 0.65
-        };
+        return aGeo;
+    };
 
-        const markerStyle = {
-          radius: 10,
-          weight: 4,
-          stroke: true
-        };
+    const setGeometryMapBounds = () => {
+        if (
+            props.geometryState?.geometry?.length &&
+            !(props.geometryState?.geometry[0].geometry.type === 'Point' && !props.geometryState?.geometry[0].radius)
+        ) {
+            const allGeosFeatureCollection = {
+                type: 'FeatureCollection',
+                features: [...props.geometryState.geometry]
+            };
+            const bboxCoords = turf.bbox(allGeosFeatureCollection);
 
-        const geo = L.geoJSON(interactObj.geometry, {
-          // Note: the result of this isn't actually used, it seems?
-          style,
-          pointToLayer: (feature: any, latLng: any) => {
-            if (feature.properties.radius) {
-              return L.circle(latLng, { radius: feature.properties.radius });
-            } else {
-              return L.circleMarker(latLng, markerStyle);
-            }
-          },
-          onEachFeature: (feature: any, layer: any) => {
-            const content = interactObj.popUpComponent(interactObj.description);
-            layer.on('click', () => {
-              // Fires on click of single feature
+            map.fitBounds([
+                [bboxCoords[1], bboxCoords[0]],
+                [bboxCoords[3], bboxCoords[2]]
+            ]);
+        }
+    };
 
-              // Formulate a table containing all attributes
-              let table = '<table><tr><th>Attribute</th><th>Value</th></tr>';
-              Object.keys(feature.properties).forEach((f) => {
-                if (f !== 'uploadedSpatial') {
-                  table += `<tr><td>${f}</td><td>${feature.properties[f]}</td></tr>`;
-                }
-              });
-              table += '</table>';
+    const updateMapOnGeometryChange = () => {
+        // upload from geometrystate props
+        // updates drawnItems with the latest geo changes, attempting to only draw new geos and delete no-longer-present ones
+        const newGeoKeys = { ...geoKeys };
+        //console.dir(props.geometryState);
 
-              const loc = turf.centroid(feature);
-              const center = [loc.geometry.coordinates[1], loc.geometry.coordinates[0]];
+        if (props.geometryState) {
+            // For each geometry, add a new layer to the drawn features
+            props.geometryState.geometry.forEach((collection) => {
+                const style = {
+                    weight: 4,
+                    opacity: 0.65
+                };
 
-              if (feature.properties.uploadedSpatial) {
-                L.popup()
-                  .setLatLng(center as L.LatLngExpression)
-                  .setContent(table)
-                  .openOn(map);
-              } else {
-                L.popup()
-                  .setLatLng(center as L.LatLngExpression)
-                  .setContent(content)
-                  .openOn(map);
-              }
+                const markerStyle = {
+                    radius: 10,
+                    weight: 4,
+                    stroke: true
+                };
 
-              interactObj.onClickCallback();
+                L.geoJSON(collection, {
+                    style,
+                    pointToLayer: (feature: any, latLng: any) => {
+                        if (feature.properties.radius) {
+                            return L.circle(latLng, { radius: feature.properties.radius });
+                        } else {
+                            return L.circleMarker(latLng, markerStyle);
+                        }
+                    },
+                    onEachFeature: (feature: any, layer: any) => {
+                        context.layerContainer.addLayer(layer);
+                    }
+                });
             });
-          }
-        });
-        newGeoKeys[key] = {
-          hash: JSON.stringify(interactObj),
-          geo: geo,
-          updated: true
-        };
-      });
-    }
-    // Drawing step:
-    Object.keys(newGeoKeys).forEach((key: any) => {
-      if (newGeoKeys[key].updated === true) {
-        // draw layers to map
-        Object.values(newGeoKeys[key].geo._layers).forEach((layer: L.Layer) => {
-          context.layerContainer.addLayer(layer);
-        });
-      } else if (newGeoKeys[key].updated === false) {
-        return;
-      } else {
-        // remove old keys (delete step)
-        Object.values(newGeoKeys[key].geo._layers).forEach((layer: L.Layer) => {
-          context.layerContainer.removeLayer(layer);
-        });
-        delete newGeoKeys[key];
-        return;
-      }
-      // reset updated status for next refresh:
-      delete newGeoKeys[key].updated;
-    });
+        }
+        if (props.interactiveGeometryState?.interactiveGeometry) {
+            props.interactiveGeometryState.interactiveGeometry.forEach((interactObj) => {
+                const key = interactObj.recordDocID || interactObj._id;
+                if (newGeoKeys[key] && newGeoKeys[key].hash === JSON.stringify(interactObj) && newGeoKeys[key] !== true) {
+                    // old unchanged geo, no need to redraw
+                    newGeoKeys[key] = {
+                        ...newGeoKeys[key],
+                        updated: false
+                    };
+                    return;
+                }
 
-    // update stored geos, mapped by key
-    setGeoKeys(newGeoKeys);
-  };
+                // else prepare new Geo for drawing:
+                const style = {
+                    color: interactObj.color,
+                    weight: 4,
+                    opacity: 0.65
+                };
 
-  useEffect(() => {
-    if (!map) {
-      return;
-    }
+                const markerStyle = {
+                    radius: 10,
+                    weight: 4,
+                    stroke: true
+                };
 
-    if (!props.geometryState?.geometry) {
-      return;
-    }
+                const geo = L.geoJSON(interactObj.geometry, {
+                    // Note: the result of this isn't actually used, it seems?
+                    style,
+                    pointToLayer: (feature: any, latLng: any) => {
+                        if (feature.properties.radius) {
+                            return L.circle(latLng, { radius: feature.properties.radius });
+                        } else {
+                            return L.circleMarker(latLng, markerStyle);
+                        }
+                    },
+                    onEachFeature: (feature: any, layer: any) => {
+                        const content = interactObj.popUpComponent(interactObj.description);
+                        layer.on('click', () => {
+                            // Fires on click of single feature
+
+                            // Formulate a table containing all attributes
+                            let table = '<table><tr><th>Attribute</th><th>Value</th></tr>';
+                            Object.keys(feature.properties).forEach((f) => {
+                                if (f !== 'uploadedSpatial') {
+                                    table += `<tr><td>${f}</td><td>${feature.properties[f]}</td></tr>`;
+                                }
+                            });
+                            table += '</table>';
+
+                            const loc = turf.centroid(feature);
+                            const center = [loc.geometry.coordinates[1], loc.geometry.coordinates[0]];
+
+                            if (feature.properties.uploadedSpatial) {
+                                L.popup()
+                                    .setLatLng(center as L.LatLngExpression)
+                                    .setContent(table)
+                                    .openOn(map);
+                            } else {
+                                L.popup()
+                                    .setLatLng(center as L.LatLngExpression)
+                                    .setContent(content)
+                                    .openOn(map);
+                            }
+
+                            interactObj.onClickCallback();
+                        });
+                    }
+                });
+                newGeoKeys[key] = {
+                    hash: JSON.stringify(interactObj),
+                    geo: geo,
+                    updated: true
+                };
+            });
+        }
+        // Drawing step:
+        Object.keys(newGeoKeys).forEach((key: any) => {
+            if (newGeoKeys[key].updated === true) {
+                // draw layers to map
+                Object.values(newGeoKeys[key].geo._layers).forEach((layer: L.Layer) => {
+                    context.layerContainer.addLayer(layer);
+                });
+            } else if (newGeoKeys[key].updated === false) {
+                return;
+            } else {
+                // remove old keys (delete step)
+                Object.values(newGeoKeys[key].geo._layers).forEach((layer: L.Layer) => {
+                    context.layerContainer.removeLayer(layer);
+                });
+                delete newGeoKeys[key];
+                return;
+            }
+            // reset updated status for next refresh:
+            delete newGeoKeys[key].updated;
+        });
+
+        // update stored geos, mapped by key
+        setGeoKeys(newGeoKeys);
+    };
+
+    useEffect(() => {
+        if (!map) {
+            return;
+        }
+
+        if (!props.geometryState?.geometry) {
+            return;
+        }
 
         setGeometryMapBounds();
         updateMapOnGeometryChange();
@@ -309,8 +311,8 @@ const EditTools = (props) =>
     return (
         <IconButton ref={divRef} className={classes.toolButton} onClick={toggleMode}>
             {multiMode ?
-                <Typography>On</Typography> :
-                <Typography>Off</Typography>}
+                <img src={multi} style={{ width: 32, height: 32 }} /> :
+                <img src={single} style={{ width: 32, height: 32 }} />}
         </IconButton>
     )
 
