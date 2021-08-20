1c6d460309b7ab102ce35dc4d096729ec125e239
Mike Wells <micheal.w.wells@gmail.com>
2021-08-20 16:03:57 -0700

    disable poi loader

--- app/src/components/map/MapContainer2.tsx
+++ app/src/components/map/MapContainer2.tsx
@@ -315,7 +315,7 @@ const MapContainer2: React.FC<IMapContainerProps> = (props) => {
             <TileLayer url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}" />
           </LayersControl.BaseLayer>
           <LayersControl.Overlay checked name="Activities">
-            <TempPOILoader pointOfInterestFilter={props.pointOfInterestFilter}></TempPOILoader>
+            {/*<TempPOILoader pointOfInterestFilter={props.pointOfInterestFilter}></TempPOILoader>*/}
             {/* this line below works - its what you need for geosjon*/}
             <GeoJSON data={props.interactiveGeometryState?.interactiveGeometry} style={interactiveGeometryStyle} />
             {/* <GeoJSON data={vanIsland} style={interactiveGeometryStyle} onEachFeature={setupFeature} /> */}
