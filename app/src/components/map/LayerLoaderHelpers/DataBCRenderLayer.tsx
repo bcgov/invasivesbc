e3dde5e513f17dab360745dde8a65675bde9970c
Mike Wells <micheal.w.wells@gmail.com>
2021-08-31 15:29:17 -0700

    add multi mode layer stub component

---
+++ app/src/components/map/LayerLoaderHelpers/DataBCRenderLayer.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { WMSTileLayer } from 'react-leaflet';
+
+export enum LayerMode {
+  WMSOnline = 'wms_online',
+  WFSOnline = 'wfs_online',
+  VectorTilesOffline = 'vector_tiles_offline',
+  RegularFeaturesOffline = 'regular_features_offline'
+}
+
+export const DataBCLayer = (props) => {
+  if (!props.mode) {
+    throw 'you missed a map mode';
+  }
+  switch (props.mode) {
+    case LayerMode.WMSOnline:
+      return <WMSTileLayer url="http://openmaps.gov.bc.ca/geo/ows" layers={props.layerName} />;
+    case LayerMode.WFSOnline:
+      return <></>;
+    case LayerMode.VectorTilesOffline:
+      return <></>;
+    case LayerMode.RegularFeaturesOffline:
+      //this is the regular geojson stuff
+      return <></>;
+  }
+};
