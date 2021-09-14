9f1b6a0076bac7c5cc00d3308c4dd4817b562229
bburciaga <burciagab16@gmail.com>
2021-09-14 10:04:58 -0700

    generated GeoJSON

--- app/src/components/map/Tools/InfoAreaDescription.tsx
+++ app/src/components/map/Tools/InfoAreaDescription.tsx
@@ -1,61 +1,77 @@
 import { IconButton, Tooltip, Typography } from '@material-ui/core';
 import InfoIcon from '@material-ui/icons/Info';
 import React, { useState, useCallback, useEffect, useContext } from 'react';
-import { useMapEvent } from 'react-leaflet';
+import { useMapEvent, GeoJSON } from 'react-leaflet';
 import { utm_zone } from './DisplayPosition';
 import { toolStyles } from './ToolBtnStyles';
 import { ThemeContext } from 'contexts/themeContext';
 
 function SetViewOnClick({ map }: any, { animateRef }: any) {
   const [position, setPosition] = useState(map?.getCenter());
+  const [geoPT, setGeoPT] = useState(null);
   const [utm, setUTM] = useState(null);
   const themeContext = useContext(ThemeContext);
   const toolClass = toolStyles();
-  var strLong = position?.lng.toFixed(4).toString();
-  var strLat = position?.lat.toFixed(4).toString();
-  var long = parseFloat(strLong);
-  var lat = parseFloat(strLat);
-  map = useMapEvent('click', (e) => {
-    map.setView(e.latlng, map.getZoom(), {
-      animate: animateRef?.current || false
-    });
+
+  const generateGeo = () => {
+    if (position) {
+      setGeoPT({
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'Point',
+              coordinates: [position?.lng, position?.lat]
+            }
+          }
+        ]
+      });
+    }
+  };
+
+  const generatePopup = () => {};
+
+  useMapEvent('click', (e) => {
+    setPosition(e.latlng);
   });
 
-  const onMove = useCallback(() => {
-    setPosition(map.getCenter());
-  }, [map]);
-
   useEffect(() => {
-    map.on('move', onMove);
-    return () => {
-      map.off('move', onMove);
-    };
-  }, [map, onMove]);
-
-  useEffect(() => {
-    if (isFinite(long) && isFinite(lat)) {
-      setUTM(utm_zone(long, lat));
+    if (isFinite(position?.lng) && isFinite(position?.lat)) {
+      setUTM(utm_zone(position?.lng as number, position?.lat as number));
+      generateGeo();
     }
   }, [position]);
 
+  /* useEffect Check
+  useEffect(() => {
+    console.dir(geoPT?.features);
+  }, [geoPT]); */
+
   return (
-    <Tooltip
-      disableFocusListener
-      classes={{ tooltip: toolClass.tooltipWidth }}
-      placement="left"
-      title={
-        utm ? (
-          <>
-            <Typography>UTM Zone {utm[0]}</Typography>
-            <Typography>UTM Northing {utm[2]}</Typography>
-            <Typography>UTM Easting {utm[1]}</Typography>{' '}
-          </>
-        ) : null
-      }>
-      <IconButton className={toolClass.toolBtnCircle}>
-        <InfoIcon className={themeContext.themeType ? toolClass.toolIconDark : toolClass.toolIconLight} />
-      </IconButton>
-    </Tooltip>
+    <>
+      {position && (
+        <Tooltip
+          disableFocusListener
+          classes={{ tooltip: toolClass.tooltipWidth }}
+          placement="left"
+          title={
+            utm && (
+              <>
+                <Typography>UTM Zone {utm[0]}</Typography>
+                <Typography>UTM Northing {utm[2]}</Typography>
+                <Typography>UTM Easting {utm[1]}</Typography>{' '}
+              </>
+            )
+          }>
+          <IconButton className={toolClass.toolBtnCircle}>
+            <InfoIcon className={themeContext.themeType ? toolClass.toolIconDark : toolClass.toolIconLight} />
+          </IconButton>
+        </Tooltip>
+      )}
+      {/*<GeoJSON data={geoPT} key={Math.random()} />*/}
+    </>
   );
 }
 
