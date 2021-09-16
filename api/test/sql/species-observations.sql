70b3dab09bf53e23f4327f10938383fe9aa8bb69
Jamie Popkin <popkinj@littleearth.ca>
2021-09-16 13:46:50 -0700

    Simple query of invasive species observation areas

--- api/test/sql/species-observations.sql
+++ api/test/sql/species-observations.sql
@@ -1,10 +1,21 @@
+/*
+  Simple query of invasive species observation areas
+*/
 select
-	species "Species Code",
-  round(sum(st_area(geom))) "Area",
-  activity_ids
+	p.species "Species Code",
+  c.code_description "Species",
+  round(sum(st_area(p.geom))) "Area",
+  p.activity_ids
 from
-  test_spatial_merge
+  test_spatial_merge p,
+  code c
+where
+  p.species = c.code_name and
+  c.code_header_id = 30 -- Invasive plant id
 group by
-  species,
-  ids
+  p.species,
+  c.code_description,
+  p.activity_ids
+order by
+  round(sum(st_area(p.geom))) desc
 ;
