6051faa4960c33f8776adb3505806bc3f9ff1d17
Jamie Popkin <popkinj@littleearth.ca>
2021-09-21 16:20:40 -0700

    Compression issues still ongoing

--- database/src/seeds/05_riso.ts.ts
+++ database/src/seeds/05_riso.ts.ts
@@ -1,22 +1,24 @@
 import * as Knex from 'knex';
+import axios from 'axios';
+import zlib from 'zlib';
+import util from 'util';
+
+const inflate = util.promisify(zlib.inflate);
 
 export async function seed(knex: Knex): Promise<void> {
-  const request = new Request(
-    'https://quartech.s3.ca-central-1.amazonaws.com/regional_invasive_species_organization_areas.sql.gz'
-  );
+  const url = 'https://quartech.s3.ca-central-1.amazonaws.com/regional_invasive_species_organization_areas.sql.gz';
+  // const { data } = await axios.get(url, { responseType: 'arraybuffer' });
+  const { data } = await axios.get(url);
 
-  const reply = await fetch(request);
-  console.log(reply);
-
-  //await knex.raw(`alter sequence ${DB_SCHEMA}.code_code_id_seq restart with 1`);
-
-  // Deletes ALL existing entries
-  // await knex("table_name").del();
-
-  // // Inserts seed entries
-  // await knex("table_name").insert([
-  //     { id: 1, colName: "rowValue1" },
-  //     { id: 2, colName: "rowValue2" },
-  //     { id: 3, colName: "rowValue3" }
-  // ]);
+  // DEBUG: Not working, callback not firing
+  const result = zlib.inflate(data, (stuff, error) => {
+    console.log('stuff', stuff);
+    // console.log('error', error);
+  });
+  console.log(result);
+  //TODO: try the node-gzip library next
+  // const output = await inflate(data);
+  // console.log(data.toString());
+  // const { data } = await axios.get(url, { responseType: 'arraybuffer', decompress: true });
+  // console.log(data.toString());
 }
