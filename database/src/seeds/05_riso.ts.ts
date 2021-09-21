44297217c9badaa86d062826da72da3ccb63e1e1
Jamie Popkin <popkinj@littleearth.ca>
2021-09-21 14:17:59 -0700

    Requesting stuff

--- database/src/seeds/05_riso.ts.ts
+++ database/src/seeds/05_riso.ts.ts
@@ -1,13 +1,22 @@
-import * as Knex from "knex";
+import * as Knex from 'knex';
 
 export async function seed(knex: Knex): Promise<void> {
-    // Deletes ALL existing entries
-    await knex("table_name").del();
+  const request = new Request(
+    'https://quartech.s3.ca-central-1.amazonaws.com/regional_invasive_species_organization_areas.sql.gz'
+  );
 
-    // Inserts seed entries
-    await knex("table_name").insert([
-        { id: 1, colName: "rowValue1" },
-        { id: 2, colName: "rowValue2" },
-        { id: 3, colName: "rowValue3" }
-    ]);
-};
+  const reply = await fetch(request);
+  console.log(reply);
+
+  //await knex.raw(`alter sequence ${DB_SCHEMA}.code_code_id_seq restart with 1`);
+
+  // Deletes ALL existing entries
+  // await knex("table_name").del();
+
+  // // Inserts seed entries
+  // await knex("table_name").insert([
+  //     { id: 1, colName: "rowValue1" },
+  //     { id: 2, colName: "rowValue2" },
+  //     { id: 3, colName: "rowValue3" }
+  // ]);
+}
