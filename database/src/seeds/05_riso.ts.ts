e9f63c4d9a94fb12181d6fec1de24afe5ebd76fc
Jamie Popkin <popkinj@littleearth.ca>
2021-09-22 11:39:48 -0700

    Cleaning up

--- database/src/seeds/05_riso.ts.ts
+++ database/src/seeds/05_riso.ts.ts
@@ -8,25 +8,4 @@ export async function seed(knex: Knex): Promise<void> {
 
   const stuff = await ungzip(data);
   console.log(stuff.toString());
-
-  // .then((uncompressed) => {
-  //   console.log('uncompressed', uncompressed.toString());
-  // })
-  // .catch((err) => {
-  //   console.error('error', err);
-  // });
-
-  // const { data } = await axios.get(url, { responseType: 'arraybuffer' });
-
-  // DEBUG: Not working, callback not firing
-  // const result = zlib.inflate(data, (stuff, error) => {
-  //   console.log('stuff', stuff);
-  //   // console.log('error', error);
-  // });
-  // console.log(result);
-  //TODO: try the node-gzip library next
-  // const output = await inflate(data);
-  // console.log(data.toString());
-  // const { data } = await axios.get(url, { responseType: 'arraybuffer', decompress: true });
-  // console.log(data.toString());
 }
