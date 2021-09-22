b1c77af7de61d3fc7c84457ec590c4195d05fa96
Jamie Popkin <popkinj@littleearth.ca>
2021-09-22 10:52:22 -0700

    Gzip is working

--- database/src/seeds/05_riso.ts.ts
+++ database/src/seeds/05_riso.ts.ts
@@ -1,21 +1,29 @@
 import * as Knex from 'knex';
 import axios from 'axios';
-import zlib from 'zlib';
-import util from 'util';
-
-const inflate = util.promisify(zlib.inflate);
+import { ungzip } from 'node-gzip';
 
 export async function seed(knex: Knex): Promise<void> {
   const url = 'https://quartech.s3.ca-central-1.amazonaws.com/regional_invasive_species_organization_areas.sql.gz';
+  const { data } = await axios.get(url, { responseType: 'arraybuffer' });
+
+  const stuff = await ungzip(data);
+  console.log(stuff.toString());
+
+  // .then((uncompressed) => {
+  //   console.log('uncompressed', uncompressed.toString());
+  // })
+  // .catch((err) => {
+  //   console.error('error', err);
+  // });
+
   // const { data } = await axios.get(url, { responseType: 'arraybuffer' });
-  const { data } = await axios.get(url);
 
   // DEBUG: Not working, callback not firing
-  const result = zlib.inflate(data, (stuff, error) => {
-    console.log('stuff', stuff);
-    // console.log('error', error);
-  });
-  console.log(result);
+  // const result = zlib.inflate(data, (stuff, error) => {
+  //   console.log('stuff', stuff);
+  //   // console.log('error', error);
+  // });
+  // console.log(result);
   //TODO: try the node-gzip library next
   // const output = await inflate(data);
   // console.log(data.toString());
