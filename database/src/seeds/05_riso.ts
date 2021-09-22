13adce64690f3ed10a284e3614d160208655ef95
Jamie Popkin <popkinj@littleearth.ca>
2021-09-22 12:40:11 -0700

    This works

--- database/src/seeds/05_riso.ts
+++ database/src/seeds/05_riso.ts
@@ -5,7 +5,8 @@ import { ungzip } from 'node-gzip';
 export async function seed(knex: Knex): Promise<void> {
   const url = 'https://quartech.s3.ca-central-1.amazonaws.com/regional_invasive_species_organization_areas.sql.gz';
   const { data } = await axios.get(url, { responseType: 'arraybuffer' });
+  const sql = await ungzip(data);
 
-  const stuff = await ungzip(data);
-  console.log(stuff.toString());
+  await knex.raw('drop table if exists regional_invasive_species_organization_areas');
+  // await knex.raw(sql.toString());
 }
