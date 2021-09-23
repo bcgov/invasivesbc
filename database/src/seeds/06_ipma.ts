78885152650c5d0934bd80d547461e7863b79291
Jamie Popkin <popkinj@littleearth.ca>
2021-09-23 14:02:39 -0700

    IPMA migration

---
+++ database/src/seeds/06_ipma.ts
@@ -0,0 +1,12 @@
+import * as Knex from 'knex';
+import axios from 'axios';
+import { ungzip } from 'node-gzip';
+
+export async function seed(knex: Knex): Promise<void> {
+  const url = 'https://quartech.s3.ca-central-1.amazonaws.com/invasive_plant_management_areas.sql.gz';
+  const { data } = await axios.get(url, { responseType: 'arraybuffer' });
+  const sql = await ungzip(data);
+
+  await knex.raw('drop table if exists invasive_plant_management_areas');
+  await knex.raw(sql.toString());
+}
