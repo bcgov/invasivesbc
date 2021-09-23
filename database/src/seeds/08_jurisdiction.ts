6ea988d4c3952c647ea8c164780c362eacb6c169
Jamie Popkin <popkinj@littleearth.ca>
2021-09-23 15:59:09 -0700

    Jurisdiction migration

---
+++ database/src/seeds/08_jurisdiction.ts
@@ -0,0 +1,12 @@
+import * as Knex from 'knex';
+import axios from 'axios';
+import { ungzip } from 'node-gzip';
+
+export async function seed(knex: Knex): Promise<void> {
+  const url = 'https://quartech.s3.ca-central-1.amazonaws.com/jurisdiction.sql.gz';
+  const { data } = await axios.get(url, { responseType: 'arraybuffer' });
+  const sql = await ungzip(data);
+
+  await knex.raw('drop table if exists jurisdiction');
+  await knex.raw(sql.toString());
+}
