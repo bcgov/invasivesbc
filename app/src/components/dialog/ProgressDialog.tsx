c54cdafb071ff3e24e164ad48110c4d7db37b5ad
Hryhorii Pertaia <rakot1997@gmail.com>
2021-09-27 16:22:19 -0700

    added progress dialog for plan pagte

---
+++ app/src/components/dialog/ProgressDialog.tsx
@@ -0,0 +1,73 @@
+import {
+  Box,
+  CircularProgress,
+  Dialog,
+  DialogTitle,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+  Typography
+} from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import ErrorIcon from '@material-ui/icons/Error';
+import DoneAllIcon from '@material-ui/icons/DoneAll';
+import DoneIcon from '@material-ui/icons/Done';
+
+export interface IProgressDialog {
+  dialogTitle: string;
+  items: IProgressDialogItem[];
+  dialogOpen: boolean;
+  error?: boolean;
+  done?: boolean;
+}
+
+export interface IProgressDialogItem {
+  name: string;
+  description?: string;
+  state: 'complete' | 'error' | 'in_progress' | 'none';
+}
+
+export const ProgressDialog = (props: IProgressDialog) => {
+  const [open, setOpen] = useState(props.dialogOpen);
+
+  useEffect(() => {
+    setOpen(props.dialogOpen);
+  }, [props.dialogOpen]);
+
+  useEffect(() => {
+    if (props.done === true || props.error === true) {
+      setTimeout(() => {
+        setOpen(false);
+      }, 1000);
+    }
+  }, [props.done, props.error]);
+
+  return (
+    <Dialog open={open} aria-labelledby="alert-dialog-title" aria-describedby="alert-dialog-description">
+      <DialogTitle id="alert-dialog-title">
+        <Typography variant="h4">
+          {props.dialogTitle}
+          {props.done && <DoneAllIcon style={{ fill: '#46A44D', marginLeft: 20, marginTop: 5 }} />}
+          {props.error && <ErrorIcon style={{ fill: '#54101B', marginLeft: 20, marginTop: 5 }} />}
+        </Typography>
+      </DialogTitle>
+      <List>
+        {props.items.map((item) => (
+          <ListItem>
+            <ListItemAvatar>
+              <Box style={{ position: 'relative', width: 25, height: 25, paddingLeft: 10 }}>
+                {item.state === 'complete' && <DoneIcon style={{ fill: '#46A44D' }} />}
+                {item.state === 'in_progress' && (
+                  <CircularProgress size={25} style={{ position: 'relative', width: 25, height: 25 }} />
+                )}
+                {item.state === 'error' && <ErrorIcon style={{ fill: '#46A44D' }} />}
+              </Box>
+            </ListItemAvatar>
+            <ListItemText primary={item.name} secondary={item.description && item.description} />
+          </ListItem>
+        ))}
+      </List>
+    </Dialog>
+  );
+};
---
+++ app/src/components/map/LayerLoaderHelpers/ActivitiesAndPoiLayer.tsx
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react';
+import { GeoJSON, useMap, useMapEvent } from 'react-leaflet';
+import { createPolygonFromBounds } from './LtlngBoundsToPoly';
+import { Feature, Geometry } from 'geojson';
+import { Layer } from 'leaflet';
+import { useDataAccess } from 'hooks/useDataAccess';
+
+export const ActivitiesAndPoiLayer = (props) => {
+  const map = useMap();
+  const mapBounds = createPolygonFromBounds(map.getBounds(), map).toGeoJSON();
+  const [activities, setActivities] = useState(null);
+  const [pois, setPois] = useState(null);
+  const dataAccess = useDataAccess();
+
+  useMapEvent('moveend', () => {
+    fetchData();
+  });
+
+  const fetchData = async () => {
+    const activitiesData = await dataAccess.getActivitiesLean({ search_feature: mapBounds });
+    const poisData = await dataAccess.getPointsOfInterestLean({ search_feature: mapBounds });
+    const activitiesFeatureArray = [];
+    const poisFeatureArray = [];
+
+    activitiesData.rows.forEach((row) => {
+      activitiesFeatureArray.push(row.geojson);
+    });
+
+    poisData.rows.forEach((row) => {
+      poisFeatureArray.push(row.geojson);
+    });
+
+    setActivities({ type: 'FeatureCollection', features: activitiesFeatureArray });
+    setPois({ type: 'FeatureCollection', features: poisFeatureArray });
+    console.log(pois);
+  };
+
+  const onEachFeature = props.customOnEachFeature
+    ? props.customOnEachFeature
+    : (feature: Feature<Geometry, any>, layer: Layer) => {
+        const popupContent = `
+          <div>
+              <p>${feature.id}</p>                  
+          </div>
+        `;
+        layer.bindPopup(popupContent);
+      };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {activities && <GeoJSON key={Math.random()} onEachFeature={onEachFeature} data={activities} />}
+      {pois && <GeoJSON key={Math.random()} onEachFeature={onEachFeature} data={pois} />}
+    </>
+  );
+};
