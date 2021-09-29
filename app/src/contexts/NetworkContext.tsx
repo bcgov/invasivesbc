550b3ed085ee380bb328180ae493b165d80d2673
Sam Warren <samwarren4@gmail.com>
2021-09-29 13:56:19 -0700

    set network connected default false on mobile, do not require idir login if network not connected

--- app/src/contexts/NetworkContext.tsx
+++ app/src/contexts/NetworkContext.tsx
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
+import { Capacitor } from '@capacitor/core';
 
 interface INetworkContext {
   connected: boolean;
   setConnected: React.Dispatch<React.SetStateAction<boolean>>;
 }
 export const NetworkContext = React.createContext<INetworkContext>({
-  connected: true,
+  connected: Capacitor.getPlatform() !== 'web' ? false : true,
   setConnected: () => {}
 });
 
 export const NetworkContextProvider: React.FC = (props) => {
-  const [connected, setConnected] = React.useState(true);
+  const [connected, setConnected] = React.useState(Capacitor.getPlatform() !== 'web' ? false : true);
 
   return <NetworkContext.Provider value={{ connected, setConnected }}>{props.children}</NetworkContext.Provider>;
 };
