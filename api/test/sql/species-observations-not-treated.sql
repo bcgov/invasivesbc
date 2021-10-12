beb2c75bdb4240b40fd99f15915ad6162efe2802
Jamie Popkin <popkinj@littleearth.ca>
2021-10-12 15:42:53 -0700

    In the thick of it now

--- api/test/sql/species-observations-not-treated.sql
+++ api/test/sql/species-observations-not-treated.sql
@@ -4,29 +4,53 @@
     1. Add geometry to output so we can debug with qGIS.
     2. Strip out the species code join to lower complexity
 */
+drop table if exists area_to_treat;
+create table area_to_treat as
 select
   c.code_description "Species",
-  round(
-    sum(
-      public.st_area(
-        p1.geom
-      )
-    )
-  ) "Area"
+  case
+    when st_intersects(p1.geom,p2.geom)
+    then st_difference(p1.geom,p2.geom)
+    else p1.geom
+    end "geom"
 from
   code c,
   public.activities_by_species p1 left outer join -- Observations
   public.activities_by_species p2 -- Treatments
   on
-    st_intersects(p1.geom,p2.geom) and
-    p1.species = p2.species and
-    p1.max_created_timestamp < p2.max_created_timestamp
+    st_intersects(p1.geom,p2.geom)
 where
   date_part('year', p1.max_created_timestamp) = date_part('year', CURRENT_DATE) and
   p1.species = c.code_name and
   p1.activity_type = 'Observation' and
-  p2.activity_type = 'Treatment' and
-  c.code_header_id = 30 -- Invasive plant id
-group by
-  c.code_description
+  p2.activity_type = 'Treatment'  and
+  c.code_header_id = 30 and -- Invasive plant id
+  p1.species = p2.species and
+  p1.max_created_timestamp < p2.max_created_timestamp
+;
+
+/**
+  This test test is trying to do the same as above
+  but keep things as simple as possible.
+*/
+drop table if exists area_to_treat2;
+create table area_to_treat2 as
+select
+  p1.species "Species",
+  case
+    when st_intersects(p1.geom,p2.geom)
+    then st_difference(p1.geom,p2.geom)
+    else p1.geom
+    end "geom"
+from
+  public.activities_by_species p1 left outer join -- Observations
+  public.activities_by_species p2 -- Treatments
+  on
+    st_intersects(p1.geom,p2.geom)
+where
+  date_part('year', p1.max_created_timestamp) = date_part('year', CURRENT_DATE) and
+  p1.activity_type = 'Observation' and
+  p2.activity_type = 'Treatment'  and
+  p1.species = p2.species and
+  p1.max_created_timestamp < p2.max_created_timestamp
 ;
