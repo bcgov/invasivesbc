dd95659cbfbd6cff6d426da3d422c7a325be9a0f
Jamie Popkin <popkinj@littleearth.ca>
2021-10-13 13:46:29 -0700

    Get the outer join to work

--- api/test/sql/species-observations-not-treated.sql
+++ api/test/sql/species-observations-not-treated.sql
@@ -46,11 +46,26 @@ from
   public.activities_by_species p1 left outer join -- Observations
   public.activities_by_species p2 -- Treatments
   on
-    st_intersects(p1.geom,p2.geom)
+    st_intersects(p2.geom,p1.geom)
 where
-  date_part('year', p1.max_created_timestamp) = date_part('year', CURRENT_DATE) and
   p1.activity_type = 'Observation' and
-  p2.activity_type = 'Treatment'  and
-  p1.species = p2.species and
-  p1.max_created_timestamp < p2.max_created_timestamp
+  p2.activity_type = 'Treatment'
+;
+
+
+
+drop table if exists area_to_treat2;
+create table area_to_treat2 as
+select
+  p1.species "Species",
+  case
+    when st_intersects(p1.geom,p2.geom)
+    then st_difference(p1.geom,p2.geom)
+    else p1.geom
+    end "geom"
+from
+  (select * from public.activities_by_species where activity_type = 'Observation') p1 left outer join -- Observations
+  (select * from public.activities_by_species where activity_type = 'Treatment') p2 -- Treatments
+  on
+    st_intersects(p2.geom,p1.geom)
 ;
