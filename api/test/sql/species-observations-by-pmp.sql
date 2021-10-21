aeee76f77c39955ed01cf998a99d97fbfe50948f
Jamie Popkin <popkinj@littleearth.ca>
2021-10-21 13:52:23 -0700

    Fixed metabase sql

--- api/test/sql/species-observations-by-pmp.sql
+++ api/test/sql/species-observations-by-pmp.sql
@@ -1,18 +1,22 @@
 /*
-  Simple query of invasive species observation areas
+  Species observations by planning unit.
+  Metabase doesn't support the insertion of 
+  table aliases. So if Metabase needs to query against
+  an attribute, make sure that you don't use an table
+  alias for that attribute.
 */
 select
   c.code_description "Species",
-  r.agency "Agency",
+  public.regional_invasive_species_organization_areas.agency "Agency",
   round(
     sum(
       public.st_area(
         case
-          when public.st_within(p.geom,r.geom)
+          when public.st_within(p.geom,public.regional_invasive_species_organization_areas.geom)
           then p.geom
           else public.st_intersection(
             p.geom,
-            public.st_transform(r.geom,3005)
+            public.st_transform(public.regional_invasive_species_organization_areas.geom,3005)
           )
           end
       )
@@ -22,16 +26,16 @@ from
   code c,
   public.activities_by_species p join
   public.regional_invasive_species_organization_areas r on
-  public.st_intersects(p.geom,public.st_transform(r.geom,3005))
+  public.st_intersects(p.geom,public.st_transform(public.regional_invasive_species_organization_areas.geom,3005))
 where
   date_part('year', p.max_created_timestamp) = date_part('year', CURRENT_DATE) and
   p.species = c.code_name and
   p.activity_type = 'Observation' and
   c.code_header_id = 30 -- Invasive plant id
-  [[and r.agent = {{agent}}]] -- TODO: Still debugging this
+  [[and {{agent}}]] -- This is for metabase
 group by
   c.code_description,
-  r.agency
+  public.regional_invasive_species_organization_areas.agency
 order by
-  r.agency
+  public.regional_invasive_species_organization_areas.agency
 ;
