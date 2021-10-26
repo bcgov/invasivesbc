bba69e649ed1f22a76624e4546805d2b73e5775c
bburciaga <burciagab16@gmail.com>
2021-10-26 10:03:55 -0700

    added new zoombar for map
    Resolved merge conflict from updated dev to popupStuff

---
+++ app/src/components/map/Tools/ZoomBar.tsx
@@ -0,0 +1,68 @@
+import { Capacitor } from '@capacitor/core';
+import { Slider } from '@material-ui/core';
+import L from 'leaflet';
+import React, { useEffect, useRef } from 'react';
+
+const POSITION_CLASSES = {
+  bottomleft: 'leaflet-bottom leaflet-left',
+  bottomright: 'leaflet-bottom leaflet-right',
+  topleft: 'leaflet-top leaflet-left',
+  topright: 'leaflet-top leaflet-right'
+};
+
+export const ZoomBar = (props) => {
+  const zoomFunction = (zoom) => {
+    props?.map.setZoom(zoom);
+  };
+
+  return (
+    <div
+      onTouchStart={() => {
+        props?.map.dragging.disable();
+        props?.map.doubleClickZoom.disable();
+      }}
+      onTouchMove={() => {
+        props?.map.dragging.disable();
+        props?.map.doubleClickZoom.disable();
+      }}
+      onTouchEnd={() => {
+        props?.map.dragging.disable();
+        props?.map.doubleClickZoom.disable();
+      }}
+      onMouseOver={() => {
+        if (Capacitor.getPlatform() == 'web') {
+          props?.map.dragging.disable();
+          props?.map.doubleClickZoom.disable();
+        }
+      }}
+      onMouseOut={() => {
+        if (Capacitor.getPlatform() == 'web') {
+          props?.map.dragging.enable();
+          props?.map.doubleClickZoom.enable();
+        }
+      }}
+      style={{
+        zIndex: 1500,
+        width: 300,
+        borderRadius: 5,
+        backgroundColor: 'white',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+      <Slider
+        style={{
+          zIndex: 1500,
+          width: 270
+        }}
+        step={1}
+        marks
+        min={1}
+        max={30}
+        onChange={(event: any, newZoom: number) => {
+          zoomFunction(newZoom);
+        }}
+      />
+    </div>
+  );
+};
