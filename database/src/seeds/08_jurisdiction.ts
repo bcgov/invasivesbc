735dafa9cc90d3df1c85a5739755f2dce34414ae
Jamie Popkin <popkinj@littleearth.ca>
2021-10-29 12:36:34 -0700

    doc

--- database/src/seeds/08_jurisdiction.ts
+++ database/src/seeds/08_jurisdiction.ts
@@ -1,7 +1,6 @@
 import * as Knex from 'knex';
 import axios from 'axios';
 import { ungzip } from 'node-gzip';
-import { join } from 'path';
 
 export async function seed(knex: Knex): Promise<void> {
   const url = 'https://nrs.objectstore.gov.bc.ca/seeds/jurisdiction.sql.gz';
@@ -12,15 +11,15 @@ export async function seed(knex: Knex): Promise<void> {
 
   /**
    * This file is too big to run all at once.
-   * Split up by lines and run separately
+   * Split up by lines, join in groups then run separately
    */
   const chunk = 1000; // Run 1000 transactions at a time
   const lines = sql.toString().split(/\r?\n/);
 
   let cluster: Array<String>;
   for (let i = 0, j = lines.length; i < j; i += chunk) {
-    cluster = lines.slice(i, i + chunk);
-    await knex.raw(cluster.join(' '));
+    cluster = lines.slice(i, i + chunk); // Array of commands
+    await knex.raw(cluster.join(' ')); // Execute
   }
 
   // for (const line of lines) {
