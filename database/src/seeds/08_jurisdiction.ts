05bc6947e223440454a4551fab2bfac021b9252e
Jamie Popkin <popkinj@littleearth.ca>
2021-11-02 13:48:31 -0700

    Clustering not working

--- database/src/seeds/08_jurisdiction.ts
+++ database/src/seeds/08_jurisdiction.ts
@@ -11,16 +11,16 @@ export async function seed(knex: Knex): Promise<void> {
    * This file is too big to run all at once.
    * Split up by lines, join in groups then run separately
    */
-  const chunk = 10; // Run 100 transactions at a time
-  const lines = sql.toString().split(/\r?\n/);
+  // const chunk = 10; // Run 100 transactions at a time
 
-  let cluster: Array<String>;
-  for (let i = 0, j = lines.length; i < j; i += chunk) {
-    cluster = lines.slice(i, i + chunk); // Array of commands
-    await knex.raw(cluster.join(' ')); // Execute
-  }
-
-  // for (const line of lines) {
-  //   await knex.raw(line);
+  // let cluster: Array<String>;
+  // for (let i = 0, j = lines.length; i < j; i += chunk) {
+  //   cluster = lines.slice(i, i + chunk); // Array of commands
+  //   await knex.raw(cluster.join(' ')); // Execute
   // }
+
+  const lines = sql.toString().split(/\r?\n/);
+  for (const line of lines) {
+    await knex.raw(line);
+  }
 }
