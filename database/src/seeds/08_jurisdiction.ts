8b67b0f8e8b71170ae9ff1a1a6bf30f238d98f99
Jamie Popkin <popkinj@littleearth.ca>
2021-11-03 13:22:12 -0700

    Logic for switching between Jurisdiction sizes

--- database/src/seeds/08_jurisdiction.ts
+++ database/src/seeds/08_jurisdiction.ts
@@ -3,21 +3,28 @@ import axios from 'axios';
 import { ungzip } from 'node-gzip';
 
 export async function seed(knex: Knex): Promise<void> {
-  const url = 'https://nrs.objectstore.gov.bc.ca/seeds/jurisdiction.sql.gz';
+  /**
+   * If we are in development use the light dataset for
+   * speeding up the development life cycle.
+   */
+  const env: string = process.env.NODE_ENV;
+  let url: string;
+  if (env.match(/dev/i)) {
+    url = 'https://nrs.objectstore.gov.bc.ca/seeds/jurisdiction_vancouver_island.sql.gz';
+  } else {
+    url = 'https://nrs.objectstore.gov.bc.ca/seeds/jurisdiction.sql.gz';
+  }
+
+  // Fetch data
   const { data } = await axios.get(url, { responseType: 'arraybuffer' });
-  const sql = await ungzip(data);
+
+  // Unzip to text
+  const sql: string = await ungzip(data);
 
   /**
    * This file is too big to run all at once.
-   * Split up by lines, join in groups then run separately
+   * Split up by lines.
    */
-  // const chunk = 10; // Run 100 transactions at a time
-
-  // let cluster: Array<String>;
-  // for (let i = 0, j = lines.length; i < j; i += chunk) {
-  //   cluster = lines.slice(i, i + chunk); // Array of commands
-  //   await knex.raw(cluster.join(' ')); // Execute
-  // }
 
   const lines = sql.toString().split(/\r?\n/);
   for (const line of lines) {
