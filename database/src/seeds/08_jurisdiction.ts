44096e034bee6aece59ae2d8f9c899d0c053969e
Jamie Popkin <popkinj@littleearth.ca>
2021-11-03 12:59:06 -0700

    Sorry... This logic corrupts the geometries.

--- database/src/seeds/08_jurisdiction.ts
+++ database/src/seeds/08_jurisdiction.ts
@@ -12,16 +12,16 @@ export async function seed(knex: Knex): Promise<void> {
    * Split up by lines, join in groups then run separately
    */
   // const chunk = 10; // Run 100 transactions at a time
+
+  // let cluster: Array<String>;
+  // for (let i = 0, j = lines.length; i < j; i += chunk) {
+  //   cluster = lines.slice(i, i + chunk); // Array of commands
+  //   await knex.raw(cluster.join(' ')); // Execute
+  // }
+
   const lines = sql.toString().split(/\r?\n/);
-  let i = 0;
-  let lineArr = [];
   for (const line of lines) {
-    lineArr.push(line);
-    if (i % 100 === 0 || lines.length - 1 - i === 0) {
-      await knex.raw(lineArr.join(''));
-      lineArr = [];
-    }
-    i++;
+    await knex.raw(line);
   }
 
   await knex.raw(`      
