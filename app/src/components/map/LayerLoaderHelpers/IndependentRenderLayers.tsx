99d38b6f287376a4734fff20016a485073e0ff69
bburciaga <burciagab16@gmail.com>
2021-11-16 15:39:33 -0800

    new files added and LAYERS.json replaces GEO_DATA.json

---
+++ app/src/components/map/LayerLoaderHelpers/IndependentRenderLayers.tsx
@@ -0,0 +1,30 @@
+import React, { useContext } from 'react';
+import { ActivitiesLayer } from './ActivitiesLayer';
+import { PoisLayer } from './PoisLayer';
+import { JurisdictionsLayer } from './JurisdictionsLayer';
+import { NetworkContext } from 'contexts/NetworkContext';
+
+export enum IndependentLayers {
+  Activities = 'LEAN_ACTIVITIES',
+  POI = 'LEAN_POI',
+  Jurisdictions = 'JURISDICTIONS'
+}
+
+export const IndependentLayer = (props) => {
+  const networkContext = useContext(NetworkContext);
+
+  if (Object.values(IndependentLayers).includes(props.layerName)) {
+    switch (props.layerName) {
+      case 'LEAN_ACTIVITIES':
+        return (
+          <ActivitiesLayer color_code={props.color_code} online={networkContext.connected} opacity={props.opacity} />
+        );
+      case 'LEAN_POI':
+        return <PoisLayer online={networkContext.connected} opacity={props.opacity} />;
+      case 'JURISDICTIONS':
+        return <JurisdictionsLayer online={networkContext.connected} opacity={props.opacity} />;
+      default:
+        return <></>;
+    }
+  }
+};
