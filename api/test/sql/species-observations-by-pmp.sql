953304c9f868a771131e94ca7b3459251a64741a
Jamie Popkin <popkinj@littleearth.ca>
2021-11-29 06:51:28 -0800

    Fixing this report query

--- api/test/sql/species-observations-by-pmp.sql
+++ api/test/sql/species-observations-by-pmp.sql
@@ -12,11 +12,17 @@ select
     sum(
       public.st_area(
         case
-          when public.st_within(p.geom,public.pest_management_plan_areas.wkb_geometry)
+          when public.st_within(
+            p.geom,
+            public.geometry(public.pest_management_plan_areas.geog)
+          )
           then p.geom
           else public.st_intersection(
             p.geom,
-            public.st_transform(public.pest_management_plan_areas.wkb_geometry,3005)
+            public.st_transform(
+              public.geometry(public.pest_management_plan_areas.geog),
+              3005
+            )
           )
           end
       )
@@ -26,7 +32,12 @@ from
   code c,
   public.activities_by_species p join
   public.pest_management_plan_areas on
-  public.st_intersects(p.geom,public.st_transform(public.pest_management_plan_areas.wkb_geometry,3005))
+  public.st_intersects(
+    p.geom,public.st_transform(
+      public.geometry(public.pest_management_plan_areas.geog),
+      3005
+    )
+  )
 where
   date_part('year', p.max_created_timestamp) = date_part('year', CURRENT_DATE) and
   p.species = c.code_name and
@@ -40,6 +51,8 @@ where
       code_header_name = 'invasive_plant_code'
   )
   [[and {{pmp_name}}]] -- This is for metabase
+  -- and pest_management_plan_areas.pmp_name = 'PMP - South Coast' -- For testing
+
 group by
   c.code_description,
   public.pest_management_plan_areas.pmp_name
