476ea1154e32fa6a0b82297e702609a66b64ad99
Jamie Popkin <popkinj@littleearth.ca>
2021-12-03 07:19:47 -0800

    Clearing tables before seeding

--- database/src/seeds/05_riso.ts
+++ database/src/seeds/05_riso.ts
@@ -7,5 +7,9 @@ export async function seed(knex: Knex): Promise<void> {
   const { data } = await axios.get(url, { responseType: 'arraybuffer' });
   const sql = await ungzip(data);
 
+  // Clear the table
+  const clear = 'truncate table public.regional_invasive_species_organization_areas;';
+  await knex.raw(clear);
+
   await knex.raw(sql.toString());
 }
