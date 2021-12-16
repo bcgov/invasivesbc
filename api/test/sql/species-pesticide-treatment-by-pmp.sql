139523d60fdae9ff2a5e39f1f345126f84121c04
Jamie Popkin <popkinj@littleearth.ca>
2021-12-16 10:05:05 -0800

    Inline spatial query of the Island

--- api/test/sql/species-pesticide-treatment-by-pmp.sql
+++ api/test/sql/species-pesticide-treatment-by-pmp.sql
@@ -1,14 +1,15 @@
 /*
   Need to isolate the following:
-  1. Product Name
+  1. Product Name           - herbicide_code??
   2. Active Ingredient
   3. PCP Registration #
-  4. Quantity Used Kg.
-  5. Area Treated Ha
-  6. Method of Application
+  4. Quantity Used Kg.      - amount_of_undiluted_herbicide_used
+  5. Area Treated Ha        - area_treated_hectares
+  6. Method of Application  - chemical_method_code
+  7. Service license        - applicator1_license
 
   Filtered by
-  1. Jurisdiction
+  1. Jurisdiction           - jurisdiction_code
   2. PMP.... or
   3. Custom Layer
 
@@ -17,35 +18,14 @@
 select
   public.st_geomFromGeoJSON(
     '{ "type": "Polygon", "coordinates": [ [ [ -128.594970703125, 50.972264889367494 ], [ -128.551025390625, 50.21909462044748 ], [ -126.221923828125, 48.95858066440977 ], [ -124.53002929687499, 48.44377831058802 ], [ -123.22265625000001, 48.16608541901253 ], [ -123.134765625, 48.777912755501845 ], [ -124.112548828125, 49.50380954152213 ], [ -125.518798828125, 50.41551870402678 ], [ -128.001708984375, 51.069016659603896 ], [ -128.594970703125, 50.972264889367494 ] ] ] }'
-  )
-;
+  ) "geom"
 
-/**
-  Select all species IDs
-  with their descriptions
-*/
-with species_codes as (
-  select
-    code_name,
-    code_description
-  from
-    code
-  where
-    code_header_id = (
-      select
-        code_header_id
-      from
-        code_header
-      where
-        code_header_name = 'invasive_plant_code'
-    )
-),
 
 /**
   Select all chemical treatment IDs
   with their descriptions
 */
-chemical_codes as (
+with chemical_codes as (
   select
     code_name,
     code_description
@@ -61,7 +41,25 @@ chemical_codes as (
         code_header_name = 'herbicide_type_code'
         -- code_header_name = 'chemical_method_code'
     )
+),
+custom_shape as (
+  select
+    public.st_setSRID(
+      public.st_geomFromGeoJSON(
+        '{ "type": "Polygon", "coordinates": [ [ [ -128.594970703125, 50.972264889367494 ], [ -128.551025390625, 50.21909462044748 ], [ -126.221923828125, 48.95858066440977 ], [ -124.53002929687499, 48.44377831058802 ], [ -123.22265625000001, 48.16608541901253 ], [ -123.134765625, 48.777912755501845 ], [ -124.112548828125, 49.50380954152213 ], [ -125.518798828125, 50.41551870402678 ], [ -128.001708984375, 51.069016659603896 ], [ -128.594970703125, 50.972264889367494 ] ] ] }'
+      ), 4326
+    ) "geom"
 )
+
+select
+  p.activity_ids
+from
+  public.treatments_by_species p join
+  custom_shape c on
+  public.st_intersects(
+    p.geom,public.st_transform(c.geom,3005)
+  )
+;
 
 /*
   Species chemical treatment by planning unit.
