55004cff2882e9c2455856e24d950ba0d6e1cdd0
bburciaga <burciagab16@gmail.com>
2022-01-05 16:39:14 -0800

    added file for the HomeRouter

---
+++ app/src/features/home/references/ReferenceIAPPSitePage.tsx
@@ -0,0 +1,33 @@
+import { IAPPSite } from 'components/points-of-interest/IAPP/IAPP-Site';
+import { useDataAccess } from 'hooks/useDataAccess';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+
+export const ReferenceIAPPSitePage: React.FC = (props) => {
+  const urlParams: { id: string } = useParams();
+  const dataAccess = useDataAccess();
+  let arr = [];
+
+  const [poi, setPOI] = useState(null);
+
+  useEffect(() => {
+    if (urlParams) {
+      arr.push(urlParams.id);
+    }
+  }, [urlParams]);
+
+  useEffect(() => {
+    if (!poi) {
+      fetchPOI();
+    } else {
+      console.log('poi', poi);
+    }
+  }, [poi]);
+
+  const fetchPOI = async () => {
+    const poiData = await dataAccess.getPointsOfInterest({ point_of_interest_ids: arr });
+    setPOI(poiData.rows[0]);
+  };
+
+  return <div>{poi && <IAPPSite record={poi} />}</div>;
+};
