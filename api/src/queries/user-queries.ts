70b78c8b717173d1cb328f445b4caabe5d79aa68
Sam Warren <samwarren4@gmail.com>
2022-01-14 14:24:27 -0800

    add renew user to queries

--- api/src/queries/user-queries.ts
+++ api/src/queries/user-queries.ts
@@ -22,3 +22,13 @@ export const getUserByBCEIDSQL = (bceid_userid: string): SQLStatement => {
     SELECT * FROM application_user WHERE bceid_userid = ${bceid_userid};
   `;
 };
+
+export const renewUserSQL = (userId: string): SQLStatement => {
+  const today = new Date();
+  const expiryDate = new Date(today.getFullYear() + 1, today.getMonth(), today.getDate()).toUTCString();
+  return SQL`
+    UPDATE application_user SET
+      expiry_date = ${expiryDate}
+    WHERE user_id = ${parseInt(userId)};
+  `;
+};
