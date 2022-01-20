27e56647f07c433fa482e6bfdc68a62193cf1107
bburciaga <burciagab16@gmail.com>
2022-01-20 15:06:56 -0800

    added helper functions for iapp_json_utils

--- api/src/utils/iapp-json-utils.ts
+++ api/src/utils/iapp-json-utils.ts
@@ -4,56 +4,8 @@ import { getDBConnection } from '../database/db';
 import { PointOfInterestSearchCriteria } from '../models/point-of-interest';
 import { getIappExtractFromDB, getSitesBasedOnSearchCriteriaSQL } from '../queries/iapp-queries';
 import { getLogger } from './logger';
+import { densityMap, distributionMap, mapAspect, mapSlope } from './iapp-payload/iapp-function-utils';
 const defaultLog = getLogger('point-of-interest');
-
-const densityMap = {
-  '0: Unknown Density': 'X',
-  '1: Low (<= 1 plant/m2)': 'L',
-  '2: Med (2-5 plants/m2)': 'M',
-  '3: High (6-10 plants/m2)': 'H',
-  '4: Dense (>10 plants/m2)': 'D'
-};
-
-const distributionMap = {
-  '0: Unknown distribution': 'NA',
-  '1: rare individual / single occurrence': 'RS',
-  '2: few sporadically': 'FS',
-  '3: single patch or clump': 'CL',
-  '4: several sporadically individuals': 'SS',
-  '5: a few patches or clumps': 'FP',
-  '6: several well-spaced patches / clumps': 'WS',
-  '7: continuous / uniform': 'CU',
-  '8: continuous with a few gaps': 'CO',
-  '9: continuous / dense': 'CD'
-};
-
-const mapSlope = (slope) => {
-  if (slope === '') return 'NA';
-  slope = Number(slope);
-  if (!slope) return 'FL';
-  if (slope < 5) return 'NF';
-  if (slope < 10) return 'GS';
-  if (slope < 15) return 'MS';
-  if (slope < 20) return 'SS';
-  if (slope < 25) return 'VS';
-  if (slope < 30) return 'ES';
-  if (slope < 45) return 'ST';
-  if (slope >= 45) return 'VT';
-  return 'NA';
-};
-
-const mapAspect = (aspect) => {
-  aspect = Number(aspect);
-  if ((aspect > 333.5 && aspect <= 360) || aspect <= 22.5) return 'N';
-  if (aspect <= 67.5) return 'NE';
-  if (aspect <= 112.5) return 'E';
-  if (aspect <= 157.5) return 'SE';
-  if (aspect <= 202.5) return 'S';
-  if (aspect <= 247.5) return 'SW';
-  if (aspect <= 292.5) return 'W';
-  if (aspect <= 333.5) return 'NW';
-  return 'NA';
-};
 
 const getSurveyObj = (row: any) => {
   return {
---
+++ api/src/utils/iapp-payload/extracts-json-utils.ts
@@ -0,0 +1,108 @@
+export const chemicalTreatmentJSON = (treatment: any) => {
+  return {
+    dilution: treatment.dilution_percent, // find dilution amount???
+    employer: treatment.employer,
+    humidity: null, // Could not find
+    map_code: treatment.invasive_plant, // convert to code
+    monitoring: [], // Could not find
+    pup_number: treatment.pup_number,
+    wind_speed: null, // Could not find
+    common_name: treatment.invasive_plant, // use common name
+    tank_mix_id: null, // Could not find tank mix id
+    temperature: null, // Could not find tempurature
+    project_code: [
+      treatment.treatment_paper_file_id // What is the difference between site_paper_file_id
+    ], //  and treatment_paper_file_id????
+    treatment_id: treatment.chemicaltreatmentid,
+    reported_area: null, // area of POI or treated area??????
+    treatment_date: treatment.site_crated_date,
+    treatment_time: null, // Could not find: no treatment time; just the date
+    wind_direction: null, // Could not find: no wind direction
+    chemical_method: treatment.method,
+    general_comment: [treatment.site_comments, treatment.treatment_comments],
+    pmra_reg_number: null, // Could not find: No pmra_reg_number
+    application_rate: treatment.application_rate,
+    herbicide_amount: treatment.amount_of_undiluted_herbicide_used, // find herbicide amount
+    mix_delivery_rate: treatment.delivery_rate,
+    chemical_method_code: treatment.method, // convert to code
+    herbicide_description: treatment.treatment_comments, // I believe this is what it should be???? COuld be wrong
+    liquid_herbicide_code: null, // Could not find: NO liquid herbicide code int chemical_treatment_extracts
+    service_licence_number: treatment.service_licence_number,
+    pmp_confirmation_number: treatment.pmp_number,
+    invasive_species_agency_code: treatment.treatment_agency
+  };
+};
+
+export const biologicalDispersalJSON = (dispersal: any) => {
+  return {
+    map_code: dispersal.invasive_plant, // convert to map code (XX)
+    utm_zone: dispersal.utm_zone,
+    agent_count: null,
+    common_name: dispersal.invasive_plant, // use common name
+    plant_count: dispersal.density,
+    utm_easting: dispersal.utm_easting,
+    project_code: [dispersal.dispersal_paper_file_id],
+    utm_northing: dispersal.utm_northing,
+    monitoring_id: dispersal.biologicaldispersalid, // What is this??? (used dispersal id)
+    count_duration: null, // Could not find or don't know what it means
+    general_comment: dispersal.site_comments,
+    monitoring_date: dispersal.inspection_date,
+    eggs_present_ind: dispersal.eggs_present,
+    pupae_present_ind: dispersal.pupea_present,
+    adults_present_ind: dispersal.adults_present,
+    larvae_present_ind: dispersal.larvae_present,
+    invasive_plant_code: dispersal.invasive_plant, // convert to code (XXX)
+    tunnels_present_ind: dispersal.tunnels_present,
+    biological_agent_code: dispersal.biological_agent,
+    oviposition_marks_ind: dispersal.oviposition_marks,
+    biological_dispersal_id: dispersal.biologicaldispersalid,
+    root_feeding_damage_ind: dispersal.rootfeeding_damage,
+    seed_feeding_damage_ind: dispersal.seedfeeding_damage,
+    foliar_feeding_damage_ind: dispersal.foliar_feeding_damage,
+    invasive_species_agency_code: dispersal.dispersal_agency // convert to code
+  };
+};
+
+export const biologicalTreatmentsJSON = (treatment: any) => {
+  return {
+    map_code: treatment.invasive_plant, // convert to code (XX)
+    utm_zone: treatment.utm_zone,
+    monitoring: [],
+    common_name: treatment.invasive_plant, // use common name
+    utm_easting: treatment.utm_easting,
+    agent_source: treatment.bioagent_source,
+    project_code: [treatment.treatment_paper_file_id],
+    treatment_id: treatment.biotreatmentid,
+    utm_northing: treatment.utm_northing,
+    stage_egg_ind: null,
+    stage_pupa_ind: null,
+    treatment_date: treatment.treatment_date,
+    collection_date: null, // could not find
+    general_comment: treatment.treatment_comments,
+    stage_larva_ind: null, // could not find
+    stage_other_ind: null, // could not find
+    release_quantity: treatment.release_quantity,
+    classified_area_code: null, // could not find
+    biological_agent_code: treatment.biological_agent,
+    biological_agent_stage_code: null,
+    invasive_species_agency_code: treatment.treatment_agency // convert to code
+  };
+};
+
+export const mechanicalTreatmenntsJSON = (treatment: any) => {
+  return {
+    employer: treatment.employer,
+    map_code: treatment.invasive_plant, // convert to code (XX)
+    monitoring: [],
+    common_name: treatment.invasive_plant, // use common name
+    project_code: [treatment.treatment_paper_file_id],
+    treatment_id: treatment.mechanicaltreatmentid,
+    mechanical_id: treatment.mechanicaltreatmentid, // what is this supposed to be???? different id??
+    reported_area: null,
+    treatment_date: treatment.treatment_date,
+    general_comment: treatment.treatment_comments,
+    mechanical_method: treatment.treatment_method,
+    mechanical_method_code: treatment.treatment_method, // convert to code
+    invasive_species_agency_code: treatment.treatment_agency
+  };
+};
