25b6f04305b2eb6e8712f539c47ef33b3aed4d90
Hryhorii Pertaia <rakot1997@gmail.com>
2022-02-08 09:42:14 -0800

    added buttons to observations (not working)

--- app/src/components/activities-list/Tables/Plant/PlantObservationsTable.tsx
+++ app/src/components/activities-list/Tables/Plant/PlantObservationsTable.tsx
@@ -3,29 +3,60 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import Box from '@mui/material/Box';
 import { AuthStateContext } from 'contexts/authStateContext';
 import { useDataAccess } from 'hooks/useDataAccess';
 import React, { useContext, useState, useEffect } from 'react';
 import { DataGrid } from '@mui/x-data-grid';
-import { CircularProgress, Grid } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
 import { activites_default_headers, mapActivitiesToDataGridRows } from '../ActivityTablesHelpers';
 import { ActivitySubtypeShortLabels } from '../../../../constants/activities';
+import makeStyles from '@mui/styles/makeStyles';
+import { Theme } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import AddIcon from '@mui/icons-material/Add';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  accordionHeader: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  accordionHeaderText: {
+    margin: 5
+  },
+  createButtonsContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'end',
+    flexGrow: 1,
+    minHeight: '50px'
+  },
+  createButton: {
+    margin: '5px'
+  }
+}));
 
 const PlantObservationsTable = () => {
+  const classes = useStyles();
   const dataAccess = useDataAccess();
   const { userInfo, rolesUserHasAccessTo } = useContext(AuthStateContext);
   const [activities, setActivities] = useState(undefined);
   const [accordionExpanded, setAccordionExpanded] = useState(true);
+  const [activitiesSelected, setActivitiesSelected] = useState(null);
+
+  const handleAccordionExpand = () => {
+    setAccordionExpanded((prev) => !prev);
+  };
 
   const subtypesList = [
     ActivitySubtypeShortLabels.Activity_Observation_PlantAquatic,
     ActivitySubtypeShortLabels.Activity_Observation_PlantTerrestrial
   ];
-
-  const handleAccordionExpand = () => {
-    setAccordionExpanded((prev) => !prev);
-  };
 
   const getActivities = async () => {
     const act_list = await dataAccess.getActivities({
@@ -37,21 +68,30 @@ const PlantObservationsTable = () => {
   };
 
   useEffect(() => {
-    console.log(userInfo);
     getActivities();
   }, []);
 
-  useEffect(() => {
-    console.log(activities);
-  }, [activities]);
-
   return (
     <Accordion expanded={accordionExpanded} onChange={handleAccordionExpand}>
-      <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="panel1a-content" id="panel1a-header">
-        <Typography>Observations</Typography>
+      <AccordionSummary className={classes.accordionHeader} expandIcon={<ExpandMoreIcon />}>
+        <Typography className={classes.accordionHeaderText} variant="h5">
+          Observations
+        </Typography>
+        <Box className={classes.createButtonsContainer}>
+          {subtypesList.map((subtype) => {
+            console.log(subtype);
+            return (
+              <>
+                <Button startIcon={<AddIcon />} className={classes.createButton} variant="contained">
+                  {subtype}
+                </Button>
+              </>
+            );
+          })}
+        </Box>
       </AccordionSummary>
+
       <AccordionDetails>
-        <Grid container></Grid>
         {!activities ? (
           <CircularProgress />
         ) : (
