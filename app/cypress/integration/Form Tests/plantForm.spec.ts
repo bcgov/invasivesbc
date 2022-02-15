4cd4dd8d7904bdf0b18b5fba905ddc4f034d7d0b
bburciaga <burciagab16@gmail.com>
2022-02-15 09:41:36 -0800

    unfinished testing doc

--- app/cypress/integration/Form Tests/plantForm.spec.ts
+++ app/cypress/integration/Form Tests/plantForm.spec.ts
@@ -1,12 +1,119 @@
 /// <reference types="cypress" />
 
+import { min } from 'cypress/types/lodash';
 import React from 'react';
 import '../../support/index';
+const { faker } = require('@faker-js/faker');
+
+const template = () => {
+  const yesNo = ['Yes', 'No'];
+  const utmZone = [9, 10, 11];
+  return {
+    created_by: null,
+    created_timestamp: null,
+    activity_id: null,
+    doc_type: null,
+    activity_type: 'Observation',
+    activity_subtype: 'Activity_Observation_PlantTerrestrial',
+    biogeoclimatic_zones: null,
+    regional_invasive_species_organization_areas: null,
+    invasive_plant_management_areas: null,
+    ownership: null,
+    regional_districts: null,
+    flnro_districts: null,
+    moti_districts: null,
+    elevation: null,
+    well_proximity: null,
+    utm_zone: null,
+    utm_northing: null,
+    utm_easting: null,
+    albers_northing: null,
+    albers_easting: null,
+    form_status: null,
+    sync_status: 'Not Saved',
+    review_status: 'Not Reviewed',
+    geom: [],
+    activity_payload: {
+      status: 'Edited',
+      version: null,
+      geometry: [],
+      form_data: {
+        activity_data: {
+          latitude: null,
+          utm_zone: null,
+          longitude: null,
+          utm_easting: null,
+          project_code: [
+            {
+              description: null
+            }
+          ],
+          utm_northing: null,
+          employer_code: null,
+          jurisdictions: [null],
+          reported_area: null,
+          general_comment: faker.random.paragraph(),
+          access_description: faker.random.sentence(),
+          activity_date_time: null,
+          location_description: faker.random.sentence(),
+          invasive_species_agency_code: null
+        },
+        activity_type_data: {
+          survey_type: null,
+          treatment_persons: [{}],
+          observation_persons: [
+            {
+              person_name: faker.name.findName()
+            }
+          ],
+          observation_type_code: null
+        },
+        activity_subtype_data: {
+          invasive_plants: [
+            {
+              occurrence: null,
+              edna_sample: faker.random.arrayElement(yesNo),
+              observation_type: null,
+              invasive_plant_code: null,
+              plant_life_stage_code: null,
+              edna_sample_information: {},
+              voucher_specimen_collected: faker.random.arrayElement(yesNo),
+              invasive_plant_density_code: null,
+              invasive_plant_distribution_code: null,
+              voucher_specimen_collection_information: {
+                voucher_sample_id: null,
+                date_voucher_collected: faker.date.between('2000-00-00', '2020-00-00'),
+                accession_number: faker.datatype.number(),
+                name_of_herbarium: null,
+                voucher_verification_completed_by: null,
+                exact_utm_coords: null,
+                date_voucher_verified: faker.date.recent()
+              }
+            }
+          ],
+          observation_plant_terrestrial_data: {
+            well_ind: null,
+            slope_code: null,
+            aspect_code: null,
+            soil_texture_code: null,
+            specific_use_code: null,
+            research_detection_ind: null
+          }
+        }
+      }
+    }
+  };
+};
 
 describe('GOING TO THE LOGIN PAGE', function () {
+  const observationName = faker.name.findName();
+  const voucherName = faker.name.findName();
+  const fakerDate = faker.date.recent();
   let activityId;
+  const dev = 'https://dev-invasivesbci.apps.silver.devops.gov.bc.ca/home/landing';
+  const local = 'localhost:3000/home/landing';
   before(() => {
-    cy.visit('localhost:3000/home/landing');
+    cy.visit(local);
   });
   it('Check if on landing page', function () {
     cy.contains('Welcome');
@@ -26,6 +133,7 @@ describe('GOING TO THE LOGIN PAGE', function () {
   });
   it('Display variable', function () {
     console.log(activityId);
+    console.log(fakerDate);
   });
   it('Place Marker', function () {
     cy.get('.leaflet-draw-draw-marker').click('center');
@@ -41,7 +149,7 @@ describe('GOING TO THE LOGIN PAGE', function () {
     // ).click('center');
     // Access Description
     cy.get('.MuiOutlinedInput-root > #root_activity_data_access_description').click('center');
-    cy.get('.MuiOutlinedInput-root > #root_activity_data_access_description').type('Only Cool Guys allowed >:(');
+    cy.get('.MuiOutlinedInput-root > #root_activity_data_access_description').type('Only Cool Guys allowed');
     cy.get('.MuiOutlinedInput-root > #root_activity_data_location_description').click('center');
     cy.get('.MuiOutlinedInput-root > #root_activity_data_location_description').type('The Coolest Location');
   });
@@ -67,7 +175,7 @@ describe('GOING TO THE LOGIN PAGE', function () {
     cy.get('[data-value="Yes"]').click();
     // Observation Person
     cy.get('.MuiOutlinedInput-root > #root_activity_type_data_activity_persons_0_person_name').click('center');
-    cy.get('.MuiOutlinedInput-root > #root_activity_type_data_activity_persons_0_person_name').type('Bobby Jones');
+    cy.get('.MuiOutlinedInput-root > #root_activity_type_data_activity_persons_0_person_name').type(observationName);
   });
   it('Observation Plant Terrestrial Information', function () {
     // Soil Texture
@@ -153,7 +261,7 @@ describe('GOING TO THE LOGIN PAGE', function () {
     // Date Voucher Collected
     cy.get(
       '#root_activity_subtype_data_TerrestrialPlants_0_voucher_specimen_collection_information_date_voucher_collected'
-    ).click('center');
+    ).dblclick('center');
     cy.get(
       '#root_activity_subtype_data_TerrestrialPlants_0_voucher_specimen_collection_information_date_voucher_collected'
     ).type('2021-09-09');
@@ -163,7 +271,7 @@ describe('GOING TO THE LOGIN PAGE', function () {
     ).click('center');
     cy.get(
       '#root_activity_subtype_data_TerrestrialPlants_0_voucher_specimen_collection_information_date_voucher_verified'
-    ).type('2022-01-01');
+    ).type('2020-10-10');
     // Name of Herbarium
     cy.get(
       '#root_activity_subtype_data_TerrestrialPlants_0_voucher_specimen_collection_information_name_of_herbarium'
@@ -173,10 +281,10 @@ describe('GOING TO THE LOGIN PAGE', function () {
     ).type('The Cool Herbarium');
     // Accession number
     cy.get(
-      '#root_activity_subtype_data_TerrestrialPlants_0_voucher_specimen_collection_information_name_of_herbarium'
+      '#root_activity_subtype_data_TerrestrialPlants_0_voucher_specimen_collection_information_accession_number'
     ).click('center');
     cy.get(
-      '#root_activity_subtype_data_TerrestrialPlants_0_voucher_specimen_collection_information_name_of_herbarium'
+      '#root_activity_subtype_data_TerrestrialPlants_0_voucher_specimen_collection_information_accession_number'
     ).type('54323');
     // Voucher Verification Completed by
     cy.get(
@@ -184,7 +292,7 @@ describe('GOING TO THE LOGIN PAGE', function () {
     ).click('center');
     cy.get(
       '.MuiOutlinedInput-root > #root_activity_subtype_data_TerrestrialPlants_0_voucher_specimen_collection_information_voucher_verification_completed_by_person_name'
-    ).type('Joe Bob');
+    ).type(voucherName);
     // Org
     cy.get(
       '.MuiOutlinedInput-root > #root_activity_subtype_data_TerrestrialPlants_0_voucher_specimen_collection_information_voucher_verification_completed_by_organization'
