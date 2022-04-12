d7fcdf2e20857780dbbb5888b6775046a6bbbb4c
Sam Warren <samwarren4@gmail.com>
2022-04-12 13:58:43 -0700

    fix user query

--- api/src/queries/createUserQuery.ts
+++ api/src/queries/createUserQuery.ts
@@ -12,6 +12,7 @@ export enum userTypeEnum {
  * @returns {SQLStatement} sql query object
  */
 export const createUserSQL = (userType: string, id: string, username: string, email: string): SQLStatement => {
+  console.log('UserType: ' + userType + ' ID: ' + id + ' Username: ' + username + ' Email: ' + email);
   switch (userType) {
     case 'idir':
       try {
@@ -26,7 +27,7 @@ export const createUserSQL = (userType: string, id: string, username: string, em
     case 'bceid':
       try {
         const returnVal = SQL`
-        insert into application_user (bceid_userid, preferred_username, email, activation_status) values (${id}, ${username}, ${email}, 0) on conflict (bceid_userid)  where bceid_userid is not null do nothing;
+        insert into application_user (bceid_userid, preferred_username, email, activation_status) values (${id}, ${username}, ${email}, 0) on conflict (bceid_userid) where bceid_userid is not null do nothing;
       `;
         return returnVal;
       } catch (e) {
