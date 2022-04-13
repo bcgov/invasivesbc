fc49d09a76e718996e1f3b25cdbf2ec2406d1982
Mike Wells <micheal.w.wells@gmail.com>
2022-04-13 16:59:37 -0700

    split api into app and server.ts so we can use supertest, set up mocha

---
+++ api/test/mocha/activity-queries.test.ts
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+import { describe } from 'mocha';
+
+import request from 'supertest';
+import { app } from '../../src/app';
+
+describe('API starts', () => {
+  it('should respond to version GET', async () => {
+    const response = await request(app).get('/misc/version');
+    const actual = response.body.message;
+    expect(actual).to.equal(true);
+  });
+});
