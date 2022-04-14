f0898909d557a909720ad741ae9ec2ca7beb5f27
Mike Wells <micheal.w.wells@gmail.com>
2022-04-14 13:13:36 -0700

    load 100 random observations on api npm run test & also render activities as clusters at outer zoom

--- app/src/components/map/LayerLoaderHelpers/ActivitiesLayerV2.tsx
+++ app/src/components/map/LayerLoaderHelpers/ActivitiesLayerV2.tsx
@@ -1,14 +1,38 @@
 import { DatabaseContext } from 'contexts/DatabaseContext';
 import { MapRequestContext } from 'contexts/MapRequestsContext';
 import { IActivitySearchCriteria } from 'interfaces/useInvasivesApi-interfaces';
+import { LatLngExpression } from 'leaflet';
 import React, { useCallback, useContext, useEffect, useMemo, useState } from 'react';
-import { useMap, useMapEvent } from 'react-leaflet';
+import { Marker, useMap, useMapEvent } from 'react-leaflet';
+import MarkerClusterGroup from 'react-leaflet-cluster';
 import { useDataAccess } from '../../../hooks/useDataAccess';
 import { GeoJSONVtLayer } from './GeoJsonVtLayer';
 import { createPolygonFromBounds } from './LtlngBoundsToPoly';
 
 export const ActivitiesLayerV2 = (props: any) => {
+  // use this use state var to only rerender when necessary
   const map = useMap();
+  enum ZoomTypes {
+    LOW = 'low',
+    MEDIUM = 'medium',
+    HIGH = 'high'
+  }
+  const [zoomType, setZoomType] = useState(ZoomTypes.LOW);
+  useMapEvent('zoomend', () => {
+    const zoom = map.getZoom();
+    console.log('zoom change', zoom);
+    if (zoom < 5) {
+      setZoomType(ZoomTypes.LOW);
+      return;
+    }
+    if (zoom > 15) {
+      setZoomType(ZoomTypes.HIGH);
+      return;
+    }
+    setZoomType(ZoomTypes.MEDIUM);
+    return;
+  });
+
   const [activities, setActivities] = useState(null);
   const dataAccess = useDataAccess();
   const options = useMemo(() => {
@@ -57,15 +81,41 @@ export const ActivitiesLayerV2 = (props: any) => {
       console.log(props.color.toUpperCase());
       console.log('activities: ' + activities.features.length);
       console.dir(activities);
-      return (
-        <GeoJSONVtLayer
-          key={'activities_layer_v2_geojson_vt' + Math.random()}
-          // opacity={props.opacity}
-          geoJSON={activities}
-          zIndex={props.zIndex}
-          options={options}
-        />
-      );
+
+      switch (zoomType) {
+        case ZoomTypes.HIGH:
+          return (
+            <GeoJSONVtLayer
+              key={'activities_layer_v2_geojson_vt' + Math.random()}
+              // opacity={props.opacity}
+              geoJSON={activities}
+              zIndex={props.zIndex}
+              options={options}
+            />
+          );
+          break;
+        default:
+          return (
+            <MarkerClusterGroup>
+              {activities.features.map((a) => {
+                console.dir(a);
+                if (a.geometry.type === 'Polygon') {
+                  console.log('poly');
+                  const position: [number, number] = [a.geometry.coordinates[0][0][1], a.geometry.coordinates[0][0][0]];
+                  console.log(position);
+
+                  return <Marker position={position} key={'activity_marker' + a.properties.activity_id} />;
+                }
+              })}
+            </MarkerClusterGroup>
+          );
+      }
     } else return <></>;
-  }, [JSON.stringify(props.filters), JSON.stringify(props.color), JSON.stringify(activities), props.zIndex]);
+  }, [
+    JSON.stringify(props.filters),
+    JSON.stringify(props.color),
+    JSON.stringify(activities),
+    props.zIndex,
+    JSON.stringify(zoomType)
+  ]);
 };
