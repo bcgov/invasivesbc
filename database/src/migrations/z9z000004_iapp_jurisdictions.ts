3c60135ef6f1b50505223875715e66cdc17367d9
Gabriele Dal Cengio <dalcengio.gabriele@gmail.com>
2022-05-10 15:18:06 -0700

    Create iapp jurisdiction list migration

---
+++ database/src/migrations/z9z000004_iapp_jurisdictions.ts
@@ -0,0 +1,56 @@
+import * as Knex from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  // Creates column from delimited species list and then creates/fills iapp_invbc_mapping mapping table
+  await knex.raw(`
+  	set search_path=invasivesbc,public;
+
+    CREATE TABLE IF NOT EXISTS iapp_jurisdictions (
+      id SERIAL PRIMARY KEY,
+      jurisdiction VARCHAR(70) NOT NULL,
+      code VARCHAR(10)
+    );
+
+    GRANT SELECT ON iapp_jurisdictions TO invasivebc;
+
+    INSERT INTO iapp_jurisdictions(jurisdiction) VALUES
+      ('CP Rail'),
+      ('Grazing Lease'),
+      ('FortisBC Inc'),
+      ('EXPIRED - British Columbia Transmission Corporation'),
+      ('CN Rail'),
+      ('Oil and Gas Companies'),
+      ('Ministry of Forests'),
+      ('Provincial Parks'),
+      ('TransCanada Pipelines'),
+      ('BC Hydro');
+    INSERT INTO iapp_jurisdictions(jurisdiction) VALUES
+      ('Mining Companies'),
+      ('Gas and Oil'),
+      ('Regional District owned land'),
+      ('BC Rail'),
+      ('Municipality owned land'),
+      ('EXPIRED - Terasen Gas Inc.'),
+      ('Lands and Natural Resource Operations'),
+      ('Westcoast Energy Inc.'),
+      ('OBSOLETE - Railroads'),
+      ('First Nations Reserves');
+    INSERT INTO iapp_jurisdictions(jurisdiction) VALUES
+      ('EXPIRED - Ministry of Environment - except Provincial Parks'),
+      ('Parks Canada'),
+      ('OBSOLETE - Municipality'),
+      ('Department of Transportation'),
+      ('Private Land'),
+      ('Department of National Defense'),
+      ('Ministry of Transportation and Infrastructure'),
+      ('OBSOLETE - Pipelines'),
+      ('IAPP Conversion - Jurisdiction not recorded');
+  `);
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw(`
+    set search_path=invasivesbc,public;
+    drop table if exists invasivesbc.iapp_jurisdictions;
+  `);
+}
