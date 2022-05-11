b0741f162eb34ace57a92afc77d2a1bd951422d2
Sam Warren <samwarren4@gmail.com>
2022-05-11 16:17:11 -0700

    consolidate getUserByIDIR and getUserByKeycloakID

--- api/src/utils/auth-utils.ts
+++ api/src/utils/auth-utils.ts
@@ -3,7 +3,7 @@
 import { verify } from 'jsonwebtoken';
 import jwksRsa from 'jwks-rsa';
 import { getLogger } from './logger';
-import { getRolesForUser, getUserByBCEID, getUserByIDIR } from './user-utils';
+import { createUser, getRolesForUser, getUserByKeycloakID, KeycloakAccountType } from './user-utils';
 import { Request } from 'express';
 
 const defaultLog = getLogger('auth-utils');
@@ -77,33 +77,39 @@ export const authenticate = async (req: InvasivesRequest) => {
 
       req.keycloakToken = decoded;
 
+      let accountType, id;
+
       if (decoded.idir_userid) {
-        getUserByIDIR(decoded.idir_userid).then((user) => {
-          req.authContext = {
-            preferredUsername: null,
-            user: null,
-            roles: []
-          };
-          req.authContext.preferredUsername = decoded['preferred_username'];
-          req.authContext.user = user;
-          getRolesForUser(user.user_id).then((roles) => {
-            req.authContext.roles = roles;
-            resolve();
-          });
-        });
+        accountType = KeycloakAccountType.idir;
+        id = decoded.idir_userid;
       } else if (decoded.bceid_userid) {
-        getUserByBCEID(decoded.bceid_userid).then((user) => {
-          req.authContext = {
-            preferredUsername: null,
-            user: null,
-            roles: []
-          };
-          req.authContext.preferredUsername = decoded['preferred_username'];
-          req.authContext.user = user;
-          getRolesForUser(user.user_id).then((roles) => {
-            req.authContext.roles = roles;
-            resolve();
-          });
+        accountType = KeycloakAccountType.bceid;
+        id = decoded.bceid_userid;
+      }
+      if (!accountType || !id) {
+        throw {
+          code: 401,
+          message: 'Invalid token - missing idir_userid or bceid_userid',
+          namespace: 'auth-utils'
+        };
+      } else {
+        getUserByKeycloakID(accountType, id).then((user) => {
+          if (user) {
+            req.authContext = {
+              preferredUsername: null,
+              user: null,
+              roles: []
+            };
+            req.authContext.preferredUsername = decoded['preferred_username'];
+            req.authContext.user = user;
+            getRolesForUser(user.user_id).then((roles) => {
+              req.authContext.roles = roles;
+              resolve();
+            });
+          } else {
+            // Create the user since they do not exist
+            createUser(decoded, accountType, id);
+          }
         });
       }
     });
