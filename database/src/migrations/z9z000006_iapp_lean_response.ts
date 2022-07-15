afe7abf96b610bdc3f749bcd837462b80ceec7f1
bburciaga <burciagab16@gmail.com>
2022-07-15 09:29:09 -0700

    made changes to migration

--- database/src/migrations/z9z000006_iapp_lean_response.ts
+++ database/src/migrations/z9z000006_iapp_lean_response.ts
@@ -1,9 +1,12 @@
-import * as Knex from 'knex';
+import Knex from 'knex';
 
-export async function up (knex: Knex): Promise<void> {
-  try {
-    const sql = `
-      SET search_path=invasivesbc,public;
+const DB_SCHEMA = process.env.DB_SCHEMA || 'invasivesbc';
+const NODE_ENV = process.env.NODE_ENV;
+
+export async function up(knex: Knex): Promise<void> {
+  const sql = `
+      set schema 'invasivesbc';
+      set search_path = invasivesbc,public;
       DROP MATERIALIZED VIEW IF EXISTS iapp_site_summary_and_geojson;
       DROP MATERIALIZED VIEW IF EXISTS iapp_site_summary;
       CREATE MATERIALIZED VIEW iapp_site_summary
@@ -11,7 +14,7 @@ export async function up (knex: Knex): Promise<void> {
         WITH jurisdiction_data
         AS (
           SELECT DISTINCT (
-              Regexp_split_to_array(jurisdictions, '(?<=\))(, )')
+              Regexp_split_to_array(jurisdictions, '(?<=\)(, )')
             ) AS jurisdictions,
             estimated_area_hectares, 
             site_id
@@ -102,16 +105,18 @@ export async function up (knex: Knex): Promise<void> {
       GRANT SELECT ON iapp_site_summary_and_geojson TO invasivebc;
       REFRESH MATERIALIZED VIEW iapp_site_summary_and_geojson;
     `;
+  // try {
     await knex.raw(sql);
-  } catch (e) {
-    console.log('Error creating iapp_site_summary or iapp_site_summary_and_geojson');
-  }
+  // } catch (e) {
+  //   console.log('Error creating iapp_site_summary or iapp_site_summary_and_geojson');
+  // }
 }
 
 export async function down(knex: Knex): Promise<void> {
   try {
     const sql = `
-      SET search_path=invasivesbc,public;
+      SET schema '${DB_SCHEMA}';
+      SET search_path = ${DB_SCHEMA},public;
       
       DROP MATERIALIZED VIEW IF EXISTS invasivesbc.iapp_site_summary_and_geojson;
       DROP MATERIALIZED VIEW IF EXISTS invasivesbc.iapp_site_summary;
