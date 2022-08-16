96b38cf763f6b1c19b35b4cc5c838c610af98b3d
Mike Wells <micheal.w.wells@gmail.com>
2022-08-16 10:26:41 -0700

    try not passing namespace for build, and set manually via cli to get around https://github.com/openshift/oc/issues/1223

--- api/.pipeline/config.js
+++ api/.pipeline/config.js
@@ -48,7 +48,7 @@ options = processOptions(options);
 
 const phases = {
   build: {
-    namespace: '7068ad-tools',
+//    namespace: '7068ad-tools',
     name: `${name}`,
     dbName: `${dbName}`,
     phase: 'build',
