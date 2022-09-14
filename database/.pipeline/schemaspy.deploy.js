640af29613250cd2104ec4f53437c4987aef021f
Robert Johnstone <rob@plasticviking.com>
2022-09-14 14:27:24 +0800

    remove  vestiges of legacy pipeline

--- database/.pipeline/schemaspy.deploy.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const deploySchemaspyTask = require('./lib/schemaspy.deploy.js');
-const config = require('./config.js');
-
-const settings = { ...config, phase: config.options.env };
-
-// deploy schemaspy
-deploySchemaspyTask(settings);
