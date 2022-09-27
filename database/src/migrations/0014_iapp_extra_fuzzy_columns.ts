4c4b0e65570c193ec09b396c24a1dc207b071582
Gabriele Dal Cengio <dalcengio.gabriele@gmail.com>
2022-09-26 17:15:11 -0700

    Update migration to include monitored for sorting

--- database/src/migrations/0014_iapp_extra_fuzzy_columns.ts
+++ database/src/migrations/0014_iapp_extra_fuzzy_columns.ts
@@ -100,7 +100,13 @@ export async function up(knex: Knex): Promise<void> {
           i.reported_area,
           i.all_species_on_site_as_array,
           i.site_paper_file_id,
-          i.agencies, 
+          i.agencies,
+          CASE WHEN (i.has_biological_treatment_monitorings OR 
+              i.has_chemical_treatment_monitorings OR
+              i.has_mechanical_treatment_monitorings) 
+            THEN 'Yes' 
+            ELSE 'No' 
+          END AS monitored,
           json_build_object('type', 'Feature', 'properties', json_build_object('site_id', i.site_id, 'species', i.all_species_on_site, 'has_surveys', i.has_surveys, 'has_biological_treatments', i.has_biological_treatments, 'has_biological_monitorings', i.has_biological_treatment_monitorings, 'has_biological_dispersals', i.has_biological_dispersals, 'has_chemical_treatments', i.has_chemical_treatments, 'has_chemical_monitorings', i.has_chemical_treatment_monitorings, 'has_mechanical_treatments', i.has_mechanical_treatments, 'has_mechanical_monitorings', i.has_mechanical_treatment_monitorings, 'earliest_survey', i.min_survey, 'latest_survey', i.max_survey, 'earliest_chemical_treatment', i.min_chemical_treatment_dates, 'latest_chemical_treatment', i.max_chemical_treatment_dates, 'earliest_chemical_monitoring', i.min_chemical_treatment_monitoring_dates, 'latest_chemical_monitoring', i.max_chemical_treatment_monitoring_dates, 'earliest_bio_dispersal', i.min_bio_dispersal_dates, 'latest_bio_dispersal', i.max_bio_dispersal_dates, 'earliest_bio_treatment', i.min_bio_treatment_dates, 'latest_bio_treatment', i.max_bio_treatment_dates, 'earliest_bio_monitoring', i.min_bio_treatment_monitoring_dates, 'latest_bio_monitoring', i.max_bio_treatment_monitoring_dates, 'earliest_mechanical_treatment', i.min_mechanical_treatment_dates, 'latest_mechanical_treatment', i.max_mechanical_treatment_dates, 'earliest_mechanical_monitoring', i.min_mechanical_treatment_monitoring_dates, 'latest_mechanical_monitoring', i.max_mechanical_treatment_monitoring_dates, 'reported_area', i.reported_area, 'jurisdictions', i.jurisdictions), 'geometry', st_asgeojson(s.geog)::jsonb) AS geojson
         FROM invasivesbc.iapp_site_summary i
           JOIN invasivesbc.iapp_spatial s ON i.site_id = s.site_id
