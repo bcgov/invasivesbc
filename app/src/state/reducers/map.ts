3e08559fab35693bebfdc0442de1ac212999cf23
Gabriele Dal Cengio <dalcengio.gabriele@gmail.com>
2023-01-18 10:34:13 -0800

    mw: rebase conflict resolve.  original: Set whatshere for activity points like IAPP records

--- app/src/state/reducers/map.ts
+++ app/src/state/reducers/map.ts
@@ -22,7 +22,8 @@ import {
   MAP_WHATS_HERE_FEATURE,
   WHATS_HERE_IAPP_ROWS_SUCCESS,
   MAP_SET_WHATS_HERE_PAGE_LIMIT_POI,
-  MAP_SET_WHATS_HERE_SECTION
+  MAP_SET_WHATS_HERE_SECTION,
+  WHATS_HERE_ACTIVITY_ROWS_SUCCESS
 } from '../actions';
 
 import { AppConfig } from '../config';
@@ -61,7 +62,15 @@ class MapState {
     this.positionTracking = false;
     this.panned = true;
     this.LeafletWhosEditing = LeafletWhosEditingEnum.NONE
-    this.whatsHere = { toggle: false, feature: null, section: 'position', iappRows: null, limit: 5, page: 0 };
+    this.whatsHere = {
+      toggle: false,
+      feature: null,
+      section: 'position',
+      iappRows: null,
+      activityRows: null,
+      limit: 5,
+      page: 0
+    };
   }
 }
 const initialState = new MapState();
@@ -308,6 +317,15 @@ function createMapReducer(configuration: AppConfig): (MapState, AnyAction) => Ma
           whatsHere: {
             ...state.whatsHere,
             iappRows: action.payload.IDs
+          }
+        }
+      }
+      case WHATS_HERE_ACTIVITY_ROWS_SUCCESS: {
+        return {
+          ...state,
+          whatsHere: {
+            ...state.whatsHere,
+            activityRows: action.payload.IDs
           }
         }
       }
