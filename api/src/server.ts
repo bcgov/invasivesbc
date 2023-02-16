f2a97a3f160b2e3d362a9efa724d1ee721c6a57d
Chris Rafuse <chris.rafuse@quartech.com>
2023-02-16 11:57:05 -0800

    comment out defaultLogger server

--- api/src/server.ts
+++ api/src/server.ts
@@ -4,16 +4,16 @@ const HOST = process.env.API_HOST || 'localhost';
 const PORT = Number(process.env.API_PORT || '3002');
 const ADMIN_PORT = 8500;
 // Start api
-const defaultLog = getLogger('app');
+// const defaultLog = getLogger('app');
 
 try {
   app.listen(PORT, () => {
-    defaultLog.info({ label: 'start api', message: `started api on ${HOST}:${PORT}/api` });
+    // defaultLog.info({ label: 'start api', message: `started api on ${HOST}:${PORT}/api` });
   });
   adminApp.listen(ADMIN_PORT, () => {
-    defaultLog.info({ label: 'start admin api', message: `started api on ${HOST}:${ADMIN_PORT}/admin` });
+    // defaultLog.info({ label: 'start admin api', message: `started api on ${HOST}:${ADMIN_PORT}/admin` });
   });
 } catch (error) {
-  defaultLog.error({ label: 'start api', message: 'error', error });
+  // defaultLog.error({ label: 'start api', message: 'error', error });
   process.exit(1);
 }
