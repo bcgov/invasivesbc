c77f50fdd632517a2e8159b9363f166aad83f465
Chris Rafuse <chris.rafuse@quartech.com>
2023-02-16 16:13:30 -0800

    comment out getLogger KML

--- api/src/utils/kml-import.ts
+++ api/src/utils/kml-import.ts
@@ -5,46 +5,46 @@ import AdmZip from 'adm-zip';
 import { FeatureCollection } from 'geojson';
 
 function KMZToKML(data: Buffer): Buffer {
-  const log = getLogger('KML');
+  // const log = getLogger('KML');
 
   const zipFile = new AdmZip(data);
 
-  log.info('parsing zip data');
+  // log.info('parsing zip data');
 
   try {
     if (!zipFile.test()) {
-      log.info('Bad compressed data');
+      // log.info('Bad compressed data');
     }
   } catch (err) {
-    log.error(err);
+    // log.error(err);
   }
 
   const mainDocumentFile = zipFile.getEntries().find((e) => {
-    log.info(e.name);
+    // log.info(e.name);
     return e.name.match(/doc\.kml/i);
   });
 
   if (mainDocumentFile) {
-    log.info('found doc.kml in the KMZ');
+    // log.info('found doc.kml in the KMZ');
     return Buffer.from(zipFile.readAsText(mainDocumentFile));
   }
 
-  log.error('no doc.kml in this KMZ!');
+  // log.error('no doc.kml in this KMZ!');
 
   throw new Error('No doc.xml in this KMZ file');
 }
 
 function GeoJSONFromKML(data: Buffer) {
-  const log = getLogger('KML');
+  // const log = getLogger('KML');
 
-  log.info(`parsing: ${data.length} bytes`);
+  // log.info(`parsing: ${data.length} bytes`);
 
   let kmlDoc: any;
 
   try {
     kmlDoc = new JSDOM(data).window.document;
   } catch (err) {
-    log.error(err);
+    // log.error(err);
     throw new Error('failed to parse KML into valid XML DOM tree');
   }
 
