42856779944224fad7b87f756d4baaadb9c41067
Micheal Wells <micheal.w.wells@gmail.com>
2023-03-31 14:49:37 -0700

    finish stubbing out chem treatment template validators

--- api/src/utils/batch/chemTreatmentValidation.ts
+++ api/src/utils/batch/chemTreatmentValidation.ts
@@ -1,45 +1,136 @@
+import { validate_herbicide_fields } from 'sharedAPI';
 import { RowValidationResult } from './validation';
 
+
+//temp param cheat cheat 
+/*
+newErrors = validate_inv_plants_arr_length(formData, errors);
+newErrors = validate_inv_plants_fields(formData, errors);
+newErrors = validate_herbicides_arr_length(formData, errors);
+newErrors = validate_general_fields(formData, errors);
+newErrors = validate_chem_app_method_value(formData, errors, businessCodes);
+newErrors = validate_herbicide_fields(
+newErrors = validate_tank_mix_fields(area, formData, errors);
+newErrors = validate_tank_mix_herbicides(formData, errors, businessCodes, herbicideDictionary, skipAppRateValidation);
+*/
+
 export const ValidateHerbicides = (rowData) => {
-  let x: any;
-  return x;
+  const appliesToFields = [];
+  let validationMessages = [];
+  let valid = true;
+
+  try {
+    //area: number, formData: IGeneralFields, errors: any, businessCodes: any, herbicideDictionary: any, skipAppRateValidation: boolean
+    //const result = validate_herbicide_fields()
+  } catch (e) {
+    console.log(e);
+    valid = false;
+  }
+
+  return { validationMessages: validationMessages, appliesToFields: appliesToFields, valid: valid };
+
   // validate_herbicides
 };
 
 export const ValidateTankMixHerbicides = (rowData) => {
-  let x: any;
-  return x;
-  // validate_tank_mix_herbicides
+  const appliesToFields = [];
+  let validationMessages = [];
+  let valid = true;
+
+  try {
+    //const result = validate_tank_mix_herbicides()
+  } catch (e) {
+    console.log(e);
+    valid = false;
+  }
+
+  return { validationMessages: validationMessages, appliesToFields: appliesToFields, valid: valid };
+
 };
 
 export const ValidateTankMixFields = (rowData) => {
-  let x: any;
-  return x;
-  // validate_tank_mix_fields
+    const appliesToFields = [];
+    let validationMessages = [];
+    let valid = true;
+  
+    try {
+      //const result = validate_tank_mix_fields()
+    } catch (e) {
+      console.log(e);
+      valid = false;
+    }
+  
+    return { validationMessages: validationMessages, appliesToFields: appliesToFields, valid: valid };
+  
+  // 
 };
 
 export const ValidateChemAppMethod = (rowData) => {
-  let x: any;
-  return x;
-  //validate_chem_app_method
+    const appliesToFields = [];
+    let validationMessages = [];
+    let valid = true;
+  
+    try {
+      //const result = validate_chem_app_method()
+    } catch (e) {
+      console.log(e);
+      valid = false;
+    }
+  
+    return { validationMessages: validationMessages, appliesToFields: appliesToFields, valid: valid };
+  
+  //
 };
 
 export const ValidateHerbicidesArray = (rowData) => {
-  let x: any;
-  return x;
-  //validate_herbicides_arr_length
+    const appliesToFields = [];
+    let validationMessages = [];
+    let valid = true;
+  
+    try {
+      //const result = validate_herbicides_arr_length()
+    } catch (e) {
+      console.log(e);
+      valid = false;
+    }
+  
+    return { validationMessages: validationMessages, appliesToFields: appliesToFields, valid: valid };
+  
+  //
 };
 
 export const ValidateInvasivePlantsFields = (rowData) => {
-  let x: any;
-  return x;
-  //validate_inv_plants_fields
+    const appliesToFields = [];
+    let validationMessages = [];
+    let valid = true;
+  
+    try {
+      //const result = validate_inv_plants_fields()
+    } catch (e) {
+      console.log(e);
+      valid = false;
+    }
+  
+    return { validationMessages: validationMessages, appliesToFields: appliesToFields, valid: valid };
+  
+  //
 };
 
 export const ValidateInvasivePlantsArray = (rowData) => {
-  let x: any;
-  return x;
-  //validate_inv_plants_arr_length
+    const appliesToFields = [];
+    let validationMessages = [];
+    let valid = true;
+  
+    try {
+      //const result = validate_inv_plants_arr_length()
+    } catch (e) {
+      console.log(e);
+      valid = false;
+    }
+  
+    return { validationMessages: validationMessages, appliesToFields: appliesToFields, valid: valid };
+  
+  //
 };
 
 export const ChemTreatmentValidators = [
