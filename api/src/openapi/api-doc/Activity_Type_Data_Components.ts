a4b65dd10fce34bcb9a10e4afb3156d59509141a
brennanwebster <Brennan.Webster@gov.bc.ca>
2023-04-25 08:43:01 -0700

    remove legacy_iapp_id from biocontrol dispersal

--- api/src/openapi/api-doc/Activity_Type_Data_Components.ts
+++ api/src/openapi/api-doc/Activity_Type_Data_Components.ts
@@ -52,6 +52,13 @@ export const Monitoring_Biocontrol = {
   type: 'object',
   required: ['activity_persons'],
   properties: {
+    // legacy_iapp_id: {
+    //   type: 'number',
+    //   maximum: 999999,
+    //   minimum: 10000,
+    //   title: 'Legacy IAPP Release ID',
+    //   'x-tooltip-text': 'IAPP Site ID'
+    // },
     activity_persons: {
       type: 'array',
       default: [{}],
