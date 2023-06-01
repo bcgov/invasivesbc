96747ab83b0f7d77e4780592c7c5d589847f6276
brennanwebster <Brennan.Webster@gov.bc.ca>
2023-06-01 13:04:07 -0700

    add mechanical terrestrial treatment

---
+++ api/src/utils/batch/templates/treatment_mechanical_terrestrial_plant_temp.ts
@@ -0,0 +1,73 @@
+import { Template, TemplateColumnBuilder } from '../definitions';
+import { DISPOSED_MATERIAL_FORMAT_CODES } from '../hard-coded-codes';
+import {
+  ActivityPersons,
+  BasicInformation,
+  BasicInformationRowValidators,
+  ProjectInformation
+} from '../shared-columns';
+
+const TreatmentMechanicalTerrestrialPlantTemp = new Template(
+  'treatment_mechanical_terrestrial_plant_temp',
+  'Treatment - Mechanical - Terrestrial Plant TEMP POINT',
+  null
+);
+TreatmentMechanicalTerrestrialPlantTemp.type = 'Treatment';
+TreatmentMechanicalTerrestrialPlantTemp.subtype = 'Activity_Treatment_MechanicalPlantTerrestrial';
+
+TreatmentMechanicalTerrestrialPlantTemp.columns = [
+  ...BasicInformation,
+  ...ProjectInformation,
+  ...ActivityPersons,
+  new TemplateColumnBuilder('Area', 'numeric').isRequired().build(),
+  new TemplateColumnBuilder(
+    'Treatment - Invasive Plant Code',
+    'codeReference',
+    'form_data.activity_subtype_data.Treatment_MechanicalPlant_Information[0].invasive_plant_code'
+  )
+    .referencesCode('invasive_plant_code')
+    .isRequired()
+    .build(),
+  new TemplateColumnBuilder(
+    'Treatment - Treated Area',
+    'numeric',
+    'form_data.activity_subtype_data.Treatment_MechanicalPlant_Information[0].treated_area'
+  )
+    .valueRange(0, null)
+    .isRequired()
+    .build(),
+  new TemplateColumnBuilder(
+    'Treatment - Mechanical Method Code',
+    'codeReference',
+    'form_data.activity_subtype_data.Treatment_MechanicalPlant_Information[0].mechanical_method_code'
+  )
+    .referencesCode('mechanical_method_code')
+    .isRequired()
+    .build(),
+  new TemplateColumnBuilder(
+    'Treatment - Disposal Method Code',
+    'codeReference',
+    'form_data.activity_subtype_data.Treatment_MechanicalPlant_Information[0].mechanical_disposal_code'
+  )
+    .referencesCode('mechanical_disposal_code')
+    .isRequired()
+    .build(),
+  new TemplateColumnBuilder(
+    'Treatment - Disposed Material Format',
+    'codeReference',
+    'form_data.activity_subtype_data.Treatment_MechanicalPlant_Information[0].disposed_material.disposed_material_input_format'
+  )
+    .hardcodedCodes(DISPOSED_MATERIAL_FORMAT_CODES)
+    .build(),
+  new TemplateColumnBuilder(
+    'Treatment - Disposed Material Amount',
+    'numeric',
+    'form_data.activity_subtype_data.Treatment_MechanicalPlant_Information[0].disposed_material.disposed_material_input_number'
+  )
+    .valueRange(0, null)
+    .build()
+];
+
+TreatmentMechanicalTerrestrialPlantTemp.rowValidators = [...BasicInformationRowValidators];
+
+export { TreatmentMechanicalTerrestrialPlantTemp };
