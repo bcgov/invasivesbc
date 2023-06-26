fc66d6fd737e2c7d5dc1688f7d8ced3a9b9ccea6
brennanwebster <Brennan.Webster@gov.bc.ca>
2023-06-26 16:27:36 -0700

    remove some plants

--- database/src/migrations/0050_even_more_code_changes.ts
+++ database/src/migrations/0050_even_more_code_changes.ts
@@ -10,9 +10,7 @@ export async function up(knex: Knex): Promise<void> {
     VALUES(79, 'AUS', 'Ausenco', 1, now(), null, now(), now(), 1, 1),
     (79, 'CFC', 'Cariboo Forest Consultants Ltd.', 1, now(), null, now(), now(), 1, 1),
     (40, 'YI', 'Yellow iris (Iris pseudacorus)', 1, now(), null, now(), now(), 1, 1),
-    (40, 'LL', 'Large yellow / spotted loosestrife (Lysimachia punctata)', 1, now(), null, now(), now(), 1, 1),
-    (78, 'PO', 'Himalayan knotweed (POLY POL Polygonum polystachyum)', 1, now(), null, now(), now(), 1, 1),
-    (78, 'GK', 'Giant knotweed (FALL SAC Fallopia sachalinensis)', 1, now(), null, now(), now(), 1, 1);
+    (40, 'LL', 'Large yellow / spotted loosestrife (Lysimachia punctata)', 1, now(), null, now(), now(), 1, 1);
     
     UPDATE code AS c
     SET code_sort_order = subquery.row_number
@@ -33,9 +31,7 @@ export async function down(knex: Knex): Promise<void> {
     WHERE (code_header_id = 79
     AND code_name IN ('AUS','CFC'))
     or (code_header_id = 40
-    AND code_name IN ('YI','LL'))
-    or (code_header_id = 78
-    AND code_name IN ('PO','GK'));
+    AND code_name IN ('YI','LL'));
 
     UPDATE code AS c
     SET code_sort_order = subquery.row_number
