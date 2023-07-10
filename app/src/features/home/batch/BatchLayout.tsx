7ffe81993bf1ce3ed16b515b09cf90c02d454a0e
Gabriele Dal Cengio <dalcengio.gabriele@gmail.com>
2023-07-10 16:45:44 -0700

    Allow dark mode for batch tables

--- app/src/features/home/batch/BatchLayout.tsx
+++ app/src/features/home/batch/BatchLayout.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { Box, Container, Stack } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectUserSettings } from 'state/reducers/userSettings';
+
 
 const BatchLayout = ({ children }) => {
+  const {darkTheme} = useSelector(selectUserSettings);
   return (
     <>
-      <Box className={'batchNav'}>
+      <Box className={`batchNav ${darkTheme? 'batchDarkNav' : ''}`}>
         <Container maxWidth={'lg'}>
           <Stack direction="row" justifyContent="start" alignItems="center" spacing={6}>
-            <Link className={'batchNav'} to={'/home/batch'}>
+            <Link to={'/home/batch'}>
               My Batches
             </Link>
-            <Link className={'batchNav'} to={'/home/batch/new'}>
+            <Link to={'/home/batch/new'}>
               Create New
             </Link>
-            <Link className={'batchNav'} to={'/home/batch/templates'}>
+            <Link to={'/home/batch/templates'}>
               Templates
             </Link>
           </Stack>
