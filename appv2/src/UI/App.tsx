780325d58c28e81ebfabb7dbe4f87f71a5d23e87
Micheal Wells <micheal.w.wells@gmail.com>
2023-07-13 09:56:14 -0700

    shim for Gabe to add landing comp to new template

--- appv2/src/UI/App.tsx
+++ appv2/src/UI/App.tsx
@@ -1,36 +1,36 @@
-import React, { useEffect, useRef } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 //import { useLocation, useNavigate } from "react-router-dom";
-import { useLocation} from "react-router-dom";
-import "./App.css";
-import Map from "./Map/Map";
-import MapControls from "./Map/MapControls";
-import Overlay from "./Overlay/Overlay";
-import { appModeEnum } from "../state/reducers/appMode";
-import { SET_APP_MODE, TOGGLE_PANEL, URL_CHANGE } from "../state/actions";
-import { URLAndLayerManager } from "../URLManager";
-import { Header } from "./Header/Header"; 
-import { Footer } from "./Footer/Footer"; 
+import { useLocation } from 'react-router-dom';
+import './App.css';
+import Map from './Map/Map';
+import MapControls from './Map/MapControls';
+import Overlay from './Overlay/Overlay';
+import { appModeEnum } from '../state/reducers/appMode';
+import { SET_APP_MODE, TOGGLE_PANEL, URL_CHANGE } from '../state/actions';
+import { URLAndLayerManager } from '../URLManager';
+import { Header } from './Header/Header';
+import { Footer } from './Footer/Footer';
+import { LandingComponent } from './Overlay/Landing/Landing';
 
 const App: React.FC = () => {
   const dispatch = useDispatch();
 
-  // the 'router' behaviour is controlled by redux-saga effect handler 
+  // the 'router' behaviour is controlled by redux-saga effect handler
   // and these useEffects
- // const navigate = useNavigate();
+  // const navigate = useNavigate();
   const location = useLocation();
   const targetURL = useSelector((state: any) => state.appMode?.url);
-  const ref = useRef(0)
+  const ref = useRef(0);
 
   useEffect(() => {
-    console.log(ref)
+    console.log(ref);
     if (location.pathname !== targetURL && ref.current === 0) {
       dispatch({
         type: URL_CHANGE,
-        payload: { url: location.pathname},
+        payload: { url: location.pathname }
       });
     }
-
 
     /*
     if (targetURL !== location.pathname && ref.current !== 0) {
@@ -38,10 +38,7 @@ const App: React.FC = () => {
       navigate(targetURL);
     }
     */
-
-
   }, [targetURL]);
-
 
   /*
   useEffect(() => {
@@ -53,37 +50,32 @@ const App: React.FC = () => {
   */
 
   // State for the overlay and app mode:
-  const toggleOverlayCallback = () =>
-    dispatch({ type: TOGGLE_PANEL });
-  const setMode = (newMode: appModeEnum) =>
+  const toggleOverlayCallback = () => dispatch({ type: TOGGLE_PANEL });
+  const setMode = (newMode: appModeEnum) => {
     dispatch({ type: SET_APP_MODE, payload: { mode: newMode } });
+  };
 
   const toggled = useSelector((state: any) => state.AppMode?.panelOpen);
-  const mode = useSelector((state: any) => state.AppMode?.mode);
-
-
-  useEffect(()=> {
-    console.log('app mode changed')
-    console.log(toggled)
-  }, [toggled])
+  const modeState = useSelector((state: any) => state.AppMode);
 
   return (
     <div className="App">
-      <Header/>
-      <URLAndLayerManager/>
-      
+      <Header />
+      <URLAndLayerManager />
+
       <Map className="Map">
         <MapControls
           className="MapControls"
           modeSetter={setMode}
-          mode={mode}
+          mode={modeState?.mode}
           showOverlay={toggled}
           toggleShowOverlay={toggleOverlayCallback}
         />
       </Map>
-      <Overlay mode={mode} showOverlay={toggled} >
-        </Overlay>
-        <Footer/>
+      <Overlay mode={modeState?.mode} showOverlay={toggled}>
+        {parseInt(modeState?.mode) === appModeEnum.Landing ? <LandingComponent /> : <></>}
+      </Overlay>
+      <Footer />
     </div>
   );
 };
--- appv2/src/UI/Overlay/Overlay.tsx
+++ appv2/src/UI/Overlay/Overlay.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Overlay.css";
 import { appModeEnum } from "../../state/reducers/appMode";
 
-const Overlay = (props: {showOverlay: boolean, mode: appModeEnum, children: React.FC[]}) => {
+const Overlay = (props) => {
   return (
     <div className={`map__overlay ${props.showOverlay ? "map__overlay--show" : ""}`}>
       {props.children}
