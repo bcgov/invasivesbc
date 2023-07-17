26d28b23071f88f601783f6b550c952374322183
Gabriele Dal Cengio <dalcengio.gabriele@gmail.com>
2023-07-17 16:39:24 -0700

    Add HDToggle button

--- appv2/src/UI/Map/Buttons/ButtonContainer.tsx
+++ appv2/src/UI/Map/Buttons/ButtonContainer.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { BaseMapToggle } from "./BaseMapToggle";
 import { FindMeToggle, LocationMarker, PanToMe } from "./FindMe";
+import { HDToggle } from "./HDToggle";
 import { LegendsButton } from "./LegendsButton";
 
 export const ButtonContainer = (props) => {
 
   return (
     <>
+      <HDToggle/>
       <BaseMapToggle/>
       <LegendsButton />
       <FindMeToggle />
