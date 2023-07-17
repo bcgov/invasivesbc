2ab4792494861689906296f53719964ec5979c39
brennanwebster <Brennan.Webster@gov.bc.ca>
2023-07-17 10:39:54 -0700

    change role description and add employers

--- database/src/migrations/0053_code_changes_july.ts
+++ database/src/migrations/0053_code_changes_july.ts
@@ -8,19 +8,20 @@ export async function up(knex: Knex): Promise<void> {
     UPDATE user_role
     SET role_description = 
       CASE 
-          WHEN role_id = 12 THEN 'Indigenous/Local Gov/Non-profit/Charity Manager - Animals'
-          WHEN role_id = 13 THEN 'Indigenous/Local Gov/Non-profit/Charity Manager - Plants'
-          WHEN role_id = 14 THEN 'Indigenous/Local Gov/Non-profit/Charity Manager - Both'
-          WHEN role_id = 15 THEN 'Indigenous/Local Gov/Non-profit/Charity Staff - Animals'
-          WHEN role_id = 16 THEN 'Indigenous/Local Gov/Non-profit/Charity Staff - Plants'
-          WHEN role_id = 17 THEN 'Indigenous/Local Gov/Non-profit/Charity Staff - Both'
+          WHEN role_id = 12 THEN 'Non-Provincial Government Manager - Animals'
+          WHEN role_id = 13 THEN 'Non-Provincial Government Manager - Plants'
+          WHEN role_id = 14 THEN 'Non-Provincial Government Manager - Both'
+          WHEN role_id = 15 THEN 'Non-Provincial Government Staff - Animals'
+          WHEN role_id = 16 THEN 'Non-Provincial Government Staff - Plants'
+          WHEN role_id = 17 THEN 'Non-Provincial Government Staff - Both'
           ELSE role_description
       END;
   
       INSERT INTO code
       (code_header_id, code_name, code_description, code_sort_order, valid_from, valid_to, created_at, updated_at, created_by_user_id, updated_by_user_id)
       VALUES(79, 'TTS', 'Tree Top Services', 1, now(), null, now(), now(), 1, 1),
-      (79, 'ONA', 'Okanagan Nation Alliance', 1, now(), null, now(), now(), 1, 1);
+      (79, 'ONA', 'Okanagan Nation Alliance', 1, now(), null, now(), now(), 1, 1),
+      (79, 'FCL', 'Forsite Consultants Ltd.', 1, now(), null, now(), now(), 1, 1);
       
       UPDATE code AS c
       SET code_sort_order = subquery.row_number
@@ -51,7 +52,7 @@ export async function down(knex: Knex): Promise<void> {
 
     DELETE FROM code
     WHERE (code_header_id = 79
-    AND code_name IN ('TTS','ONA'));
+    AND code_name IN ('TTS','ONA', 'FCL'));
     
     UPDATE code AS c
     SET code_sort_order = subquery.row_number
