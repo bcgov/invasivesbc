ac15745256b250780b8ea226bbcd2f430ae6c7a1
Gabriele Dal Cengio <dalcengio.gabriele@gmail.com>
2023-07-27 14:08:11 -0700

    Add missing files and fix table css for whats here overlay

---
+++ appv2/src/UI/Map/Buttons/WhatsHereButton.tsx
@@ -0,0 +1,93 @@
+import React, { useEffect, useRef } from "react";
+import { useDispatch } from "react-redux";
+import * as L from "leaflet";
+import 'leaflet-draw';
+import 'leaflet-draw/dist/leaflet.draw.css';
+import { useMap, useMapEvent } from "react-leaflet";
+import { IconButton, Tooltip } from "@mui/material";
+import { toolStyles } from "UI/Styles/ToolStyles";
+import { useSelector } from "util/use_selector";
+import { selectMap } from "state/reducers/map";
+import { selectUserSettings } from "state/reducers/userSettings";
+import { MAP_TOGGLE_WHATS_HERE, MAP_WHATS_HERE_FEATURE } from "state/actions";
+
+import DocumentScannerIcon from '@mui/icons-material/DocumentScanner';
+
+export const WhatsHereButton = (props) => {
+  const map = useMap();
+  const mapState = useSelector(selectMap);
+  const {darkTheme} = useSelector(selectUserSettings);
+  const dispatch = useDispatch();
+  const toolClass = toolStyles();
+  const [show, setShow] = React.useState(false);
+
+  const divRef = useRef();
+  useEffect(() => {
+    try {
+      L.DomEvent.disableClickPropagation(divRef?.current);
+      L.DomEvent.disableScrollPropagation(divRef?.current);
+    } catch (e) {}
+  }, []);
+  if (mapState && mapState?.whatsHere && map) {
+    return (
+      <div
+        ref={divRef}
+        className="map-btn">
+        <Tooltip
+          open={show}
+          onMouseEnter={() => setShow(true)}
+          onMouseLeave={() => setShow(false)}
+          title={`What's here?`}
+          placement="top-end">
+          <span>
+            <IconButton
+              onClick={() => {
+                dispatch({ type: MAP_TOGGLE_WHATS_HERE, payload: {toggle: !mapState.whatsHere.toggle} });
+              }}
+              className={
+                'leaflet-control-zoom leaflet-bar leaflet-control ' +
+                ' ' +
+                ((mapState?.whatsHere as any)?.toggle ? toolClass.selected : (darkTheme? toolClass.notSelectedDark : toolClass.notSelected))
+              }
+              sx={{ color: '#000' }}>
+              <DocumentScannerIcon />
+            </IconButton>
+          </span>
+        </Tooltip>
+      </div>
+    );
+  } else {
+    return <></>;
+  }
+};
+
+
+//temporary fix to type is undefined error
+(window as any).type = undefined;
+
+export const WhatsHereDrawComponent = (props) => {
+  const map = useMap();
+  const dispatch = useDispatch();
+  const ref = useRef();
+
+  const mapState = useSelector(selectMap);
+
+  useEffect(() => {
+    if ((mapState?.whatsHere as any)?.toggle == true && (mapState?.whatsHere as any)?.feature == null) {
+      ref.current = new (L as any).Draw.Rectangle(map);
+      (ref.current as any).enable();
+    }
+
+    return () => {
+      if (ref.current) (ref.current as any).disable();
+    };
+  }, [mapState?.whatsHere]);
+
+  useMapEvent('draw:created' as any, (e) => {
+    if ((mapState?.whatsHere as any).toggle && (mapState?.whatsHere as any)?.feature === null) {
+      dispatch({ type: MAP_WHATS_HERE_FEATURE, payload: { feature: e.layer.toGeoJSON() } });
+    }
+  });
+
+  return <></>;
+};
---
+++ appv2/src/UI/Overlay/WhatsHere/WhatsHereTable.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import center from "@turf/center";
+
+import { useSelector } from "util/use_selector";
+import { selectMap } from "state/reducers/map";
+import { selectUserSettings } from "state/reducers/userSettings";
+import { calc_utm } from "util/utm";
+import { BottomNavigation, BottomNavigationAction, Button, Grid, TableContainer } from "@mui/material";
+import { RenderTableActivity, RenderTablePOI, RenderTablePosition } from "util/WhatsHereTableHelpers";
+import { MAP_SET_WHATS_HERE_SECTION, MAP_TOGGLE_WHATS_HERE } from "state/actions";
+
+import "./WhatsHereTable.css";
+
+import AdjustIcon from '@mui/icons-material/Adjust';
+import FolderIcon from '@mui/icons-material/Folder';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+export const createDataUTM = (name: string, value: any) => {
+  return { name, value };
+};
+
+export const WhatsHereTable = (props) => {
+  const { darkTheme } = useSelector(selectUserSettings);
+  const mapState = useSelector(selectMap);
+
+  const position = mapState?.whatsHere?.feature?.geometry ? center(mapState?.whatsHere?.feature?.geometry)?.geometry.coordinates : [0, 0];
+  const utmResult = calc_utm(position[0], position[1]);
+  const utmRows = [
+    createDataUTM('Zone', utmResult[0]),
+    createDataUTM('Easting', utmResult[1]),
+    createDataUTM('Northing', utmResult[2])
+  ];
+  const dispatch = useDispatch();
+
+  const popupOnClose = () => {
+    dispatch({ type: MAP_TOGGLE_WHATS_HERE });
+  };
+
+  const handleChange = (_event: React.ChangeEvent<{}>, newSection: string) => {
+    dispatch({
+      type: MAP_SET_WHATS_HERE_SECTION,
+      payload: {
+        section: newSection
+      }
+    });
+  };
+
+  return (
+    <>
+      {mapState?.whatsHere?.section ? (
+        <div
+          id="whatsherepopup"
+          className="whatshere-table">
+          <Grid container justifyContent="center">
+            <Grid item>
+              <Button onClick={popupOnClose}>Close</Button>
+            </Grid>
+          </Grid>
+          <TableContainer className="whatshere-position">
+            <RenderTablePosition rows={utmRows} />
+            <RenderTableActivity />
+            {/*section == 'databc' && <RenderTableDataBC rows={databc} />*/}
+            <RenderTablePOI />
+          </TableContainer>
+          <Grid container justifyContent="center">
+            <BottomNavigation
+              value={mapState?.whatsHere?.section}
+              onChange={handleChange}>
+              <BottomNavigationAction value="position" label="Position" icon={<LocationOnIcon />} />
+              <BottomNavigationAction value="invasivesbc" label="InvasivesBC" icon={<FolderIcon />} />
+              {/*<BottomNavigationAction value="databc" label="Data BC" icon={<StorageIcon />} />*/}
+              <BottomNavigationAction value="iapp" label="IAPP" icon={<AdjustIcon />} />
+            </BottomNavigation>
+          </Grid>
+
+        </div>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+};
