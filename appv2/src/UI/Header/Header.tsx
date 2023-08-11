90add686d14dfbfa2f5bf0f9f418c5c397b49009
Gabriele Dal Cengio <dalcengio.gabriele@gmail.com>
2023-08-11 12:55:00 -0700

    Add rest of the tabs for the v2 header with auth check for admin

--- appv2/src/UI/Header/Header.tsx
+++ appv2/src/UI/Header/Header.tsx
@@ -9,6 +9,7 @@ import { selectAuth } from 'state/reducers/auth';
 import { Route, useHistory } from 'react-router';
 import ManageSearchIcon from '@mui/icons-material/ManageSearch';
 import AssignmentIcon from '@mui/icons-material/Assignment';
+import { AdminPanelSettings, Assessment, FileUpload, Home, School } from '@mui/icons-material';
 
 const Tab = (props: any) => {
   const history = useHistory();
@@ -73,8 +74,24 @@ export const Header: React.FC = () => {
   return (
     <div className="HeaderBar">
       <ButtonWrapper>
+        <Tab
+          path={'/Landing'}
+          label="Home"
+          panelOpen={true}
+          panelFullScreen={false}>
+          <Home />
+        </Tab>
+
         <Tab path="/Records" label="Records" panelOpen={true} panelFullScreen={false}>
           <ManageSearchIcon />
+        </Tab>
+
+        <Tab
+          path={'/Training'}
+          label="Training"
+          panelOpen={true}
+          panelFullScreen={false}>
+          <School />
         </Tab>
 
         <Tab
@@ -84,6 +101,45 @@ export const Header: React.FC = () => {
           panelFullScreen={false}>
           <AssignmentIcon />
         </Tab>
+
+        <Tab
+          path={'/Records/IAPP:' + userSettingsState?.activeIAPP}
+          label="Current IAPP"
+          panelOpen={true}
+          panelFullScreen={false}>
+          <img
+            alt="iapp logo"
+            src={'/assets/iapp_logo.gif'}
+            style={{ maxWidth: '1rem', marginBottom: '0px' }}
+          />
+        </Tab>
+
+        <Tab
+          path={'/Batch'}
+          label="Batch"
+          panelOpen={true}
+          panelFullScreen={false}>
+          <FileUpload />
+        </Tab>
+
+        <Tab
+          path={'/Reports'}
+          label="Reports"
+          panelOpen={true}
+          panelFullScreen={false}>
+          <Assessment />
+        </Tab>
+
+        {authState.roles.find((role) => role.role_id === 18) ?
+          <Tab
+            path={'/Admin'}
+            label="Admin"
+            panelOpen={true}
+            panelFullScreen={false}>
+            <AdminPanelSettings />
+          </Tab> :
+          <></>
+        }
 
         {authState.authenticated ? <LogoutButton /> : <LoginButton />}
       </ButtonWrapper>
