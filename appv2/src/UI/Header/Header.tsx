67062b9cc2ad27ca5fdc2773b64fdd5606e0dd90
Micheal Wells <micheal.w.wells@gmail.com>
2023-08-11 11:49:42 -0700

    wip new tab buttons

--- appv2/src/UI/Header/Header.tsx
+++ appv2/src/UI/Header/Header.tsx
@@ -4,13 +4,35 @@ import React from 'react';
 import './Header.css';
 import { IconButton } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
-import { AUTH_SIGNIN_REQUEST, AUTH_SIGNOUT_REQUEST } from 'state/actions';
+import { AUTH_SIGNIN_REQUEST, AUTH_SIGNOUT_REQUEST, TOGGLE_PANEL } from 'state/actions';
 import { selectAuth } from 'state/reducers/auth';
+import { Route, useHistory } from 'react-router';
+import ManageSearchIcon from '@mui/icons-material/ManageSearch';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+
+const Tab = (props: any) => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const panelState = useSelector((state: any) => state.AppMode?.panelOpen);
+
+
+  return (
+    <div className="Tab" onClick={() => {history.push(props.path); if(props.panelOpen && !panelState) dispatch({type: TOGGLE_PANEL})}}>
+      <div className="Tab__Content">{props.children}</div>
+      <div className="Tab__Label">{props.label}</div>
+      <Route path={props.path} render={(props) => <div className="Tab__Indicator"></div>} />
+    </div>
+  );
+};
 
 export const Header: React.FC = () => {
   const dispatch = useDispatch();
   const authState = useSelector(selectAuth);
+  const userSettingsState = useSelector((state: any) => state.userSettings);
 
+  const ButtonWrapper = (props: any) => {
+    return <div className="ButtonWrapper">{props.children}</div>;
+  };
 
   const LoginButton = () => {
     return (
@@ -44,9 +66,18 @@ export const Header: React.FC = () => {
     );
   };
 
-  return (
-    <div className="HeaderBar">
-        {authState.authenticated ? <LogoutButton /> : <LoginButton />}
-    </div>
-  );
+  return <div className="HeaderBar">
+    
+    
+    <ButtonWrapper>
+      <Tab path="/Records" label="Records" panelOpen={true} panelFullScreen={false}>
+        <ManageSearchIcon/>
+      </Tab>
+    
+      <Tab path={"/Records/Activity:" + userSettingsState?.activeActivity} label="Current Activity" panelOpen={true} panelFullScreen={false}>
+        <AssignmentIcon/>
+      </Tab>
+    </ButtonWrapper>
+    
+    {authState.authenticated ? <LogoutButton /> : <LoginButton />}</div>;
 };
