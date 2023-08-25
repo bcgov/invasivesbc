1fd371951afe3f88d27369cd4ea74acf20b24ed4
Micheal Wells <micheal.w.wells@gmail.com>
2023-08-25 15:08:54 -0700

    geoman disable global edit by default (and still compile and render)

--- appv2/src/UI/Map/ActivityGeo.tsx
+++ appv2/src/UI/Map/ActivityGeo.tsx
@@ -1,14 +1,41 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { GeoJSON, useMap, useMapEvent } from 'react-leaflet';
-import { selectActivity } from 'state/reducers/activity';
-import { GeoEditTools } from './GeoEditTools';
-//import L from 'leaflet';
-import L from 'leaflet';
 import '@geoman-io/leaflet-geoman-free';
 import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css';
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectActivity } from 'state/reducers/activity';
 import { ACTIVITY_UPDATE_GEO_REQUEST } from 'state/actions';
-//L.PM.setOptIn(true);
+import { GeoJSON, useMap, useMapEvent } from 'react-leaflet';
+import type { LayerGroup } from 'leaflet';
+
+import * as L from 'leaflet';
+import { useLeafletContext } from '@react-leaflet/core';
+
+export const GeoEditTools = (props) => {
+  const { map, layerContainer } = useLeafletContext();
+  const container = (layerContainer as LayerGroup) || map;
+
+  if (!container) {
+    console.warn('[GEOMAN-CONTROLS] No map or container instance found');
+    return null;
+  }
+
+  const initTools = async () => {
+    L.PM.setOptIn(true);
+    map.pm.addControls({
+      position: 'topleft',
+      drawCircle: false,
+
+    });
+  };
+
+  useEffect(() => {
+    initTools();
+    return () => {
+      map?.pm?.removeControls();
+    };
+  }, []);
+  return null;
+};
 
 export const ActivityGeo = (props) => {
   const activityState = useSelector(selectActivity);
@@ -16,9 +43,7 @@ export const ActivityGeo = (props) => {
   const map = useMap();
   const dispatch = useDispatch();
 
-
-  
-  const [lastLayer, setLastLayer] = useState(null)
+  const [lastLayer, setLastLayer] = useState(null);
 
   /*  useMapEvent('pm:drawend', (e) => {
     //e.layer.options.pmIgnore = false;
@@ -29,20 +54,24 @@ export const ActivityGeo = (props) => {
   })*/
   useMapEvent('pm:create', (e) => {
     e.layer.options.pmIgnore = false;
-    L.PM.reInitLayer(e.layer);
-    const { layer } = e
-    if(lastLayer)
-    {
-      map.removeLayer(lastLayer)
+    (L as any).PM.reInitLayer(e.layer);
+    const { layer } = e;
+    if (lastLayer) {
+      map.removeLayer(lastLayer);
     }
-    setLastLayer(layer)
+    setLastLayer(layer);
 
-    layer.on('pm:update',  (e) => {
+    layer.on('pm:update', (e) => {
+      dispatch({ type: 'update', payload: { event: e } });
+      dispatch({ type: ACTIVITY_UPDATE_GEO_REQUEST, payload: { geometry: [layer.toGeoJSON()] } });
+    });
+    dispatch({ type: 'create', payload: { event: e } });
+  });
+  /*layer.on('pm:markerdragend',  (e) => {
       dispatch({type: 'update', payload: { event: e}})
       dispatch({type: ACTIVITY_UPDATE_GEO_REQUEST, payload: {geometry: [layer.toGeoJSON()] }})
     })
-    dispatch({ type: 'create', payload: { event: e } });
-  });
+    */
   useMapEvent('pm:globaldrawmodetoggled', (e) => {
     //    e.layer.options.pmIgnore = false;
     //    L.PM.reInitLayer(e.layer);
@@ -63,7 +92,14 @@ export const ActivityGeo = (props) => {
     case 'READ':
       return <>{activityState?.activity?.geometry ? <GeoJSON data={activityState?.activity?.geometry} /> : <></>}</>;
     case 'EDIT':
-      return <><GeoEditTools /> <GeoJSON data={activityState?.activity?.geometry} /></> ;
+      if(map?.pm)
+      return (
+        <>
+          <GeoEditTools /> <GeoJSON  pmIgnore={true} key={Math.random()} data={activityState?.activity?.geometry} />
+        </>
+      );
+      else
+      return null;
     default:
       return <></>;
   }
--- appv2/src/UI/Map/Map.tsx
+++ appv2/src/UI/Map/Map.tsx
@@ -11,7 +11,7 @@ const Map = (props: any) => {
   const mapState = useSelector(selectMap)
   return (
     <div className="map">
-      <MapContainer center={[50.5, 30.5]} zoom={13} className="map__leaflet">
+      <MapContainer id="themap"  center={[50.5, 30.5]} zoom={13} className="map__leaflet">
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
