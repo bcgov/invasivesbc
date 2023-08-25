9a6f94f0fba3ccba63b14c4924015e41d24c18df
brennanwebster <Brennan.Webster@gov.bc.ca>
2023-08-25 14:02:41 -0700

    add employers to code table

---
+++ database/src/migrations/0059_code_changes_add_employers.ts
@@ -0,0 +1,44 @@
+import { Knex } from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.raw(
+    `
+    set search_path = invasivesbc,public;
+
+    INSERT INTO code
+    (code_header_id, code_name, code_description, code_sort_order, valid_from, valid_to, created_at, updated_at, created_by_user_id, updated_by_user_id)
+    VALUES(44, 'VAA', 'Victoria Airport Authority', 1, now(), null, now(), now(), 1, 1),
+    (79, 'VAA', 'Victoria Airport Authority', 1, now(), null, now(), now(), 1, 1),
+    (79, 'SHCL', 'Shearing Consultants Limited', 1, now(), null, now(), now(), 1, 1),
+    (79, 'CCD', 'Cariboo Chilcotin Divers Limited', 1, now(), null, now(), now(), 1, 1);
+    
+    UPDATE code AS c
+    SET code_sort_order = subquery.row_number
+    FROM (
+      SELECT code_id, ROW_NUMBER() OVER (PARTITION BY code_header_id ORDER BY code_header_id, code_description) AS row_number
+      FROM code
+    ) AS subquery
+    WHERE c.code_id = subquery.code_id;
+`
+  );
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.raw(`
+  set search_path = invasivesbc,public;
+    
+  DELETE FROM code
+  WHERE (code_header_id = 79
+  AND code_name IN ('VAA','SHCL','CCD'))
+  OR (code_header_id = 44 and code_name in ('VAA'));
+
+ 
+  UPDATE code AS c
+  SET code_sort_order = subquery.row_number
+  FROM (
+    SELECT code_id, ROW_NUMBER() OVER (PARTITION BY code_header_id ORDER BY code_header_id, code_description) AS row_number
+    FROM code
+  ) AS subquery
+  WHERE c.code_id = subquery.code_id;
+  `);
+}
