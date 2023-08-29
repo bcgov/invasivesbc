a418c17c0ea6bcd0dabe2df69290fc0af3792084
Gabriele Dal Cengio <dalcengio.gabriele@gmail.com>
2023-08-29 12:45:37 -0700

    Edit selectors and theme of training + get video thumbnails working

--- appv2/src/UI/Overlay/Training/Training.tsx
+++ appv2/src/UI/Overlay/Training/Training.tsx
@@ -1,16 +1,14 @@
 import { Box, Container, Grid, Typography } from '@mui/material';
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { selectUserSettings } from 'state/reducers/userSettings';
 import { useSelector } from 'util/use_selector';
-import { selectTrainingVideos } from 'state/reducers/training_videos';
 import Spinner from 'UI/Spinner/Spinner';
 import { TRAINING_VIDEOS_LIST_REQUEST } from 'state/actions';
 import "UI/Styles/training.scss";
 
 export const TrainingPage = () => {
-  const { list: videos, working } = useSelector(selectTrainingVideos);
-  const { darkTheme } = useSelector(selectUserSettings);
+  const working = useSelector((state: any) => state.TrainingVideos?.working);
+  const videos = useSelector((state: any) => state.TrainingVideos?.list);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -22,7 +20,7 @@ export const TrainingPage = () => {
   }
 
   return (
-    <Container className={`training ${darkTheme ? 'trainingDark' : ''}`} maxWidth={false}>
+    <Container className={`training`} maxWidth={false}>
       <Box style={{ paddingTop: '30px', paddingBottom: '10px', display: 'flex', justifyContent: 'center' }}>
         <Typography variant="h4" align="center">
           Training Videos
@@ -34,7 +32,7 @@ export const TrainingPage = () => {
           <Grid key={v.id} item xs={12} lg={6}>
             <div className={'trainingVideo'}>
               <label>{v.title}</label>
-              <video width={568} height={320} controls preload={'none'}>
+              <video width={568} height={320} controls preload="metadata">
                 <source src={v.src} type={'video/mp4'} />
               </video>
             </div>
