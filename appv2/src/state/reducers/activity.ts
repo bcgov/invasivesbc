757c04cf84b84164e277643b48fb5333163fa351
Micheal Wells <micheal.w.wells@gmail.com>
2023-12-27 09:04:23 -0800

    fridays state cleanup - use Immer consistently in map and usersettings

--- appv2/src/state/reducers/activity.ts
+++ appv2/src/state/reducers/activity.ts
@@ -1,122 +1,91 @@
-import { calculateGeometryArea } from 'utils/geometryHelpers';
 import {
-  ACTIVITY_UPDATE_GEO_REQUEST,
-  ACTIVITY_UPDATE_GEO_SUCCESS,
-  ACTIVITY_UPDATE_GEO_FAILURE,
-  ACTIVITY_UPDATE_AUTOFILL_REQUEST,
-  ACTIVITY_UPDATE_AUTOFILL_SUCCESS,
-  ACTIVITY_UPDATE_AUTOFILL_FAILURE,
-  ACTIVITY_UPDATE_PHOTO_REQUEST,
-  ACTIVITY_UPDATE_PHOTO_SUCCESS,
-  ACTIVITY_UPDATE_PHOTO_FAILURE,
-  ACTIVITY_LINK_RECORD_REQUEST,
-  ACTIVITY_LINK_RECORD_SUCCESS,
-  ACTIVITY_LINK_RECORD_FAILURE,
-  ACTIVITY_PERSIST_REQUEST,
-  ACTIVITY_PERSIST_SUCCESS,
-  ACTIVITY_PERSIST_FAILURE,
-  ACTIVITY_SAVE_REQUEST,
-  ACTIVITY_SAVE_SUCCESS,
-  ACTIVITY_SUBMIT_REQUEST,
-  ACTIVITY_SUBMIT_SUCCESS,
-  ACTIVITY_SUBMIT_FAILURE,
-  ACTIVITY_DELETE_REQUEST,
+  ACTIVITY_ADD_PHOTO_SUCCESS,
+  ACTIVITY_COPY_SUCCESS,
+  ACTIVITY_CREATE_REQUEST,
+  ACTIVITY_CREATE_SUCCESS,
+  ACTIVITY_DELETE_PHOTO_SUCCESS,
   ACTIVITY_DELETE_SUCCESS,
-  ACTIVITY_DELETE_FAILURE,
-  ACTIVITY_GET_INITIAL_STATE_REQUEST,
-  ACTIVITY_SET_ACTIVE_REQUEST,
+  ACTIVITY_EDIT_PHOTO_SUCCESS,
+  ACTIVITY_ERRORS,
+  ACTIVITY_GET_FAILURE,
+  ACTIVITY_GET_REQUEST,
   ACTIVITY_GET_SUCCESS,
-  ACTIVITY_ON_FORM_CHANGE_SUCCESS,
   ACTIVITY_GET_SUGGESTED_JURISDICTIONS_SUCCESS,
   ACTIVITY_GET_SUGGESTED_PERSONS_SUCCESS,
   ACTIVITY_GET_SUGGESTED_TREATMENT_IDS_SUCCESS,
-  ACTIVITY_CREATE_SUCCESS,
-  ACTIVITY_ADD_PHOTO_REQUEST,
-  ACTIVITY_DELETE_PHOTO_SUCCESS,
-  ACTIVITY_ADD_PHOTO_SUCCESS,
-  ACTIVITY_EDIT_PHOTO_SUCCESS,
-  ACTIVITY_COPY_SUCCESS,
+  ACTIVITY_ON_FORM_CHANGE_SUCCESS,
   ACTIVITY_PASTE_SUCCESS,
-  ACTIVITY_TOGGLE_NOTIFICATION_SUCCESS,
+  ACTIVITY_SAVE_SUCCESS,
   ACTIVITY_SET_CURRENT_HASH_SUCCESS,
   ACTIVITY_SET_SAVED_HASH_SUCCESS,
   ACTIVITY_SET_UNSAVED_NOTIFICATION,
-  ACTIVITY_GET_REQUEST,
-  ACTIVITY_GET_FAILURE,
-  ACTIVITY_CREATE_REQUEST,
-  ACTIVITY_ERRORS
+  ACTIVITY_TOGGLE_NOTIFICATION_SUCCESS,
+  ACTIVITY_UPDATE_GEO_SUCCESS
 } from '../actions';
 
-import { AppConfig } from '../config';
 import { createNextState } from '@reduxjs/toolkit';
+import { immerable } from 'immer';
 import { getCustomErrorTransformer } from 'rjsf/business-rules/customErrorTransformer';
+import { AppConfig } from '../config';
 
 class ActivityState {
+  [immerable] = true;
+  activity: any;
+  current_activity_hash: string;
+  error: boolean;
+  failCode: number;
   initialized: boolean;
   loading: boolean;
-  error: boolean;
-  activity: any;
+  notification: any;
+  saved_activity_hash: string;
   suggestedJurisdictions: [];
   suggestedPersons: [];
   suggestedTreatmentIDs: [];
-  current_activity_hash: string;
-  saved_activity_hash: string;
   unsaved_notification: any;
-  failCode: number;
   constructor() {
     this.initialized = false;
     this.current_activity_hash = null;
     this.saved_activity_hash = null;
   }
-  notification: any;
 }
 const initialState = new ActivityState();
 
 function createActivityReducer(configuration: AppConfig): (ActivityState, AnyAction) => ActivityState {
   return (state = initialState, action) => {
-    switch (action.type) {
-      case ACTIVITY_ERRORS: {
-        const nextState= createNextState(state, (draftState) => {
-          if(action.payload.errors !== undefined)
-          draftState.activityErrors = getCustomErrorTransformer()(action.payload.errors)
-        })
-        return nextState;
-      }
-      case ACTIVITY_DELETE_SUCCESS: {
-        return {
-          ...new ActivityState()
-        };
-      }
-      case ACTIVITY_CREATE_REQUEST: {
-        return {
-          activity_copy_buffer: state.activity_copy_buffer,
-          ...new ActivityState()
-        };
-      }
-      case ACTIVITY_GET_FAILURE: {
-        return {
-          ...state,
-          loading: false,
-          failCode: action.payload?.failNetworkObj?.status
-        };
-      }
-      case ACTIVITY_GET_REQUEST: {
-        return {
-          ...state,
-          failCode: null,
-          loading: true
-        };
-      }
-      case ACTIVITY_GET_SUCCESS: {
-        return {
-          ...state,
-          activity: { ...action.payload.activity },
-          suggestedTreatmentIDs: [],
-          loading: false
-        };
-      }
-      case ACTIVITY_UPDATE_GEO_SUCCESS: {
-        const nextState = createNextState(state, (draftState) => {
+    return createNextState(state, (draftState) => {
+      switch (action.type) {
+        case ACTIVITY_ERRORS: {
+          if (action.payload.errors !== undefined)
+            draftState.activityErrors = getCustomErrorTransformer()(action.payload.errors);
+          break;
+        }
+        case ACTIVITY_DELETE_SUCCESS: {
+          draftState = new ActivityState();
+          break;
+        }
+        case ACTIVITY_CREATE_REQUEST: {
+          const activity_copy_buffer = draftState.activity_copy_buffer;
+          draftState = new ActivityState();
+          draftState.activity_copy_buffer = activity_copy_buffer;
+          break;
+        }
+        case ACTIVITY_GET_FAILURE: {
+          draftState.loading = false;
+          draftState.failCode = action.payload?.failNetworkObj?.status;
+          break;
+        }
+        case ACTIVITY_GET_REQUEST: {
+          draftState.failCode = null;
+          draftState.loading = true;
+          break;
+        }
+        case ACTIVITY_GET_SUCCESS: {
+          draftState.activity = { ...action.payload.activity };
+          draftState.suggestedTreatmentIDs = [];
+          draftState.loading = false;
+          break;
+        }
+        case ACTIVITY_UPDATE_GEO_SUCCESS: {
           draftState.activity.geometry = action.payload.geometry;
           draftState.activity.form_data.activity_data.latitude = action.payload.lat;
           draftState.activity.form_data.activity_data.longitude = action.payload.long;
@@ -125,130 +94,82 @@ function createActivityReducer(configuration: AppConfig): (ActivityState, AnyAct
           draftState.activity.form_data.activity_data.utm_northing = action.payload.utm ? action.payload.utm[2] : null;
           draftState.activity.form_data.activity_data.reported_area = action.payload.reported_area;
           draftState.activity.form_data.activity_subtype_data.Well_Information = action.payload.Well_Information;
-        });
-        return nextState;
-      }
-      case ACTIVITY_ON_FORM_CHANGE_SUCCESS: {
-        const nextState = createNextState(state, (draftState) => {
+          break;
+        }
+        case ACTIVITY_ON_FORM_CHANGE_SUCCESS: {
           draftState.activity.form_data = action.payload.activity.form_data;
-        });
-        return nextState;
-      }
-      case ACTIVITY_GET_SUGGESTED_JURISDICTIONS_SUCCESS: {
-        return {
-          ...state,
-          suggestedJurisdictions: [...action.payload.jurisdictions]
-        };
-      }
-      case ACTIVITY_GET_SUGGESTED_PERSONS_SUCCESS: {
-        if (state.activity.activity_subtype === 'Observation' && !state.activity.initial_autofill_done) {
-          return {
-            ...state,
-            suggestedPersons: [...action.payload.suggestedPersons]
-          };
+          break;
         }
-        if (state.activity.activity_subtype === 'Treatment' && !state.activity.initial_autofill_done) {
-          return {
-            ...state,
-            suggestedPersons: [...action.payload.suggestedPersons]
-          };
+        case ACTIVITY_GET_SUGGESTED_JURISDICTIONS_SUCCESS: {
+          draftState.suggestedJurisdictions = [...action.payload.jurisdictions];
+          break;
         }
-        return {
-          ...state,
-          suggestedPersons: [...action.payload.suggestedPersons]
-        };
-      }
-      case ACTIVITY_GET_SUGGESTED_TREATMENT_IDS_SUCCESS: {
-        return {
-          ...state,
-          suggestedTreatmentIDs: [...action.payload.suggestedTreatmentIDs]
-        };
-      }
-      case ACTIVITY_CREATE_SUCCESS: {
-        return {
-          ...state,
-          activeActivity: action.payload.activity_id,
-          current_activity_hash: null,
-          saved_activity_hash: null
-        };
-      }
-      case ACTIVITY_SAVE_SUCCESS: {
-        return {
-          ...state,
-          activity: { ...action.payload.activity }
-        };
-      }
-      case ACTIVITY_TOGGLE_NOTIFICATION_SUCCESS: {
-        return {
-          ...state,
-          notification: action.payload.notification
-        };
-      }
-      case ACTIVITY_COPY_SUCCESS: {
-        return {
-          ...state,
-          activity_copy_buffer: {
+        case ACTIVITY_GET_SUGGESTED_PERSONS_SUCCESS: {
+          if (draftState.activity.activity_subtype === 'Observation' && !draftState.activity.initial_autofill_done) {
+            draftState.suggestedPersons = [...action.payload.suggestedPersons];
+          }
+          if (draftState.activity.activity_subtype === 'Treatment' && !draftState.activity.initial_autofill_done) {
+            draftState.suggestedPersons = [...action.payload.suggestedPersons];
+          }
+          draftState.suggestedPersons = [...action.payload.suggestedPersons];
+          break;
+        }
+        case ACTIVITY_GET_SUGGESTED_TREATMENT_IDS_SUCCESS: {
+          draftState.suggestedTreatmentIDs = [...action.payload.suggestedTreatmentIDs];
+          break;
+        }
+        case ACTIVITY_CREATE_SUCCESS: {
+          draftState.activeActivity = action.payload.activity_id;
+          draftState.current_activity_hash = null;
+          draftState.saved_activity_hash = null;
+          break;
+        }
+        case ACTIVITY_SAVE_SUCCESS: {
+          draftState.activity = { ...action.payload.activity };
+          break;
+        }
+        case ACTIVITY_TOGGLE_NOTIFICATION_SUCCESS: {
+          draftState.notification = action.payload.notification;
+          break;
+        }
+        case ACTIVITY_COPY_SUCCESS: {
+          draftState.activity_copy_buffer = {
             form_data: action.payload.form_data
-          }
-        };
+          };
+          break;
+        }
+        case ACTIVITY_PASTE_SUCCESS: {
+          draftState.form_data = draftState.activity_copy_buffer.form_data;
+          break;
+        }
+        case ACTIVITY_ADD_PHOTO_SUCCESS: {
+          draftState.activity.media.push(action.payload.photo);
+          break;
+        }
+        case ACTIVITY_DELETE_PHOTO_SUCCESS: {
+          draftState.activity = action.payload.activity;
+          break;
+        }
+        case ACTIVITY_EDIT_PHOTO_SUCCESS: {
+          draftState.activity.media = action.payload.media;
+          break;
+        }
+        case ACTIVITY_SET_CURRENT_HASH_SUCCESS: {
+          draftState.current_activity_hash = action.payload.current;
+          break;
+        }
+        case ACTIVITY_SET_SAVED_HASH_SUCCESS: {
+          draftState.saved_activity_hash = action.payload.saved;
+          break;
+        }
+        case ACTIVITY_SET_UNSAVED_NOTIFICATION: {
+          draftState.unsaved_notification = action.payload.unsaved_notification;
+          break;
+        }
+        default:
+          return state;
       }
-      case ACTIVITY_PASTE_SUCCESS: {
-        const newFormData = JSON.parse(JSON.stringify(state.activity_copy_buffer.form_data));
-        return {
-          ...state,
-          activity: {
-            ...state.activity,
-            form_data: {
-              ...newFormData
-            }
-          }
-        };
-      }
-      case ACTIVITY_ADD_PHOTO_SUCCESS: {
-        return {
-          ...state,
-          activity: {
-            ...state.activity,
-            media: [...state.activity.media, action.payload.photo]
-          }
-        };
-      }
-      case ACTIVITY_DELETE_PHOTO_SUCCESS: {
-        return {
-          ...state,
-          activity: action.payload.activity
-        };
-      }
-      case ACTIVITY_EDIT_PHOTO_SUCCESS: {
-        return {
-          ...state,
-          activity: {
-            ...state.activity,
-            media: [...action.payload.media]
-          }
-        };
-      }
-      case ACTIVITY_SET_CURRENT_HASH_SUCCESS: {
-        return {
-          ...state,
-          current_activity_hash: action.payload.current
-        };
-      }
-      case ACTIVITY_SET_SAVED_HASH_SUCCESS: {
-        return {
-          ...state,
-          saved_activity_hash: action.payload.saved
-        };
-      }
-      case ACTIVITY_SET_UNSAVED_NOTIFICATION: {
-        return {
-          ...state,
-          unsaved_notification: action.payload.unsaved_notification
-        };
-      }
-      default:
-        return state;
-    }
+    }) as unknown as ActivityState;
   };
 }
 
