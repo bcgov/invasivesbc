5bc0386b95c351cbd09c90c93dc5e6710a0229ed
Robert Johnstone <rob@plasticviking.com>
2024-01-22 18:14:04 -0400

    Finishing up export record ticket -- delete stale exports at end of cronjob run

--- api/src/geojson-exporter.ts
+++ api/src/geojson-exporter.ts
@@ -1,12 +1,13 @@
 import { getLogger } from './utils/logger';
 import { getDBConnection } from './database/db';
-import { doActivityAndIAPPExports } from './utils/export-geojson';
+import { cleanupOldExports, doActivityAndIAPPExports } from './utils/export-geojson';
 
 const defaultLog = getLogger('geojson-exporter');
 
 async function run() {
   const connection = await getDBConnection();
   await doActivityAndIAPPExports(connection);
+  await cleanupOldExports(connection);
   await connection.release();
   defaultLog.info({ message: 'run complete' });
 }
--- api/src/queries/export-record-queries.ts
+++ api/src/queries/export-record-queries.ts
@@ -1,10 +1,20 @@
 import { SQL } from 'sql-template-strings';
 
-export const GET_LATEST_EXPORT_METADATA =
-  SQL`SELECT DISTINCT ON (e.export_type) e.export_type as "type",
-                                         e.export_time as "time",
-                                         e.last_record,
-                                         e.file_reference
-      FROM export_records e
-      order by export_type asc, export_time desc
-  `;
+export const GET_LATEST_EXPORT_METADATA = SQL`SELECT DISTINCT ON (e.export_type) e.export_type as "type",
+                                                                                 e.export_time as "time",
+                                                                                 e.last_record,
+                                                                                 e.file_reference
+                                              FROM export_records e
+                                              order by export_type asc, export_time desc
+`;
+
+export const STALE_EXPORTS_SQL = SQL`select id,
+                                            file_reference
+                                     from export_records
+                                     order by export_time desc
+                                     offset 6`;
+
+export const DELETE_STALE_EXPORT_RECORD = (id) =>
+  SQL`delete
+      from export_records
+      where id = ${id}`;
