1233c9cc32133984abc60e554d59d545cfedbb16
Micheal Wells <micheal.w.wells@gmail.com>
2024-01-22 14:40:30 -0800

    Default Whats here to activities tab, obsolete the whats here marker and let the highlight comp handle drawing the geo.  Delete som pan to stuff that causes issues.  Closes #3092

--- appv2/src/UI/Map/Buttons/ButtonContainer.tsx
+++ appv2/src/UI/Map/Buttons/ButtonContainer.tsx
@@ -29,7 +29,6 @@ export const ButtonContainer = (props) => {
       <AccuracyMarker />
       <LocationMarker />
       <PanToMe />
-      {isAuth && <WhatsHereMarker />}
       {isAuth && <WhatsHereCurrentRecordHighlighted />}
       {isAuth && <WhatsHereDrawComponent />}
       <NewRecord/>
