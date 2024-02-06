51f0a52b091ef63400c941099091758f14ebb910
Micheal Wells <micheal.w.wells@gmail.com>
2024-02-06 08:34:22 -0800

    Disable donuts and some label on zoom end data processing

--- appv2/src/UI/Map/Map.tsx
+++ appv2/src/UI/Map/Map.tsx
@@ -1,6 +1,6 @@
 import 'leaflet/dist/leaflet.css';
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, useMap } from 'react-leaflet';
 import './Map.css';
 import * as L from 'leaflet';
 import OfflineLayers from './OfflineLayers';
@@ -9,23 +9,21 @@ import { RecordSetLayersRenderer } from './RecordSetLayersRenderer';
 import { VectorOverviewLayer } from './VectorOverviewLayer';
 import { RENDER_DEBUG } from 'UI/App';
 
+const MapUnmounter = (props) => {
+  const map = useMap();
+  useEffect(() => {
+    return () => {
+      map.clearAllEventListeners();
+      map.remove();
+    };
+  }, []);
+  return <></>;
+};
 
 const Map = (props: any) => {
   let ref = useRef(0);
   ref.current += 1;
-  if(RENDER_DEBUG)
-  console.log('%cMap.tsx render' + ref.current.toString(), 'color: yellow');
-
-  const initializedAuth = useSelector((state: any) => 
-  {
-    return state.Auth?.initialized
-  }, (prev, next) => { return JSON.stringify(prev) === JSON.stringify(next) });
-
-
-  useEffect(()=> {
-    console.log('%cmap rerendering from auth', 'color: yellow');
-    console.log('auth', initializedAuth)
-  },[initializedAuth])
+  if (RENDER_DEBUG) console.log('%cMap.tsx render' + ref.current.toString(), 'color: yellow');
 
   return (
     <div className="map">
@@ -40,7 +38,8 @@ const Map = (props: any) => {
           center={[51, -128]}
           zoom={5}
           className="map__leaflet">
-          <RecordSetLayersRenderer />  
+          <RecordSetLayersRenderer />
+          <MapUnmounter />
           {props.children}
           <OfflineLayers />
           <VectorOverviewLayer />
