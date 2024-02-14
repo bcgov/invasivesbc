4d605f764a50d809312ccdb6e7e9ec2bb708e00f
Micheal Wells <micheal.w.wells@gmail.com>
2024-02-14 10:59:18 -0800

    Only rebuild layer source and add layer if tableFilterHash updated, clear old hash layers

--- appv2/src/UI/Map2/Map.tsx
+++ appv2/src/UI/Map2/Map.tsx
@@ -22,14 +22,27 @@ export const Map = (props: any) => {
 
   const toggledOnLayers = storeLayers.filter((layer: any) => layer.toggledOn);
 
-  useEffect(()=> {
-    maplibregl.setRTLTextPlugin('https://unpkg.com/@mapbox/mapbox-gl-rtl-text@0.2.3/mapbox-gl-rtl-text.min.js', true);
-  },[])
+  useEffect(() => {
+    try {
+      //   maplibregl.setRTLTextPlugin('https://unpkg.com/@mapbox/mapbox-gl-rtl-text@0.2.3/mapbox-gl-rtl-text.min.js', true);
+    } catch (e) {
+      console.log('error setting rtl text plugin', e);
+    }
+
+    return () => {
+      if (map.current) {
+        try {
+          map.current.remove();
+        } catch (e) {
+          console.log('error removing map', e);
+        }
+      }
+    };
+  }, []);
 
   useEffect(() => {
     if (map.current) return;
     // add the PMTiles plugin to the maplibregl global.
-
 
     const protocol = new Protocol();
     maplibregl.addProtocol('pmtiles', (request) => {
@@ -91,13 +104,13 @@ export const Map = (props: any) => {
               id: 'wms-test-layer',
               type: 'raster',
               source: 'wms-test-source2',
-              minzoom: 0,
+              minzoom: 0
             },
             {
               id: 'wms-test-layer2',
               type: 'raster',
               source: 'wms-test-source',
-              minzoom: 0,
+              minzoom: 0
             },
             {
               id: 'invasives-vector',
@@ -108,7 +121,7 @@ export const Map = (props: any) => {
                 'fill-color': 'steelblue'
               },
               minzoom: 0,
-              maxzoom: 24,
+              maxzoom: 24
             },
             {
               id: 'buildings',
@@ -119,7 +132,7 @@ export const Map = (props: any) => {
                 'fill-color': 'steelblue'
               },
 
-              minzoom: 0,
+              minzoom: 0
             }
           ]
         }
@@ -130,161 +143,21 @@ export const Map = (props: any) => {
   useEffect(() => {
     if (!map.current) return;
 
-    // update to only map over whats new
+    console.log('in hook that adds stuff');
+
+    // Create new sources and layers if they don't exist, and blow away those with stale tableFilterHash's
     storeLayers.map((layer: any) => {
       if (layer.layerState.mapToggle && layer.geoJSON && layer.loading === false) {
-        if (map.current.getLayer(layer.recordSetID)) {
-          try {
-            map.current.removeLayer(layer.recordSetID);
-          } catch (e) {
-            console.log('error removing layer', e);
+        if (layer.type === 'Activity') {
+          deleteStaleActivityLayer(map.current, layer);
+          const existingSource = map.current.getSource('recordset-layer-' + layer.recordSetID + '-hash-' + layer.tableFiltersHash)
+          if(existingSource === undefined)
+          {
+            createActivityLayer(map.current, layer);
           }
-        }
-        if (map.current.getLayer('label-' + layer.recordSetID)) {
-          try {
-            map.current.removeLayer('label-' + layer.recordSetID);
-          } catch (e) {
-            console.log('error removing layer', e);
-          }
-        }
-
-        if (map.current.getSource(layer.recordSetID)) {
-          try {
-            map.current.removeSource(layer.recordSetID);
-          } catch (e) {
-            console.log('error removing source', e);
-          }
-        }
-
-        map.current
-          .addSource(layer.recordSetID, {
-            type: 'geojson',
-            data: layer.geoJSON
-          })
-          .addLayer({
-            id: layer.recordSetID,
-            source: layer.recordSetID,
-            type: layer.type === 'IAPP' ? 'circle' : 'fill',
-            paint:
-              layer.type === 'IAPP'
-                ? {
-                    'circle-color': layer.layerState.color,
-                    'circle-radius': 3
-                  }
-                : {
-                    'fill-color': layer.layerState.color,
-                    'fill-outline-color': layer.layerState.color,
-                    'fill-opacity': 0.5
-                  },
-            minzoom: 0,
-            maxzoom: 24
-          });
-
-          if(layer.type === 'Activity') {
-            map.current.addLayer({
-            id: 'polygon-border-' + layer.recordSetID,
-            source: layer.recordSetID,
-            type: 'line',
-            paint:
-                 {
-                    'line-color': layer.layerState.color,
-                    'line-opacity': 1,
-                    'line-width': 3,
-                  },
-
-            })
-          }
-
-        if (layer.layerState.labelToggle) {
-          console.log('layer type', layer.type);
-
-          if(layer.type === 'IAPP') {
-          map.current.addLayer({
-            id: 'label-' + layer.recordSetID,
-            type: 'symbol',
-            source: layer.recordSetID,
-            // Doc for styling text https://maplibre.org/maplibre-style-spec/expressions/
-            layout: {
-              //                'icon-image': 'dog-park-11',
-              'text-field': [
-                'format',
-                ['concat', 'IAPP-', ['to-string', ['get','site_id']]],
-                { 'font-scale': 1.3 },
-                '\n',
-                {},
-                ['to-string', ['get', 'species_on_site']],
-                { 'font-scale': 1.0 }
-              ],
-              // the actual font names that work are here https://github.com/openmaptiles/fonts/blob/gh-pages/fontstacks.json
-              'text-font': ['literal', ['Open Sans Bold']],
-              // 'text-font': ['literal', ['Open Sans Semibold']],
-              'text-offset': [0, 0.6],
-              'text-anchor': 'top'
-            },
-            paint: {
-              'text-color': 'black',
-              'text-halo-color': 'white',
-              'text-halo-width': 1,
-              'text-halo-blur': 1
-            },
-            minzoom: 10
-          });
-        }
-        else
-        {
-          map.current.addLayer({
-            id: 'label-' + layer.recordSetID,
-            type: 'symbol',
-            source: layer.recordSetID,
-            layout: {
-              //                'icon-image': 'dog-park-11',
-              'text-field': [
-                'format',
-                ['upcase', ['get', 'short_id']],
-                { 'font-scale': 1.3 },
-                '\n',
-                {},
-                ['upcase', ['concat', ['get', 'type'], ' +  ', ['get', 'species_positive'], ' - ', ['get', 'species_negative']]],
-                { 'font-scale': 1.0 }
-              ],
-              // the actual font names that work are here https://github.com/openmaptiles/fonts/blob/gh-pages/fontstacks.json
-              'text-font': ['literal', ['Open Sans Bold']],
-              // 'text-font': ['literal', ['Open Sans Semibold']],
-              'text-offset': [0, 0.6],
-              'text-anchor': 'top'
-            },
-            paint: {
-              'text-color': 'black',
-              'text-halo-color': 'white',
-              'text-halo-width': 1,
-              'text-halo-blur': 1
-            },
-            minzoom: 10
-          }); 
-        }
-        }
-      } else {
-        if (map.current.getLayer(layer.recordSetID)) {
-          try {
-            map.current.removeLayer(layer.recordSetID);
-          } catch (e) {
-            console.log('error removing layer', e);
-          }
-        }
-        if (map.current.getLayer('label-' + layer.recordSetID)) {
-          try {
-            map.current.removeLayer('label-' + layer.recordSetID);
-          } catch (e) {
-            console.log('error removing layer', e);
-          }
-        }
-
-        if (map.current.getSource(layer.recordSetID)) {
-          try {
-            map.current.removeSource(layer.recordSetID);
-          } catch (e) {
-            console.log('error removing source', e);
-          }
+        } else if (layer.type === 'IAPP') {
+          deleteIAPPLayer(map.current, layer);
+          createIAPPLayer(map.current, layer);
         }
       }
     });
@@ -305,4 +178,200 @@ export const Map = (props: any) => {
       <div ref={mapContainer} className="Map" />
     </div>
   );
+};
+
+const createActivityLayer = (map: any, layer: any) => {
+  const layerID = 'recordset-layer-' + layer.recordSetID + '-hash-' + layer.tableFiltersHash;
+  console.log('creating')
+  console.log('typeof', typeof map);
+  map
+    .addSource(layerID, {
+      type: 'geojson',
+      data: layer.geoJSON
+    })
+    .addLayer({
+      id: layerID,
+      source: layerID,
+      type: 'fill',
+      paint: {
+        'fill-color': layer.layerState.color,
+        'fill-outline-color': layer.layerState.color,
+        'fill-opacity': 0.5
+      },
+      minzoom: 0,
+      maxzoom: 24
+    });
+
+  map.addLayer({
+    id: 'polygon-border-' + layerID,
+    source: layerID,
+    type: 'line',
+    paint: {
+      'line-color': layer.layerState.color,
+      'line-opacity': 1,
+      'line-width': 3
+    }
+  });
+
+  map.addLayer({
+    id: 'polygon-circle-' + layerID,
+    source: layerID,
+    type: 'circle',
+    paint: {
+      'circle-color': layer.layerState.color,
+      'circle-radius': 3
+    },
+    maxzoom: 10
+  });
+
+  map.addLayer({
+    id: 'label-' + layerID,
+    type: 'symbol',
+    source: layerID,
+    layout: {
+      //                'icon-image': 'dog-park-11',
+      'text-field': [
+        'format',
+        ['upcase', ['get', 'short_id']],
+        { 'font-scale': 1.3 },
+        '\n',
+        {},
+        [
+          'upcase',
+          ['concat', ['get', 'type'], ' +  ', ['get', 'species_positive'], ' - ', ['get', 'species_negative']]
+        ],
+        { 'font-scale': 1.0 }
+      ],
+      // the actual font names that work are here https://github.com/openmaptiles/fonts/blob/gh-pages/fontstacks.json
+      'text-font': ['literal', ['Open Sans Bold']],
+      // 'text-font': ['literal', ['Open Sans Semibold']],
+      'text-offset': [0, 0.6],
+      'text-anchor': 'top'
+    },
+    paint: {
+      'text-color': 'black',
+      'text-halo-color': 'white',
+      'text-halo-width': 1,
+      'text-halo-blur': 1
+    },
+    minzoom: 10
+  });
+};
+
+const deleteStaleActivityLayer = (map: any, layer: any) => {
+  if(!map) {
+    return;
+  }
+  const newLayerID = 'recordset-layer-' + layer.recordSetID + '-hash-' + layer.tableFiltersHash;
+  //get all layers for recordset
+  const allLayersForRecordSet = map.getLayersOrder().filter((mapLayer: any) => {
+    return (
+      mapLayer.includes('recordset-layer-' + layer.recordSetID) ||
+      mapLayer.includes('label-' + layer.recordSetID) ||
+      mapLayer.includes('polygon-border-' + layer.recordSetID) ||
+      mapLayer.includes('polygon-circle-' + layer.recordSetID)
+    );
+  });
+
+  const stale = allLayersForRecordSet.filter((mapLayer) => {
+    return !mapLayer.includes(layer.tableFiltersHash);
+  });
+
+  stale.map((staleLayer) => {
+    try {
+      map.removeLayer(staleLayer);
+    } catch (e) {
+      console.log('error removing layer' + staleLayer);
+    }
+  });
+
+  // get all sources:
+  console.log(map.style.sourceCaches);
+
+  const staleSources = Object.keys(map.style.sourceCaches).filter((source) => {
+    return source.includes('recordset-layer-' + layer.recordSetID) && !source.includes(layer.tableFiltersHash);
+  });
+
+  staleSources?.map((staleSource) => {
+    if (map.getSource(staleSource)) {
+      try {
+        map.removeSource(staleSource);
+      } catch (e) {
+        console.log('error removing source', e);
+      }
+    }
+  });
+};
+
+const createIAPPLayer = (map: any, layer: any) => {
+  map
+    .addSource(layer.recordSetID, {
+      type: 'geojson',
+      data: layer.geoJSON
+    })
+    .addLayer({
+      id: layer.recordSetID,
+      source: layer.recordSetID,
+      type: 'circle',
+      paint: {
+        'circle-color': layer.layerState.color,
+        'circle-radius': 3
+      },
+      minzoom: 0,
+      maxzoom: 24
+    });
+
+  map.addLayer({
+    id: 'label-' + layer.recordSetID,
+    type: 'symbol',
+    source: layer.recordSetID,
+    layout: {
+      //                'icon-image': 'dog-park-11',
+      'text-field': [
+        'format',
+        ['concat', 'IAPP-', ['to-string', ['get', 'site_id']]],
+        { 'font-scale': 1.3 },
+        '\n',
+        {},
+        ['to-string', ['get', 'species_on_site']],
+        { 'font-scale': 1.0 }
+      ],
+      // the actual font names that work are here
+      'text-font': ['literal', ['Open Sans Bold']],
+      'text-offset': [0, 0.6],
+      'text-anchor': 'top'
+    },
+    paint: {
+      'text-color': 'black',
+      'text-halo-color': 'white',
+      'text-halo-width': 1,
+      'text-halo-blur': 1
+    },
+    minzoom: 10
+  });
+};
+
+const deleteIAPPLayer = (map: any, layer: any) => {
+  if (map.getLayer(layer.recordSetID)) {
+    try {
+      map.removeLayer(layer.recordSetID);
+    } catch (e) {
+      console.log('error removing layer', e);
+    }
+  }
+  if (map.getLayer('label-' + layer.recordSetID)) {
+    try {
+      map.removeLayer('label-' + layer.recordSetID);
+    } catch (e) {
+      console.log('error removing layer', e);
+    }
+  }
+
+  if (map.getSource(layer.recordSetID)) {
+    try {
+      map.removeSource(layer.recordSetID);
+    } catch (e) {
+      console.log('error removing source', e);
+    }
+  }
 };
