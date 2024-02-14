10a9c0c15c43d4fa01a107d3ae8aea9aed6d8e6d
Micheal Wells <micheal.w.wells@gmail.com>
2024-02-13 17:11:06 -0800

    Tile fallback on max zoom, better labels, polygon outlines

--- appv2/src/UI/Map2/Map.tsx
+++ appv2/src/UI/Map2/Map.tsx
@@ -22,8 +22,11 @@ export const Map = (props: any) => {
 
   const toggledOnLayers = storeLayers.filter((layer: any) => layer.toggledOn);
 
+  useEffect(()=> {
+    maplibregl.setRTLTextPlugin('https://unpkg.com/@mapbox/mapbox-gl-rtl-text@0.2.3/mapbox-gl-rtl-text.min.js', true);
+  },[])
+
   useEffect(() => {
-    maplibregl.setRTLTextPlugin('https://unpkg.com/@mapbox/mapbox-gl-rtl-text@0.2.3/mapbox-gl-rtl-text.min.js', false);
     if (map.current) return;
     // add the PMTiles plugin to the maplibregl global.
 
@@ -54,6 +57,7 @@ export const Map = (props: any) => {
     p.getHeader().then((h) => {
       map.current = new maplibregl.Map({
         container: mapContainer.current,
+        maxZoom: 24,
         zoom: h.maxZoom - 2,
         center: [h.centerLon, h.centerLat],
         style: {
@@ -65,12 +69,14 @@ export const Map = (props: any) => {
               tiles: [
                 'https://openmaps.gov.bc.ca/geo/ows?bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.3.0&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&raster-opacity=0.5&layers=WHSE_IMAGERY_AND_BASE_MAPS.MOT_ROAD_FEATURES_INVNTRY_SP'
               ],
-              tileSize: 256
+              tileSize: 256,
+              maxzoom: 24
             },
             'wms-test-source2': {
               type: 'raster',
               tiles: ['https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'],
-              tileSize: 256
+              tileSize: 256,
+              maxzoom: 18
             },
             example_source: {
               type: 'vector',
@@ -86,14 +92,12 @@ export const Map = (props: any) => {
               type: 'raster',
               source: 'wms-test-source2',
               minzoom: 0,
-              maxzoom: 22
             },
             {
               id: 'wms-test-layer2',
               type: 'raster',
               source: 'wms-test-source',
               minzoom: 0,
-              maxzoom: 22
             },
             {
               id: 'invasives-vector',
@@ -104,7 +108,7 @@ export const Map = (props: any) => {
                 'fill-color': 'steelblue'
               },
               minzoom: 0,
-              maxzoom: 22
+              maxzoom: 24,
             },
             {
               id: 'buildings',
@@ -116,7 +120,6 @@ export const Map = (props: any) => {
               },
 
               minzoom: 0,
-              maxzoom: 22
             }
           ]
         }
@@ -170,13 +173,65 @@ export const Map = (props: any) => {
                   }
                 : {
                     'fill-color': layer.layerState.color,
+                    'fill-outline-color': layer.layerState.color,
                     'fill-opacity': 0.5
                   },
             minzoom: 0,
-            maxzoom: 22
+            maxzoom: 24
           });
 
+          if(layer.type === 'Activity') {
+            map.current.addLayer({
+            id: 'polygon-border-' + layer.recordSetID,
+            source: layer.recordSetID,
+            type: 'line',
+            paint:
+                 {
+                    'line-color': layer.layerState.color,
+                    'line-opacity': 1,
+                    'line-width': 3,
+                  },
+
+            })
+          }
+
         if (layer.layerState.labelToggle) {
+          console.log('layer type', layer.type);
+
+          if(layer.type === 'IAPP') {
+          map.current.addLayer({
+            id: 'label-' + layer.recordSetID,
+            type: 'symbol',
+            source: layer.recordSetID,
+            // Doc for styling text https://maplibre.org/maplibre-style-spec/expressions/
+            layout: {
+              //                'icon-image': 'dog-park-11',
+              'text-field': [
+                'format',
+                ['concat', 'IAPP-', ['to-string', ['get','site_id']]],
+                { 'font-scale': 1.3 },
+                '\n',
+                {},
+                ['to-string', ['get', 'species_on_site']],
+                { 'font-scale': 1.0 }
+              ],
+              // the actual font names that work are here https://github.com/openmaptiles/fonts/blob/gh-pages/fontstacks.json
+              'text-font': ['literal', ['Open Sans Bold']],
+              // 'text-font': ['literal', ['Open Sans Semibold']],
+              'text-offset': [0, 0.6],
+              'text-anchor': 'top'
+            },
+            paint: {
+              'text-color': 'black',
+              'text-halo-color': 'white',
+              'text-halo-width': 1,
+              'text-halo-blur': 1
+            },
+            minzoom: 10
+          });
+        }
+        else
+        {
           map.current.addLayer({
             id: 'label-' + layer.recordSetID,
             type: 'symbol',
@@ -186,14 +241,14 @@ export const Map = (props: any) => {
               'text-field': [
                 'format',
                 ['upcase', ['get', 'short_id']],
-                { 'font-scale': 1.8 },
+                { 'font-scale': 1.3 },
                 '\n',
                 {},
-                ['downcase', ['get', 'type']],
-                { 'font-scale': 0.6 }
+                ['upcase', ['concat', ['get', 'type'], ' +  ', ['get', 'species_positive'], ' - ', ['get', 'species_negative']]],
+                { 'font-scale': 1.0 }
               ],
               // the actual font names that work are here https://github.com/openmaptiles/fonts/blob/gh-pages/fontstacks.json
-              'text-font': ['literal', ['Open Sans Light']],
+              'text-font': ['literal', ['Open Sans Bold']],
               // 'text-font': ['literal', ['Open Sans Semibold']],
               'text-offset': [0, 0.6],
               'text-anchor': 'top'
@@ -201,10 +256,12 @@ export const Map = (props: any) => {
             paint: {
               'text-color': 'black',
               'text-halo-color': 'white',
-              'text-halo-width': 2,
+              'text-halo-width': 1,
               'text-halo-blur': 1
-            }
-          });
+            },
+            minzoom: 10
+          }); 
+        }
         }
       } else {
         if (map.current.getLayer(layer.recordSetID)) {
