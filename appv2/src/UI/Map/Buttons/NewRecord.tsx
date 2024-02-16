5ee6585c5d121a922923f16d221e56d917915e9a
Micheal Wells <micheal.w.wells@gmail.com>
2024-02-16 09:06:33 -0800

    Jump to record working, adding WMS layers there but needs zorder

--- appv2/src/UI/Map/Buttons/NewRecord.tsx
+++ appv2/src/UI/Map/Buttons/NewRecord.tsx
@@ -1,18 +1,14 @@
 import React, { useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import * as L from 'leaflet';
-import { useMap } from 'react-leaflet';
 import { IconButton, Tooltip } from '@mui/material';
-//import { toolStyles } from 'UI/Styles/ToolStyles';
 import { useSelector } from 'util/use_selector';
 import FiberNewIcon from '@mui/icons-material/FiberNew';
 import { OPEN_NEW_RECORD_MENU } from 'state/actions';
-import { useHistory } from 'react-router';
 import 'UI/Global.css';
 
 export const NewRecord = (props) => {
   const dispatch = useDispatch();
- // const toolClass = toolStyles();
   const divRef = useRef();
   const isAuth = useSelector((state: any) => state.Auth?.authenticated);
 
