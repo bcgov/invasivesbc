2e4f575e8cc41d7c17a0e01a14d1c2bd15de58ed
Micheal Wells <micheal.w.wells@gmail.com>
2024-03-01 16:47:41 -0800

    example dispatching action to store in test and checking for new state

--- appv2/src/tests/state.test.tsx
+++ appv2/src/tests/state.test.tsx
@@ -1,6 +1,7 @@
 import { beforeEach, describe, expect, it } from 'vitest';
 import { store } from '../main';
 import { waitFor } from '@testing-library/react';
+import { MAP_TOGGLE_BASEMAP } from 'state/actions';
 
 describe('App init state, pre-login', function () {
   beforeEach(async () => {
@@ -14,9 +15,18 @@ describe('App init state, pre-login', function () {
   it('Map array has expected layers logged out', function () {
     const state = store.getState();
 
-    // We don't pass state.Map directly to expect or else it logs the whole state blob
     const mapLayerArrayLength = state.Map.layers.length
     expect(mapLayerArrayLength).toBe(0);
   });
+
+
+  it('Map state lets you toggle topo/sat', () => {
+    store.dispatch({type: MAP_TOGGLE_BASEMAP})
+    const state = store.getState();
+    expect(state.Map.baseMapToggle).toBe(true)
+    store.dispatch({type: MAP_TOGGLE_BASEMAP})
+    const state2 = store.getState();
+    expect(state2.Map.baseMapToggle).toBe(false)
+  })
 
 });
