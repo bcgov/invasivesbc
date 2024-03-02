e85739451a9295006d8c5b3047b7f62d04a0866e
Micheal Wells <micheal.w.wells@gmail.com>
2024-03-01 16:24:59 -0800

    missing vitest conf

---
+++ appv2/vitest.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig, mergeConfig } from 'vitest/config'
+import viteConfig from './vite.config'
+
+export default defineConfig(configEnv => mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      exclude: ['packages/template/*'],
+      setupFiles: ['./src/setupTests.ts']
+    },
+  })
+))
