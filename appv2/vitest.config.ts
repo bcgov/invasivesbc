e771c795e9faeb05f5a0c01e229cd7fa5c555539
Micheal Wells <micheal.w.wells@gmail.com>
2024-03-08 16:30:57 -0800

    prove out mocking api calls for tests, add test config var so we dont spam console with redux logger (WIP - var ignored still)

--- appv2/vitest.config.ts
+++ appv2/vitest.config.ts
@@ -1,12 +1,15 @@
-import { defineConfig, mergeConfig } from 'vitest/config'
-import viteConfig from './vite.config'
+import { defineConfig, mergeConfig } from 'vitest/config';
+import viteConfig from './vite.config';
 
-export default defineConfig(configEnv => mergeConfig(
-  viteConfig,
-  defineConfig({
-    test: {
-      exclude: ['packages/template/*'],
-      setupFiles: ['./src/setupTests.ts']
-    },
-  })
-))
+export default defineConfig((configEnv) =>
+  mergeConfig(
+    viteConfig,
+    defineConfig({
+      test: {
+        exclude: ['packages/template/*'],
+        setupFiles: ['./src/setupTests.ts']
+      },
+      define: { CONFIGURATION_TEST: true }
+    })
+  )
+);
