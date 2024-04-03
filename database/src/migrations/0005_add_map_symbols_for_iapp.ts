9e098520e5ae8bf3bdba511a20411fb5ee73128c
brennanwebster <Brennan.Webster@gov.bc.ca>
2024-04-03 09:41:37 -0700

    fix migration

--- database/src/migrations/0005_add_map_symbols_for_iapp.ts
+++ database/src/migrations/0005_add_map_symbols_for_iapp.ts
@@ -7,6 +7,152 @@ export async function up(knex: Knex) {
     `
     set search_path = invasivesbc,public;
 
+    alter table iapp_invbc_mapping 
+    add column if not exists iapp_description varchar(100);
+
+    delete from iapp_invbc_mapping 
+    where mapping_id = 45;
+    
+    UPDATE code
+    SET code_description = 
+           CASE
+            when code_name = 'L' then '1 | <= 1plant/m2 (Low)'
+            when code_name = 'M' then '2 | 2-5 plants/m2 (Med)'
+            when code_name = 'H' then '3 | 6-10 plants/m2 (High)'
+            when code_name = 'D' then '4 | >10 plants/m2 (Dense)'
+            end
+    where code_header_id = 27 and code_name in ('L', 'M', 'H', 'D');
+    
+    UPDATE code
+    SET code_description = 
+           CASE
+            when code_name = 'RS' then '1 | rare individual, a single occurrence'
+            when code_name = 'FS' then '2 | few sporadically occurring individuals'
+            when code_name = 'CL' then '3 | single patch or clump of a species'
+            when code_name = 'SS' then '4 | several sporadically occurring individuals'
+            when code_name = 'FP' then '5 | a few patches or clumps of a species'
+            when code_name = 'WS' then '6 | several well-spaced patches or clumps'
+            when code_name = 'CU' then '7 | continuous uniform occurrence of well-spaced individuals'
+            when code_name = 'CO' then '8 | continuous occurrence of a species with a few gaps in the distribution'
+            when code_name = 'CD' then '9 | continuous dense occurrence of a species'
+            end
+    where code_header_id = 29 and code_name in ('RS', 'FS', 'CL', 'SS', 'FP', 'WS', 'CU', 'CO', 'CD');
+    
+    update code 
+    set code_description = 'Reed canary grass (Phalaris arundinacea)'
+    where code_header_id = 39 and code_name = 'RE';
+    
+    update code 
+    set code_description = 'Giant mannagrass / reed sweetgrass (Glyceria maxima)'
+    where code_header_id = 39 and code_name = 'SW';
+    
+    UPDATE iapp_invbc_mapping
+    SET invbc_name = 
+           CASE
+            when char_code = 'NA' then 'North Africa grass (Ventenata dubia)'
+            when char_code = 'RE' then 'Reed canary grass (Phalaris arundinacea)'
+            when char_code = 'TS' then 'Fuller''s Teasel (Dipsacus fullonum)'
+            when char_code = 'RC' then 'European common reed (Phragmites australis subsp. australis)'
+            when char_code = 'GK' then 'Giant knotweed (Reynoutria / Fallopia sachalinensis)'
+            when char_code = 'SW' then 'Giant mannagrass / reed sweetgrass (Glyceria maxima)'
+            end
+    where char_code in ('NA', 'RE', 'TS', 'RC', 'GK', 'SW');
+    
+    update iapp_invbc_mapping
+    set char_code =
+        case 
+    when iapp_name = 'Didymo (DIDY GEM)' then 'DI'
+    when iapp_name = 'Knapweed species (CENT SPP)' then 'KS'
+    when iapp_name = 'Sowthistle species (SONC SPP)' then 'SO'
+    when iapp_name = 'Yellow hawkweed (HIER PRA)' then 'YH'
+        end
+    where iapp_name in ('Didymo (DIDY GEM)', 'Knapweed species (CENT SPP)', 'Sowthistle species (SONC SPP)', 'Yellow hawkweed (HIER PRA)');
+    
+    UPDATE iapp_invbc_mapping
+    SET iapp_description = 
+        CASE 
+    WHEN char_code = 'BB' THEN 'Bachelor''s button (Centaurea cyanus)'
+    WHEN char_code = 'KB' THEN 'Bighead knapweed (Centaurea macrocephala)'
+    WHEN char_code = 'BP' THEN 'Bigleaf periwinkle / large periwinkle (Vinca major)'
+    WHEN char_code = 'BO' THEN 'Bohemian knotweed (Fallopia x bohemica)'
+    WHEN char_code = 'ED' THEN 'Brazilian waterweed (Egeria densa)'
+    WHEN char_code = 'UR' THEN 'Bur buttercup (Ceratocephalus testiculatus)'
+    WHEN char_code = 'BU' THEN 'Burdock species (Arctium spp)'
+    WHEN char_code = 'BD' THEN 'Butterfly bush (Buddleja davidii)'
+    WHEN char_code = 'LC' THEN 'Cherry laurel (Prunus laurocerasus)'
+    WHEN char_code = 'RC' THEN 'Common reed (Phragmites australis subsp. australis)'
+    WHEN char_code = 'CU' THEN 'Cudweed (Gnaphalium uliginosum)'
+    WHEN char_code = 'CL' THEN 'Cutleaf blackberry (Rubus laciniatus)'
+    WHEN char_code = 'DT' THEN 'Dalmatian toadflax (Linaria dalmatica)'
+    WHEN char_code = 'DR' THEN 'Dame''s rocket (Hesperis matronalis)'
+    WHEN char_code = 'SL' THEN 'Daphne / spurge laurel (Daphne laureola)'
+    WHEN char_code = 'BF' THEN 'False brome (Brachypodium sylvaticum)'
+    WHEN char_code = 'FL' THEN 'Fragrant water lily (Nymphaea odorata subsp. odorata)'
+    WHEN char_code = 'GK' THEN 'Giant knotweed (Fallopia sachalinensis)'
+    WHEN char_code = 'GP' THEN 'Globe-pod hoary cress (Lepidium appelianum al-shehbaz)'
+    WHEN char_code = 'GS' THEN 'Groundsel (Senecio vulgaris)'
+    WHEN char_code = 'HC' THEN 'Heart-podded hoary cress (Lepidium draba)'
+    WHEN char_code = 'BI' THEN 'Hedge false bindweed (Calystegia sepium)'
+    WHEN char_code = 'GR' THEN 'Herb robert (Geranium robertianum)'
+    WHEN char_code = 'PO' THEN 'Himalayan knotweed (Polygonum polystachyum)'
+    WHEN char_code = 'JK' THEN 'Japanese knotweed (Fallopia japonica)'
+    WHEN char_code = 'JW' THEN 'Japanese wireweed (Sargassum muticum)'
+    WHEN char_code = 'KH' THEN 'King devil hawkweed (Hieracium floribundum)'
+    WHEN char_code = 'KO' THEN 'Kochia (Kochia scoparia)'
+    WHEN char_code = 'LT' THEN 'Lady''s-thumb (Polygonum persicaria)'
+    WHEN char_code = 'LL' THEN 'Large yellow loosestrife / spotted loosestrife (Lysimachia punctata)'
+    WHEN char_code = 'LH' THEN 'Lens-pod hoary cress (Lepidium chalepense l.)'
+    WHEN char_code = 'RF' THEN 'Lesser celandine / fig buttercup (Ranunculus ficaria)'
+    WHEN char_code = 'MX' THEN 'Maltese star thistle (Centaurea melitensis)'
+    WHEN char_code = 'MT' THEN 'Marsh plume thistle/Marsh thistle (Cirsium palustre)'
+    WHEN char_code = 'MG' THEN 'Meadow goats-beard (Tragopogon pratensis)'
+    WHEN char_code = 'MH' THEN 'Meadow hawkweed (Hieracium caespitosum)'
+    WHEN char_code = 'MK' THEN 'Meadow knapweed (Centaurea debeauxii)'
+    WHEN char_code = 'ME' THEN 'Mouse ear hawkweed (Hieracium pilosella)'
+    WHEN char_code = 'NS' THEN 'Nightshade (Solanum spp)'
+    WHEN char_code = 'NT' THEN 'Nodding thistle (Carduus nutans)'
+    WHEN char_code = 'OM' THEN 'Old man''s beard / traveller''s joy (Clematis vitalba)'
+    WHEN char_code = 'OH' THEN 'Orange hawkweed (Hieracium aurantiacum)'
+    WHEN char_code = 'PF' THEN 'Parrot feather (Myriophyllum aquaticum)'
+    WHEN char_code = 'PS' THEN 'Perennial sow thistle (Sonchus arvensis)'
+    WHEN char_code = 'LP' THEN 'Portuguese laurel (Prunus lusitanica)'
+    WHEN char_code = 'PC' THEN 'Prickly comfrey (Symphytum asperum)'
+    WHEN char_code = 'PV' THEN 'Puncturevine (Tribulus terrestris)'
+    WHEN char_code = 'PD' THEN 'Purple deadnettle (Lamium purpureum)'
+    WHEN char_code = 'QA' THEN 'Queen anne''s lace / wild carrot (Daucus carota)'
+    WHEN char_code = 'QH' THEN 'Queen devil hawkweed (Hieracium praealtum)'
+    WHEN char_code = 'RK' THEN 'Russian knapweed (Acroptilon repens)'
+    WHEN char_code = 'RT' THEN 'Russian thistle (Salsola kali)'
+    WHEN char_code = 'SN' THEN 'Salt-meadow cord grass (Spartina patens)'
+    WHEN char_code = 'SH' THEN 'Scentless chamomile (Matricaria perforata)'
+    WHEN char_code = 'SP' THEN 'Shepherd''s-purse (Capsella bursa-pastoris)'
+    WHEN char_code = 'HG' THEN 'Smooth cat''s ear (Hypochaeris glabra)'
+    WHEN char_code = 'SX' THEN 'Spotted hawkweed (Hieracium maculatum)'
+    WHEN char_code = 'SK' THEN 'Spotted knapweed (Centaurea biebersteinii)'
+    WHEN char_code = 'SJ' THEN 'St. John''s wort/Saint John''s wort/ Goatweed (Hypericum perforatum)'
+    WHEN char_code = 'TH' THEN 'Tall hawkweed (Hieracium piloselloides)'
+    WHEN char_code = 'TR' THEN 'Tansy ragwort (Senecio jacobaea)'
+    WHEN char_code = 'TS' THEN 'Teasel (Dipsacus fullonum)'
+    WHEN char_code = 'VL' THEN 'Velvet leaf (Abutilon theophrasti)'
+    WHEN char_code = 'NO' THEN 'Watercress (Nasturtium officinale)'
+    WHEN char_code = 'WG' THEN 'Western goat''s-beard (Tragopogon dubius)'
+    WHEN char_code = 'WP' THEN 'Whiplash hawkweed (Hieracium flagellare)'
+    WHEN char_code = 'WC' THEN 'White cockle (Lychnis alba)'
+    WHEN char_code = 'SR' THEN 'White spanish broom (Cytisus multiflorus)'
+    WHEN char_code = 'WB' THEN 'Wild buckwheat (Polygonum convolvulus)'
+    WHEN char_code = 'WM' THEN 'Wild mustard (Sinapis arvensis)'
+    WHEN char_code = 'WO' THEN 'Wild oats (Avena fatua)'
+    WHEN char_code = 'PW' THEN 'Wild parsnip (Pastinaca sativa)'
+    WHEN char_code = 'YD' THEN 'Yellow devil hawkweed (Hieracium glomeratum)'
+    WHEN char_code = 'HS' THEN 'Yellow hawkweed species (Hieracium spp)'
+    WHEN char_code = 'YN' THEN 'Yellow nutsedge (Cyperus esculentus)'
+    WHEN char_code = 'DI' THEN 'Didymo (Didymosphenia geminata)'
+    WHEN char_code = 'KS' THEN 'Knapweed species (Centaurea spp.)'
+    WHEN char_code = 'SO' THEN 'Sowthistle species (Sonchus species)'
+    WHEN char_code = 'YH' THEN 'Yellow hawkweed (Hieracium pratense)'
+            ELSE invbc_name
+        END;
+
 
     drop view if exists iapp_site_summary_slow cascade;
     
