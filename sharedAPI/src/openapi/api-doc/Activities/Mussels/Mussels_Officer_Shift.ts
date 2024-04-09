747da15bedc82380b8b6c38c74a109671a789959
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-04-09 12:21:11 -0700

    stub Shift Overview section

---
+++ sharedAPI/src/openapi/api-doc/Activities/Mussels/Mussels_Officer_Shift.ts
@@ -0,0 +1,12 @@
+import { ShiftOverview } from "../../Components/Mussels_Officer_Shift_Sub_Forms";
+
+export const Activity_Officer_Shift = {
+  type: 'object',
+  properties: {
+    activity_data: {},
+    activity_type_data: {},
+    activity_subtype_data: {
+      ...ShiftOverview
+    }
+  }
+}
