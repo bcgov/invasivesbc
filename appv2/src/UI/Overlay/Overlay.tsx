7553d3ce0e3c395d892246e498dc2bf68e82d5fd
Robert Johnstone <rob@plasticviking.com>
2024-04-25 15:44:35 +0800

    change how button container margin is computed

--- appv2/src/UI/Overlay/Overlay.tsx
+++ appv2/src/UI/Overlay/Overlay.tsx
@@ -6,8 +6,8 @@ import { RENDER_DEBUG } from 'UI/App';
 const Overlay = (props) => {
   const ref = useRef(0);
   ref.current += 1;
-  if(RENDER_DEBUG)
-  console.log('%cOverlay render:' + ref.current.toString(), 'color: yellow');
+  if (RENDER_DEBUG)
+    console.log('%cOverlay render:' + ref.current.toString(), 'color: yellow');
 
   const panelOpen = useSelector((state: any) => state.AppMode.panelOpen);
   const fullScreen = useSelector((state: any) => state.AppMode?.panelFullScreen);
@@ -15,22 +15,22 @@ const Overlay = (props) => {
   useEffect(() => {
     if (!fullScreen) {
       const buttonContainer = document.getElementById('map-btn-container');
-      if(!panelOpen) {
+      if (!panelOpen) {
         if (buttonContainer) buttonContainer.style.marginBottom = 0 + 'vh';
       } else {
         const overlayDiv = document.getElementById('overlaydiv');
-        if (overlayDiv && buttonContainer) buttonContainer.style.marginBottom = overlayDiv.style.height;
+        if (overlayDiv && buttonContainer) buttonContainer.style.marginBottom = `${overlayDiv.clientHeight}px`;
       }
     }
   }, [panelOpen, fullScreen]);
 
   return (
     <div
-    id='overlaydiv'
+      id='overlaydiv'
       className={`map__overlay ${panelOpen && !fullScreen ? 'map__overlay--show' : ''} ${
         panelOpen && fullScreen ? 'map__overlay--show-fullscreen' : ''
       }`}>
-        <div className={`mapOverlayContents `}> {props.children}</div>
+      <div className={`mapOverlayContents `}> {props.children}</div>
     </div>
   );
 };
