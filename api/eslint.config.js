c2ae4afd37c273d40b1c653ee3ec249623031dd1
Robert Johnstone <rob@plasticviking.com>
2024-05-15 21:51:47 +0800

    add eslint import rules

--- api/eslint.config.js
+++ api/eslint.config.js
@@ -13,11 +13,42 @@
 //CJS version
 const eslint = require('@eslint/js');
 const tseslint = require('typescript-eslint');
-
 module.exports = tseslint.config(eslint.configs.recommended, ...tseslint.configs.recommended, {
+  plugins: {
+    import: require('eslint-plugin-import')
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx']
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true
+      }
+    }
+  },
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/ban-types': ['error', { types: { object: false, extendDefaults: true } }]
+    '@typescript-eslint/ban-types': ['error', { types: { object: false, extendDefaults: true } }],
+    'import/no-unresolved': 'error',
+    'import/no-duplicates': 'error',
+    'import/no-relative-packages': 'error',
+    'import/no-commonjs': 'error',
+    'import/exports-last': 'error',
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/order': 'error',
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: [
+          {
+            group: ['../*'],
+            message: 'No relative imports'
+          }
+        ]
+      }
+    ]
   },
   files: ['src/**/*.ts', 'src/**/*.js']
 });
