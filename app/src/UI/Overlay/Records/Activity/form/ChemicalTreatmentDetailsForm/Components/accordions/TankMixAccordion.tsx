37d3fe64890eaf8c2812cd59a188733793587c0e
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-07-12 12:28:53 -0700

    Remove stale commented code and unused import

--- app/src/UI/Overlay/Records/Activity/form/ChemicalTreatmentDetailsForm/Components/accordions/TankMixAccordion.tsx
+++ app/src/UI/Overlay/Records/Activity/form/ChemicalTreatmentDetailsForm/Components/accordions/TankMixAccordion.tsx
@@ -1,25 +1,23 @@
 import { Accordion, AccordionSummary, Typography, AccordionDetails, Box } from '@mui/material';
-import React, { useContext, useRef } from 'react';
+import { useContext, useRef } from 'react';
 import TankMix from '../single-objects/TankMix';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import { ChemicalTreatmentDetailsContext } from '../../ChemicalTreatmentDetailsContext';
 import { RENDER_DEBUG } from 'UI/App';
-//import { useFormStyles } from '../../formStyles';
 
 const TankMixAccordion = () => {
   const ref = useRef(0);
   ref.current += 1;
-  if (RENDER_DEBUG) console.log('%c TankMixAccordion render:' + ref.current.toString(), 'color: yellow');
+  if (RENDER_DEBUG) { console.log('%c TankMixAccordion render:' + ref.current.toString(), 'color: yellow'); }
+
   const formDataContext = useContext(ChemicalTreatmentDetailsContext);
   const { formDetails } = formDataContext;
 
-  //const classes = useFormStyles();
   const tankMixOn = formDetails.form_data.tank_mix;
   return (
     <Accordion expanded={tankMixOn} disabled={!tankMixOn}>
       <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="tank-mix-content" id="tank-mix-header">
         <Typography
-          // color={herbicidesArrErrors?.length > 0 ? 'error' : 'textPrimary'}
           style={{ width: '33%', flexShrink: 0 }}
           variant="h5"
         >
@@ -27,9 +25,7 @@ const TankMixAccordion = () => {
         </Typography>
       </AccordionSummary>
       <AccordionDetails>
-        <Box
-        //  className={classes.accordionBody}
-        >
+        <Box>
           <TankMix />
         </Box>
       </AccordionDetails>
