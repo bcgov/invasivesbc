0cd06640131a61819ad51896656d2f8b24d107e6
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-07-12 12:28:08 -0700

    Remove stale commented code and unused state hook

--- app/src/UI/Overlay/Records/Activity/form/ChemicalTreatmentDetailsForm/Components/accordions/InvasivePlantsAccordion.tsx
+++ app/src/UI/Overlay/Records/Activity/form/ChemicalTreatmentDetailsForm/Components/accordions/InvasivePlantsAccordion.tsx
@@ -1,64 +1,55 @@
-import React, { useContext, useRef, useState } from 'react';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { Accordion, AccordionSummary, Typography, AccordionDetails, Box, Button } from '@mui/material';
+import { useContext, useRef } from 'react';
+import { Typography, Button } from '@mui/material';
 import { ChemicalTreatmentDetailsContext } from '../../ChemicalTreatmentDetailsContext';
 import AddIcon from '@mui/icons-material/Add';
 import InvasivePlant from '../single-objects/InvasivePlant';
-//import { useFormStyles } from '../../formStyles';
-
-
 import '../../../../Form.css'
 import { RENDER_DEBUG } from 'UI/App';
 
 const InvasivePlantsAccordion = () => {
   const ref = useRef(0);
   ref.current += 1;
-  if(RENDER_DEBUG)
-  console.log('%c InvasivePlantsAccordion render:' + ref.current.toString(), 'color: yellow');
+  if (RENDER_DEBUG) { console.log('%c InvasivePlantsAccordion render:' + ref.current.toString(), 'color: yellow'); }
   const formDataContext = useContext(ChemicalTreatmentDetailsContext);
   const { formDetails, setFormDetails } = formDataContext;
-  const [accordionExpanded, setAccordionExpanded] = useState(true);
-  //const classes = useFormStyles();
 
   return (
     <div id="invasive_plants_section">
-        <Typography
-          style={{ width: '33%', flexShrink: 0 }}
-          // color={invasivePlantsArrErrors?.length > 0 ? 'error' : 'textPrimary'}
-          variant="h5">
-          Invasive Plants
-        </Typography>
+      <Typography
+        style={{ width: '33%', flexShrink: 0 }}
+        variant="h5">
+        Invasive Plants
+      </Typography>
 
-            <div id="invasive_plants_list">
-            {formDetails.form_data?.invasive_plants?.map((species, index) => {
-              return <InvasivePlant species={species} key={index} index={index} 
-             // classes={classes} 
-              />;
-            })}</div>
+      <div id="invasive_plants_list">
+        {formDetails.form_data?.invasive_plants?.map((species, index) => (
+          <InvasivePlant species={species} key={index} index={index} />
+        ))}
+      </div>
 
-            <Button
-              id="btn_add_invasive_plant"
-              disabled={formDetails.disabled}
-              onClick={() => {
-                setFormDetails((prevDetails) => {
-                  const newSpeciesArr = prevDetails?.form_data?.invasive_plants
-                    ? [...prevDetails?.form_data?.invasive_plants]
-                    : [];
-                  newSpeciesArr.push({ invasive_plant_code: null, index: newSpeciesArr.length });
-                  return {
-                    ...prevDetails,
-                    form_data: {
-                      ...prevDetails.form_data,
-                      invasive_plants: newSpeciesArr
-                    }
-                  };
-                });
-              }}
-              variant="contained"
-              startIcon={<AddIcon />}
-              color="primary">
-              Add Invasive Plant
-            </Button>
+      <Button
+        id="btn_add_invasive_plant"
+        disabled={formDetails.disabled}
+        onClick={() => {
+          setFormDetails((prevDetails) => {
+            const newSpeciesArr = prevDetails?.form_data?.invasive_plants
+              ? [...prevDetails?.form_data?.invasive_plants]
+              : [];
+            newSpeciesArr.push({ invasive_plant_code: null, index: newSpeciesArr.length });
+            return {
+              ...prevDetails,
+              form_data: {
+                ...prevDetails.form_data,
+                invasive_plants: newSpeciesArr
+              }
+            };
+          });
+        }}
+        variant="contained"
+        startIcon={<AddIcon />}
+        color="primary">
+        Add Invasive Plant
+      </Button>
     </div>
   );
 };
