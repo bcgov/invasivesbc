f2ff612407a8e64ce85e82d6a7561337a7b80c2c
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-07-23 09:46:00 -0700

    Add util for checking roles from authContext

---
+++ api/src/utils/isAdminFromAuthContext.ts
@@ -0,0 +1,16 @@
+type UserRole = {
+  role_id: number,
+  role_name: string,
+  role_description: string
+}
+/**
+ * @desc Check users token for auth credential
+ * @param req User Request 
+ * @param authRoles List of role_name suitable for given check
+ * @returns user has access rights
+ */
+const isAdminFromAuthContext = (req: any, authRoles: string[] = ['master_administrator']): boolean => {
+  return req?.authContext?.roles.some((role: UserRole) => authRoles.includes(role.role_name));
+};
+
+export default isAdminFromAuthContext;
