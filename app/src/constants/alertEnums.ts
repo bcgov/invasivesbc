3246e7c8c6b9d1056dae4271cfcfd9d0826f8a1a
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-07-24 07:37:04 -0700

    Create enums for alerts

---
+++ app/src/constants/alertEnums.ts
@@ -0,0 +1,12 @@
+export enum AlertSubjects {
+  Map = 'map',
+  Form = 'form',
+  Photo = 'photo',
+}
+
+export enum AlertSeverity {
+  Error = 'error',
+  Info = 'info',
+  Success = 'success',
+  Warning = 'warning',
+}
