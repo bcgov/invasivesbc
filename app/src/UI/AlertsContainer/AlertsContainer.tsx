70f9d8bbc3a018192131efe8fcfa40195ce9b9d9
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-07-25 11:55:12 -0700

    add clear all button to alerts

--- app/src/UI/AlertsContainer/AlertsContainer.tsx
+++ app/src/UI/AlertsContainer/AlertsContainer.tsx
@@ -1,9 +1,9 @@
-import { Alert, AlertTitle, Icon } from "@mui/material";
+import { Alert, AlertTitle, Button, Icon } from "@mui/material";
 import { MapOutlined, Assignment, InsertPhoto } from "@mui/icons-material";
 import { useDispatch, useSelector } from "react-redux";
 import './AlertsContainer.css';
 import AlertMessage from "interfaces/AlertMessage";
-import { CLEAR_ALERT } from "state/actions";
+import { CLEAR_ALERT, CLEAR_ALERTS } from "state/actions";
 import { AlertSubjects } from "constants/alertEnums";
 
 const AlertsContainer = () => {
@@ -26,10 +26,23 @@ const AlertsContainer = () => {
       payload: { id }
     })
   }
+  const handleClearAll = () => dispatch({ type: CLEAR_ALERTS })
   const alerts = useSelector((state: any) => state.AlertsAndPrompts.alerts || [])
 
   return (
     <div className="alertsContainer">
+      {
+        alerts.length > 0 &&
+        <Button
+          variant="contained"
+          onClick={handleClearAll}
+          size="small"
+          color="info"
+          sx={{ width: '125pt', marginLeft: 'auto' }}
+        >
+          Clear All Alerts
+        </Button>
+      }
       {alerts.map((alert: AlertMessage) => (
         <Alert
           key={alert.id}
