72b721547a334f74c916f0b51ab22435121ceae2
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-07-26 15:15:09 -0700

    added enum for GeoShapes

---
+++ app/src/constants/geoShapes.ts
@@ -0,0 +1,11 @@
+enum GeoShapes {
+  Point = "Point",
+  Line = "Line",
+  LineString = "LineString",
+  Polygon = "Polygon",
+  Multipoint = "Multipoint",
+  MultiLineString = "MultiLineString",
+  MultiPolygon = "MultiPolygon",
+}
+
+export default GeoShapes;
