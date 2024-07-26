63079568f81e8ee734b92fb5fc033727f2a72f9d
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-07-26 15:16:53 -0700

    remove null return, convert to use Enum

--- app/src/utils/geometryHelpers.ts
+++ app/src/utils/geometryHelpers.ts
@@ -1,6 +1,7 @@
 import area from '@turf/area';
 import centroid from '@turf/centroid';
 import * as turf from '@turf/helpers';
+import GeoShapes from 'constants/geoShapes';
 import { Feature } from 'geojson';
 
 /**
@@ -10,12 +11,12 @@ import { Feature } from 'geojson';
  */
 export function calculateGeometryArea(geometry: Feature[]) {
   //zero if linestring:
-  if(geometry?.[geometry?.length - 1]?.geometry?.type === 'LineString') {
+  if (geometry?.[geometry?.length - 1]?.geometry?.type === GeoShapes.LineString) {
     return 0;
   }
   let totalArea = 0;
 
-  if (!geometry || !geometry.length || geometry[geometry.length - 1].geometry.type === 'LineString') {
+  if (!geometry || !geometry.length || geometry[geometry.length - 1].geometry.type === GeoShapes.LineString) {
     return totalArea;
   }
 
@@ -32,15 +33,15 @@ export function calculateGeometryArea(geometry: Feature[]) {
 
     Otherwise, calculate the area of the polygon using turf
   */
-  if (geo.geometry.type === 'Point' && !geo.properties.hasOwnProperty('radius')) {
+  if (geo.geometry.type === GeoShapes.Point && !geo.properties.hasOwnProperty('radius')) {
     totalArea = 1;
-  } else if (geo.geometry.type === 'Point' && geo.properties.hasOwnProperty('radius')) {
+  } else if (geo.geometry.type === GeoShapes.Point && geo.properties.hasOwnProperty('radius')) {
     totalArea = Math.PI * Math.pow(geo.properties.radius, 2);
-  } else if (geo.geometry.type === 'Polygon') {
+  } else if (geo.geometry.type === GeoShapes.Polygon) {
     totalArea = area(turf.polygon(geo.geometry['coordinates']));
   }
 
-  return parseFloat(totalArea.toFixed(0));
+  return Math.floor(totalArea);
 }
 
 /**
@@ -61,25 +62,22 @@ export function calculateLatLng(geom: Feature[]) {
     Calculations based on business rules as to how anchor points need to be calculated
     for different geometry types
   */
-  if (geo.type === 'Point') {
+  if (geo.type === GeoShapes.Point) {
     latitude = geo.coordinates[1];
     longitude = firstCoord;
-  } else if (geo.type === 'LineString' && geo.coordinates.length > 1) {
+  } else if (geo.type === GeoShapes.LineString && geo.coordinates.length > 1) {
     latitude = firstCoord[1];
     longitude = firstCoord[0];
   } else if (geom[0]?.properties?.isRectangle) {
     latitude = firstCoord[0][1];
     longitude = firstCoord[0][0];
   } else {
-    if(!(geom?.[0]?.coordinates?.length > 1)) {
-      return null;
-    }
     const centerPoint = centroid(geom[0] as any) //center(turf.polygon(geo['coordinates'])).geometry;
     latitude = centerPoint.geometry.coordinates[1];
     longitude = centerPoint.geometry.coordinates[0];
   }
 
-  if(!latitude || !longitude) {
+  if (!latitude || !longitude) {
     return null;
   }
   const latlng = {
