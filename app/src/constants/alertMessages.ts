d908e9a5740f7fe0da6772314ef1cc2e17c90447
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-07-30 10:10:42 -0700

    move out WithinBC check to own util function, move defined Alerts to constants.

---
+++ app/src/constants/alertMessages.ts
@@ -0,0 +1,59 @@
+import AlertMessage from 'interfaces/AlertMessage';
+import { AlertSeverity, AlertSubjects } from './alertEnums';
+
+const mappingAlertMessages: Record<string, AlertMessage> = {
+  // Errors
+  notWithinBC: {
+    content: 'Activity is not within BC',
+    severity: AlertSeverity.Error,
+    subject: AlertSubjects.Map
+  },
+  noAreaCalculated: {
+    content: 'The calculated area of your shape is 0, please add more points',
+    severity: AlertSeverity.Error,
+    subject: AlertSubjects.Map
+  },
+  willContainIntersections: {
+    severity: AlertSeverity.Error,
+    subject: AlertSubjects.Map,
+    content: 'Closing this geometry will result in a shape intersection being formed. Please adjust appropriately.'
+  },
+  containsIntersections: {
+    severity: AlertSeverity.Error,
+    subject: AlertSubjects.Map,
+    content: 'Activity geometry intersects itself'
+  },
+  trackMyPathStoppedEarly: {
+    content: '"Track my path" stopped.',
+    severity: AlertSeverity.Info,
+    subject: AlertSubjects.Map
+  },
+
+  // Info
+  canEditInfo: {
+    content: 'You can adjust or add to your geometry manually by selecting the blue line, and moving points as needed',
+    severity: AlertSeverity.Info,
+    subject: AlertSubjects.Map
+  },
+
+  // Warnings
+  wellsInsideTreatmentArea: {
+    content: 'Warning! Wells inside treatment area',
+    severity: AlertSeverity.Warning,
+    subject: AlertSubjects.Map
+  },
+
+  // Success
+  trackingStarted: {
+    content: 'Start walking to draw a geometry. Click the button again to stop.',
+    severity: AlertSeverity.Info,
+    subject: AlertSubjects.Map
+  },
+  trackingStoppedSuccess: {
+    content: 'Tracking Completed Successfully',
+    severity: AlertSeverity.Success,
+    subject: AlertSubjects.Map
+  }
+};
+
+export default mappingAlertMessages;
