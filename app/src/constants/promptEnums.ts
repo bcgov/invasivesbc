a63d82bcb4497812f3d1ba5288ae9397de8203f5
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-08-09 15:39:12 -0700

    Doc link add

--- app/src/constants/promptEnums.ts
+++ app/src/constants/promptEnums.ts
@@ -1,6 +1,7 @@
 /**
  * @desc Enums for prompt system, used to classify prop types, ensuring correct modals are displayed
  * @summary Enums for user prompts modals
+ * @external {@link https://github.com/bcgov/invasivesbc/wiki/User-Prompt-System }
  */
 export enum PromptTypes {
   Confirmation,
