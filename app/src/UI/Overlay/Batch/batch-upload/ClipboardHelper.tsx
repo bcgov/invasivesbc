409bbea9c81d05dfa317fe29201acabf00b9ecfd
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-08-19 13:32:32 -0700

    Move Clipboard Helper to Alert

--- app/src/UI/Overlay/Batch/batch-upload/ClipboardHelper.tsx
+++ app/src/UI/Overlay/Batch/batch-upload/ClipboardHelper.tsx
@@ -1,20 +1,39 @@
 import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import { IconButton } from '@mui/material';
-import React from 'react';
+import { AlertSeverity, AlertSubjects } from 'constants/alertEnums';
+import { useDispatch } from 'react-redux';
+import { NEW_ALERT } from 'state/actions';
 
 export const CopyToClipboardButton = (props: { content: string }) => {
+  const dispatch = useDispatch();
+
+  const copyToClipboard = async ({ value }: any) => {
+    if (!navigator.clipboard) {
+      dispatch({
+        type: NEW_ALERT,
+        payload: {
+          content: 'No clipboard supported',
+          subject: AlertSubjects.Form,
+          severity: AlertSeverity.Error
+        }
+      });
+      return;
+    }
+    await navigator.clipboard.writeText(value);
+    dispatch({
+      type: NEW_ALERT,
+      payload: {
+        content: 'Copied to clipboard!',
+        subject: AlertSubjects.Form,
+        severity: AlertSeverity.Success,
+        autoClose: 3
+      }
+    });
+  };
+
   return (
-    <IconButton onClick={() => copyToClipboard({ value: props.content, message: 'Copied to clipboard!' })}>
-      <ContentCopyIcon></ContentCopyIcon>
+    <IconButton onClick={() => copyToClipboard({ value: props.content })}>
+      <ContentCopyIcon />
     </IconButton>
   );
-};
-
-export const copyToClipboard = async ({ message, value }: any) => {
-  if (!navigator.clipboard) {
-    console.error('No clipboard support');
-    return;
-  }
-  await navigator.clipboard.writeText(value);
-  alert(message);
 };
