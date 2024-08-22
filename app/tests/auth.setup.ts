eb0c3cb4f77231e0d35b469995ce8fcdac490abc
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-08-22 16:08:33 -0700

    add test to check for env in setup

--- app/tests/auth.setup.ts
+++ app/tests/auth.setup.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
-import { test as setup } from '@playwright/test';
+import { test as setup, expect } from '@playwright/test';
+
 const authFile = 'playwright/.auth/user.json';
 
 dotenv.config();
@@ -7,9 +8,10 @@ dotenv.config();
 const TEST_USER = process.env.TEST_USER;
 const TEST_USER_PASS = process.env.TEST_USER_PASS;
 
-if (!TEST_USER || !TEST_USER_PASS) {
-  throw new Error('Missing ENVs, check testUser or testUserPass');
-}
+setup("Test ENV's present", async () => {
+  expect(TEST_USER).toBeTruthy();
+  expect(TEST_USER_PASS).toBeTruthy();
+});
 
 setup('authenticate', async ({ page }) => {
   await page.goto('http://localhost:3000/home/landing');
@@ -20,9 +22,9 @@ setup('authenticate', async ({ page }) => {
     .click();
   await page.getByRole('menuitem', { name: 'Login' }).click();
   await page.getByRole('link', { name: 'IDIR' }).click();
-  await page.locator('#user').fill(TEST_USER);
+  await page.locator('#user').fill(TEST_USER!);
   await page.locator('#user').press('Tab');
-  await page.getByLabel('Password').fill(TEST_USER_PASS);
+  await page.getByLabel('Password').fill(TEST_USER_PASS!);
   await page.getByLabel('Password').press('Enter');
 
   await page.context().storageState({ path: authFile });
