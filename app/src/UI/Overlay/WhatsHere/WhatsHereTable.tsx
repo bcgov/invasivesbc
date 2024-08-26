8b05281b2cdf6a495dda733920d3f48667a1f6d9
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-08-26 15:57:20 -0700

    tidy rendering logic

--- app/src/UI/Overlay/WhatsHere/WhatsHereTable.tsx
+++ app/src/UI/Overlay/WhatsHere/WhatsHereTable.tsx
@@ -1,27 +1,27 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import center from '@turf/center';
-
-import { useSelector } from 'utils/use_selector';
-import { calc_utm } from 'utils/utm';
-import { Button, Grid, Tab, TableContainer, Tabs } from '@mui/material';
+import { Button, Divider, Grid, Tab, TableContainer, Tabs } from '@mui/material';
 import { MAP_SET_WHATS_HERE_SECTION } from 'state/actions';
-
-import './WhatsHereTable.css';
-
 import AdjustIcon from '@mui/icons-material/Adjust';
 import FolderIcon from '@mui/icons-material/Folder';
 import { useHistory } from 'react-router';
+
 import { OverlayHeader } from '../OverlayHeader';
 import { RenderTableActivity } from './Subcomponents/RenderTableActivity';
 import RenderTablePOI from './Subcomponents/RenderTablePOI';
+import { useSelector } from 'utils/use_selector';
+import { calc_utm } from 'utils/utm';
+import './WhatsHereTable.css';
 
 export const createDataUTM = (name: string, value: any) => {
   return { name, value };
 };
 
-export const WhatsHereTable = (props) => {
-  const whatsHere = useSelector((state: any) => state.Map?.whatsHere);
+export const WhatsHereTable = () => {
+  const dispatch = useDispatch();
+
+  const whatsHere = useSelector((state) => state.Map?.whatsHere);
   const history = useHistory();
 
   const position = whatsHere?.feature?.geometry ? center(whatsHere?.feature?.geometry)?.geometry.coordinates : [0, 0];
@@ -31,7 +31,6 @@ export const WhatsHereTable = (props) => {
     createDataUTM('Easting', utmResult[1]),
     createDataUTM('Northing', utmResult[2])
   ];
-  const dispatch = useDispatch();
 
   const handleChange = (_event: React.ChangeEvent<{}>, newSection: string) => {
     dispatch({
@@ -51,17 +50,13 @@ export const WhatsHereTable = (props) => {
     }
   };
 
-  const getActivityDescriptionForOpenButton = () => {
-    return `Open InvasivesBC record: ${whatsHere?.clickedActivityDescription}`;
-  };
-  const getIAPPDescriptionForOpenButton = () => {
-    return `Open IAPP record: ${whatsHere?.clickedIAPPDescription}`;
-  };
+  const getActivityDescriptionForOpenButton = () => `Open InvasivesBC record: ${whatsHere?.clickedActivityDescription}`;
+  const getIAPPDescriptionForOpenButton = () => `Open IAPP record: ${whatsHere?.clickedIAPPDescription}`;
 
   return (
     <div className="whatshere-container">
       <OverlayHeader />
-      {whatsHere?.section ? (
+      {whatsHere?.section && (
         <div className="whatshere-table-container">
           <div className="whatshere_back_button">
             <Button onClick={() => history.goBack()} variant="contained">
@@ -69,9 +64,8 @@ export const WhatsHereTable = (props) => {
             </Button>
           </div>
           <div id="whatsherepopup" className="whatshere-table">
-            <Grid className="whatshere-header" container justifyContent="center" sx={{ mb: 2, pb: 1 }}>
+            <Grid className="whatshere-header" container justifyContent="center" sx={{ mb: 2 }}>
               <div className="whatshere-title">
-                {' '}
                 What's Here: <br /> {`UTM: Z-${utmRows[0]?.value} E-${utmRows[1]?.value} N-${utmRows[2]?.value}`}
               </div>
               <Tabs value={whatsHere?.section} onChange={handleChange} centered>
@@ -80,33 +74,26 @@ export const WhatsHereTable = (props) => {
               </Tabs>
             </Grid>
             <Grid container spacing={2} justifyContent="center">
-              <Grid item>
-                {whatsHere?.section === 'invasivesbc' && whatsHere?.clickedActivity ? (
+              <Grid item sx={{ mb: 1 }}>
+                {whatsHere?.section === 'invasivesbc' && whatsHere?.clickedActivity && (
                   <Button variant="contained" onClick={goToRecord}>
                     {getActivityDescriptionForOpenButton()}
                   </Button>
-                ) : (
-                  <></>
                 )}
-                {whatsHere?.section === 'iapp' && whatsHere?.clickedIAPP ? (
+                {whatsHere?.section === 'iapp' && whatsHere?.clickedIAPP && (
                   <Button variant="contained" onClick={goToRecord}>
                     {getIAPPDescriptionForOpenButton()}
                   </Button>
-                ) : (
-                  <></>
                 )}
               </Grid>
             </Grid>
 
             <TableContainer className="whatshere-position">
-              <RenderTableActivity />
-              {/*section == 'databc' && <RenderTableDataBC rows={databc} />*/}
-              <RenderTablePOI />
+              {whatsHere?.section === 'invasivesbc' && <RenderTableActivity />}
+              {whatsHere?.section === 'iapp' && <RenderTablePOI />}
             </TableContainer>
           </div>
         </div>
-      ) : (
-        <></>
       )}
     </div>
   );
