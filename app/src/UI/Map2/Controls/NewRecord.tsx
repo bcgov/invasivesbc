a1f4c26e004a45abaa4d14473581764bb0a3bc1a
Robert Johnstone <rob@plasticviking.com>
2024-08-30 18:41:43 +0800

    fix missing newrecord button when working offline

--- app/src/UI/Map2/Controls/NewRecord.tsx
+++ app/src/UI/Map2/Controls/NewRecord.tsx
@@ -1,43 +1,37 @@
 import React, { useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { IconButton, Tooltip } from '@mui/material';
-import { useSelector } from 'utils/use_selector';
 import FiberNewIcon from '@mui/icons-material/FiberNew';
 import { OPEN_NEW_RECORD_MENU } from 'state/actions';
 import 'UI/Global.css';
 
-export const NewRecord = (props) => {
+export const NewRecord = () => {
   const dispatch = useDispatch();
   const divRef = useRef();
-  const isAuth = useSelector((state: any) => state.Auth?.authenticated);
 
   const [show, setShow] = React.useState(false);
 
-  if (isAuth) {
-    return (
-      <div ref={divRef} className="map-btn">
-        <Tooltip
-          open={show}
-          classes={{ tooltip: 'toolTip' }}
-          onMouseEnter={() => setShow(true)}
-          onMouseLeave={() => setShow(false)}
-          title={`New Record`}
-          placement="top-end"
-        >
-          <span>
-            <IconButton
-              className={'button'}
-              onClick={() => {
-                dispatch({ type: OPEN_NEW_RECORD_MENU });
-              }}
-            >
-              <FiberNewIcon />
-            </IconButton>
-          </span>
-        </Tooltip>
-      </div>
-    );
-  } else {
-    return <></>;
-  }
+  return (
+    <div ref={divRef} className="map-btn">
+      <Tooltip
+        open={show}
+        classes={{ tooltip: 'toolTip' }}
+        onMouseEnter={() => setShow(true)}
+        onMouseLeave={() => setShow(false)}
+        title={`New Record`}
+        placement="top-end"
+      >
+        <span>
+          <IconButton
+            className={'button'}
+            onClick={() => {
+              dispatch({ type: OPEN_NEW_RECORD_MENU });
+            }}
+          >
+            <FiberNewIcon />
+          </IconButton>
+        </span>
+      </Tooltip>
+    </div>
+  );
 };
