1015f83a31620ac4d2f30487183eb66ff39febcf
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-09-11 08:25:43 -0700

    Delete biological_agent_code when invasive_plant_code updates

--- app/src/state/sagas/activity/dataAccess.ts
+++ app/src/state/sagas/activity/dataAccess.ts
@@ -386,6 +386,8 @@ export function* handle_ACTIVITY_ON_FORM_CHANGE_REQUEST(action) {
 
       // Fire handlers to filter the agents list based on the selected plant code, only fire when value has changed
       if (plantCode && plantCode !== prevPlantCode) {
+        // Reset the biological_agent_code, since list has been updated
+        delete updatedFormData.activity_subtype_data.Biocontrol_Collection_Information[0].biological_agent_code;
         yield put({ type: ACTIVITY_GET_SUGGESTED_BIOCONTROL_AGENTS, payload: { plantCode } });
       }
       //auto fills total release quantity (only on biocontrol release activity)
