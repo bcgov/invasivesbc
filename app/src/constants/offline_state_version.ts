1bc5f371e9065e5b8c0117d34e88ce33a8cb9f7a
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-09-23 12:41:04 -0700

    Add new map layer to defaults, sort

--- app/src/constants/offline_state_version.ts
+++ app/src/constants/offline_state_version.ts
@@ -1,5 +1,5 @@
 /*
 handle purging the offline storage on app version upgrade
  */
-export const CURRENT_MIGRATION_VERSION = 20240907;
+export const CURRENT_MIGRATION_VERSION = 20240908;
 export const MIGRATION_VERSION_KEY = '_persistedMigrationVersion';
