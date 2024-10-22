9940cae0d751fb2fe79a8c70ee8f657120b09870
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-10-22 10:45:00 -0700

    Create stateful tooltip component

---
+++ app/src/UI/TooltipWithIcon/TooltipWithIcon.tsx
@@ -0,0 +1,26 @@
+import { HelpOutline } from '@mui/icons-material';
+import { Tooltip } from '@mui/material';
+import { useState } from 'react';
+
+type PropTypes = {
+  tooltipText: string;
+};
+const TooltipWithIcon = ({ tooltipText }: PropTypes) => {
+  const [showTooltip, setShowTooltip] = useState<boolean>(false);
+  return (
+    <Tooltip
+      open={showTooltip}
+      classes={{ tooltip: 'toolTip' }}
+      onMouseEnter={setShowTooltip.bind(this, true)}
+      onMouseLeave={setShowTooltip.bind(this, false)}
+      onFocus={setShowTooltip.bind(this, true)}
+      onBlur={setShowTooltip.bind(this, false)}
+      title={tooltipText}
+      placement="top-end"
+    >
+      <HelpOutline />
+    </Tooltip>
+  );
+};
+
+export default TooltipWithIcon;
