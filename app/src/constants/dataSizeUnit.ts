55970d854a3ce49bfbc32909eaf802ac213f6f92
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-10-22 14:48:22 -0700

    Create DataSizeUnits Enum

---
+++ app/src/constants/dataSizeUnit.ts
@@ -0,0 +1,15 @@
+enum DataSizeUnits {
+  Bytes = 'B',
+  Kilobytes = 'KB',
+  Kibibytes = 'KiB',
+  Megabytes = 'MB',
+  Mebibytes = 'MiB',
+  Gigabytes = 'GB',
+  Gibibytes = 'GiB',
+  Terabytes = 'TB',
+  Tebibytes = 'TiB',
+  Petabytes = 'PB',
+  Pebibytes = 'PiB'
+}
+
+export default DataSizeUnits;
