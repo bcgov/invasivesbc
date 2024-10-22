39826d0e02d7822b6d46d00b330049ddab83946a
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-10-22 11:26:56 -0700

    Start of Cache Panel UI enhancement

--- app/src/UI/Overlay/TileCache/TileCachePanel.tsx
+++ app/src/UI/Overlay/TileCache/TileCachePanel.tsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import { useDispatch } from 'utils/use_selector';
 import TileCache from 'state/actions/cache/TileCache';
 import { OverlayHeader } from 'UI/Overlay/OverlayHeader';
 import { TileCacheCreationPanel } from 'UI/Overlay/TileCache/TileCacheCreationPanel';
 import { TileCacheList } from 'UI/Overlay/TileCache/TileCacheList';
 import { TileCacheDownloadProgress } from 'UI/Overlay/TileCache/TileCacheDownloadProgress';
+import './tileCache.css';
 
 const TileCachePanel = () => {
   const dispatch = useDispatch();
@@ -26,9 +27,16 @@ const TileCachePanel = () => {
       <OverlayHeader />
       <div id={`offline-map-overlay`}>
         <h2>Offline Map</h2>
-
+        <p className="subheader">Manage your map data for offline access</p>
+        <h3>Create Cached Maps</h3>
         <TileCacheCreationPanel />
+        <p>
+          <i>TileCacheDownloadProgress</i>
+        </p>
         <TileCacheDownloadProgress />
+        <p>
+          <i>TileCacheList</i>
+        </p>
         <TileCacheList />
       </div>
     </>
