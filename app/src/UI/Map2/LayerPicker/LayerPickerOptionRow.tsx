2e3399562314519706abbaca74603e761a444264
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-10-23 14:45:13 -0700

    Restore Original functionality, remove commented code

--- app/src/UI/Map2/LayerPicker/LayerPickerOptionRow.tsx
+++ app/src/UI/Map2/LayerPicker/LayerPickerOptionRow.tsx
@@ -8,13 +8,21 @@ type Layer = {
 type PropTypes = {
   onClick: (val: Layer) => void;
   layer: Layer;
+  lastEntry: boolean;
 };
-const LayerPickerItem = ({ onClick, layer }: PropTypes) => {
+const LayerPickerItem = ({ onClick, layer, lastEntry }: PropTypes) => {
   return (
-    <li className="lp-layers-item">
-      <button onClick={() => onClick(layer)}>{layer?.toggle ? <Visibility /> : <VisibilityOff />}</button>
-      <p>{layer.title}</p>
-    </li>
+    <>
+      <li className="lp-layers-item">
+        <button onClick={() => onClick(layer)}>{layer?.toggle ? <Visibility /> : <VisibilityOff />}</button>
+        <p>{layer.title ?? 'Layer name is null'}</p>
+      </li>
+      {!lastEntry && (
+        <li>
+          <hr />
+        </li>
+      )}
+    </>
   );
 };
 export default LayerPickerItem;
