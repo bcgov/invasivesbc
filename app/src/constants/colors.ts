6c87915c015e567738a7e8727fd2a695b3f5c7ce
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-10-24 13:12:23 -0700

    Lower font size, move colours to constants

--- app/src/constants/colors.ts
+++ app/src/constants/colors.ts
@@ -1,3 +1,16 @@
 export const bcBlue = '#003366';
 export const bcYellow = '#FCBA19';
 export const black = '#16161D';
+
+export const RECORD_COLOURS = [
+  '#FFFFFF',
+  '#2A81CB',
+  '#FFD326',
+  '#CB2B3E',
+  '#2AAD27',
+  '#CB8427',
+  '#CAC428',
+  '#9C2BCB',
+  '#7B7B7B',
+  '#3D3D3D'
+];
