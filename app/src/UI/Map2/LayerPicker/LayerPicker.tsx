611b835d064b264f7640207b4a827a888f0ea1f7
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-10-28 13:10:44 -0700

    Add and implement Accordion in LayerPicker

--- app/src/UI/Map2/LayerPicker/LayerPicker.tsx
+++ app/src/UI/Map2/LayerPicker/LayerPicker.tsx
@@ -2,15 +2,16 @@ import { useSelector } from 'react-redux';
 import { MOBILE } from 'state/build-time-config';
 import LayersIcon from '@mui/icons-material/Layers';
 import CloseIcon from '@mui/icons-material/Close';
-import { IconButton } from '@mui/material';
+import { IconButton, Switch } from '@mui/material';
 import './LayerPicker.css';
 import { useState } from 'react';
 import LpModules from 'constants/LpModules';
 import LayerPickerPathOption from './LayerPickerPathRow';
-import { ArrowBackIos } from '@mui/icons-material';
+import { ArrowBackIos, Expand, Menu } from '@mui/icons-material';
 import LpLayers from './LpLayers/LpLayers';
 import LpRecordSet from './LpRecordSet/LpRecordSet';
 import LpOfflineMaps from './LpOfflineMaps/LpOfflineMaps';
+import Accordion from 'UI/Accordion/Accordion';
 
 export const LayerPicker = () => {
   const closeLayerPicker = () => {
@@ -18,7 +19,7 @@ export const LayerPicker = () => {
     setPickerPath(LpModules.Init);
   };
   const [pickerPath, setPickerPath] = useState<LpModules>(LpModules.Init);
-  const [showAsAccordion] = useState<boolean>(false);
+  const [showAsAccordion, setShowAsAccordion] = useState<boolean>(false);
   const [showLayerPicker, setShowLayerPicker] = useState<boolean>(false);
   const isAuth = useSelector((state: any) => state.Auth?.authenticated);
 
@@ -35,48 +36,70 @@ export const LayerPicker = () => {
   return (
     <div id="layer-picker-container" className="layer-picker-pos">
       <div>
-        {pickerPath !== LpModules.Init && (
-          <div className="layer-context">
-            <IconButton onClick={() => setPickerPath(LpModules.Init)}>
-              <ArrowBackIos />
-            </IconButton>
-            {pickerPath}
-          </div>
-        )}
+        <div className="layer-context">
+          {pickerPath !== LpModules.Init ? (
+            <>
+              <IconButton onClick={() => setPickerPath(LpModules.Init)}>
+                <ArrowBackIos />
+              </IconButton>
+              {pickerPath}
+            </>
+          ) : (
+            <>
+              <Menu />
+              <Switch checked={showAsAccordion} onChange={() => setShowAsAccordion((prev) => !prev)} />
+              <Expand />
+            </>
+          )}
+        </div>
         <IconButton onClick={closeLayerPicker}>
           <CloseIcon />
         </IconButton>
       </div>
-      {
+      <div className="lp-content">
         {
-          [LpModules.Init]: (
-            <>
-              {showAsAccordion ? (
-                <></>
-              ) : (
-                <ul className="path-ul">
-                  <LayerPickerPathOption clickHandler={setPickerPath} pathVal={LpModules.DataBcLayers} />
-                  <li>
-                    <hr />
-                  </li>
-                  {MOBILE && (
-                    <>
-                      <LayerPickerPathOption clickHandler={setPickerPath} pathVal={LpModules.MapTiles} />
-                      <li>
-                        <hr />
-                      </li>
-                    </>
-                  )}
-                  <LayerPickerPathOption clickHandler={setPickerPath} pathVal={LpModules.Recordsets} />
-                </ul>
-              )}
-            </>
-          ),
-          [LpModules.DataBcLayers]: <LpLayers />,
-          [LpModules.Recordsets]: <LpRecordSet closePicker={closeLayerPicker} />,
-          [LpModules.MapTiles]: <LpOfflineMaps closePicker={closeLayerPicker} />
-        }[pickerPath]
-      }
+          {
+            [LpModules.Init]: (
+              <>
+                {showAsAccordion ? (
+                  <>
+                    <Accordion icon={'map'} title={LpModules.DataBcLayers}>
+                      <LpLayers />
+                    </Accordion>
+                    <Accordion icon={'save'} title={LpModules.Recordsets}>
+                      <LpRecordSet closePicker={closeLayerPicker} />
+                    </Accordion>
+                    {MOBILE && (
+                      <Accordion icon="manage_search" title={LpModules.MapTiles}>
+                        <LpOfflineMaps closePicker={closeLayerPicker} />
+                      </Accordion>
+                    )}
+                  </>
+                ) : (
+                  <ul className="path-ul">
+                    <LayerPickerPathOption clickHandler={setPickerPath} pathVal={LpModules.DataBcLayers} />
+                    <li>
+                      <hr />
+                    </li>
+                    {MOBILE && (
+                      <>
+                        <LayerPickerPathOption clickHandler={setPickerPath} pathVal={LpModules.MapTiles} />
+                        <li>
+                          <hr />
+                        </li>
+                      </>
+                    )}
+                    <LayerPickerPathOption clickHandler={setPickerPath} pathVal={LpModules.Recordsets} />
+                  </ul>
+                )}
+              </>
+            ),
+            [LpModules.DataBcLayers]: <LpLayers />,
+            [LpModules.Recordsets]: <LpRecordSet closePicker={closeLayerPicker} />,
+            [LpModules.MapTiles]: <LpOfflineMaps closePicker={closeLayerPicker} />
+          }[pickerPath]
+        }
+      </div>
     </div>
   );
 };
