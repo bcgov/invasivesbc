cbbc3292cf7f6de6a90188f43fe88e0a7548eb12
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-10-28 10:29:01 -0700

    convert map_toggle_overlay to action class, remove cached tiles from PrimaryLayerSelect.tsx

--- app/src/UI/Map2/Controls/PrimaryLayerSelect.tsx
+++ app/src/UI/Map2/Controls/PrimaryLayerSelect.tsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { IconButton, Tooltip } from '@mui/material';
 import { useSelector } from 'utils/use_selector';
-import { MAP_CHOOSE_BASEMAP, MAP_TOGGLE_OVERLAY } from 'state/actions';
+import { MAP_CHOOSE_BASEMAP } from 'state/actions';
 import 'UI/Global.css';
 import { MAP_DEFINITIONS } from 'UI/Map2/helpers/layer-definitions';
 import { DeviceUnknown, Hd, Landscape, Map, SaveAlt, Sd, SignalCellularNodata } from '@mui/icons-material';
@@ -10,10 +10,8 @@ import { DeviceUnknown, Hd, Landscape, Map, SaveAlt, Sd, SignalCellularNodata } 
 export const PrimaryLayerSelect = () => {
   const dispatch = useDispatch();
 
-  const { baseMapLayer, availableBaseMapLayers, enabledOverlayLayers, availableOverlayLayers } = useSelector(
-    (state) => state.Map
-  );
-  const offlineDefinitions = useSelector((state) => state.TileCache?.mapSpecifications) || [];
+  const { baseMapLayer, availableBaseMapLayers } = useSelector((state) => state.Map);
+  const offlineDefinitions = useSelector((state) => state.TileCache?.mapSpecifications) ?? [];
 
   const [toolTip, setToolTip] = useState('');
 
@@ -63,35 +61,6 @@ export const PrimaryLayerSelect = () => {
                 className={'basemap-btn'}
                 onClick={() => {
                   dispatch({ type: MAP_CHOOSE_BASEMAP, payload: l });
-                }}
-              >
-                {renderIcon(found)}
-              </IconButton>
-            </Tooltip>
-          </div>
-        );
-      })}
-
-      {availableOverlayLayers.map((l) => {
-        const found = [...MAP_DEFINITIONS, ...offlineDefinitions].find((d) => d.name == l);
-        if (!found) {
-          return;
-        }
-
-        return (
-          <div className={enabledOverlayLayers.includes(l) ? 'selected' : ''} key={l}>
-            <Tooltip
-              open={toolTip == l}
-              onMouseEnter={() => setToolTip(l)}
-              onMouseLeave={() => setToolTip('')}
-              classes={{ tooltip: 'toolTip' }}
-              title={found.tooltip}
-              placement="top-end"
-            >
-              <IconButton
-                className={'basemap-btn'}
-                onClick={() => {
-                  dispatch({ type: MAP_TOGGLE_OVERLAY, payload: l });
                 }}
               >
                 {renderIcon(found)}
