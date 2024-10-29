c92b17a6cff7894ba15573e279de2be34bb792e1
Robert Johnstone <rob@plasticviking.com>
2024-10-29 11:08:47 +0800

    Revert "persist LP accordion state, adjust CSS"
    This reverts commit 1ee3944ec9a3bb9f8ea2096401880498cc6b2f43.

--- app/src/UI/Accordion/Accordion.tsx
+++ app/src/UI/Accordion/Accordion.tsx
@@ -3,18 +3,11 @@ import './Accordion.css';
 import { Icon } from '@mui/material';
 import { ExpandLess, ExpandMore } from '@mui/icons-material';
 
-/**
- * @desc Common Accordion Component
- * @property {string} title MUI font icon name
- * @external {@link https://mui.com/material-ui/icons/#icon-font-icons}
- * @property {ReactNode} children Accordion Contents
- * @property {string} title Title text for Accordion
- */
-interface PropTypes {
+type PropTypes = {
   title: string;
   children: ReactNode;
   icon?: string;
-}
+};
 const Accordion = ({ title, children, icon }: PropTypes) => {
   const [open, setOpen] = useState<boolean>(false);
 
--- app/src/UI/Map2/LayerPicker/LayerPicker.tsx
+++ app/src/UI/Map2/LayerPicker/LayerPicker.tsx
@@ -1,3 +1,4 @@
+import { useSelector } from 'react-redux';
 import { MOBILE } from 'state/build-time-config';
 import LayersIcon from '@mui/icons-material/Layers';
 import CloseIcon from '@mui/icons-material/Close';
@@ -11,20 +12,16 @@ import LpLayers from './LpLayers/LpLayers';
 import LpRecordSet from './LpRecordSet/LpRecordSet';
 import LpOfflineMaps from './LpOfflineMaps/LpOfflineMaps';
 import Accordion from 'UI/Accordion/Accordion';
-import { useDispatch, useSelector } from 'utils/use_selector';
-import UserSettings from 'state/actions/userSettings/UserSettings';
 
 export const LayerPicker = () => {
   const closeLayerPicker = () => {
     setShowLayerPicker(false);
     setPickerPath(LpModules.Init);
   };
-  const toggleLayerPickerAccordion = () => dispatch(UserSettings.toggleLayerPickerAccordion());
   const [pickerPath, setPickerPath] = useState<LpModules>(LpModules.Init);
+  const [showAsAccordion, setShowAsAccordion] = useState<boolean>(false);
   const [showLayerPicker, setShowLayerPicker] = useState<boolean>(false);
-  const isAuth = useSelector((state) => state.Auth.authenticated);
-  const accordionMode = useSelector((state) => state.UserSettings.layerPickerIsAccordion);
-  const dispatch = useDispatch();
+  const isAuth = useSelector((state: any) => state.Auth?.authenticated);
 
   if (!isAuth) {
     return;
@@ -50,7 +47,7 @@ export const LayerPicker = () => {
           ) : (
             <>
               <Menu />
-              <Switch checked={accordionMode} onChange={toggleLayerPickerAccordion} />
+              <Switch checked={showAsAccordion} onChange={() => setShowAsAccordion((prev) => !prev)} />
               <Expand />
             </>
           )}
@@ -64,7 +61,7 @@ export const LayerPicker = () => {
           {
             [LpModules.Init]: (
               <>
-                {accordionMode ? (
+                {showAsAccordion ? (
                   <>
                     <Accordion icon={'map'} title={LpModules.DataBcLayers}>
                       <LpLayers />
