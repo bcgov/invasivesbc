a7ff25fb327f92bc2ba3318d3217d54b8c3ccbd6
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-10-29 14:42:41 -0700

    Add alerts for Network status

--- app/src/constants/alertMessages.ts
+++ app/src/constants/alertMessages.ts
@@ -102,4 +102,18 @@ const mappingAlertMessages: Record<string, AlertMessage> = {
   }
 };
 
+export const networkAlertMessages: Record<string, AlertMessage> = {
+  userWentOffline: {
+    content: 'You are now offline, Some functionality may be limited',
+    severity: AlertSeverity.Warning,
+    subject: AlertSubjects.Network,
+    autoClose: 6
+  },
+  userWentOnline: {
+    content: 'You are back online. Functionality is restored',
+    severity: AlertSeverity.Info,
+    subject: AlertSubjects.Network,
+    autoClose: 6
+  }
+};
 export default mappingAlertMessages;
