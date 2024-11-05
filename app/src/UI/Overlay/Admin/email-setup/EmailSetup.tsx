a75b3cde751405933e7a695abd706a1fd1d97ebf
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-11-05 14:48:20 -0800

    Add CSS to Email Setup, mobile responsive

--- app/src/UI/Overlay/Admin/email-setup/EmailSetup.tsx
+++ app/src/UI/Overlay/Admin/email-setup/EmailSetup.tsx
@@ -1,16 +1,13 @@
-import { Container, Grid } from '@mui/material';
-import React from 'react';
 import EmailSettings from './EmailSettings';
 import EmailTemplates from './EmailTemplates';
+import './Email.css';
 
-const EmailSetup = (props) => {
+const EmailSetup = () => {
   return (
-    <Container style={{ padding: '2rem' }}>
-      <Grid container spacing={4}>
-        <EmailSettings />
-        <EmailTemplates />
-      </Grid>
-    </Container>
+    <section id="email-setup">
+      <EmailSettings />
+      <EmailTemplates />
+    </section>
   );
 };
 export default EmailSetup;
