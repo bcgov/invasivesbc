f6fafdaae068dc7d73f6097ce7ac361d1cd95bbe
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-11-06 11:33:44 -0800

    redesign Map Legends, mobile friendly

---
+++ app/src/UI/Overlay/Legend/LayerSourcesTable.tsx
@@ -0,0 +1,26 @@
+import mapLayers from './mapLayers';
+
+const LayerSourcesTable = () => {
+  return (
+    <div id="layer-source-table">
+      <table>
+        <thead>
+          <tr>
+            <th>Layer Picker Label</th>
+            <th>Object Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {mapLayers.map((layer) => (
+            <tr>
+              <td>{layer.layerPickerLabel}</td>
+              <td>{layer.objectName}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default LayerSourcesTable;
