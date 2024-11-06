87c98d498369240c8351b2e3f97fa5b8198aab74
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-11-06 09:12:05 -0800

    OfflineDataSync mobile friendly

--- app/src/UI/OfflineDataSync/OfflineDataSyncDialog.tsx
+++ app/src/UI/OfflineDataSync/OfflineDataSyncDialog.tsx
@@ -1,30 +1,27 @@
-import React from 'react';
-import { Dialog, DialogContent, DialogTitle } from '@mui/material';
+import { Dialog, DialogTitle, IconButton } from '@mui/material';
 import { selectOfflineActivity } from 'state/reducers/offlineActivity';
 import { useSelector } from 'utils/use_selector';
 import { useDispatch } from 'react-redux';
 import { ACTIVITY_OFFLINE_SYNC_DIALOG_SET_STATE } from 'state/actions';
 import { OfflineDataSyncTable } from 'UI/OfflineDataSync/OfflineDataSyncTable';
-
+import { Close } from '@mui/icons-material';
 import './OfflineDataSync.css';
 
 export const OfflineDataSyncDialog = () => {
   const { statusDialogOpen } = useSelector(selectOfflineActivity);
   const dispatch = useDispatch();
-
+  const handleClose = () => dispatch({ type: ACTIVITY_OFFLINE_SYNC_DIALOG_SET_STATE, payload: { open: false } });
   return (
-    <Dialog
-      open={statusDialogOpen}
-      maxWidth={'xl'}
-      onClose={() => {
-        dispatch({ type: ACTIVITY_OFFLINE_SYNC_DIALOG_SET_STATE, payload: { open: false } });
-      }}
-    >
-      <DialogTitle>Offline Sync Status</DialogTitle>
-
-      <DialogContent>
+    <Dialog open={statusDialogOpen} id="offline-data-sync-dialog" maxWidth={'xl'} onClose={handleClose}>
+      <div className="dialog-header">
+        <DialogTitle>Offline Sync Status</DialogTitle>
+        <IconButton color="inherit" onClick={handleClose}>
+          <Close />
+        </IconButton>
+      </div>
+      <div className="dialog-content">
         <OfflineDataSyncTable />
-      </DialogContent>
+      </div>
     </Dialog>
   );
 };
