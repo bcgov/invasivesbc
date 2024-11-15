971b42a6290c9b01af7f3a33976a15d7bc6516e2
brennanwebster <Brennan.Webster@gov.bc.ca>
2024-11-15 12:16:35 -0800

    make biocontrol records purple

--- app/src/state/sagas/userSettings.ts
+++ app/src/state/sagas/userSettings.ts
@@ -92,11 +92,18 @@ function* handle_USER_SETTINGS_GET_INITIAL_STATE_REQUEST(action) {
         }
       ],
       colorScheme: {
-        Biocontrol: '#845ec2',
-        FREP: '#de852c',
-        Monitoring: '#2138e0',
-        Observation: '#399c3e',
-        Treatment: '#c6c617'
+        Activity_Biocontrol_Collection: '#845ec2',
+        Activity_Biocontrol_Release: '#845ec2',
+        Activity_Monitoring_BiocontrolDispersal_TerrestrialPlant: '#845ec2',
+        Activity_Monitoring_BiocontrolRelease_TerrestrialPlant: '#845ec2',
+        Activity_Monitoring_ChemicalTerrestrialAquaticPlant: '#2138e0',
+        Activity_Monitoring_MechanicalTerrestrialAquaticPlant: '#2138e0',
+        Activity_Observation_PlantAquatic: '#399c3e',
+        Activity_Observation_PlantTerrestrial: '#399c3e',
+        Activity_Treatment_ChemicalPlantAquatic: '#c6c617',
+        Activity_Treatment_ChemicalPlantTerrestrial: '#c6c617',
+        Activity_Treatment_MechanicalPlantAquatic: '#c6c617',
+        Activity_Treatment_MechanicalPlantTerrestrial: '#c6c617'
       },
       cacheMetadata: {
         status: UserRecordCacheStatus.NOT_CACHED
@@ -107,11 +114,18 @@ function* handle_USER_SETTINGS_GET_INITIAL_STATE_REQUEST(action) {
       recordSetType: RecordSetType.Activity,
       recordSetName: 'All InvasivesBC Activities',
       colorScheme: {
-        Biocontrol: '#845ec2',
-        FREP: '#de852c',
-        Monitoring: '#2138e0',
-        Observation: '#399c3e',
-        Treatment: '#c6c617'
+        Activity_Biocontrol_Collection: '#845ec2',
+        Activity_Biocontrol_Release: '#845ec2',
+        Activity_Monitoring_BiocontrolDispersal_TerrestrialPlant: '#845ec2',
+        Activity_Monitoring_BiocontrolRelease_TerrestrialPlant: '#845ec2',
+        Activity_Monitoring_ChemicalTerrestrialAquaticPlant: '#2138e0',
+        Activity_Monitoring_MechanicalTerrestrialAquaticPlant: '#2138e0',
+        Activity_Observation_PlantAquatic: '#399c3e',
+        Activity_Observation_PlantTerrestrial: '#399c3e',
+        Activity_Treatment_ChemicalPlantAquatic: '#c6c617',
+        Activity_Treatment_ChemicalPlantTerrestrial: '#c6c617',
+        Activity_Treatment_MechanicalPlantAquatic: '#c6c617',
+        Activity_Treatment_MechanicalPlantTerrestrial: '#c6c617'
       },
       cacheMetadata: {
         status: UserRecordCacheStatus.NOT_ELIGIBLE
