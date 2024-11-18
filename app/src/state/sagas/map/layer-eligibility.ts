2d105d7436d9e385aa56394143eb64355b6a8e46
Meghna Holla <35996935+meghna0593@users.noreply.github.com>
2024-11-18 13:11:25 -0800

    Public layer not visible (#3692)
    * Enable public layer
    
    * remove commented out code

--- app/src/state/sagas/map/layer-eligibility.ts
+++ app/src/state/sagas/map/layer-eligibility.ts
@@ -75,6 +75,17 @@ function* recomputeEligibleMapLayers(action) {
   ) {
     yield put(MapActions.updateAvailableBaseMaps(UPDATED_BASEMAP_LIST));
     yield put(MapActions.updateAvailableOverlays(UPDATED_OVERLAY_LIST));
+    
+  }
+  
+  const { enabledOverlayLayers } = yield select((state) => state.Map);
+  
+  if (
+    !AUTHENTICATED &&
+    UPDATED_OVERLAY_LIST.includes('public_layer') &&
+    !enabledOverlayLayers.includes('public_layer')
+  ) {
+    yield put(MapActions.toggleOverlay('public_layer'));
   }
 }
 
