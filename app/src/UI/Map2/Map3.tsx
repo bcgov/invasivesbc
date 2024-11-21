214c59231243a33ee009d0b891adfbf63be403e6
Meghna Holla <meghna0593@gmail.com>
2024-11-20 16:01:33 -0800

    format code, clean up, add zoom values and color to points and polygon on record set layer

--- app/src/UI/Map2/Map3.tsx
+++ app/src/UI/Map2/Map3.tsx
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import { useSelector } from 'react-redux';
 import { useEffect, useRef, useState } from 'react';
-import Map, {Source, Layer} from 'react-map-gl/maplibre';
+import Map, { Source, Layer } from 'react-map-gl/maplibre';
 import { getCurrentJWT } from 'state/sagas/auth/auth';
 import maplibregl from 'maplibre-gl';
 import { PMTiles, Protocol } from 'pmtiles';
 import { RecordSetLayers } from './RecordSetLayers3';
 
-export const MainMap= ({children}) =>{
-    const pmtilesProtocol = new Protocol();
+export const MainMap = ({ children }) => {
+  const pmtilesProtocol = new Protocol();
   maplibregl.addProtocol('pmtiles', (request) => {
     return new Promise((resolve, reject) => {
       const callback = (err, data) => {
@@ -28,146 +28,113 @@ export const MainMap= ({children}) =>{
 
   // this is so we share one instance across the JS code and the map renderer
   pmtilesProtocol.add(p);
-    const  API_BASE  = useSelector((state: any) => state.Configuration.current.API_BASE);
+  const API_BASE = useSelector((state: any) => state.Configuration.current.API_BASE);
   const authenticated = useSelector((state: any) => state.Auth.authenticated);
   const [currentAuthHeader, setCurrentAuthHeader] = useState<string>('');
-  const [mapLoaded, setMapLoaded] = useState(false);
   const authHeaderRef = useRef<string>();
   authHeaderRef.current = currentAuthHeader;
 
-  const mapRef = useRef(null);
-    
-    const handleMapLoad = ()=> {
-        const map = mapRef.current?.getMap();
-        console.log("Inside ", mapRef.current);
-        
-        if (map) {
-            map.addSource('wms-source', {
-                type: 'raster',
-                tiles: [
-                    "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-                ],
-                tileSize: 256,
-            });
 
-            map.addLayer({
-                id:'wms-layer',
-                type:'raster',
-                source: 'wms-source',
-                
-            });
-        }
-    };  
 
-    const handleInitialLoad = () =>{
-        console.log("Called");
-        
-        setMapLoaded(true);
+  useEffect(() => {
+    if (!authenticated) {
+      return;
     }
 
-    useEffect(() => {
-        if (!authenticated) {
-          return;
-        }
-    
-        // get it once with no delay
-        getCurrentJWT().then((header) => {
-          setCurrentAuthHeader(header);
-        });
-    
-        // and then regularly thereafter
-        const id = setInterval(() => {
-          getCurrentJWT().then((header) => {
-            setCurrentAuthHeader(header);
-          });
-        }, 10000);
-    
-        return () => {
-          clearInterval(id);
-        };
-      }, [authenticated]);
-    
-      // for logged in or out layers:
-      const getAuthHeaderCallback = () => {
-        if(authHeaderRef.current === undefined) {
-          console.error('requested access before header received');
-          return '';
-        }
-        return authHeaderRef.current;
-      };
-    const transformRequest = (url, resourceType) => {
-        if (url.includes(API_BASE)) {
-            return {
-              url,
-              headers: {
-                Authorization: getAuthHeaderCallback()
-              }
-            };
-          }
-          return {
-            url
-          };
-      }
-    return (
-        <div className="map-containing-block">
-            <div className="MapWrapper">
-                <Map
-                    // ref={mapRef}
-                    initialViewState={{
-                        longitude: -123.333959,
-                        latitude: 48.4229865,
-                        zoom:12,
-                    }}
-                    minZoom={1}
-                    attributionControl={false}
-                    transformRequest={transformRequest}
-                    // mapStyle="https://wms.wheregroup.com/tileserver/style/osm-bright.json"
-                    mapLib={maplibregl}
-                    onLoad={handleInitialLoad}
-                    // onLoad={handleMapLoad}
-                    interactiveLayerIds={['pmtiles-layer']}
-                >
-                    <Source
-                        id='wms-source'
-                        type='raster'
-                        tiles={[
-                            "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-                        ]}
-                        tileSize={256}
-                    >
-                        <Layer
-                            id='wms-layer'
-                            type='raster'
-                            source='wms-source'
+    // get it once with no delay
+    getCurrentJWT().then((header) => {
+      setCurrentAuthHeader(header);
+    });
 
-                        />
-                    </Source>
-                    
-                    <Source
-                        id='pmtiles-source'
-                        type='vector'
-                        tiles={[
-                            `pmtiles://${PMTILES_URL}`
-                        ]}
-                    
-                    >
-                        <Layer
-                            id='pmtiles-layer'
-                            type='circle'
-                            source='pmtiles-source'
-                            source-layer='invasives'
-                            paint={{ 'circle-color': '#0905f5', 'circle-opacity': 1.0 }}
-                            layout={{visibility: authenticated? 'none': 'visible'}}
-                        />
-                    </Source>
-                    
-                    <RecordSetLayers/>
-                </Map>
-                <div id="LoadingMap" className={!true ? 'loadingMap' : 'loadedMap'}>
-                    Loading tiles...
-                </div>
-                {children}
-            </div>
+    // and then regularly thereafter
+    const id = setInterval(() => {
+      getCurrentJWT().then((header) => {
+        setCurrentAuthHeader(header);
+      });
+    }, 10000);
+
+    return () => {
+      clearInterval(id);
+    };
+  }, [authenticated]);
+
+  // for logged in or out layers:
+  const getAuthHeaderCallback = () => {
+    if (authHeaderRef.current === undefined) {
+      console.error('requested access before header received');
+      return '';
+    }
+    return authHeaderRef.current;
+  };
+  const transformRequest = (url, resourceType) => {
+    if (url.includes(API_BASE)) {
+      return {
+        url,
+        headers: {
+          Authorization: getAuthHeaderCallback()
+        }
+      };
+    }
+    return {
+      url
+    };
+  }
+  return (
+    <div className="map-containing-block">
+      <div className="MapWrapper">
+        <Map
+          initialViewState={{
+            longitude: -123.333959,
+            latitude: 48.4229865,
+            zoom: 12,
+          }}
+          minZoom={1}
+          attributionControl={false}
+          transformRequest={transformRequest}
+          mapLib={maplibregl}
+          interactiveLayerIds={['pmtiles-layer']}
+        >
+          <Source
+            id='wms-source'
+            type='raster'
+            tiles={[
+              "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+            ]}
+            tileSize={256}
+          >
+            <Layer
+              id='wms-layer'
+              type='raster'
+              source='wms-source'
+
+            />
+          </Source>
+
+          <Source
+            id='pmtiles-source'
+            type='vector'
+            tiles={[
+              `pmtiles://${PMTILES_URL}`
+            ]}
+
+          >
+            <Layer
+              id='pmtiles-layer'
+              type='circle'
+              source='pmtiles-source'
+              source-layer='invasives'
+              paint={{ 'circle-color': '#0905f5', 'circle-opacity': 1.0 }}
+              layout={{ visibility: authenticated ? 'none' : 'visible' }}
+            />
+          </Source>
+
+          <RecordSetLayers />
+        </Map>
+        <div id="LoadingMap" className={!true ? 'loadingMap' : 'loadedMap'}>
+          Loading tiles...
         </div>
-    )
+        {children}
+      </div>
+    </div>
+  )
 };
