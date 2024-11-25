ef946a8d8a12a3e2074f3a299e78483b42ecbc1e
Meghna Holla <meghna0593@gmail.com>
2024-11-25 15:37:25 -0800

    use tsx instead of ts-node, update zoom levels

--- app/src/UI/Map2/Map3.tsx
+++ app/src/UI/Map2/Map3.tsx
@@ -50,7 +50,7 @@ const mapStyleSources: MapStyleSourceDefinition[] = [
       tiles: ['https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'],
       attribution: 'Powered by ESRI',
       tileSize: 256,
-      maxzoom: 18
+      maxzoom: 24
     }
   },
   {
@@ -60,7 +60,7 @@ const mapStyleSources: MapStyleSourceDefinition[] = [
       tiles: ['https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}'],
       attribution: 'Powered by ESRI',
       tileSize: 256,
-      maxzoom: 18
+      maxzoom: 24
     }
   }
 ];
