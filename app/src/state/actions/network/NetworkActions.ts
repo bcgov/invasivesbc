da52b2c85f1c23063806af3d2831948dfb3e2bb6
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-11-28 11:42:43 -0800

    update NetworkActions.ts

--- app/src/state/actions/network/NetworkActions.ts
+++ app/src/state/actions/network/NetworkActions.ts
@@ -4,7 +4,13 @@ class NetworkActions {
   private static readonly PREFIX = 'NetworkActions';
   static readonly online = createAction(`${this.PREFIX}/online`);
   static readonly offline = createAction(`${this.PREFIX}/offline`);
-  static readonly checkMobileNetworkStatus = createAction(`${this.PREFIX}/checkMobileNetworkStatus`);
+  static readonly checkMobileNetworkStatus = createAction(
+    `${this.PREFIX}/checkMobileNetworkStatus`,
+    (cancel: boolean = false) => ({ payload: cancel })
+  );
   static readonly userLostConnection = createAction(`${this.PREFIX}/userLostConnection`);
+  static readonly attemptToReconnectFailed = createAction(`${this.PREFIX}/attemptToReconnectFailed`);
+  static readonly automaticReconnectFailed = createAction(`${this.PREFIX}/automaticReconnectFailed`);
+  static readonly checkInitConnection = createAction(`${this.PREFIX}/checkInitConnection`);
 }
 export default NetworkActions;
