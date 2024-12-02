b6ec503110cf50c68c86d7df0d470e54711298de
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-12-02 14:01:12 -0800

    Adjust network connectivity to use two boolean flags

--- app/src/state/reducers/network.ts
+++ app/src/state/reducers/network.ts
@@ -2,20 +2,45 @@ import { createNextState } from '@reduxjs/toolkit';
 import { Draft } from 'immer';
 import NetworkActions from 'state/actions/network/NetworkActions';
 
-interface Network {
+const HEARTBEATS_TO_FAILURE = 4;
+
+/**
+ * @desc Network State properties
+ * @property { boolean } administrativeStatus user opt-in status for connecting to network
+ * @property { boolean } connected Combination variable for User wanting network connection, and connection being available
+ * @property { number }  consecutiveHeartbeatFailures current number of concurrent failed health checks
+ * @property { boolean } operationalStatus status of connection to the API
+ */
+export interface NetworkState {
+  administrativeStatus: boolean;
   connected: boolean;
+  consecutiveHeartbeatFailures: number;
+  operationalStatus: boolean;
 }
 
-function createNetworkReducer(initialStatus: Network) {
-  const initialState: Network = {
-    ...initialStatus
-  };
+const initialState: NetworkState = {
+  administrativeStatus: true,
+  connected: true,
+  consecutiveHeartbeatFailures: 0,
+  operationalStatus: true
+};
 
+function createNetworkReducer() {
   return (state = initialState, action) => {
-    return createNextState(state, (draftState: Draft<Network>) => {
-      if (NetworkActions.online.match(action)) {
+    return createNextState(state, (draftState: Draft<NetworkState>) => {
+      if (NetworkActions.setAdministrativeStatus.match(action)) {
+        draftState.administrativeStatus = action.payload;
+        draftState.consecutiveHeartbeatFailures = HEARTBEATS_TO_FAILURE;
+      } else if (NetworkActions.updateConnectionStatus.match(action)) {
+        if (action.payload) {
+          draftState.consecutiveHeartbeatFailures = 0;
+        } else {
+          draftState.consecutiveHeartbeatFailures++;
+        }
+        draftState.operationalStatus = draftState.consecutiveHeartbeatFailures < HEARTBEATS_TO_FAILURE;
+      } else if (NetworkActions.online.match(action)) {
         draftState.connected = true;
-      } else if (NetworkActions.offline.match(action) || NetworkActions.userLostConnection.match(action)) {
+      } else if (NetworkActions.offline.match(action)) {
         draftState.connected = false;
       }
     });
@@ -23,5 +48,5 @@ function createNetworkReducer(initialStatus: Network) {
 }
 
 const selectNetworkConnected: (state) => boolean = (state) => state.Network.connected;
-
-export { selectNetworkConnected, createNetworkReducer };
+const selectNetworkState: (state) => NetworkState = (state) => state.Network;
+export { createNetworkReducer, selectNetworkConnected, selectNetworkState };
