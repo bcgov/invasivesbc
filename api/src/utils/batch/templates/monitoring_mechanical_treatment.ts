040f7edcbc67b721efaac98de5b13994fe6efa9b
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-12-03 16:01:38 -0800

    Change linked_id from 'text' to'point' in templates

--- api/src/utils/batch/templates/monitoring_mechanical_treatment.ts
+++ api/src/utils/batch/templates/monitoring_mechanical_treatment.ts
@@ -22,7 +22,7 @@ MonitoringMechanical.columns = [
 
   new TemplateColumnBuilder(
     'Monitoring - Linked Treatment ID',
-    'text',
+    'linked_id',
     'form_data.activity_type_data.linked_id'
   ).build(),
 
