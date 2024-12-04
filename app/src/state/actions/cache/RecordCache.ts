cca4f9874992725a80093e68532f84f69d3ec36e
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-12-04 14:08:23 -0800

    Implement loadRecordSetSourceMetadata in locaforage-cache

--- app/src/state/actions/cache/RecordCache.ts
+++ app/src/state/actions/cache/RecordCache.ts
@@ -1,5 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { UserRecordSet } from 'interfaces/UserRecordSet';
 import { RootState } from 'state/reducers/rootReducer';
+import getBoundingBoxFromRecordsetFilters from 'utils/getBoundingBoxFromRecordsetFilters';
 import { RecordCacheServiceFactory } from 'utils/record-cache/context';
 
 class RecordCache {
@@ -24,13 +26,23 @@ class RecordCache {
       const state: RootState = getState() as RootState;
 
       const idsToCache: string[] = state.Map.layers.find((l) => l.recordSetID == spec.setId).IDList;
+      const recordSet: UserRecordSet = state.UserSettings.recordSets[spec.setId]?.tableFilters;
+      const bbox = await getBoundingBoxFromRecordsetFilters(recordSet);
 
       await service.download({
         idsToCache,
         setId: spec.setId,
         API_BASE: state.Configuration.current.API_BASE
       });
-      return { cachedIds: idsToCache };
+
+      const { cachedCentroid, cachedGeoJson } = await service.loadRecordsetSourceMetadata(idsToCache);
+
+      return {
+        cachedIds: idsToCache,
+        bbox,
+        cachedGeoJson,
+        cachedCentroid
+      };
     }
   );
 }
