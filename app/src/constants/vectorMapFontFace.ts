66908c137b7a464b43bed5e07fd58dbacd973231
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-12-09 12:04:11 -0800

    Move VECTOR_MAP_FONT_FACE to constant

---
+++ app/src/constants/vectorMapFontFace.ts
@@ -0,0 +1,6 @@
+import { MOBILE } from 'state/build-time-config';
+
+// available from CDN, but not in asset pack
+const VECTOR_MAP_FONT_FACE = MOBILE ? 'Noto Sans Bold' : 'Open Sans Bold';
+
+export default VECTOR_MAP_FONT_FACE;
