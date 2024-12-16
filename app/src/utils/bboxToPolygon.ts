128a0420e41f154bd2644087751bc71f7e5664a9
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2024-12-16 13:06:27 -0800

    add bboxToPolygon

---
+++ app/src/utils/bboxToPolygon.ts
@@ -0,0 +1,28 @@
+import { Feature, Polygon } from '@turf/helpers';
+
+/**
+ * @desc Takes bounding from recordset metadata and convert into a Geojson Polygon
+ * @param bbox Cached bounding box
+ * @returns {Feature<Polygon>} Geojson Bounding box
+ */
+function bboxToPolygon(bbox: Record<PropertyKey, number>): Feature<Polygon> {
+  const { minLatitude, minLongitude, maxLatitude, maxLongitude } = bbox;
+  return {
+    type: 'Feature',
+    geometry: {
+      type: 'Polygon',
+      coordinates: [
+        [
+          [minLongitude, minLatitude],
+          [minLongitude, maxLatitude],
+          [maxLongitude, maxLatitude],
+          [maxLongitude, minLatitude],
+          [minLongitude, minLatitude]
+        ]
+      ]
+    },
+    properties: {}
+  };
+}
+
+export default bboxToPolygon;
