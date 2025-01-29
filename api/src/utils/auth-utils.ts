d5f6f15b0b5a4a3b4e2a467f012dd526c761f8c8
Matthew Logan <62873746+LocalNewsTV@users.noreply.github.com>
2025-01-29 13:55:15 -0800

    [INV-3808] Unhandled Promise rejections in Auth (#3814)
    * Add Request type to bearerHandler
    
    * trim errors to function, add optional chaining, handle promises better
    
    * Add catch to getUserByKeycloakID call, prevent db crash when can't connect

--- api/src/utils/auth-utils.ts
+++ api/src/utils/auth-utils.ts
@@ -17,6 +17,15 @@ const APP_CERTIFICATE_URL =
   process.env.APP_CERTIFICATE_URL ||
   'https://dev.loginproxy.gov.bc.ca/auth/realms/standard/protocol/openid-connect/certs';
 
+const rejectWithErr = (issue: string, code: number = 401) =>
+  new Error(issue, {
+    cause: {
+      code: code,
+      message: issue,
+      namespace: 'auth-utils'
+    }
+  });
+
 // so we have type information available to endpoints
 export interface InvasivesRequest extends Request {
   keycloakToken: any;
@@ -57,12 +66,12 @@ function retrieveKey(header, callback) {
   });
 }
 
-export const authenticate = async (req: InvasivesRequest) => {
+export const authenticate = async (req: InvasivesRequest): Promise<void> => {
   const MDC = MDCAsyncLocal.getStore();
 
   defaultLog.debug({ label: 'authenticate', message: 'authenticating user' });
 
-  const filterForSelectable = req.header('filterforselectable') === 'true' ? true : false;
+  const filterForSelectable = req.header('filterforselectable') === 'true';
   const authHeader = req.header('Authorization');
 
   const isPublicURL = [
@@ -81,11 +90,7 @@ export const authenticate = async (req: InvasivesRequest) => {
   } catch (error) {
     defaultLog.info({ label: 'authenticate', message: 'malformed auth token received' });
 
-    throw {
-      code: 400,
-      message: 'Authorization header parse failure',
-      namespace: 'auth-utils'
-    };
+    throw rejectWithErr('Authorization header parse failure', 400);
   }
 
   if (isPublicURL && (req.method === 'GET' || req.method === 'POST') && !token) {
@@ -104,139 +109,102 @@ export const authenticate = async (req: InvasivesRequest) => {
 
   if (!token) {
     defaultLog.info({ label: 'authenticate', message: 'missing or malformed auth token received' });
-
-    throw {
-      code: 401,
-      message: 'Authorization header parse failure',
-      namespace: 'auth-utils'
-    };
+    throw rejectWithErr('Authorization header parse failure');
   }
 
   return new Promise<void>((resolve, reject) => {
     verify(token, retrieveKey, {}, function (error, decoded: Record<string, any>) {
-      if (error) {
-        defaultLog.error({ label: 'authenticate', message: 'token verification failure', error });
-        reject({
-          code: 401,
-          message: 'Token decode failure',
-          namespace: 'auth-utils'
-        });
-        return;
+      if (!decoded || error) {
+        if (error) defaultLog.error({ label: 'authenticate', message: 'token verification failure', error });
+        reject(rejectWithErr('Token decode Failure'));
       }
-      if (!decoded) {
-        reject({
-          code: 401,
-          message: 'Token decode failure',
-          namespace: 'auth-utils'
-        });
-        return;
-      }
-
       req.keycloakToken = decoded;
 
       let accountType, id;
 
-      if (!decoded['identity_provider']) {
-        reject({
-          code: 401,
-          message: 'Invalid token - missing identity provider',
-          namespace: 'auth-utils'
-        });
-        return;
-      }
-
-      if (decoded.identity_provider === 'idir') {
+      if (decoded?.identity_provider === 'idir') {
         accountType = KeycloakAccountType.idir;
-        if (!decoded['idir_user_guid']) {
-          reject({
-            code: 401,
-            message: 'Invalid token - missing idir guid',
-            namespace: 'auth-utils'
-          });
-          return;
+        if (!decoded?.idir_user_guid) {
+          reject(rejectWithErr('Invalid token - missing idir guid'));
         }
         id = decoded.idir_user_guid;
       } else if (decoded.identity_provider === 'bceidbusiness') {
         accountType = KeycloakAccountType.bceid;
-        if (!decoded['bceid_user_guid']) {
-          reject({
-            code: 401,
-            message: 'Invalid token - missing bceid guid',
-            namespace: 'auth-utils'
-          });
-          return;
+        if (!decoded?.bceid_user_guid) {
+          reject(rejectWithErr('Invalid token - missing bceid guid'));
         }
         id = decoded.bceid_user_guid;
       } else {
-        reject({
-          code: 401,
-          message: 'Invalid token - missing idir_userid or bceid_userid',
-          namespace: 'auth-utils'
-        });
-        return;
+        reject(rejectWithErr('Invalid token - Missing idir_userid or bceid_userid'));
       }
 
-      getUserByKeycloakID(accountType, id).then((user) => {
-        const createIfNeeded = new Promise((resolve: any) => {
-          if (!user) {
-            defaultLog.info({ label: 'authenticate', message: `first creating new user ${id}` });
-            createUser(decoded, accountType, id).then(() => {
-              getUserByKeycloakID(accountType, id).then((newUser) => {
-                user = newUser;
-                resolve();
-              });
-            });
-          } else {
-            resolve();
-          }
-        });
-
-        createIfNeeded.then(() => {
-          req.authContext = {
-            preferredUsername: null,
-            friendlyUsername: null,
-            user: null,
-            roles: [],
-            filterForSelectable: false
-          };
-          req.authContext.preferredUsername = decoded['preferred_username'];
-          if (decoded['idir_username']) {
-            req.authContext.friendlyUsername = decoded['idir_username'].toLowerCase() + '@idir';
-          }
-          if (decoded['bceid_username']) {
-            req.authContext.friendlyUsername = decoded['bceid_username'].toLowerCase() + '@bceid-business';
-          }
-
-          req.authContext.filterForSelectable = filterForSelectable;
-          req.authContext.user = user;
-
-          MDC.request.user = req.authContext.preferredUsername || 'unresolved';
-
-          getRolesForUser(user.user_id)
-            .then((roles) => {
-              req.authContext.roles = roles;
-              MDC.additionalContext.authContext = req.authContext;
-            })
-            .catch((error) => {
-              defaultLog.error({ label: 'authenticate', message: 'failed looking up roles', error });
-              reject(error);
-            })
-            .then(() => {
-              // check if user has beta access
-              getV2BetaAccessForUser(user.user_id)
-                .then((betaAccess) => {
-                  defaultLog.debug({ label: 'authenticate', message: 'looked up v2beta', betaAccess });
-                  req.authContext.v2beta = betaAccess;
-                  //req.authContext.user.v2beta = betaAccess;
-                  resolve();
+      getUserByKeycloakID(accountType, id)
+        .then((user) => {
+          const createIfNeeded = new Promise((resolve: any) => {
+            if (!user) {
+              defaultLog.info({ label: 'authenticate', message: `first creating new user ${id}` });
+              createUser(decoded, accountType, id)
+                .then(() => {
+                  getUserByKeycloakID(accountType, id)
+                    .then((newUser) => {
+                      user = newUser;
+                      resolve();
+                    })
+                    .catch((err: Error) => {
+                      reject(err);
+                    });
                 })
-                .catch((error) => {
-                  defaultLog.error({ label: 'authenticate', message: 'failed looking up beta access', error });
-                  reject(error);
-                });
-            });
-        });
-      });
+                .catch((err: Error) => reject(err));
+            }
+            resolve();
+          });
+
+          createIfNeeded.then(() => {
+            req.authContext = {
+              preferredUsername: null,
+              friendlyUsername: null,
+              user: null,
+              roles: [],
+              filterForSelectable: false
+            };
+            req.authContext.preferredUsername = decoded?.preferred_username;
+            if (decoded?.idir_username) {
+              req.authContext.friendlyUsername = decoded.idir_username.toLowerCase() + '@idir';
+            }
+            if (decoded?.bceid_username) {
+              req.authContext.friendlyUsername = decoded.bceid_username.toLowerCase() + '@bceid-business';
+            }
+
+            req.authContext.filterForSelectable = filterForSelectable;
+            req.authContext.user = user;
+
+            MDC.request.user = req.authContext.preferredUsername || 'unresolved';
+
+            getRolesForUser(user.user_id)
+              .then((roles) => {
+                req.authContext.roles = roles;
+                MDC.additionalContext.authContext = req.authContext;
+              })
+              .catch((error: Error) => {
+                defaultLog.error({ label: 'authenticate', message: 'failed looking up roles', error });
+                reject(error);
+              })
+              .then(() => {
+                // check if user has beta access
+                getV2BetaAccessForUser(user.user_id)
+                  .then((betaAccess) => {
+                    defaultLog.debug({ label: 'authenticate', message: 'looked up v2beta', betaAccess });
+                    req.authContext.v2beta = betaAccess;
+                    resolve();
+                  })
+                  .catch((error: Error) => {
+                    defaultLog.error({ label: 'authenticate', message: 'failed looking up beta access', error });
+                    reject(error);
+                  });
+              });
+          });
+        })
+        .catch((err: Error) => reject(err));
     });
   });
 };
