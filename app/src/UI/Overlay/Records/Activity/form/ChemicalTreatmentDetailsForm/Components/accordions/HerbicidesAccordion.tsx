2d79baab4dfd72c9fb9273f3251bb59aef4ed086
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-01-29 11:18:35 -0800

    Destructure props, remove commented code, remove nested div/unused vars

--- app/src/UI/Overlay/Records/Activity/form/ChemicalTreatmentDetailsForm/Components/accordions/HerbicidesAccordion.tsx
+++ app/src/UI/Overlay/Records/Activity/form/ChemicalTreatmentDetailsForm/Components/accordions/HerbicidesAccordion.tsx
@@ -1,13 +1,14 @@
-import React, { useContext, useRef } from 'react';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { Accordion, AccordionSummary, Typography, AccordionDetails, Box, Button } from '@mui/material';
+import { useContext, useRef } from 'react';
+import { Typography, Box, Button } from '@mui/material';
 import { ChemicalTreatmentDetailsContext } from '../../ChemicalTreatmentDetailsContext';
 import Herbicide from '../single-objects/Herbicide';
 import AddIcon from '@mui/icons-material/Add';
 import { RENDER_DEBUG } from 'UI/App';
-//import { useFormStyles } from '../../formStyles';
 
-const HerbicidesAccordion = (props) => {
+type PropTypes = {
+  insideTankMix: boolean;
+};
+const HerbicidesAccordion = ({ insideTankMix }: PropTypes) => {
   const ref = useRef(0);
   ref.current += 1;
   if (RENDER_DEBUG) {
@@ -16,86 +17,60 @@ const HerbicidesAccordion = (props) => {
   const formDataContext = useContext(ChemicalTreatmentDetailsContext);
   const { formDetails, setFormDetails } = formDataContext;
 
-  //  const classes = useFormStyles();
-  const tankMixOn = formDetails.form_data.tank_mix;
-
   return (
     <div id="herbicides_section">
       <Typography variant="h5">Herbicides</Typography>
-      <div
-        id="herbicides_list"
-        // className={classes.listContainer}
-      >
-        {props.insideTankMix
+      <div id="herbicides_list">
+        {insideTankMix
           ? formDetails.form_data?.tank_mix_object?.herbicides?.map((herbicide, index) => (
-              <Herbicide
-                insideTankMix={props.insideTankMix}
-                //          classes={classes}
-                key={index}
-                index={index}
-                herbicide={herbicide}
-              />
+              <Herbicide insideTankMix={insideTankMix} key={index} index={index} herbicide={herbicide} />
             ))
           : formDetails?.form_data?.herbicides?.map((herbicide, index) => (
-              <Herbicide
-                insideTankMix={props.insideTankMix}
-                //         classes={classes}
-                key={index}
-                index={index}
-                herbicide={herbicide}
-              />
+              <Herbicide insideTankMix={insideTankMix} key={index} index={index} herbicide={herbicide} />
             ))}
       </div>
-
-      <Box
-      // className={classes.accordionBody}
-      >
-        <Box
-          component="div"
-          //className={classes.centerBox}
-        >
-          <Button
-            disabled={formDetails.disabled}
-            id="btn_add_herbicide"
-            onClick={() => {
-              if (props.insideTankMix) {
-                setFormDetails((prevDetails) => {
-                  const newHerbicidesArr = [...prevDetails.form_data.tank_mix_object.herbicides];
-                  newHerbicidesArr.push({ index: newHerbicidesArr.length });
-                  return {
-                    ...prevDetails,
-                    form_data: {
-                      ...prevDetails.form_data,
-                      skipAppRateValidation: false,
-                      tank_mix_object: {
-                        ...prevDetails.form_data.tank_mix_object,
-                        herbicides: newHerbicidesArr
-                      }
-                    }
-                  };
-                });
-              } else {
-                setFormDetails((prevDetails) => {
-                  const newHerbicidesArr = [...prevDetails.form_data.herbicides];
-                  newHerbicidesArr.push({ index: newHerbicidesArr.length });
-                  return {
-                    ...prevDetails,
-                    form_data: {
-                      ...prevDetails.form_data,
-                      skipAppRateValidation: false,
+      <Box component="div">
+        <Button
+          disabled={formDetails.disabled}
+          id="btn_add_herbicide"
+          onClick={() => {
+            if (insideTankMix) {
+              setFormDetails((prevDetails) => {
+                const newHerbicidesArr = [...prevDetails.form_data.tank_mix_object.herbicides];
+                newHerbicidesArr.push({ index: newHerbicidesArr.length });
+                return {
+                  ...prevDetails,
+                  form_data: {
+                    ...prevDetails.form_data,
+                    skipAppRateValidation: false,
+                    tank_mix_object: {
+                      ...prevDetails.form_data.tank_mix_object,
                       herbicides: newHerbicidesArr
                     }
-                  };
-                });
-              }
-            }}
-            variant="contained"
-            startIcon={<AddIcon />}
-            color="primary"
-          >
-            Add Herbicide
-          </Button>
-        </Box>
+                  }
+                };
+              });
+            } else {
+              setFormDetails((prevDetails) => {
+                const newHerbicidesArr = [...prevDetails.form_data.herbicides];
+                newHerbicidesArr.push({ index: newHerbicidesArr.length });
+                return {
+                  ...prevDetails,
+                  form_data: {
+                    ...prevDetails.form_data,
+                    skipAppRateValidation: false,
+                    herbicides: newHerbicidesArr
+                  }
+                };
+              });
+            }
+          }}
+          variant="contained"
+          startIcon={<AddIcon />}
+          color="primary"
+        >
+          Add Herbicide
+        </Button>
       </Box>
     </div>
   );
