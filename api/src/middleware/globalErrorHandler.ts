6d77586ae9fdc3eb7b35f6d0527a85d30db17634
Meghna Holla <meghna0593@gmail.com>
2025-02-03 14:04:01 -0800

    update error handling

--- api/src/middleware/globalErrorHandler.ts
+++ api/src/middleware/globalErrorHandler.ts
@@ -1,19 +1,24 @@
 import { NextFunction, Request, Response } from 'express';
 import { getLogger } from 'utils/logger';
 
-export class CustomError extends Error {
-  code: number;
-  constructor(message?: string, code?: number) {
-    super(message);
-    this.code = code;
-  }
-}
+// export class CustomError extends Error {
+//   status?: number;
+
+//   constructor(message?: string, status?: number) {
+//     super(message);
+
+//     if (status) {
+//       this.status = status;
+//     }
+//   }
+// }
 const globalErrorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   const logger = getLogger('Error');
-  const code = err instanceof CustomError ? err.code : 500;
+
+  const code = err.status || err.code || 500;
   const errorResponse = {
     req: req.body,
-    error: err.message || 'Internal Server Error',
+    error: code + err.message || 'Internal Server Error',
     method: req.method,
     namespace: req.url
   };
