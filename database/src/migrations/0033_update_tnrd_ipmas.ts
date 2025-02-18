26107a69a89556aa1f66ef9a9992fb9202ffbfb5
brennanwebster <Brennan.Webster@gov.bc.ca>
2025-02-18 14:23:04 -0800

    update TNRD IPMAS in invasive_plant_management_areas

---
+++ database/src/migrations/0033_update_tnrd_ipmas.ts
@@ -0,0 +1,28 @@
+import { Knex } from 'knex';
+import axios from 'axios';
+import { ungzip } from 'node-gzip';
+
+export async function up(knex: Knex): Promise<void> {
+  try {
+    const url = 'https://nrs.objectstore.gov.bc.ca/seeds/TNRD_IPMAS.sql.gz';
+    const { data } = await axios.get(url, { responseType: 'arraybuffer' });
+    const sql = await ungzip(data);
+
+    await knex.raw(sql.toString());
+  } catch (e) {
+    console.error('Failed to insert IPMAS data:', e);
+    throw e;
+  }
+}
+
+export async function down(knex: Knex): Promise<void> {
+  try {
+    await knex.raw(`
+      DELETE FROM public.invasive_plant_management_areas
+      WHERE agency_cd = 'TNRD';
+    `);
+  } catch (e) {
+    console.error('Failed to rollback IPMAS data:', e);
+    throw e;
+  }
+}
