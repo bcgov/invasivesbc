fcce38a78a08555aa1faeac8e885a5e32f9f827b
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-02-24 14:51:51 -0800

    Add endpoint for batching records to cache

---
+++ api/src/paths/record-caching.ts
@@ -0,0 +1,132 @@
+import { RequestHandler } from 'express';
+import { Operation } from 'express-openapi';
+import { SQLStatement } from 'sql-template-strings';
+import { ALL_ROLES, SECURITY_ON } from 'constants/misc';
+import { getDBConnection } from 'database/db';
+import { getActivityHistorySQL, getCacheActivitiesSQL } from 'queries/activity-queries';
+import { getFileFromS3 } from 'utils/file-utils';
+import { getLogger } from 'utils/logger';
+import { getMediaItemsList } from 'paths/media';
+import { PoolClient } from 'pg';
+import { InvasivesRequest } from 'utils/auth-utils';
+
+const NAMESPACE = 'record-caching';
+const defaultLog = getLogger(NAMESPACE);
+
+const POST: Operation = [postActivity()];
+
+POST.apiDoc = {
+  description: 'Fetches a single activity based on its primary key.',
+  tags: ['activity'],
+  security: SECURITY_ON
+    ? [
+        {
+          Bearer: ALL_ROLES
+        }
+      ]
+    : [],
+  requestBody: {
+    description: 'List of Ids to fetch',
+    content: {
+      'application/json': {
+        schema: {
+          type: 'object',
+          properties: {
+            recordIds: {
+              description: 'Ids to fetch',
+              type: 'array',
+              items: {
+                type: 'string'
+              }
+            },
+            recordType: {
+              description: 'Type of records to fetch (Activity, IAPP)',
+              type: 'string'
+            }
+          }
+        }
+      }
+    }
+  },
+  responses: {
+    200: {
+      description: 'Activity get response object array.',
+      content: {
+        'application/json': {
+          schema: {
+            type: 'object',
+            properties: {}
+          }
+        }
+      }
+    },
+    401: {
+      $ref: '#/components/responses/401'
+    },
+    503: {
+      $ref: '#/components/responses/503'
+    },
+    default: {
+      $ref: '#/components/responses/default'
+    }
+  }
+};
+
+/**
+ * @desc Fetch a batch of records for caching via their IDs
+ * @return { RequestHandler }
+ */
+function postActivity(): RequestHandler {
+  return async (req: InvasivesRequest, res) => {
+    if (req.authContext.roles.length === 0) return res.status(401).json({ message: 'No Role for user' });
+
+    const { recordIds, recordType } = req.body;
+    let connection: PoolClient;
+
+    try {
+      connection = await getDBConnection();
+      const resObj: Record<PropertyKey, any> = {};
+
+      if (recordType === 'Activity') {
+        const sqlStatement: SQLStatement = getCacheActivitiesSQL(recordIds);
+        (await connection.query(sqlStatement.text, sqlStatement.values)).rows.forEach(async (a) => {
+          resObj[a.activity_id] = { ...a, ...a.activity_payload };
+          delete resObj[a.activity_id].activity_payload;
+          if (a?.media_keys?.length > 0) {
+            try {
+              const response = await Promise.all(a?.media_keys?.map(async (key: string) => getFileFromS3(key)));
+              resObj[a.activity_id].media = getMediaItemsList(response, a.media_keys);
+            } catch (ex) {
+              defaultLog.error({
+                label: NAMESPACE,
+                message: 'Error occured while fetching media from bucket',
+                body: req.body.idList
+              });
+            }
+          }
+        });
+        for (const id of recordIds) {
+          const sql = getActivityHistorySQL(id);
+          resObj[id].activity_history = (await connection.query(sql.text, sql.values)).rows;
+        }
+        return res.status(200).json(resObj);
+      }
+      return res.status(400).json({
+        message: 'Bad Request. Record type not supported or missing',
+        namespace: NAMESPACE
+      });
+    } catch (error) {
+      return res.status(500).json({
+        message: 'Unable to fetch ids in list.',
+        request: req.body,
+        error: error,
+        namespace: NAMESPACE,
+        code: 500
+      });
+    } finally {
+      connection?.release();
+    }
+  };
+}
+
+export { POST };
