870c2cdd5b59cdbb5c71132b0e4ab0dc4e94ccf1
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-02-26 14:07:53 -0800

    Remove original getSelectColumnsByRecordSetType

--- app/src/state/sagas/map/dataAccess.ts
+++ app/src/state/sagas/map/dataAccess.ts
@@ -22,6 +22,7 @@ import { RecordCacheServiceFactory } from 'utils/record-cache/context';
 import GeoShapes from 'constants/geoShapes';
 import { selectNetworkConnected } from 'state/reducers/network';
 import { selectUserSettings } from 'state/reducers/userSettings';
+import getSelectColumnsByRecordSetType from 'sharedAPI/src/getSelectColumnsByRecordSetType';
 import { PayloadAction } from '@reduxjs/toolkit';
 import IappActions, { IappTableRowRequest } from 'state/actions/activity/Iapp';
 import Activity from 'state/actions/activity/Activity';
@@ -414,58 +415,4 @@ export function* handle_MAP_WHATS_HERE_INIT_GET_ACTIVITY(action) {
 
   yield put(WhatsHere.map_init_get_activity_ids_fetched(recordSetUniqueFilteredIDs));
   yield put(WhatsHere.activity_rows_request());
-}
-
-export function getSelectColumnsByRecordSetType(recordSetType: any) {
-  let columns: string[] = [];
-  if (recordSetType === 'Activity') {
-    columns = [
-      'activity_id',
-      'short_id',
-      'activity_type',
-      'activity_subtype',
-      'project_code',
-      'jurisdiction_display',
-      'invasive_plant',
-      'species_positive_full',
-      'species_negative_full',
-      'has_current_positive',
-      'has_current_negative',
-      'current_positive_species',
-      'current_negative_species',
-      'species_treated_full',
-      'species_biocontrol_full',
-      'created_by',
-      'updated_by',
-      'agency',
-      'regional_invasive_species_organization_areas',
-      'regional_districts',
-      'invasive_plant_management_areas',
-      'biogeoclimatic_zones',
-      'elevation',
-      'batch_id',
-      'geom'
-    ];
-  } else {
-    columns = [
-      'site_id',
-      'site_paper_file_id',
-      'jurisdictions_flattened',
-      'min_survey',
-      'all_species_on_site',
-      'max_survey',
-      'agencies',
-      'biological_agent',
-      'has_biological_treatments',
-      'has_chemical_treatments',
-      'has_mechanical_treatments',
-      'has_biological_dispersals',
-      'monitored',
-      'regional_district',
-      'regional_invasive_species_organization',
-      'invasive_plant_management_area',
-      'geojson'
-    ];
-  }
-  return columns;
 }
