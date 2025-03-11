fe762b793df9968f37594950bd69761cf23a908e
Robert Johnstone <rob@plasticviking.com>
2025-03-11 22:33:54 +0800

    disable maxbounds on mobile, and improve the background colour and fallback tile (#3902)
    * disable maxbounds on mobile, and improve the background colour and fallback tile
    
    * remove unused import, and extra log

--- app/src/UI/LegacyMap/Map.tsx
+++ app/src/UI/LegacyMap/Map.tsx
@@ -12,12 +12,12 @@ import {
 } from 'UI/LegacyMap/helpers/functional/layer-definitions';
 import { Context } from 'utils/tile-cache/context';
 import {
-  removeOfflineActivitiesLayer,
   rebuildLayersOnTableHashUpdate,
   refreshColoursOnColourUpdate,
   refreshOfflineActivitiesLayer,
   refreshVisibilityOnToggleUpdate,
-  removeLayersOnNetworkConnectivityChange
+  removeLayersOnNetworkConnectivityChange,
+  removeOfflineActivitiesLayer
 } from 'UI/LegacyMap/helpers/functional/recordset-layers';
 import {
   addWMSLayersIfNotExist,
@@ -36,7 +36,7 @@ import { DEFAULT_LOCAL_LAYERS } from 'state/reducers/map';
 import { MapContext } from 'UI/LegacyMap/helpers/components/MapContext';
 import { InvasivesMap } from 'UI/LegacyMap/InvasivesMap';
 import { PositionMarkers } from 'UI/LegacyMap/helpers/components/PositionMarkers';
-import maplibregl, { LngLatBoundsLike } from 'maplibre-gl';
+import maplibregl from 'maplibre-gl';
 import { MEMORY_CONSTRAINED_DEVICE, MOBILE } from 'state/build-time-config';
 import { PMTiles, Protocol } from 'pmtiles';
 import { TileCacheService } from 'utils/tile-cache';
@@ -133,11 +133,6 @@ export const Map = ({ children }) => {
 
     /* map can have platform-specific options */
     const platformOptions = (() => {
-      if (MOBILE) {
-        return {
-          maxBounds: [-141.7761, 46.41459, -114.049, 60.00678] as LngLatBoundsLike
-        };
-      }
       return {};
     })();
 
