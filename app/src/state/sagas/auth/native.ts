9d650e0cabd4569037ad7297139403ee9fef426b
Robert Johnstone <rob@plasticviking.com>
2025-03-24 22:03:48 +0800

    - Revalidate tokens on mobile when coming back online or opening sync (#3953)
    window
    - Protect persisted offline activities from `purge` action

--- app/src/state/sagas/auth/native.ts
+++ app/src/state/sagas/auth/native.ts
@@ -1,7 +1,11 @@
-import { put, takeLatest } from 'redux-saga/effects';
+import { all, put, takeLatest } from 'redux-saga/effects';
+import { nanoid } from '@reduxjs/toolkit';
 import { USERINFO_CLEAR_REQUEST } from 'state/actions';
 import AuthBridge from 'utils/auth/authBridge';
 import { AuthActions } from 'state/actions/auth/Auth';
+import NetworkActions from 'state/actions/network/NetworkActions';
+import { AlertSeverity, AlertSubjects } from 'constants/alertEnums';
+import Alerts from 'state/actions/alerts/Alerts';
 
 function* handleSigninRequest() {
   const authResult = yield AuthBridge.authStart({});
@@ -45,10 +49,32 @@ function* initializeAuthentication() {
   );
 }
 
+function* checkTokenValidity() {
+  // verify that we can recover our auth session, sign out if we can't.
+  const { error } = yield AuthBridge.token({});
+  if (error) {
+    // token has become invalid. log us out and tell the user why
+    yield all([
+      put(AuthActions.signoutRequest()),
+      put(
+        Alerts.create({
+          severity: AlertSeverity.Warning,
+          subject: AlertSubjects.Network,
+          title: 'Session Expired',
+          id: nanoid(),
+          content:
+            'Your authentication session has expired. This can occur if you have been offline for an extended period and reconnected. You have been signed out.'
+        })
+      )
+    ]);
+  }
+}
+
 const nativeAuthEffects = [
   takeLatest(AuthActions.signinRequest.type, handleSigninRequest),
   takeLatest(AuthActions.signoutRequest.type, handleSignoutRequest),
-  takeLatest(AuthActions.initializeRequest.type, initializeAuthentication)
+  takeLatest(AuthActions.initializeRequest.type, initializeAuthentication),
+  takeLatest([NetworkActions.online.type, AuthActions.tokenValidationRequest.type], checkTokenValidity)
 ];
 
 export { nativeAuthEffects };
