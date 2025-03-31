0103e3b43f27c0125784d4b9a695bc6a304603dd
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-03-31 15:57:52 -0700

    Refactor WhatsHere button

--- app/src/UI/LegacyMap/Controls/WhatsHereButton.tsx
+++ app/src/UI/LegacyMap/Controls/WhatsHereButton.tsx
@@ -1,82 +1,52 @@
-import React, { useEffect, useRef } from 'react';
-import { useDispatch } from 'react-redux';
 import { IconButton, Tooltip } from '@mui/material';
-import { useSelector } from 'utils/use_selector';
-import HourglassTopIcon from '@mui/icons-material/HourglassTop';
-
-import DocumentScannerIcon from '@mui/icons-material/DocumentScanner';
+import { useDispatch, useSelector } from 'utils/use_selector';
 import { useHistory } from 'react-router-dom';
 import 'UI/Global.css';
 import { AlertSeverity, AlertSubjects } from 'constants/alertEnums';
 import Alerts from 'state/actions/alerts/Alerts';
 import WhatsHere from 'state/actions/whatsHere/WhatsHere';
+import { HourglassTop, TravelExplore } from '@mui/icons-material';
+import { useState } from 'react';
 
-export const WhatsHereButton = (props) => {
+export const WhatsHereButton = () => {
+  const handleWhatsHere = () => {
+    if (whatsHere.toggle == false) {
+      dispatch(WhatsHere.toggle(true));
+      dispatch(
+        Alerts.create({
+          content: 'Outline a region on the map to view records in the area.',
+          autoClose: 5,
+          severity: AlertSeverity.Info,
+          subject: AlertSubjects.Map
+        })
+      );
+    } else {
+      history.goBack();
+    }
+  };
+
   const dispatch = useDispatch();
   const history = useHistory();
-  const whatsHere = useSelector((state: any) => state.Map.whatsHere);
-  const [show, setShow] = React.useState(false);
+  const whatsHere = useSelector((state) => state.Map.whatsHere);
+  const [show, setShow] = useState(false);
 
-  const divRef = useRef();
-
-  if (whatsHere) {
-    return (
-      <div ref={divRef} className={whatsHere.toggle ? 'map-btn-selected' : 'map-btn'}>
-        <Tooltip
-          open={show}
-          onMouseEnter={() => setShow(true)}
-          onMouseLeave={() => setShow(false)}
-          classes={{ tooltip: 'toolTip' }}
-          title={`What's here?`}
-          placement="top-end"
-        >
-          <span>
-            <IconButton
-              className={'button'}
-              onClick={() => {
-                if (whatsHere.toggle == false) {
-                  dispatch(WhatsHere.toggle(true));
-                  dispatch(
-                    Alerts.create({
-                      content: 'Outline a region on the map to view records in the area.',
-                      autoClose: 5,
-                      severity: AlertSeverity.Info,
-                      subject: AlertSubjects.Map
-                    })
-                  );
-                } else {
-                  history.goBack();
-                }
-              }}
-            >
-              {whatsHere.loadingActivities || whatsHere.loadingIAPP ? <HourglassTopIcon /> : <></>}
-              <DocumentScannerIcon />
-            </IconButton>
-          </span>
-        </Tooltip>
-      </div>
-    );
-  } else {
-    return <></>;
-  }
-};
-
-//temporary fix to type is undefined error
-(window as any).type = undefined;
-
-export const WhatsHereDrawComponent = (props) => {
-  const ref = useRef();
-  const whatsHere = useSelector((state: any) => state.Map?.whatsHere);
-
-  useEffect(() => {
-    if ((whatsHere as any)?.toggle == true && (whatsHere as any)?.feature == null) {
-      //      ref.current = new (L as any).Draw.Rectangle(map);
-      //     (ref.current as any).enable();
-    }
-
-    return () => {
-      if (ref.current) (ref.current as any).disable();
-    };
-  }, [whatsHere]);
-  return <></>;
+  return (
+    <div className={whatsHere.toggle ? 'map-btn-selected' : 'map-btn'}>
+      <Tooltip
+        open={show}
+        onMouseEnter={() => setShow(true)}
+        onMouseLeave={() => setShow(false)}
+        classes={{ tooltip: 'toolTip' }}
+        title={`What's here?`}
+        placement="top-end"
+      >
+        <span>
+          <IconButton className={'button'} onClick={handleWhatsHere}>
+            {(whatsHere.loadingActivities || whatsHere.loadingIAPP) && <HourglassTop />}
+            <TravelExplore />
+          </IconButton>
+        </span>
+      </Tooltip>
+    </div>
+  );
 };
