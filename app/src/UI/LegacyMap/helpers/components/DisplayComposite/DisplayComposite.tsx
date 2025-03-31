9880b61cdbcca61185f385afa273ac730560b618
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-03-31 11:13:17 -0700

    Add description to DisplayComposite

--- app/src/UI/LegacyMap/helpers/components/DisplayComposite/DisplayComposite.tsx
+++ app/src/UI/LegacyMap/helpers/components/DisplayComposite/DisplayComposite.tsx
@@ -2,6 +2,9 @@ import AccuracyDisplay from '../AccuracyDisplay/AccuracyDisplay';
 import Coordinates from '../Coordinates/Coordinates';
 import './DisplayComposite.css';
 
+/**
+ * @desc Lets the info components auto-position instead of relying on absolute positioning.
+ */
 const DisplayComposite = () => {
   return (
     <div id="map-display-composite">
