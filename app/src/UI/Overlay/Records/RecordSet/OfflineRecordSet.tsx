7332e7167912e88a14398fdedbf4cee4188d6057
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-04-10 14:38:50 -0700

    console.log -> console.error

--- app/src/UI/Overlay/Records/RecordSet/OfflineRecordSet.tsx
+++ app/src/UI/Overlay/Records/RecordSet/OfflineRecordSet.tsx
@@ -66,7 +66,7 @@ export const OfflineRecordSet = ({ setID }: PropTypes) => {
   try {
     unsyncedOfflineActivities = transformOfflineActivitiesForRecordTable(unsyncedOfflineActivities, listOptions);
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 
   return (
