85b0d465cb690470072afadb1f64b344b56b6684
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-04-23 14:28:52 -0700

    remove static height

--- app/src/UI/IosDownloadLink/IosDownloadLink.tsx
+++ app/src/UI/IosDownloadLink/IosDownloadLink.tsx
@@ -7,7 +7,7 @@ const IosDownloadLink = () => {
   if (LINK === 'unset') return;
   return (
     <a href={LINK} target="_blank" rel="external">
-      <img src={iosDownload} alt="Download InvasivesBC For iOS devices" height="55" />
+      <img src={iosDownload} alt="Download InvasivesBC For iOS devices" />
     </a>
   );
 };
