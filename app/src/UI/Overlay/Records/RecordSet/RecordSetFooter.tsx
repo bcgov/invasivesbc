b679ed6800ab23db7ef023759844e9c106327734
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-04-29 14:48:05 -0700

    Footer uses Recordset prop, remove layer dependency

--- app/src/UI/Overlay/Records/RecordSet/RecordSetFooter.tsx
+++ app/src/UI/Overlay/Records/RecordSet/RecordSetFooter.tsx
@@ -1,25 +1,27 @@
 import { ArrowLeftIcon, ArrowRightIcon } from '@mui/x-date-pickers/icons';
+import { UserRecordSet } from 'interfaces/UserRecordSet';
 import { useDispatch } from 'react-redux';
 import { PAGE_OR_LIMIT_UPDATE } from 'state/actions';
 import { useSelector } from 'utils/use_selector';
 
-const RecordSetFooter = (props) => {
-  const layer = useSelector((state) => state.Map.layers?.filter((layer) => layer.recordSetID === props.setID)[0]);
-  const recordTable = useSelector((state) => state.Map.recordTables?.[props.setID]);
+type PropTypes = {
+  recordSet: UserRecordSet;
+};
+const RecordSetFooter = ({ recordSet }: PropTypes) => {
+  const recordTable = useSelector((state) => state.Map.recordTables?.[recordSet.id]);
+  const loading = recordTable?.loading;
+  const totalRecords = recordSet.idList.length;
 
-  const loading = layer?.loading || recordTable?.loading;
-
-  const totalRecords = layer?.IDList?.length;
   const loaded = !loading;
   const firstRowIndex = recordTable?.page * recordTable?.limit;
   const lastRowIndex =
     totalRecords < firstRowIndex + recordTable?.limit ? totalRecords : firstRowIndex + recordTable?.limit;
-  let recordDisplayString = 'Loading...';
 
+  let recordDisplayString = 'Loading...';
   if (loaded) {
     if (totalRecords !== undefined && totalRecords > 0 && !isNaN(firstRowIndex) && !isNaN(lastRowIndex)) {
       recordDisplayString = `${firstRowIndex + 1} to ${lastRowIndex} of ${totalRecords} records`;
-    } else if (layer?.IDList && totalRecords < 1) {
+    } else if (totalRecords === 0) {
       recordDisplayString = 'No records found';
     }
   }
@@ -33,7 +35,7 @@ const RecordSetFooter = (props) => {
     dispatch({
       type: PAGE_OR_LIMIT_UPDATE,
       payload: {
-        setID: props.setID,
+        setID: recordSet.id,
         page: recordTable?.page - 1,
         limit: recordTable?.limit
       }
@@ -43,7 +45,7 @@ const RecordSetFooter = (props) => {
     dispatch({
       type: PAGE_OR_LIMIT_UPDATE,
       payload: {
-        setID: props.setID,
+        setID: recordSet.id,
         page: recordTable?.page + 1,
         limit: recordTable?.limit
       }
