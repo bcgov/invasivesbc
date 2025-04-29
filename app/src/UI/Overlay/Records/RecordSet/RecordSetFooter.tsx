585ce9eaba4eee3d06dee1a43d2db29cac32e693
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-04-29 14:59:30 -0700

    Condense RecordSetFooter

--- app/src/UI/Overlay/Records/RecordSet/RecordSetFooter.tsx
+++ app/src/UI/Overlay/Records/RecordSet/RecordSetFooter.tsx
@@ -8,58 +8,47 @@ type PropTypes = {
   recordSet: UserRecordSet;
 };
 const RecordSetFooter = ({ recordSet }: PropTypes) => {
+  const handleUpdatePage = (change: number) => {
+    dispatch({
+      type: PAGE_OR_LIMIT_UPDATE,
+      payload: {
+        setID: recordSet.id,
+        page: recordTable?.page + change,
+        limit: recordTable?.limit
+      }
+    });
+  };
+
+  const dispatch = useDispatch();
   const recordTable = useSelector((state) => state.Map.recordTables?.[recordSet.id]);
+
   const loading = recordTable?.loading;
   const totalRecords = recordSet.idList.length;
-
-  const loaded = !loading;
   const firstRowIndex = recordTable?.page * recordTable?.limit;
   const lastRowIndex =
     totalRecords < firstRowIndex + recordTable?.limit ? totalRecords : firstRowIndex + recordTable?.limit;
+  const shouldDisplayNext = totalRecords > lastRowIndex && !loading;
+  const shouldDisplayPrevious = firstRowIndex > 0 && !loading;
 
-  let recordDisplayString = 'Loading...';
-  if (loaded) {
-    if (totalRecords !== undefined && totalRecords > 0 && !isNaN(firstRowIndex) && !isNaN(lastRowIndex)) {
-      recordDisplayString = `${firstRowIndex + 1} to ${lastRowIndex} of ${totalRecords} records`;
-    } else if (totalRecords === 0) {
-      recordDisplayString = 'No records found';
+  const recordDisplayString = (() => {
+    if (!loading) {
+      if (totalRecords && totalRecords > 0 && !isNaN(firstRowIndex) && !isNaN(lastRowIndex)) {
+        return `${firstRowIndex + 1} to ${lastRowIndex} of ${totalRecords} records`;
+      } else if (totalRecords === 0) {
+        return 'No records found';
+      }
     }
-  }
-
-  const shouldDisplayNextButton = totalRecords > lastRowIndex && !loading;
-  const shouldDisplayPreviousButton = firstRowIndex > 0 && !loading;
-
-  const dispatch = useDispatch();
-
-  const onClickPrevious = () => {
-    dispatch({
-      type: PAGE_OR_LIMIT_UPDATE,
-      payload: {
-        setID: recordSet.id,
-        page: recordTable?.page - 1,
-        limit: recordTable?.limit
-      }
-    });
-  };
-  const onClickNext = () => {
-    dispatch({
-      type: PAGE_OR_LIMIT_UPDATE,
-      payload: {
-        setID: recordSet.id,
-        page: recordTable?.page + 1,
-        limit: recordTable?.limit
-      }
-    });
-  };
+    return 'Loading...';
+  })();
 
   return (
     <div className="recordSet_footer">
       <div className="recordSet_pagePrevious">
-        {shouldDisplayPreviousButton ? <ArrowLeftIcon onClick={onClickPrevious} /> : <></>}
+        {shouldDisplayPrevious && <ArrowLeftIcon onClick={handleUpdatePage.bind(this, -1)} />}
       </div>
       <div className="recordSet_pageOfAndTotal">{recordDisplayString}</div>
       <div className="recordSet_pageNext">
-        {shouldDisplayNextButton ? <ArrowRightIcon onClick={onClickNext} /> : <></>}
+        {shouldDisplayNext && <ArrowRightIcon onClick={handleUpdatePage.bind(this, 1)} />}
       </div>
     </div>
   );
