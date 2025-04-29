fd73f59e76b9fe8872ddab6cd41cc7e91eb750c8
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-04-29 14:30:04 -0700

    revert

--- app/src/state/actions/activity/Activity.ts
+++ app/src/state/actions/activity/Activity.ts
@@ -54,7 +54,7 @@ interface IGetIdsForRecordsetOnline extends IGetIdsForRecordset {
 }
 
 interface IGetIdsForRecordsetSuccess extends IGetIdsForRecordset {
-  IDList: number;
+  IDList: Array<string | number>;
 }
 
 class Activity {
