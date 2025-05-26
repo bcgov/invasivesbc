e89dd026609c30c0d0b820d6e1ba4b16a0885e14
Robert Johnstone <rob@plasticviking.com>
2025-05-26 15:54:30 +0800

    update eslint config for compatibility

--- api/eslint.config.js
+++ api/eslint.config.js
@@ -17,7 +17,22 @@ module.exports = tseslint.config(eslint.configs.recommended, ...tseslint.configs
   },
   rules: {
     '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/ban-types': ['error', { types: { object: false, extendDefaults: true } }],
+    '@typescript-eslint/no-empty-object-type': 'error',
+    '@typescript-eslint/no-unsafe-function-type': 'error',
+    '@typescript-eslint/no-wrapper-object-types': 'error',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        vars: 'all',
+        args: 'after-used',
+        ignoreRestSiblings: true,
+        destructuredArrayIgnorePattern: '^_',
+        caughtErrors: 'all',
+        caughtErrorsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_' /* prefix a function argument with _ to indicate it is intentionally unused */
+      }
+    ],
     'import/no-unresolved': 'error',
     'import/no-duplicates': 'error',
     'import/no-relative-packages': 'error',
@@ -27,12 +42,12 @@ module.exports = tseslint.config(eslint.configs.recommended, ...tseslint.configs
     'import/newline-after-import': 'error',
     'import/order': 'error',
     'no-restricted-imports': [
-      'error',
+      'warn',
       {
         patterns: [
           {
             group: ['../*'],
-            message: 'No relative parent imports, use full path'
+            message: 'No relative imports'
           }
         ]
       }
