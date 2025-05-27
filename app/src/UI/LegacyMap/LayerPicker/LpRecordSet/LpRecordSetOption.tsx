75f55f0f5723c779b06a48a16713bf6a24cf44a7
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-05-27 07:56:43 -0700

    Add test ids to lpRecordSetOption

--- app/src/UI/LegacyMap/LayerPicker/LpRecordSet/LpRecordSetOption.tsx
+++ app/src/UI/LegacyMap/LayerPicker/LpRecordSet/LpRecordSetOption.tsx
@@ -22,13 +22,17 @@ const LpRecordSetOption = ({
   const getBgColor = () => (recordSet?.color ? `${recordSet.color}25` : 'white');
   return (
     <>
-      <li className="lp-record-set-option" style={{ backgroundColor: getBgColor() }}>
+      <li data-testid="record-set" className="lp-record-set-option" style={{ backgroundColor: getBgColor() }}>
         <div>
           <Tooltip
             classes={{ tooltip: 'toolTip' }}
             title="Toggle viewing the labels on the map for this layer.  If more than 200 are in the extent, you may need to zoom in to see what you are looking for.  For people on slow computers - it recalculates on drag and zoom so fewer small drags will decrease loading time."
           >
-            <IconButton disabled={!recordSet.mapToggle} onClick={() => toggleLabelVisibility(recordSet.id!)}>
+            <IconButton
+              data-testid="label-toggle"
+              disabled={!recordSet.mapToggle}
+              onClick={() => toggleLabelVisibility(recordSet.id)}
+            >
               {recordSet.labelToggle ? <Label /> : <LabelOff />}
             </IconButton>
           </Tooltip>
@@ -36,13 +40,13 @@ const LpRecordSetOption = ({
             classes={{ tooltip: 'toolTip' }}
             title="Toggle viewing the layer on the map, and including these records in the Whats Here search results."
           >
-            <IconButton onClick={() => toggleVisibility(recordSet.id!)}>
+            <IconButton data-testid="layer-toggle" onClick={() => toggleVisibility(recordSet.id)}>
               {recordSet?.mapToggle ? <Layers /> : <LayersClear />}
             </IconButton>
           </Tooltip>
           {canColour && (
             <Tooltip classes={{ tooltip: 'toolTip' }} title="Change the colour of this layer.">
-              <IconButton onClick={() => cycleColour(recordSet.id!)}>
+              <IconButton data-testid="cycle-color" onClick={() => cycleColour(recordSet.id)}>
                 <Palette />
               </IconButton>
             </Tooltip>
