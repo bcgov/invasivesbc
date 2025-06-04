e483f547601f8ee2361076233da740016b3235d6
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-06-04 15:51:39 -0700

    Apply temporary flag on Navigating to User Guide

--- app/src/UI/Layout/OverlayLayout/Header/Mobile/HeaderPopover.tsx
+++ app/src/UI/Layout/OverlayLayout/Header/Mobile/HeaderPopover.tsx
@@ -16,6 +16,8 @@ import { NetworkStateControl } from 'UI/Layout/OverlayLayout/Header/NetworkState
 import 'UI/Layout/OverlayLayout/Header/Mobile/MobileHeader.css';
 import { LayoutMode } from 'UI/Layout/Routes/PrimaryNavigation';
 import EventActions from 'state/actions/events/EventActions';
+import guideEntries from 'UI/Features/UserGuide/guideEntries';
+import faqEntries from 'UI/Features/UserGuide/faqEntries';
 
 const LogoutButton = () => {
   const dispatch = useDispatch();
@@ -81,6 +83,9 @@ const HeaderPopover = () => {
 
   const { MOBILE } = useSelector((state) => state.Configuration.current.build);
 
+  // Since populating the content is separate work, apply a temporary boolean to not render beforehand
+  const shouldLoadMobileUserGuide = guideEntries.length + faqEntries.length > 0;
+
   useEffect(() => {
     if (!MOBILE || authenticated || workingOffline) {
       setOfflineUserSelectionAvailable(false);
@@ -124,12 +129,14 @@ const HeaderPopover = () => {
                     What's New
                   </button>
                 </li>
-                <li>
-                  <button onClick={() => history.push('/Guide')}>
-                    <HelpCenter />
-                    User Guide
-                  </button>
-                </li>
+                {MOBILE && shouldLoadMobileUserGuide && (
+                  <li>
+                    <button onClick={() => history.push('/Guide')}>
+                      <HelpCenter />
+                      User Guide
+                    </button>
+                  </li>
+                )}
               </>
             )}
             {offlineUserSelectionAvailable && (
