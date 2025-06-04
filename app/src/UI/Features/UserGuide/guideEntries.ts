bb7e142b514f78d33e618c2309afa1ddc0d6e25d
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-06-04 15:30:35 -0700

    Create spots for guide entries and faq entries

---
+++ app/src/UI/Features/UserGuide/guideEntries.ts
@@ -0,0 +1,22 @@
+import UserGuideEntry from 'interfaces/UserGuideEntry';
+
+const guideEntries: Array<UserGuideEntry> = [
+  // {
+  //   title: '',
+  //   titleIcon: '',
+  //   tags: [''],
+  //   content: [
+  //     {
+  //       text: [''],
+  //       images: [
+  //         {
+  //           imgSource: '/assets/androidDownload.png', // source should begin with '/' which references ./app/public
+  //           caption: ''
+  //         } // Can add as many images as needed.
+  //       ]
+  //     } // As as many of these entries per section as needed
+  //   ]
+  // },
+];
+
+export default guideEntries;
