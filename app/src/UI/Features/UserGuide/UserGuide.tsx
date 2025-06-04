81115c6150008a9cb20f325b10ca2de98c564dab
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-06-04 15:06:46 -0700

    Create User Guide page

--- app/src/UI/Features/UserGuide/UserGuide.tsx
+++ app/src/UI/Features/UserGuide/UserGuide.tsx
@@ -1,5 +1,31 @@
-const UserGuide = () => {
-  return <div id="user-guide"></div>;
-};
+import faqEntries from './Faq/faqEntries';
+import Guide from './Guide/Guide';
+import guideEntries from './Guide/guideEntries';
+import './UserGuide.css';
+
+const UserGuide = () => (
+  <div id="user-guide-page">
+    <div className="content">
+      <h1>User Guide & FAQ</h1>
+      <p>A quick overview to help you get started and make the most of the features available.</p>
+      <section className="user-guide-section">
+        <h2>User Guide</h2>
+        {guideEntries.map((guide) => (
+          <Guide entry={guide} />
+        ))}
+      </section>
+      <section className="faq-section">
+        <h2>Frequently Asked Questions</h2>
+        <p>
+          Here you'll find answers to some common questions. We hope this helps, but feel free to reach out if you need
+          more information.
+        </p>
+        {faqEntries.map((faq) => (
+          <Guide entry={faq} />
+        ))}
+      </section>
+    </div>
+  </div>
+);
 
 export default UserGuide;
