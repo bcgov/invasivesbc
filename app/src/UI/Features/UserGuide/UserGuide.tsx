90b687c5724337988caeed19cc4a3a6fd8229be6
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-06-04 15:11:31 -0700

    Don't render section if no entries present

--- app/src/UI/Features/UserGuide/UserGuide.tsx
+++ app/src/UI/Features/UserGuide/UserGuide.tsx
@@ -8,22 +8,26 @@ const UserGuide = () => (
     <div className="content">
       <h1>User Guide & FAQ</h1>
       <p>A quick overview to help you get started and make the most of the features available.</p>
-      <section className="user-guide-section">
-        <h2>User Guide</h2>
-        {guideEntries.map((guide) => (
-          <Guide entry={guide} />
-        ))}
-      </section>
-      <section className="faq-section">
-        <h2>Frequently Asked Questions</h2>
-        <p>
-          Here you'll find answers to some common questions. We hope this helps, but feel free to reach out if you need
-          more information.
-        </p>
-        {faqEntries.map((faq) => (
-          <Guide entry={faq} />
-        ))}
-      </section>
+      {guideEntries.length > 0 && (
+        <section className="user-guide-section">
+          <h2>User Guide</h2>
+          {guideEntries.map((guide) => (
+            <Guide entry={guide} />
+          ))}
+        </section>
+      )}
+      {faqEntries.length > 0 && (
+        <section className="faq-section">
+          <h2>Frequently Asked Questions</h2>
+          <p>
+            Here you'll find answers to some common questions. We hope this helps, but feel free to reach out if you
+            need more information.
+          </p>
+          {faqEntries.map((faq) => (
+            <Guide entry={faq} />
+          ))}
+        </section>
+      )}
     </div>
   </div>
 );
