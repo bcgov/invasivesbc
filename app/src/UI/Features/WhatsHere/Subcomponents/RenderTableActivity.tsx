904df2d3e0772a3f29e116ceae3258819d753df2
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-06-17 15:25:01 -0700

    Remove log

--- app/src/UI/Features/WhatsHere/Subcomponents/RenderTableActivity.tsx
+++ app/src/UI/Features/WhatsHere/Subcomponents/RenderTableActivity.tsx
@@ -18,7 +18,6 @@ const RenderTableActivity = ({ setAnchorEl }: PropTypes) => {
   const whatsHere = useSelector((state) => state.Map?.whatsHere);
 
   const dispatchUpdatedID = (params) => {
-    console.log(params);
     dispatch(
       UserSettings.Map.setHoveredRecordset({
         id: params.row.id,
