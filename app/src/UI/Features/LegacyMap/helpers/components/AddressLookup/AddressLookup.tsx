9061b5506d69cbf87d96eec2c0dc256800d8219a
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-07-09 11:18:48 -0700

    Add UTM option

--- app/src/UI/Features/LegacyMap/helpers/components/AddressLookup/AddressLookup.tsx
+++ app/src/UI/Features/LegacyMap/helpers/components/AddressLookup/AddressLookup.tsx
@@ -5,19 +5,21 @@ import { useDispatch, useSelector } from 'utils/use_selector';
 import { getCurrentJWT } from 'state/sagas/auth/auth';
 import debounce from 'lodash.debounce';
 import { Feature } from 'geojson';
+import MapIcon from '@mui/icons-material/Map';
 import { Home, Place } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
 import UserSettings from 'state/actions/userSettings/UserSettings';
 import { point } from '@turf/helpers';
+import { calc_lat_long_from_utm } from 'utils/utm';
 
 const AddressLookup = () => {
   const DELAY_IN_MS = 750;
   const MINIMUM_LOOKUP_LENGTH = 5;
   enum Mode {
     ADDRESS,
-    COORDINATE
+    COORDINATE,
+    UTM
   }
-
   /**
    * @desc Fire API Request to get locations, prevent spamming API with debounce + minimum search length
    */
@@ -39,12 +41,29 @@ const AddressLookup = () => {
     []
   );
 
-  /** @desc Cycle between Address and Coordinate Modes */
-  const handleModeChange = () => setMode((prev) => (prev === Mode.ADDRESS ? Mode.COORDINATE : Mode.ADDRESS));
+  /**
+   * @desc Cycle between all available modes
+   */
+  const handleModeChange = () => {
+    const totalModes = Object.keys(Mode).length / 2;
+    setMode((prevMode) => (prevMode + 1) % totalModes);
+  };
+
+  /**
+   * @desc Query API for Related Addresses
+   */
   const handleAddressChange = (evt: ChangeEvent<HTMLInputElement>) => {
     setAddress(evt.target.value);
     debouncedApiCall(evt.target.value);
   };
+
+  /**
+   * @desc Handle Coordinate changes, ensure values are valid (no letters + in range)
+   * @param value new value
+   * @param min Min range
+   * @param max Max range
+   * @param setter Correct useState Handler
+   */
   const coordChangeHandler = (value: string, min: number, max: number, setter: (input: string) => void) => {
     const floatReg = /^[+-]?\d*(?:[.,]\d*)?$/;
     if (!value) {
@@ -72,19 +91,66 @@ const AddressLookup = () => {
     }
   };
 
+  const handleUtmChange = (value: string, setter: (input: string | undefined) => void) => {
+    const regex = /^[+-]?\d*(?:[.,]\d*)?$/;
+    if (!value) {
+      setter(undefined);
+    } else if (regex.test(value)) {
+      setter(value);
+    }
+  };
+
+  const handleSubmitUtm = () => {
+    if (utmResults.length === 0) return;
+    const shape = point([utmResults[0], utmResults[1]]);
+    const label = `
+      Zone: ${Number(zone).toLocaleString()}<br/>
+      Easting: ${Number(easting).toLocaleString()}<br/>
+      Northing: ${Number(northing).toLocaleString()}
+    `;
+    markLocationOnMap(shape, label);
+  };
+
   const dispatch = useDispatch();
 
   const base_url = useSelector((state) => state.Configuration.current.runtime.API_BASE);
   const connected = useSelector((state) => state.Network.connected);
   const authorizedUser = useSelector((state) => state.Auth.loggedInOrWorkingOffline);
 
+  // Address Mode states
   const [address, setAddress] = useState<string>('');
-  const [disabled, setDisabled] = useState<boolean>(true);
-  const [lat, setLat] = useState<string>('');
-  const [long, setLong] = useState<string>('');
-  const [mode, setMode] = useState<Mode>(Mode.ADDRESS);
   const [showSuggestions, setShowSuggestions] = useState<boolean>(false);
   const [suggestions, setSuggestions] = useState<IParsedAddress[]>([]);
+
+  // LatLong Mode states
+  const [lat, setLat] = useState<string>('');
+  const [long, setLong] = useState<string>('');
+  const [disabled, setDisabled] = useState<boolean>(true);
+
+  // UTM States
+  const [northing, setNorthing] = useState<string>();
+  const [easting, setEasting] = useState<string>();
+  const [zone, setZone] = useState<string>();
+  const [utmResults, setUtmResults] = useState<number[]>([]);
+
+  // Mode
+  const [mode, setMode] = useState<Mode>(Mode.UTM);
+
+  /**
+   * Calculate lat long from UTM.
+   */
+  useEffect(() => {
+    try {
+      if (zone !== undefined && easting !== undefined && northing !== undefined) {
+        setUtmResults(calc_lat_long_from_utm(parseFloat(zone), parseFloat(easting), parseFloat(northing)) ?? []);
+      } else {
+        setUtmResults([]);
+      }
+    } catch (ex) {
+      console.error(ex);
+      setUtmResults([]);
+    }
+  }, [zone, easting, northing]);
 
   useEffect(() => {
     setDisabled(!lat || !long);
@@ -168,6 +234,53 @@ const AddressLookup = () => {
                 onClick={handleGoToCoordinates}
               >
                 Go to Location
+              </button>
+            </>
+          ),
+          [Mode.UTM]: (
+            <>
+              <div className="flex-row">
+                <div>
+                  <input
+                    type="text"
+                    value={zone ?? ''}
+                    onKeyDown={(event) => {
+                      if (event.code === 'Enter') {
+                        handleSubmitUtm();
+                      }
+                    }}
+                    onChange={(e) => handleUtmChange(e.target.value, setZone)}
+                    placeholder="Zone"
+                  />
+                  <input
+                    type="text"
+                    value={easting ?? ''}
+                    onKeyDown={(event) => {
+                      if (event.code === 'Enter') {
+                        handleSubmitUtm();
+                      }
+                    }}
+                    onChange={(e) => handleUtmChange(e.target.value, setEasting)}
+                    placeholder="Easting"
+                  />
+                  <input
+                    type="text"
+                    value={northing ?? ''}
+                    onKeyDown={(event) => {
+                      if (event.code === 'Enter') {
+                        handleSubmitUtm();
+                      }
+                    }}
+                    onChange={(e) => handleUtmChange(e.target.value, setNorthing)}
+                    placeholder="Northing"
+                  />
+                </div>
+                <IconButton onClick={handleModeChange}>
+                  <MapIcon />
+                </IconButton>
+              </div>
+              <button data-testid="utm-button" disabled={utmResults.length === 0} onClick={handleSubmitUtm}>
+                Go to location
               </button>
             </>
           )
