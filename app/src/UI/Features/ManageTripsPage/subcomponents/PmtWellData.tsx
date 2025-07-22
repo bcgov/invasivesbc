4fd7884afff4dcfe819180d56aad3d8b6784baaf
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-07-22 14:39:22 -0700

    MVP Well Cache

--- app/src/UI/Features/ManageTripsPage/subcomponents/PmtWellData.tsx
+++ app/src/UI/Features/ManageTripsPage/subcomponents/PmtWellData.tsx
@@ -1,9 +1,28 @@
-import { IPlanMyTripCacheStatuses, IPlanMyTripRepositoryMetadata } from 'utils/plan-my-trip-cache';
+import { Button } from '@mui/material';
+import bbox from '@turf/bbox';
+import PlanMyTrip from 'state/actions/planMyTrip/PlanMyTrip';
+import { IPlanMyTripRepositoryMetadata } from 'utils/plan-my-trip-cache';
+import { useDispatch } from 'utils/use_selector';
 
 type PropTypes = {
   trip: IPlanMyTripRepositoryMetadata;
 };
 const PmtWellData = ({ trip }: PropTypes) => {
-  return <div>Well Data</div>;
+  const dispatch = useDispatch();
+  const handleDownload = () => {
+    const [minX, minY, maxX, maxY] = bbox(trip.geojson);
+    const bounds = {
+      minLatitude: minY,
+      maxLatitude: maxY,
+      minLongitude: minX,
+      maxLongitude: maxX
+    };
+    dispatch(PlanMyTrip.downloadWellData({ tripId: trip.id, bounds }));
+  };
+  return (
+    <Button variant="contained" color="primary" onClick={handleDownload}>
+      Download Well Data
+    </Button>
+  );
 };
 export default PmtWellData;
