3b0d2bb5ae83c4159a94c565b38d3c6e63bd1568
Matthew Logan <62873746+LocalNewsTV@users.noreply.github.com>
2025-07-24 19:40:54 -0700

    [INV-4214] Populate offline map options in layerpicker (#4215)
    * Populate offline map options in layerpicker
    
    * Cleanup for use case
    
    * Only fire if no maps available

--- app/src/UI/Features/LegacyMap/LayerPicker/LpOfflineMaps/LpOfflineMaps.tsx
+++ app/src/UI/Features/LegacyMap/LayerPicker/LpOfflineMaps/LpOfflineMaps.tsx
@@ -3,6 +3,9 @@ import 'UI/Features/LegacyMap/LayerPicker/LpOfflineMaps/LpOfflineMaps.css';
 import LpOfflineMapsOptions from 'UI/Features/LegacyMap/LayerPicker/LpOfflineMaps/LpOfflineMapsOption';
 import TooltipWithIcon from 'UI/Reusable/TooltipWithIcon/TooltipWithIcon';
 import { InvasivesMapLayerDefinitionWithState } from 'UI/Features/LegacyMap/helpers/functional/layers-hook';
+import { useDispatch } from 'utils/use_selector';
+import TileCache from 'state/actions/cache/TileCache';
+import { useEffect } from 'react';
 
 type PropTypes = {
   closePicker: () => void;
@@ -13,6 +16,15 @@ type PropTypes = {
 const LpOfflineMaps = ({ closePicker, setOverlayState, layers }: PropTypes) => {
   const cachedToolTipText =
     'Use this option to show or hide the map tiles you’ve previously downloaded to your device.';
+
+  const dispatch = useDispatch();
+
+  // Ensure TileCache repositoryList is up to date, if available.
+  useEffect(() => {
+    if (layers.filter((l) => l.mode === 'overlay' && l.selectionMode === 'offline-layers').length === 0) {
+      dispatch(TileCache.repositoryList());
+    }
+  }, []);
 
   return (
     <div id="lp-offline-maps">
