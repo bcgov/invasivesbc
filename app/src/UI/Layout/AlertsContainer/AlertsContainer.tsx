9050b83e32fda6da8f805c6ee9c9a4a162afd5c7
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-08-05 13:37:52 -0700

    Add Trip subject for Alerts

--- app/src/UI/Layout/AlertsContainer/AlertsContainer.tsx
+++ app/src/UI/Layout/AlertsContainer/AlertsContainer.tsx
@@ -3,7 +3,7 @@
  * @external {@link https://github.com/bcgov/invasivesbc/wiki/User-Alert-System }
  */
 import { Alert, AlertTitle, Button, Icon } from '@mui/material';
-import { Assignment, InsertPhoto, Lock, MapOutlined, Save, Wifi } from '@mui/icons-material';
+import { Assignment, InsertPhoto, Lock, Luggage, MapOutlined, Save, Wifi } from '@mui/icons-material';
 import { useDispatch } from 'react-redux';
 import 'UI/Layout/AlertsContainer/AlertsContainer.css';
 import AlertMessage from 'interfaces/AlertMessage';
@@ -34,6 +34,8 @@ const AlertsContainer = () => {
         return <Save />;
       case AlertSubjects.Authentication:
         return <Lock />;
+      case AlertSubjects.PlanMyTrip:
+        return <Luggage />;
       default:
         break;
     }
