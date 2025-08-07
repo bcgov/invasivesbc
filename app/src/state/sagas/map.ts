0183958ebf7ce3a2025afe9542bf497cb74c0616
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-08-07 12:51:57 -0700

    turn off kml layer before delete

--- app/src/state/sagas/map.ts
+++ app/src/state/sagas/map.ts
@@ -68,6 +68,7 @@ import { normalizeToPolygonCoordinates } from 'utils/geometryHelpers';
 import { GEO_TRACKING_FEATURE } from 'UI/Features/LegacyMap/helpers/functional/constants';
 import { isPaused, isTracking } from 'utils/geoTrackingHelpers';
 import PlanMyTrip from 'state/actions/planMyTrip/PlanMyTrip';
+import { shallowEqual } from 'react-redux';
 
 function* handle_USER_SETTINGS_GET_INITIAL_STATE_SUCCESS() {
   yield put(MapActions.initRequest());
@@ -618,6 +619,7 @@ function* handle_REMOVE_CUSTOM_LAYER(action) {
 }
 
 function* handle_REMOVE_SERVER_BOUNDARY(action) {
+  yield put(UserSettings.KML.toggle(action.payload.id, false));
   yield put(UserSettings.KML.delete(action.payload.id));
 }
 
