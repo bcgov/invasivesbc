d9bade2081aa586e9e913d9bc83b8bb4e3da37a5
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-08-12 09:18:40 -0700

    Refactor to add TileCacheDownloadProgress to section

--- app/src/UI/Features/ManageTripsPage/subcomponents/ManageMyTrips/ManageMyTrips.tsx
+++ app/src/UI/Features/ManageTripsPage/subcomponents/ManageMyTrips/ManageMyTrips.tsx
@@ -1,16 +1,20 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './ManageMyTrips.css';
 import { PlanMyTripCacheServiceFactory } from 'utils/plan-my-trip-cache/context';
 import { IPlanMyTripRepositoryMetadata, PlanMyTripCacheService } from 'utils/plan-my-trip-cache';
 import { useSelector } from 'utils/use_selector';
 import MyTrip from './subcaches/MyTrip';
+import Accordion from 'UI/Reusable/Accordion/Accordion';
+import { TileCacheDownloadProgress } from 'UI/Features/TileCache/TileCacheDownloadProgress';
+import { Downloading } from '@mui/icons-material';
+import EmptyCollection from 'UI/Features/LegacyMap/LayerPicker/EmptyCollection/EmptyCollection';
 
 const ManageMyTrips = () => {
   const [tripService, setTripService] = useState<PlanMyTripCacheService>();
   const [repositories, setRepositories] = useState<IPlanMyTripRepositoryMetadata[]>([]);
   const lastUpdate = useSelector((state) => state.PlanMyTrip?.lastUpdate);
-  console.log(repositories, tripService);
 
+  // Update Repositories when changes noted or service ready
   useEffect(() => {
     if (!tripService) return;
     (async () => {
@@ -19,6 +23,7 @@ const ManageMyTrips = () => {
     })();
   }, [tripService, lastUpdate]);
 
+  // Get Service on Page load
   useEffect(() => {
     (async () => {
       const service = await PlanMyTripCacheServiceFactory.getPlatformInstance();
@@ -28,10 +33,21 @@ const ManageMyTrips = () => {
 
   return (
     <div id="manage-my-trips">
-      <h2>My Trips</h2>
-      {repositories.map((r) => (
-        <MyTrip trip={r} key={r.id} />
-      ))}
+      <div className="my-trips">
+        <h2>My Trips</h2>
+        <p>These are the trips you currently have planned</p>
+        {repositories.map((r) => (
+          <MyTrip trip={r} key={r.id} />
+        ))}
+        {repositories.length === 0 && <EmptyCollection text="You have no trips planned" />}
+      </div>
+      <div className="downloads">
+        <h2>Downloads</h2>
+        <p>Active Downloads and Progress can be found here</p>
+        <Accordion title="Map Download Progress" icon={<Downloading />}>
+          <TileCacheDownloadProgress />
+        </Accordion>
+      </div>
     </div>
   );
 };
