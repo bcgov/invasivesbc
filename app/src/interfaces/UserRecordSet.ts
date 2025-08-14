087b12959842a16c4a22cd577e679d1733d7e028
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-08-14 15:15:32 -0700

    remove colourscheme from record type

--- app/src/interfaces/UserRecordSet.ts
+++ app/src/interfaces/UserRecordSet.ts
@@ -29,20 +29,6 @@ export interface UserRecordSet {
   id: string;
   idList: Array<string | number>;
   ids_to_filter?: Array<string | number>; // May include Activity Short ID's, or Site IDs for IAPP
-  colorScheme?: {
-    Activity_Biocontrol_Collection: string;
-    Activity_Biocontrol_Release: string;
-    Activity_Monitoring_BiocontrolDispersal_TerrestrialPlant: string;
-    Activity_Monitoring_BiocontrolRelease_TerrestrialPlant: string;
-    Activity_Monitoring_ChemicalTerrestrialAquaticPlant: string;
-    Activity_Monitoring_MechanicalTerrestrialAquaticPlant: string;
-    Activity_Observation_PlantAquatic: string;
-    Activity_Observation_PlantTerrestrial: string;
-    Activity_Treatment_ChemicalPlantAquatic: string;
-    Activity_Treatment_ChemicalPlantTerrestrial: string;
-    Activity_Treatment_MechanicalPlantAquatic: string;
-    Activity_Treatment_MechanicalPlantTerrestrial: string;
-  };
   color?: string;
   drawOrder: number;
   expanded: boolean;
