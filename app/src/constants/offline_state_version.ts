58a7be4e0733c5a7336f9544a052196024aea693
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-08-19 15:24:37 -0700

    bump migration key

--- app/src/constants/offline_state_version.ts
+++ app/src/constants/offline_state_version.ts
@@ -1,5 +1,5 @@
 /*
   handle purging the offline storage on app version upgrade
 */
-export const CURRENT_MIGRATION_VERSION = 20250817;
+export const CURRENT_MIGRATION_VERSION = 20250819;
 export const MIGRATION_VERSION_KEY = '_persistedMigrationVersion';
