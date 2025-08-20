522a8f98775e5004af9a470017bf95ab696efae8
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-08-20 07:21:35 -0700

    Eliminate old configuration state values/actions

--- app/src/state/reducers/configuration.ts
+++ app/src/state/reducers/configuration.ts
@@ -1,49 +1,15 @@
-import { createNextState } from '@reduxjs/toolkit';
-import { Draft } from 'immer';
-import moment from 'moment/moment';
 import { AppConfig } from 'state/configuration/runtime-config';
-import { EXPORT_CONFIG_LOAD_ERROR, EXPORT_CONFIG_LOAD_REQUEST, EXPORT_CONFIG_LOAD_SUCCESS } from 'state/actions';
 import { UnifiedConfig } from 'state/configuration/unified-config';
 
 interface ConfigurationState {
   current: UnifiedConfig;
-  exportConfigLoading: boolean;
-  exportConfigFreshUntil: number | null;
-  exportConfig: null | unknown;
 }
 
 function createConfigurationReducerWithDefaultState(configuration: UnifiedConfig) {
   const initialState: ConfigurationState = {
-    current: configuration,
-    exportConfig: null,
-    exportConfigLoading: false,
-    exportConfigFreshUntil: 0
+    current: configuration
   };
-
-  return (state = initialState, action) => {
-    return createNextState(state, (draftState: Draft<ConfigurationState>) => {
-      switch (action.type) {
-        case EXPORT_CONFIG_LOAD_REQUEST: {
-          draftState.exportConfigFreshUntil = null;
-          draftState.exportConfigLoading = true;
-          draftState.exportConfig = null;
-          break;
-        }
-        case EXPORT_CONFIG_LOAD_ERROR: {
-          draftState.exportConfigFreshUntil = null;
-          draftState.exportConfigLoading = false;
-          draftState.exportConfig = null;
-          break;
-        }
-        case EXPORT_CONFIG_LOAD_SUCCESS: {
-          draftState.exportConfigFreshUntil = moment().add('15', 'minutes').valueOf();
-          draftState.exportConfigLoading = true;
-          draftState.exportConfig = action.payload;
-          break;
-        }
-      }
-    });
-  };
+  return (state = initialState, _action) => state;
 }
 
 const selectConfiguration: (state) => AppConfig = (state) => state.Configuration.current.runtime;
