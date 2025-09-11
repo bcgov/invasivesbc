bbd17f9f96df512e54858db55ef6b3d780793a39
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-09-11 09:55:28 -0700

    init commit LpGlobalFilters component

---
+++ app/src/UI/Features/LegacyMap/LayerPicker/LpGlobalFilters/LpGlobalFilters.tsx
@@ -0,0 +1,42 @@
+import { FilterAlt, FilterAltOff } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
+import { MapRecordsetLayerFilterCategory } from '../../helpers/functional/layer-definitions/reusable-layer-specifications';
+import TooltipWithIcon from 'UI/Reusable/TooltipWithIcon/TooltipWithIcon';
+import { useDispatch, useSelector } from 'utils/use_selector';
+import UserSettings from 'state/actions/userSettings/UserSettings';
+import './lpGlobalFilters.css';
+
+const LpGlobalFilters = () => {
+  const MAP_FILTER_TOOLTIP_TEXT = 'Show or hide the record layers on the map. This setting applies to all record sets.';
+  const handleToggleGlobalMapFilter = (key: MapRecordsetLayerFilterCategory) => {
+    dispatch(UserSettings.Map.toggleGlobalMapFilter(key));
+  };
+
+  const dispatch = useDispatch();
+
+  const globalMapFilters = useSelector((state) => state.Map.globalMapFilters);
+  return (
+    <div id="lp-global-map-filters">
+      <h3>
+        Map Filters <TooltipWithIcon tooltipText={MAP_FILTER_TOOLTIP_TEXT} />
+      </h3>
+      <ul>
+        {Object.entries(globalMapFilters).map(([key, value], index) => (
+          <>
+            {index !== 0 && <hr />}
+            <li
+              key={key}
+              onClick={() => handleToggleGlobalMapFilter(key as MapRecordsetLayerFilterCategory)}
+              className="lp-global-map-filter-option"
+            >
+              <IconButton>{value ? <FilterAlt /> : <FilterAltOff />}</IconButton>
+              {key}
+            </li>
+          </>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default LpGlobalFilters;
