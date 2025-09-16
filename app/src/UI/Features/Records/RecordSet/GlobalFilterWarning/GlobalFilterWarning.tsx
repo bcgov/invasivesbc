5ba867610ab5c6cafeddbd30392af7e52be83d07
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-09-16 13:39:52 -0700

    eliminate magic number

--- app/src/UI/Features/Records/RecordSet/GlobalFilterWarning/GlobalFilterWarning.tsx
+++ app/src/UI/Features/Records/RecordSet/GlobalFilterWarning/GlobalFilterWarning.tsx
@@ -7,14 +7,16 @@ import { WarningAmberRounded } from '@mui/icons-material';
 import TooltipWithIcon from 'UI/Reusable/TooltipWithIcon/TooltipWithIcon';
 
 const GlobalFilterWarning = () => {
+  const START_OF_SUBTYPES = 2; // ["!in", "activity_subtype", ...subtypes]
   const globalMapFilters = useSelector(selectGlobalRecordsetFilters);
+
   const alertMessage = useMemo<ReactNode>(() => {
     if (globalMapFilters == undefined) return null;
     return (
       <div className="global-filter-tooltip">
         <p>Global filters are active. The map will not show the following data:</p>
         <ul>
-          {(globalMapFilters as unknown as Array<string>).slice(2).map((f) => (
+          {(globalMapFilters as unknown as Array<string>).slice(START_OF_SUBTYPES).map((f) => (
             <li key={f}>{ActivitySubtypeShortLabels?.[f]}</li>
           ))}
         </ul>
