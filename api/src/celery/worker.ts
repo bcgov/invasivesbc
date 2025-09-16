426b858fb12c2c758fdfee49e4ce197007689e28
Robert Johnstone <rob@plasticviking.com>
2025-09-16 21:15:04 +0800

    Vector tile data access APIs

--- api/src/celery/worker.ts
+++ api/src/celery/worker.ts
@@ -1,19 +1,21 @@
 import { createWorker } from 'celery-node';
 import { AMQP_BROKER } from './config';
 import { getLogger } from 'utils/logger';
+import { MapGenerationService } from 'utils/map-generator/map-generation-service';
 
 const defaultLog = getLogger('worker');
 
-const worker = createWorker(
-  AMQP_BROKER,
-  AMQP_BROKER
-);
+const worker = createWorker(AMQP_BROKER, AMQP_BROKER);
 
 // demo task
 worker.register('tasks.add', (a: number, b: number) => a + b);
+worker.register('tasks.mapGeneration.process', async (id: string) => await MapGenerationService.processRequest(id));
 
-worker.start().then(() => {
-  defaultLog.info({ message: 'Worker startup' });
-}).catch(e => {
-  defaultLog.error({ message: 'Worker error caught', error: e });
-});
+worker
+  .start()
+  .then(() => {
+    defaultLog.info({ message: 'Worker startup' });
+  })
+  .catch((e) => {
+    defaultLog.error({ message: 'Worker error caught', error: e });
+  });
