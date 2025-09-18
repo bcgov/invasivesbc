2eb1d2fef66260c942bf0446db5ae7bfd8c77423
Matthew Logan <62873746+LocalNewsTV@users.noreply.github.com>
2025-09-18 14:56:31 -0700

    [INV-4295] Add Warning when Global filters Applied (#4300)
    * Implement globalFilterWarning
    
    * Refactor ToolTipWithIcon to have overridable Icon
    
    * Add GlobalFilterWarning to Recordset banner
    
    * Text revision
    
    * eliminate magic number
    
    * Bump recordset margin
    
    * expand comment
    
    Signed-off-by: LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
    
    * Bump migration key
    
    ---------
    
    Signed-off-by: LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>

--- app/src/constants/offline_state_version.ts
+++ app/src/constants/offline_state_version.ts
@@ -1,5 +1,5 @@
 /*
   handle purging the offline storage on app version upgrade
 */
-export const CURRENT_MIGRATION_VERSION = 20250903;
+export const CURRENT_MIGRATION_VERSION = 20250918;
 export const MIGRATION_VERSION_KEY = '_persistedMigrationVersion';
