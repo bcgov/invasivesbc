de87db6e77f5ca1e8b4fb7275bce6b5743441b68
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-11-03 15:58:01 -0800

    Fix Geotracking Render

--- app/src/UI/Features/LegacyMap/Controls/TrackingButtonsContainer.tsx
+++ app/src/UI/Features/LegacyMap/Controls/TrackingButtonsContainer.tsx
@@ -7,7 +7,7 @@ const TrackingButtonsContainer = () => {
   const { status } = useSelector((state) => state.Map.track_me_draw_geo);
   const tracking = isTracking(status);
   const url = useSelector((state) => state.AppMode.url);
-  const isInActivity = url?.includes('Activity:');
+  const isInActivity = url?.includes('Activity');
 
   return (
     <>
