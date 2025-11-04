1a0d185d9d2f19c91385cd6e01132104cf73c34f
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-11-04 09:48:35 -0800

    Add security logs to requests where user lacks privilege

--- api/src/utils/validateRole.ts
+++ api/src/utils/validateRole.ts
@@ -1,4 +1,6 @@
 import { InvasivesRequest } from './auth-utils';
+import createSecurityLog from './createSecurityLog';
+import LoggerHandler from './endpoints/LoggerHandler';
 
 type ApiDocWithSecurity = {
   security?: Array<Record<string, string[]>>;
@@ -10,10 +12,17 @@ type ApiDocWithSecurity = {
  * @param auth User Authentication
  * @returns {Boolean} User has any required role
  */
-const verifyUserRole = (apiDoc: ApiDocWithSecurity, auth: InvasivesRequest): boolean => {
+const verifyUserRole = (apiDoc: ApiDocWithSecurity, req: InvasivesRequest): boolean => {
   const requiredRoles: Array<string> = apiDoc?.security?.[0]?.Bearer;
   if (!requiredRoles || requiredRoles.length === 0) return true;
-  return auth.authContext.roles.some(({ role_name }) => requiredRoles.includes(role_name));
+  const userHasAccess = req.authContext.roles.some(({ role_name }) => requiredRoles.includes(role_name));
+  if (!userHasAccess) {
+    new LoggerHandler(req?.route?.path).info(
+      'User lacks sufficient role(s)',
+      createSecurityLog(req, { eventType: 'unauthorized' }) as unknown as Record<PropertyKey, unknown>
+    );
+  }
+  return userHasAccess;
 };
 
 export default verifyUserRole;
