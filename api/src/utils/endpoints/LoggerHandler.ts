96f916c541432c0764f04f3710e177ebcca80e1f
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-11-03 16:12:31 -0800

    Logger WIP

--- api/src/utils/endpoints/LoggerHandler.ts
+++ api/src/utils/endpoints/LoggerHandler.ts
@@ -1,33 +1,35 @@
 import { LoggerWithContext } from 'utils/logger';
 
 class LoggerHandler extends LoggerWithContext {
-  private readonly namespace: string;
-  constructor(namespace: string = 'undefined') {
-    super(namespace);
-    this.namespace = namespace;
+  constructor(label: string = 'undefined') {
+    super(label);
   }
 
-  private buildLog(message: string, params?: Record<PropertyKey, unknown>) {
-    return {
-      ...params,
-      message,
-      namespace: `/api/${this.namespace}`,
-      
-      MDC: LoggerWithContext._loadMDC()
-    };
-  }
-  override info = (message: string, params?: Record<PropertyKey, unknown>) => this._instance.info(this.buildLog(message, params));
+  private readonly buildLog = (message: string, params?: Record<PropertyKey, unknown>) => ({
+    ...params,
+    message,
+    MDC: LoggerWithContext._loadMDC()
+  });
 
-  override debug = (message: string, params?: Record<PropertyKey, unknown>) => this._instance.debug(this.buildLog(message, params));
-
-  override warn = (message: string, params?: Record<PropertyKey, unknown>) => this._instance.warn(this.buildLog(message, params));
-
-  override error = (message: string, params?) => {
-    const stack = new Error().stack;
-    this._instance.error({message, ...params}, {
-      callingContext: stack
-    });
+  /* 
+    Logger Priority Order when settings ENV's 
+      1. Error
+      2. Warn
+      3. Info
+      4. Verbose
+      5. Debug
+  */
+  override error = (message: string, params: Error) => {
+    this._instance.error(params.stack, this.buildLog(message));
   };
+  override warn = (message: string, params?: Record<PropertyKey, unknown>) =>
+    this._instance.warn(this.buildLog(message, params));
+  override info = (message: string, params?: Record<PropertyKey, unknown>) =>
+    this._instance.info(this.buildLog(message, params));
+  verbose = (message: string, params?: Record<PropertyKey, unknown>) =>
+    this._instance.verbose(this.buildLog(message, params));
+  override debug = (message: string, params?: Record<PropertyKey, unknown>) =>
+    this._instance.debug(this.buildLog(message, params));
 }
 
 export default LoggerHandler;
--- api/src/utils/logger.ts
+++ api/src/utils/logger.ts
@@ -1,88 +1,108 @@
 import winston from 'winston';
 import _ from 'lodash';
 import YAML from 'js-yaml';
+import { InvasivesRequest } from './auth-utils';
 import { MDCAsyncLocal } from 'mdc';
+
+interface Context {
+  additionalContext: {
+    authContext?: InvasivesRequest['authContext'];
+    [key: PropertyKey]: unknown;
+  };
+  [key: PropertyKey]: unknown;
+}
 
 class LoggerWithContext {
   _instance: winston.Logger;
+  protected outputLabel: string;
 
   constructor(outputLabel = 'default') {
+    this.outputLabel = outputLabel;
+
     this._instance = winston.loggers.get(outputLabel, {
-      transports: [
-        new winston.transports.Console({
-          level: process.env.LOG_LEVEL || 'debug',
-          format: winston.format.combine(
-            winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-            winston.format.errors({ stack: true }),
-            winston.format.colorize(),
-            winston.format.printf(({ level, timestamp, label, message, ...meta }) => {
-              const optionalLabel = label ? ` ${label} - ` : '';
-              const preamble = `[${timestamp}] (${level}) (${outputLabel}):${optionalLabel}`;
-              //
-              let formattedMessage = '';
-              switch (typeof message) {
-                case 'object':
-                  try {
-                    formattedMessage = YAML.dump(message);
-                  } catch (_e) {
-                    formattedMessage = JSON.stringify(message, null, 2);
-                  }
-
-                  break;
-                case 'string':
-                case 'number':
-                case 'boolean':
-                case 'bigint':
-                  formattedMessage = `${message}`;
-                  break;
-                case 'undefined':
-                  formattedMessage = 'undefined';
-                  break;
-                default:
-                  formattedMessage = 'Unknown message';
-                  break;
-              }
-              const additionalContext = {
-                ...meta
-              };
-              _.forOwn(additionalContext, (prop) => {
-                if (prop === null) {
-                  delete additionalContext[prop];
-                }
-              });
-
-              if (additionalContext['MDC'] !== undefined && additionalContext['MDC'] === null) {
-                delete additionalContext['MDC'];
-              }
-
-              let formattedAdditionalContext = null;
-
-              if (_.keys(additionalContext).length > 0) {
-                try {
-                  formattedAdditionalContext = YAML.dump(additionalContext);
-                } catch (e) {
-                  try {
-                    formattedAdditionalContext = JSON.stringify(additionalContext, null, 2);
-                  } catch (f) {
-                    formattedAdditionalContext = 'Error in logger while dumping additional context object.';
-                  }
-                }
-              }
-
-              if (formattedAdditionalContext) {
-                const spacer = '-'.repeat(40);
-                return `${preamble}${formattedMessage}\n${formattedAdditionalContext}${spacer}`;
-              } else {
-                return `${preamble}${formattedMessage}`;
-              }
-            })
-          )
-        })
-      ]
+      transports: [this.createConsoleTransport()],
+      exceptionHandlers: [this.createConsoleTransport()]
     });
   }
 
-  static _loadMDC() {
+  private createConsoleTransport = () =>
+    new winston.transports.Console({
+      level: process.env.LOG_LEVEL || 'debug',
+      format: winston.format.combine(
+        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        winston.format.colorize({ all: true }),
+        winston.format.errors({ stack: true }),
+        winston.format.printf((args) => this.print(args))
+      )
+    });
+
+  protected print({ level, timestamp, message, ...meta }: Record<PropertyKey, unknown>) {
+    const preamble = `[${timestamp}][${level}][${this.outputLabel}]: `;
+    const formattedMessage: string = (() => {
+      switch (typeof message) {
+        case 'object': {
+          try {
+            return YAML.dump(message);
+          } catch (_e) {
+            return JSON.stringify(message, null, 2);
+          }
+        }
+        case 'string':
+        case 'number':
+        case 'boolean':
+        case 'bigint':
+          return `${message}`;
+        case 'undefined':
+          return 'undefined';
+        default:
+          return 'Unknown message';
+      }
+    })();
+
+    const { MDC, ...everythingElse } = meta;
+    let additionalContext = _.cloneDeep(everythingElse);
+
+    const formattedAdditionalContext = (() => {
+      const authContext: InvasivesRequest['authContext'] = (MDC as Context)?.additionalContext?.authContext;
+      if (authContext) {
+        const user_id: string = authContext?.user?.user_id ?? 'nil';
+        const roles: string = authContext?.roles?.map(({ role_name }) => role_name).join(', ') ?? 'nil';
+        const username: string = authContext?.user?.preferred_username ?? 'nil';
+        const activation_status: boolean = authContext?.user?.activation_status;
+
+        additionalContext = {
+          ...additionalContext
+        };
+
+        _.forOwn(additionalContext, (value: unknown, key: PropertyKey) => {
+          if (value === null || value === undefined) {
+            delete additionalContext[key];
+          }
+        });
+        if (_.keys(additionalContext).length > 0) {
+          try {
+            const activation = activation_status ? '' : '[USER IS NOT ACTIVATED]';
+            /**
+             * @example  ID: 1 | User: JohnSmith@provider | Roles: general_contractor, specialist
+             *           Result: [23]
+             */
+            return activation + `ID: ${user_id} | User: ${username} | Roles: ${roles}\n` + YAML.dump(additionalContext);
+          } catch (_e) {
+            try {
+              return JSON.stringify(additionalContext, null, 2);
+            } catch (_f) {
+              return 'Error in logger while dumping additional context object.';
+            }
+          }
+        }
+      }
+      return '';
+    })();
+    const spacer = formattedAdditionalContext ? '-'.repeat(40) : '';
+    return `${preamble}${formattedMessage}\n${formattedAdditionalContext ?? ''}${spacer}`;
+  }
+
+  static readonly _loadMDC = () => {
     const MDC = MDCAsyncLocal.getStore();
     if (MDC !== undefined) {
       let timeDelta: null | number = null;
@@ -91,28 +111,25 @@ class LoggerWithContext {
       return MDC;
     }
     return null;
-  }
+  };
 
-  info(params) {
+  // Methods for Calling Logs
+  public info(params) {
     this._instance.info({ ...params, MDC: LoggerWithContext._loadMDC() });
   }
 
-  debug(params) {
+  public debug(params) {
     this._instance.debug({ ...params, MDC: LoggerWithContext._loadMDC() });
   }
 
-  warn(params) {
+  public warn(params) {
     this._instance.warn({ ...params, MDC: LoggerWithContext._loadMDC() });
   }
 
-  error(params) {
-    const stack = new Error().stack;
-    this._instance.error(params, {
-      callingContext: stack
-    });
+  public error(message, error) {
+    this._instance.error({ message, error, MDC: LoggerWithContext._loadMDC() });
   }
 }
-
 
 export const getLogger = function (logLabel: string): LoggerWithContext {
   return new LoggerWithContext(logLabel);
