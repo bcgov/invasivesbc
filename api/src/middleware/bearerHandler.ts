6106ea3b3f5b8bff61da1077e9b93a8bcb559ada
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-11-05 15:24:09 -0800

    remove try/catch in bearerHandler, allow cascade to global handler

--- api/src/middleware/bearerHandler.ts
+++ api/src/middleware/bearerHandler.ts
@@ -1,21 +1,15 @@
 import { Request } from 'express';
 import { MDC, MDCAsyncLocal } from 'mdc';
 import { authenticate, InvasivesRequest } from 'utils/auth-utils';
-import LoggerHandler from 'utils/endpoints/LoggerHandler';
 import verifyUserRole from 'utils/validateRole';
 
 const bearerHandler = async (req: Request) => {
-  try {
-    let mdc = MDCAsyncLocal.getStore();
-    if (!mdc) {
-      mdc = new MDC();
-      await MDCAsyncLocal.run(mdc, authenticate, <InvasivesRequest>req);
-    } else {
-      await authenticate(<InvasivesRequest>req);
-    }
-  } catch (e) {
-    new LoggerHandler('bearerHandler').error(e);
-    return false;
+  let mdc = MDCAsyncLocal.getStore();
+  if (!mdc) {
+    mdc = new MDC();
+    await MDCAsyncLocal.run(mdc, authenticate, <InvasivesRequest>req);
+  } else {
+    await authenticate(<InvasivesRequest>req);
   }
   return verifyUserRole(req as InvasivesRequest);
 };
