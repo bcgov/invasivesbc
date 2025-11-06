9238b4989b5e93d362e798ade742670b403b907b
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-11-06 15:51:52 -0800

    refactor biocontrol-treatments

--- api/src/paths/biocontrol-treatments.ts
+++ api/src/paths/biocontrol-treatments.ts
@@ -2,71 +2,32 @@ import { RequestHandler } from 'express';
 import { Operation } from 'express-openapi';
 import SQL, { SQLStatement } from 'sql-template-strings';
 
-import { ALL_ROLES, SECURITY_ON } from 'constants/misc';
-import { getDBConnection } from 'database/db';
-import { getLogger } from 'utils/logger';
-import { PoolClient } from 'pg';
+import { ALL_ROLES } from 'constants/misc';
+import OpenAPISpec from 'utils/OpenAPISpec';
+import QueryHandler from 'utils/endpoints/QueryHandler';
 
-const defaultLog = getLogger('biocontrol-treatments');
-
-export const GET: Operation = [getBiocontrolTreatments()];
-
-GET.apiDoc = {
-  description: 'Fetches all Biocontrol treatment code pairings',
-  tags: ['biocontrol-treatments'],
-  security: SECURITY_ON ? [{ Bearer: ALL_ROLES }] : [],
-  responses: {
-    200: {
-      description: 'Biocontrol treatments response object',
-      content: {
-        'application/json': {
-          schema: {
-            properties: {}
-          }
+const GET: Operation = [getBiocontrolTreatments()];
+new OpenAPISpec('Fetches all Biocontrol treatment code pairings', ['biocontrol-treatments'])
+  .security(ALL_ROLES)
+  .response(200, {
+    description: 'Biocontrol treatments response object',
+    content: {
+      'application/json': {
+        schema: {
+          properties: {}
         }
       }
-    },
-    401: {
-      $ref: '#/components/responses/401'
-    },
-    503: {
-      $ref: '#/components/responses/503'
-    },
-    default: {
-      $ref: '#/components/responses/default'
     }
-  }
-};
+  })
+  .build(GET);
 
 function getBiocontrolTreatments(): RequestHandler {
-  return async (req, res, next) => {
-    let connection: PoolClient | undefined;
-
-    try {
-      connection = await getDBConnection();
-      const sqlStatement: SQLStatement = SQL`SELECT plant_code_name, agent_code_name
-                                             FROM plant_agent_treatment;`;
-      const response = await connection.query(sqlStatement.text, sqlStatement.values);
-      const result = response?.rows ?? null;
-
-      return res.status(200).json({
-        message: 'Biocontrol Treatments retrieved',
-        request: req.body,
-        result: result,
-        namespace: 'biocontrol-treatments',
-        code: 200
-      });
-    } catch (error) {
-      defaultLog.error({ label: 'getBiocontrolTreatments', message: 'error', error });
-      return res.status(500).json({
-        message: 'Database encountered an error',
-        request: req.body,
-        error: error,
-        namespace: 'biocontrol-treatments',
-        code: 500
-      });
-    } finally {
-      connection?.release();
-    }
+  return async (_r, res, _n) => {
+    const sqlStatement: SQLStatement = SQL`SELECT plant_code_name, agent_code_name
+                                           FROM plant_agent_treatment;`;
+    const { rows } = await new QueryHandler().query(sqlStatement);
+    return res.status(200).json({ result: rows });
   };
 }
+
+export { GET };
