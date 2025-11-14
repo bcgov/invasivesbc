485e3cdba3c53d201f61c79d175af6ab2ea49a09
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-11-13 16:12:01 -0800

    tidy execution. Submit batch records in groups of 10

--- api/src/utils/batch/execution.ts
+++ api/src/utils/batch/execution.ts
@@ -1,15 +1,17 @@
 import { randomUUID } from 'crypto';
 import { PoolClient } from 'pg';
 import moment from 'moment';
-import { activity_create_function, ActivityLetter, autofillChemFields, populateSpeciesArrays } from 'sharedAPI';
-import SQL from 'sql-template-strings';
+import {
+  activity_create_function,
+  ActivityLetter,
+  ActivitySubtype,
+  autofillChemFields,
+  populateSpeciesArrays
+} from 'sharedAPI';
+import SQL, { SQLStatement } from 'sql-template-strings';
 import { Template } from './definitions';
 import { mapTemplateFields } from './blob-utils';
-import { commit as commitContext } from 'utils/context-queries';
-import LoggerHandler from 'utils/endpoints/LoggerHandler';
 import { InvasivesRequest } from 'utils/auth-utils';
-
-const logger = new LoggerHandler('batch-execution');
 
 interface BatchExecutionResult {
   createdActivityIDs: string[];
@@ -26,7 +28,10 @@ function _mapToDBObject(row, status, type, subtype, userInfo): MappedForDB {
   const uuidToCreate = randomUUID();
   const shortYear = moment().format().substr(2, 2);
   const shortId = shortYear + ActivityLetter[subtype] + uuidToCreate.substr(0, 8).toUpperCase();
-
+  const tankMix = {
+    no: 'Chemical Treatment (No Tank Mix) - Application Method',
+    yes: 'Chemical Treatment (If Tank Mix) - Application Method'
+  };
   let mapped = activity_create_function(
     type,
     subtype,
@@ -46,19 +51,9 @@ function _mapToDBObject(row, status, type, subtype, userInfo): MappedForDB {
       mapped.form_data.activity_data.invasive_species_agency_code.join();
   }
 
-  if (['Activity_Treatment_ChemicalPlantTerrestrial', 'Activity_Treatment_ChemicalPlantAquatic'].includes(subtype)) {
-    const chemicalMethodSprayCodes = row.data[
-      'Chemical Treatment (If Tank Mix) - Application Method'
-    ]?.templateColumn.codes.map((codeObj) => {
-      return codeObj.code;
-    });
-
-    const chemicalMethodCodes = row.data[
-      'Chemical Treatment (No Tank Mix) - Application Method'
-    ]?.templateColumn.codes.map((codeObj) => {
-      return codeObj.code;
-    });
-
+  if ([ActivitySubtype.Treatment_ChemicalPlant, ActivitySubtype.Treatment_ChemicalPlantAquatic].includes(subtype)) {
+    const chemicalMethodSprayCodes = row.data[tankMix.yes]?.templateColumn.codes.map((codeObj) => codeObj.code);
+    const chemicalMethodCodes = row.data[tankMix.no]?.templateColumn.codes.map((codeObj) => codeObj.code);
     mapped = autofillChemFields(mapped, chemicalMethodSprayCodes, chemicalMethodCodes);
   }
 
@@ -73,22 +68,16 @@ function _mapToDBObject(row, status, type, subtype, userInfo): MappedForDB {
     const subtypeData = record?.form_data?.activity_subtype_data;
     const path = blobPathMapping[record.activity_subtype];
 
-    if (!subtypeData || !path || !Array.isArray(subtypeData[path])) {
-      return record;
-    }
+    if (!subtypeData || !path || !Array.isArray(subtypeData[path])) return record;
 
     subtypeData[path].forEach((item) => {
-      if (!item.actual_biological_agents && !item.estimated_biological_agents) {
-        return;
-      }
-
+      if (!item.actual_biological_agents && !item.estimated_biological_agents) return;
       if (item.actual_biological_agents) {
         const actualTotal = item.actual_biological_agents.reduce((sum, agent) => sum + agent.release_quantity, 0);
         item.total_bio_agent_quantity_actual = actualTotal;
         item.total_bio_agent_quantity_estimated = 0;
         delete item.estimated_biological_agents;
       }
-
       if (item.estimated_biological_agents) {
         const estimatedTotal = item.estimated_biological_agents.reduce((sum, agent) => sum + agent.release_quantity, 0);
         item.total_bio_agent_quantity_estimated = estimatedTotal;
@@ -101,10 +90,8 @@ function _mapToDBObject(row, status, type, subtype, userInfo): MappedForDB {
   }
 
   mapped = updateTotalBioAgentQuantity(mapped);
-
   mapped = populateSpeciesArrays(mapped);
-
-  mapped['form_data']['form_status'] = status;
+  mapped.form_data.form_status = status;
 
   const geog = mapped.geog;
   delete mapped.geog;
@@ -126,15 +113,33 @@ interface Config {
   validatedBatchData: any;
   desiredFinalStatus: 'Draft' | 'Submitted';
   errorRowsBehaviour: 'Draft' | 'Skip';
-  userInfo: any;
-  req: InvasivesRequest;
+  userInfo: InvasivesRequest['authContext']['user'];
 }
+
 export const BatchExecutionService = {
   executeBatch: async (config: Config): Promise<BatchExecutionResult> => {
-    const { dbConnection, id, template, validatedBatchData, desiredFinalStatus, errorRowsBehaviour, userInfo, req } =
-      config;
+    /**
+     * @desc bulk insert batch values, empty array after upload.
+     */
+    const batchUploadCurrentEntries = async (): Promise<void> => {
+      if (insertValues.length === 0) return; // Array is empty, skip transaction.
+      const sql = SQL`
+        INSERT INTO activity_incoming_data(
+          activity_id,          short_id,     activity_payload,  batch_id,         activity_type,
+          activity_subtype,     form_status,  created_by,        updated_by,       created_by_with_guid,
+          updated_by_with_guid, geog,         row_number,        species_positive, species_negative,
+          species_treated,      platform_src         
+        ) VALUES
+      `;
+      insertValues.forEach((row, index) => {
+        sql.append(row);
+        if (index !== insertValues.length - 1) sql.append(',');
+      });
+      await dbConnection.query(sql.text, sql.values);
+      insertValues.length = 0; // empty array.
+    };
 
-    logger.info(`Starting batch exec run, status->${desiredFinalStatus}, error rows->${errorRowsBehaviour}`);
+    const { dbConnection, id, template, validatedBatchData, desiredFinalStatus, errorRowsBehaviour, userInfo } = config;
     const createdIds = [];
     const sql = SQL`
       SELECT status
@@ -142,28 +147,21 @@ export const BatchExecutionService = {
       WHERE id = ${id}
       AND status = 'NEW'
     `;
-    const statusQueryResult = await dbConnection.query(sql.text, sql.values);
-    if (statusQueryResult.rowCount !== 1) throw new Error('Batch not in executable status');
+    const statusQueryResult = (await dbConnection.query(sql.text, sql.values)).rowCount;
+    if (statusQueryResult !== 1) throw new Error('Batch not in executable status');
 
     await dbConnection.query(SQL`BEGIN`);
 
+    const insertValues: Array<SQLStatement> = [];
     try {
       for (const [index, row] of validatedBatchData.rows.entries()) {
-        let errorRow = false;
-        if (row.rowValidationResult.find((vr) => !vr.valid)) {
-          errorRow = true;
-        } else {
-          Object.values(row.data).forEach((propertyValue: any) => {
-            if (
-              (propertyValue.validationMessages.length > 0 &&
-                propertyValue.validationMessages.find((vm) => vm.severity === 'error')) ||
-              row.RowValidationResult
-            ) {
-              errorRow = true;
-            }
-          });
-        }
-        if (errorRow && errorRowsBehaviour === 'Skip') continue;
+        const hasErrorRow =
+          row.rowValidationResult.some(({ valid }) => !valid) ||
+          Object.values(row.data).some(
+            (v: any) => v?.validationMessages?.some(({ severity }) => severity === 'error') || row.RowValidationResult
+          );
+
+        if (hasErrorRow && errorRowsBehaviour === 'Skip') continue;
 
         const {
           id: activityId,
@@ -182,39 +180,18 @@ export const BatchExecutionService = {
         const sTreat = JSON.stringify(payload['species_treated']);
         const sNeg = JSON.stringify(payload['species_negative']);
         const sPos = JSON.stringify(payload['species_positive']);
-        const errEntry = errorRow ? errorRowsBehaviour : desiredFinalStatus;
-        const qc = SQL`
-          INSERT INTO activity_incoming_data(
-            activity_id,          short_id,    activity_payload, batch_id,         activity_type,
-            activity_subtype,     form_status, created_by,       updated_by,       created_by_with_guid,
-            updated_by_with_guid, geog,        row_number,       species_positive, species_negative,
-            species_treated,      platform_src
-          )
-          VALUES (
-            ${activityId},       ${shortId},                ${payload},                     ${id},                          ${template.type},
-            ${template.subtype}, ${errEntry},               ${userInfo.preferred_username}, ${userInfo.preferred_username}, ${guid},
-            ${guid},             ${geog},                   ${index},                       ${sPos},                        ${sNeg},
-            ${sTreat},           ${payload['platform_src']}
-          )
-        `;
+        const errEntry = hasErrorRow ? errorRowsBehaviour : desiredFinalStatus;
 
-        logger.debug('[executeBatch]: Inserting new Entry', {
-          activity_id: activityId,
-          short_id: shortId,
-          batch_id: Number(id)
-        });
-
-        await dbConnection.query(qc.text, qc.values);
-        // Add BCGW Data
-        const { latitude, longitude } = payload?.form_data?.activity_data;
-        const context = {
-          activity_id: activityId,
-          latitude,
-          longitude,
-          db: dbConnection
-        };
-        await commitContext(context);
+        insertValues.push(SQL` (
+            ${activityId},        ${shortId},                 ${payload},                     ${id},                          ${template.type},
+            ${template.subtype},  ${errEntry},                ${userInfo.preferred_username}, ${userInfo.preferred_username}, ${guid},
+            ${guid},              ${geog},                    ${index},                       ${sPos},                        ${sNeg},
+            ${sTreat},            ${payload['platform_src']} 
+          )
+        `);
+        if (insertValues.length >= 10) await batchUploadCurrentEntries();
       }
+      await batchUploadCurrentEntries(); // Insert any remaining rows
     } catch (e) {
       await dbConnection.query(SQL`ROLLBACK`);
       throw e;
@@ -226,8 +203,6 @@ export const BatchExecutionService = {
       WHERE id = ${id}
       AND status = 'NEW';  
     `);
-
-    logger.info('[executeBatch]: Finishing Exec Run', { id });
     return { createdActivityIDs: createdIds };
   }
 };
