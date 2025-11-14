2bed8f930b962495e82808e1bc4fc4008ee2dee5
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2025-11-13 16:07:03 -0800

    Add admin endpoint to call autofill on a batch upload

---
+++ api/src/paths/batch/autofill/{id}.ts
@@ -0,0 +1,31 @@
+import { RequestHandler } from 'express';
+import { Operation } from 'express-openapi';
+import { Role } from 'constants/misc';
+import OpenAPISpec from 'utils/OpenAPISpec';
+import { autofillBatch } from 'utils/batch/autofillBatch';
+
+const GET: Operation = [batchAutofill()];
+
+new OpenAPISpec('Retro populate the autofill details for a legacy batch record', ['batch'])
+  .security([Role.MASTER_ADMINISTRATOR])
+  .parameters({
+    description: 'batch record id',
+    in: 'path',
+    name: 'id',
+    required: true,
+    schema: {
+      type: 'string',
+      pattern: '^[0-9]+$'
+    }
+  })
+  .response(204, { description: 'The resource updated successfully' })
+  .build(GET);
+
+function batchAutofill(): RequestHandler {
+  return async (req, res) => {
+    const id = parseInt(req.params.id);
+    await autofillBatch(id);
+    return res.sendStatus(204);
+  };
+}
+export { GET };
