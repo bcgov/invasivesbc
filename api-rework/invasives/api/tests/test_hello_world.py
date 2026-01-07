0c21cc0da587d779dd39b5fa92ae2806f1d4411a
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-01-07 15:14:09 -0800

    Implement simple Tests

--- api-rework/invasives/api/tests/test_hello_world.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-
-class HelloWorld(TestCase):
-    def setUp(self):
-      print("Hello World")
-
-    def test_hello_world(self):
-        self.assertEqual("Hello World", "Hello World")
