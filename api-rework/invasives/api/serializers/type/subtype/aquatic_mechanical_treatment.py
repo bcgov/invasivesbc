681ed3071f2e7aa6834d990e675315208e4c3848
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-01-14 09:15:45 -0800

    Add aquatic mechanical treatment serializer

---
+++ api-rework/invasives/api/serializers/type/subtype/aquatic_mechanical_treatment.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from api.models.activity import AquaticPlantMechanicalTreatment
+from api.serializers.common import ShorelineTypesSerializer
+
+class AquaticPlantMechanicalTreatmentSerializer(serializers.ModelSerializer):
+   class Meta:
+      model = AquaticPlantMechanicalTreatment
+      fields = (
+        "disposed_material_amount",
+        "disposed_material_format",
+        "disposal_method",
+        "invasive_plant",
+        "mechanical_method",
+        "treated_area_msq",
+      )
+
+class AquaticPlantTreatmentMechanicalSerializer(serializers.Serializer):
+  mechanical_treatments = AquaticPlantMechanicalTreatmentSerializer(source="aquaticplantmechanicaltreatment_set", many=True)
+  authorization_info = serializers.CharField(source="aquaticmechanicalauthorization.authorization_information")
+  shoreline_types = ShorelineTypesSerializer(source="shorelinetypes_set", many=True)
