4030be1ed40d8f153ad6fcb124c05caf80469203
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-01-15 10:57:00 -0800

    Add Mech/Chem monitoring serializers

---
+++ api-rework/invasives/api/serializers/type/subtype/mechanical_treatment_monitoring.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from api.serializers.common import TreatmentMonitoringInfoSerializer
+
+
+class MechanicalMonitoringSerializer(serializers.Serializer):
+  treatment_monitoring_information = serializers.SerializerMethodField()
+
+  def get_treatment_monitoring_information(self, obj):
+    return TreatmentMonitoringInfoSerializer(obj, context=self.context).data
+
+  def to_representation(self, instance):
+    """Flatten"""
+    ret = super().to_representation(instance)
+    info_data = ret.pop('treatment_monitoring_information', None)
+
+    if info_data and isinstance(info_data, dict):
+        ret.update(info_data)
+    return ret
