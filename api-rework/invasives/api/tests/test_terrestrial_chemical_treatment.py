253859a998860c52a235874b23b5ba654518beda
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-01-21 13:31:46 -0800

    Implement Subtype Tests

--- api-rework/invasives/api/tests/test_terrestrial_chemical_treatment.py
+++ api-rework/invasives/api/tests/test_terrestrial_chemical_treatment.py
@@ -16,3 +16,34 @@ class TerrestrialChemicalTreatmentTest(BaseActivitySubtypeTest):
 
     def test_casting_fixture_into_serializer(self):
         self.casting_fixture_into_serializer(expected_subtype_key='well_information')
+
+    def test_subtype_values(self):
+      record = self.fetch_a().json()
+      sd = record["subtype_data"]
+      self.assertIsNotNone(sd)
+
+      self.assertEqual(sd["service_license_number"], "0")
+      self.assertEqual(sd["pesticide_use_permit"], "none")
+      self.assertEqual(sd["pest_management_plan"], "BCGOV")
+      self.assertEqual(sd["temperature_c"], 23)
+      self.assertEqual(sd["wind_speed_kmh"], 33)
+      self.assertEqual(sd["application_start_time"], "2025-01-01T00:00:00Z")
+      self.assertEqual(sd["wind_direction"], "NW")
+      self.assertEqual(sd["humidity"], 30)
+      self.assertEqual(sd["treatment_notice_signs"], "Yes")
+      self.assertEqual(sd["precautionary_statement"], "COM")
+      self.assertEqual(sd["ntz_reduction_bool"], True)
+      self.assertEqual(sd["rationale_for_ntz_reduction"], "stated rationale")
+      self.assertEqual(sd["additional_unmapped_well_water_bool"], True)
+      self.assertEqual(sd["pest_injury_threshold_determination_bool"], True)
+
+      wells = sd["well_information"]
+      self.assertEqual(len(wells), 3)
+
+      for well in wells:
+          self.assertIsNotNone(well["well_tag_number"])
+          self.assertIsNotNone(well["distance"])
+
+      if sd["chem_treatment_details"] != "NOT IMPLEMENTED":
+          #TODO
+          self.fail("UPDATE TESTS FOR CHEM TREATMENT DETAILS")
