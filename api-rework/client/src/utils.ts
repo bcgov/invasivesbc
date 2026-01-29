8e8f6c38e3eac35683e49d85f5e5acec6592d108
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-01-29 14:39:47 -0800

    refactor truthy

--- api-rework/client/src/utils.ts
+++ api-rework/client/src/utils.ts
@@ -1,7 +1,7 @@
 import { KeyValue } from 'constants';
 
 const stringify = (val: string | KeyValue) => {
-  if (!val) return 'NO DATA';
+  if (val == undefined) return 'NO DATA';
 
   if (typeof val === 'string' || typeof val === 'number') {
     return val;
