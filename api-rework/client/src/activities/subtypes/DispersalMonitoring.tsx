ef44d17f4657311fce22fb823a969f6a169ee20d
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-02-02 11:42:13 -0800

    Split microsites

--- api-rework/client/src/activities/subtypes/DispersalMonitoring.tsx
+++ api-rework/client/src/activities/subtypes/DispersalMonitoring.tsx
@@ -1,18 +1,14 @@
 import { SubtypeData } from 'constants';
 import BiocontrolMonitoring from './common/BiocontrolMonitoring';
 import WeatherConditions from './common/WeatherConditions';
-import TextInput from 'common-components/inputs/TextInput';
-import Fieldset from 'common-components/inputs/Fieldset';
 import TargetPlantPhenology from './common/TargetPlantPhenology';
+import MicrositeConditions from './common/MicrositeConditions';
 
 const DispersalMonitoring = ({ subtypeData }: SubtypeData) => {
   return (
     <>
       <WeatherConditions subtypeData={subtypeData} />
-      <Fieldset label={'microsite conditions'}>
-        <TextInput label={'site surface shape'} value={subtypeData?.microsite_conditions?.site_surface_shape} />
-        <TextInput label={'mesoslope position'} value={subtypeData?.microsite_conditions?.mesoslope_position} />
-      </Fieldset>
+      <MicrositeConditions microsite_conditions={subtypeData?.microsite_conditions} />
       <BiocontrolMonitoring monitoring_information={subtypeData?.monitoring_information} />
       <TargetPlantPhenology targetPlantPhenology={subtypeData?.target_plant_phenology} />
     </>
