81908c30bd3a587c72bacc3f1c24b47c91f38d79
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-02-05 10:39:29 -0800

    add mock enums file

---
+++ app/src/UI/Features/Records/Activity/forms/enums.ts
@@ -0,0 +1,122 @@
+import FormCode from 'interfaces/FormCode';
+
+const YesNoUnknown: Array<FormCode> = [
+  {
+    full_name: 'Yes',
+    code: 'Yes',
+    table: 'YesNoUnknown'
+  },
+  {
+    full_name: 'No',
+    code: 'No',
+    table: 'YesNoUnknown'
+  },
+  {
+    full_name: 'Unknown',
+    code: 'Unknown',
+    table: 'YesNoUnknown'
+  }
+];
+
+const YesNo: Array<FormCode> = [
+  {
+    full_name: 'Yes',
+    code: 'Yes',
+    table: 'YesNoUnknown'
+  },
+  {
+    full_name: 'No',
+    code: 'No',
+    table: 'YesNoUnknown'
+  }
+];
+
+const ObservationType: Array<FormCode> = [
+  {
+    full_name: 'Positive Observation',
+    code: 'Positive Observation',
+    table: 'ObservationType'
+  },
+  {
+    full_name: 'Negative Observation',
+    code: 'Negative Observation',
+    table: 'ObservationType'
+  }
+];
+
+const WaterbodyType: Array<FormCode> = [
+  {
+    full_name: 'Bog',
+    code: 'Bog',
+    table: 'WaterbodyType'
+  },
+  {
+    full_name: 'Confined Pond',
+    code: 'Confined Pond',
+    table: 'WaterbodyType'
+  },
+  {
+    full_name: 'Discharging Pond',
+    code: 'Discharging Pond',
+    table: 'WaterbodyType'
+  },
+  {
+    full_name: 'Ditch',
+    code: 'Ditch',
+    table: 'WaterbodyType'
+  },
+  {
+    full_name: 'Intertidal',
+    code: 'Intertidal',
+    table: 'WaterbodyType'
+  },
+  {
+    full_name: 'Lake',
+    code: 'Lake',
+    table: 'WaterbodyType'
+  },
+  {
+    full_name: 'River',
+    code: 'River',
+    table: 'WaterbodyType'
+  },
+  {
+    full_name: 'Slough',
+    code: 'Slough',
+    table: 'WaterbodyType'
+  },
+  {
+    full_name: 'Stream',
+    code: 'Stream',
+    table: 'WaterbodyType'
+  }
+];
+
+const WaterLevelManagement = [
+  {
+    full_name: 'Dam',
+    code: 'Dam',
+    table: 'WaterLevelManagement'
+  },
+  {
+    full_name: 'None',
+    code: 'None',
+    table: 'WaterLevelManagement'
+  },
+  {
+    full_name: 'Other',
+    code: 'Other',
+    table: 'WaterLevelManagement'
+  },
+  {
+    full_name: 'Station',
+    code: 'Station',
+    table: 'WaterLevelManagement'
+  },
+  {
+    full_name: 'Weir',
+    code: 'Weir',
+    table: 'WaterLevelManagement'
+  }
+];
+export { ObservationType, YesNo, YesNoUnknown, WaterbodyType, WaterLevelManagement };
