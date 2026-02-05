da07a0c07988c8db9186708112d3676073e5ac08
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-02-05 14:12:55 -0800

    init commit multi-select

---
+++ app/src/UI/Features/Records/Activity/forms/common/MultiSelect/MultiSelect.tsx
@@ -0,0 +1,58 @@
+import { Controller, FieldValues, useFormContext, Path } from 'react-hook-form';
+import Select from 'react-select';
+import { RegisterOptions } from 'react-hook-form';
+import FormCode from 'interfaces/FormCode';
+import TooltipWithIcon from 'UI/Reusable/TooltipWithIcon/TooltipWithIcon';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import './MultiSelect.css';
+
+interface PropTypes<T extends FieldValues> {
+  name: Path<T>;
+  label?: string;
+  options: Array<FormCode>;
+  rules?: RegisterOptions<T, Path<T>>;
+  isSearchable?: boolean;
+  tooltip?: string;
+  placeholder?: string;
+}
+
+export function MultiSelect<T extends FieldValues>({
+  name,
+  label,
+  options,
+  rules,
+  isSearchable = false,
+  tooltip,
+  placeholder
+}: PropTypes<T>) {
+  const { control } = useFormContext<T>();
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      rules={rules}
+      render={({ field: { onChange, value, ref }, fieldState: { error } }) => (
+        <div className="form-multi-select-input">
+          {label && (
+            <label htmlFor={name}>
+              {label} {tooltip && <TooltipWithIcon tooltipText={tooltip} />}
+            </label>
+          )}
+          <Select
+            ref={ref}
+            isMulti
+            placeholder={placeholder}
+            isSearchable={isSearchable}
+            options={options.map((o) => ({ label: o.full_name, value: o.code }))}
+            onChange={(val) => onChange(val.map((c) => c.value))}
+            className="react-select"
+          />
+          <ErrorMessage error={error} />
+        </div>
+      )}
+    />
+  );
+}
+
+export default MultiSelect;
