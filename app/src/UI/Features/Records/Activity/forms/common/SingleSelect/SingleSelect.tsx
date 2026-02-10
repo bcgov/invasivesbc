c9daedc78b54c3f19fec490f9b901189b52f99d6
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-02-09 16:08:03 -0800

    update SingleSelect to handle primitives and code pairings

--- app/src/UI/Features/Records/Activity/forms/common/SingleSelect/SingleSelect.tsx
+++ app/src/UI/Features/Records/Activity/forms/common/SingleSelect/SingleSelect.tsx
@@ -29,31 +29,37 @@ export function SingleSelect<T extends FieldValues>({
   width
 }: PropTypes<T>) {
   const { control } = useFormContext<T>();
-
   return (
     <Controller
       name={name}
       control={control}
       rules={rules}
-      render={({ field: { onChange, ref }, fieldState: { error } }) => (
-        <div className={`form-single-select-input ${getInputWidth(width)}`}>
-          {label && (
-            <label htmlFor={name}>
-              {label} {tooltip && <TooltipWithIcon tooltipText={tooltip} />}
-            </label>
-          )}
-          <Select
-            ref={ref}
-            placeholder={placeholder}
-            isSearchable={isSearchable}
-            options={options.map((o) => ({ label: o.full_name, value: o.code }))}
-            onChange={onChange}
-            className="select-input"
-            noOptionsMessage={() => <option>No options available</option>}
-          />
-          <ErrorMessage error={error} label={label} />
-        </div>
-      )}
+      render={({ field: { onChange, ref, value }, fieldState: { error } }) => {
+        const mappedOptions = options.map((o) => ({
+          label: o.full_name,
+          value: o.code
+        }));
+        return (
+          <div className={`form-single-select-input ${getInputWidth(width)}`}>
+            {label && (
+              <label htmlFor={name}>
+                {label} {tooltip && <TooltipWithIcon tooltipText={tooltip} />}
+              </label>
+            )}
+            <Select
+              ref={ref}
+              placeholder={placeholder}
+              isSearchable={isSearchable}
+              options={mappedOptions}
+              value={mappedOptions.find((o) => o.value === (value?.code ?? value)) || null}
+              onChange={(opt) => onChange(opt?.value)}
+              className="select-input"
+              noOptionsMessage={() => 'No options available'}
+            />
+            <ErrorMessage error={error} label={label} />
+          </div>
+        );
+      }}
     />
   );
 }
