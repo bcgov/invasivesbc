a8528ae933c425decc5306d4437279d86cea0ac0
brennanwebster <Brennan.Webster@gov.bc.ca>
2026-02-10 15:58:00 -0800

    disable batch temp point templates

--- api/src/utils/batch/template-utils.ts
+++ api/src/utils/batch/template-utils.ts
@@ -11,43 +11,21 @@ import { TreatmentChemicalTerrestrialPlant } from './templates/treatment_chemica
 import { TreatmentChemicalAquaticPlant } from './templates/treatment_chemical_aquatic_plant';
 import { TreatmentMechanicalAquaticPlant } from './templates/treatment_mechanical_aquatic_plant';
 import { TreatmentMechanicalTerrestrialPlant } from './templates/treatment_mechanical_terrestrial_plant';
-import { ObservationTerrestrialPlantTemp } from './templates/observation_terrestrial_plant_temp';
-import { TreatmentChemicalTerrestrialPlantTemp } from './templates/treatment_chemical_terrestrial_plant_temp';
-import { TreatmentMechanicalTerrestrialPlantTemp } from './templates/treatment_mechanical_terrestrial_plant_temp';
-import { ObservationAquaticPlantTemp } from './templates/observation_aquatic_plant_temp';
-import { TreatmentChemicalAquaticPlantTemp } from './templates/treatment_chemical_aquatic_plant_temp';
-import { TreatmentMechanicalAquaticPlantTemp } from './templates/treatment_mechanical_aquatic_plant_temp';
 import { MonitoringBiocontrolReleaseTerrestrialPlant } from './templates/monitoring_biocontrol_release_terrestrial_plant';
 import { getDBConnection } from 'database/db';
-import { BiocontrolReleaseTemp } from './templates/biocontrol_release_temp';
-import { BiocontrolCollectionTemp } from './templates/biocontrol_collection_temp';
-import { MonitoringBiocontrolDispersalTerrestrialPlantTemp } from './templates/monitoring_biocontrol_dispersal_terrestrial_plant_temp';
-import { MonitoringChemicalTemp } from './templates/monitoring_chemical_treatment_temp';
-import { MonitoringMechanicalTemp } from './templates/monitoring_mechanical_treatment_temp';
 
 const templateList: Template[] = [
   ObservationTerrestrialPlant,
-  ObservationTerrestrialPlantTemp,
   ObservationAquaticPlant,
-  ObservationAquaticPlantTemp,
   TreatmentChemicalTerrestrialPlant,
-  TreatmentChemicalTerrestrialPlantTemp,
   TreatmentChemicalAquaticPlant,
-  TreatmentChemicalAquaticPlantTemp,
   TreatmentMechanicalTerrestrialPlant,
-  TreatmentMechanicalTerrestrialPlantTemp,
   TreatmentMechanicalAquaticPlant,
-  TreatmentMechanicalAquaticPlantTemp,
   MonitoringChemical,
-  MonitoringChemicalTemp,
   MonitoringMechanical,
-  MonitoringMechanicalTemp,
   BiocontrolRelease,
-  BiocontrolReleaseTemp,
   BiocontrolCollection,
-  BiocontrolCollectionTemp,
   MonitoringBiocontrolDispersalTerrestrialPlant,
-  MonitoringBiocontrolDispersalTerrestrialPlantTemp,
   MonitoringBiocontrolReleaseTerrestrialPlant
 ];
 
