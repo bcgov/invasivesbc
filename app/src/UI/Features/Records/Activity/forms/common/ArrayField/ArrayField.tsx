574dd3be2ee3e35c1ff524704db0b6ed863887f4
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-02-11 11:50:38 -0800

    update ArrayField to hold array level validation, and debounce validation to avoid spiking memory

--- app/src/UI/Features/Records/Activity/forms/common/ArrayField/ArrayField.tsx
+++ app/src/UI/Features/Records/Activity/forms/common/ArrayField/ArrayField.tsx
@@ -1,36 +1,65 @@
-import { useFormContext, useFieldArray, ArrayPath, FieldValues } from 'react-hook-form';
+import {
+  useFormContext,
+  useFieldArray,
+  FieldValues,
+  UseFieldArrayProps,
+  FieldArrayPath,
+  useWatch
+} from 'react-hook-form';
 import Fieldset from '../Fieldset/Fieldset';
 import './arrayField.css';
 import ErrorMessage from '../ErrorMessage/ErrorMessage';
 import { Width } from '../utils';
+import { useCallback, useEffect } from 'react';
+import debounce from 'lodash.debounce';
 
-interface PropTypes<T extends FieldValues> {
-  emptyValue: any; // What to append when "Add" is clicked
+// FieldArrayPath ensures the name provided is specifically a valid array path
+interface PropTypes<T extends FieldValues, Name extends FieldArrayPath<T>> {
+  emptyValue: any;
   label: string;
-  name: ArrayPath<T>;
+  name: Name;
+  rules?: UseFieldArrayProps<T, Name>['rules'];
   tooltip?: string;
   width?: Width;
   renderRow: (index: number, remove: (index: number) => void) => React.ReactNode;
 }
-
-export function ArrayField<T extends FieldValues>({
+const TRIGGER_DELAY = 500; //ms
+export function ArrayField<T extends FieldValues, Name extends FieldArrayPath<T>>({
   emptyValue,
   label,
   name,
   renderRow,
+  rules,
   tooltip,
   width
-}: PropTypes<T>) {
+}: PropTypes<T, Name>) {
+  /**
+   * Ensures access to errors whether top level, or in a sub state (represented by dot notation)
+   */
+  const getNestedError = (errorObj: any, name: string) =>
+    name.split('.').reduce((acc, part) => acc && acc[part], errorObj);
+
+  const debouncedTrigger = useCallback(
+    debounce(() => trigger(name as any), TRIGGER_DELAY),
+    []
+  );
   const {
     control,
-    formState: { errors }
+    formState: { errors },
+    trigger
   } = useFormContext<T>();
-  const { fields, append, remove } = useFieldArray({ control, name });
+  const { fields, append, remove } = useFieldArray({ control, name, rules });
+  const watchedValues = useWatch({ control, name: name as any });
 
-  const rootError = (errors[name] as any)?.root;
+  // Trigger Array level validation when any internal values change
+  useEffect(() => {
+    if (watchedValues) debouncedTrigger();
+  }, [watchedValues, trigger, name]);
+
+  const rootError = getNestedError(errors, name)?.root;
   return (
     <Fieldset label={label} tooltip={tooltip} width={width}>
-      <div className={'field-array'}>
+      <div className="field-array">
         <div className="field-array-entries">
           {fields.map((field, index) => (
             <div key={field.id} className="field-array-row">
@@ -38,11 +67,10 @@ export function ArrayField<T extends FieldValues>({
             </div>
           ))}
         </div>
-
-        <ErrorMessage error={rootError} label={label} />
         <button type="button" className="add-entry" onClick={() => append(emptyValue)}>
           + Add {label}
         </button>
+        {rootError && <ErrorMessage error={rootError} label={label} />}
       </div>
     </Fieldset>
   );
