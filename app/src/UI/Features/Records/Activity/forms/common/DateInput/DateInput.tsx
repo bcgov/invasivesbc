1eb8173d57616bf72c2a6faae101ffb1d089bbc9
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-02-11 15:19:36 -0800

    correct Required Bool on DateInput

--- app/src/UI/Features/Records/Activity/forms/common/DateInput/DateInput.tsx
+++ app/src/UI/Features/Records/Activity/forms/common/DateInput/DateInput.tsx
@@ -11,13 +11,13 @@ interface PropTypes extends InputHTMLAttributes<HTMLInputElement> {
   includeTime?: boolean;
   label?: string;
   tooltip?: string;
-  required: boolean;
+  required?: boolean;
   width?: Width;
 }
 
 // Use forwardRef so Hook Form can manage the input focus
 export const DateInput = forwardRef<HTMLInputElement, PropTypes>(
-  ({ includeTime = false, width, label, required, error, tooltip, ...props }, ref) => {
+  ({ includeTime = false, width, label, required = false, error, tooltip, ...props }, ref) => {
     return (
       <div className={`form-date-input ${getInputWidth(width)}`}>
         {label && (
