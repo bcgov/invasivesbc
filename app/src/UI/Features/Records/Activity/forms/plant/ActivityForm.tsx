6934fa156489f15b6b2e35db59bac5c02d571204
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-02-11 11:51:26 -0800

    update Validation on fields, add more form buttons

--- app/src/UI/Features/Records/Activity/forms/plant/ActivityForm.tsx
+++ app/src/UI/Features/Records/Activity/forms/plant/ActivityForm.tsx
@@ -6,8 +6,8 @@ import Fieldset from '../common/Fieldset/Fieldset';
 import DateInput from '../common/DateInput/DateInput';
 import NumberInput from '../common/NumberInput/NumberInput';
 import TextArea from '../common/TextArea/TextArea';
-import { checkSum, minArrayLength, noFutureDate } from '../common/validators';
-import { useCallback, useEffect } from 'react';
+import { checkSum, maxValue, minArrayLength, minValue, noFutureDate, noRepeatKey } from '../common/validators';
+import { MouseEvent, useCallback, useEffect } from 'react';
 import ArrayField from '../common/ArrayField/ArrayField';
 import SubtypeComposite from './SubtypeComposite';
 import { FormSchema } from './subtypeInterfaces';
@@ -20,19 +20,45 @@ import debounce from 'lodash.debounce';
 import FormActions from 'state/actions/activity/FormActions';
 import Alerts from 'state/actions/alerts/Alerts';
 import tripAlertMessages from 'constants/alerts/tripAlerts';
+import CreatableSelect from '../common/CreatableSelect.tsx/CreatableSelect';
+import Accordion from 'UI/Reusable/Accordion/Accordion';
+import Prompt from 'state/actions/prompts/Prompt';
+import { Debug } from 'UI/Reusable/Predicates/Debug';
 
-const FORM_UPDATE_THROTTLE_DELAY = 500; //ms
-const FORM_UPDATE_MAX_DELAY = 3000; //ms
+const FORM_UPDATE_THROTTLE_DELAY = 1000; //ms
+const FORM_UPDATE_MAX_DELAY = 5000; //ms
 
 const ActivityForm = () => {
   /**
    * @desc Initiate Mouseclick on Polygon draw icon, alert user to start drawing
    */
-  const handleDrawStart = () => {
+  const handleDrawStart = (evt: MouseEvent<HTMLButtonElement>) => {
+    evt?.preventDefault();
     (document.getElementsByClassName('mapbox-gl-draw_polygon')?.[0] as HTMLButtonElement).click();
     dispatch(Alerts.create(tripAlertMessages.drawToolClicked));
   };
 
+  const saveToDraft = (evt: MouseEvent<HTMLButtonElement>) => {
+    evt.preventDefault();
+    alert('Not implemented');
+  };
+
+  const handleClear = (evt: MouseEvent<HTMLButtonElement>) => {
+    evt.preventDefault();
+    dispatch(
+      Prompt.confirmation({
+        prompt: 'Do you want to clear your form? You will lose all progress.',
+        title: 'Clear Form',
+        confirmText: 'Clear form',
+        callback: (confirmation: boolean) => {
+          if (confirmation) {
+            dispatch(FormActions.clearFormState());
+            reset();
+          }
+        }
+      })
+    );
+  };
   /**
    * Update Geometry related fields when Redux state of Geom changes
    */
@@ -49,7 +75,7 @@ const ActivityForm = () => {
 
     GEOM_FIELDS.forEach((f) => {
       if (geometry_details?.[f] == undefined) {
-        resetField(f);
+        resetField(f, { defaultValue: '' });
       } else {
         setValue(f, geometry_details[f]);
       }
@@ -59,7 +85,7 @@ const ActivityForm = () => {
 
   // Redux state Handler
   const debouncedFormChange = useCallback(
-    debounce(() => dispatch(FormActions.updateFormState(getValues())), FORM_UPDATE_THROTTLE_DELAY, {
+    debounce(() => dispatch(FormActions.updateState(getValues())), FORM_UPDATE_THROTTLE_DELAY, {
       maxWait: FORM_UPDATE_MAX_DELAY,
       leading: false,
       trailing: true
@@ -71,16 +97,12 @@ const ActivityForm = () => {
 
   const codes = useSelector((state) => state.ActivityPage?.formCodes);
   const geometry_details = useSelector((state) => state.ActivityPage?.geometry_details);
+  const formType = useSelector((state) => state.ActivityPage.formType);
   const initState = useSelector((state) => state.ActivityPage?.formState);
 
   // Assign Props to sole variable to pass into FormProvider
   const methods = useForm<FormSchema>({
-    defaultValues: initState ?? {
-      jurisdictions: [{ jurisdiction: '', percent_covered: 0 }],
-      projects: [{ description: '' }],
-      funding_agencies: [{ invasive_species_agency_code: '' }]
-    },
-    mode: 'onChange'
+    mode: 'all'
   });
 
   // Destructure props used at this level
@@ -90,10 +112,10 @@ const ActivityForm = () => {
     getValues,
     control,
     formState,
+    reset,
     resetField,
     setValue,
     trigger,
-    unregister,
     watch,
     formState: { errors, isDirty }
   } = methods;
@@ -108,7 +130,6 @@ const ActivityForm = () => {
    *  - Register geom key (Shape on map, not assigned to component)
    */
   useEffect(() => {
-    register('jurisdictions', { validate: (val) => checkSum(val, 100, 'percent_covered') });
     register('geom');
   }, [register]);
 
@@ -124,14 +145,25 @@ const ActivityForm = () => {
   }, [geometry_details]);
 
   useEffect(() => {
-    // Debounce all internal state updates to persist in Redux
-    debouncedFormChange();
-  }, [allFormValues]);
+    // Only sync with redux when user changes something
+    if (isDirty) {
+      debouncedFormChange();
+    }
+  }, [allFormValues, isDirty]);
+
+  useEffect(() => {
+    // Triggers on initial load, sets data from redux state
+    if (initState !== undefined && !isDirty) {
+      const mutableState = structuredClone(initState);
+      reset({ ...mutableState });
+    }
+  }, []);
 
   return (
     <div className="activity-page">
       <FormProvider {...methods}>
         <form autoComplete={'off'} className="activity-form" onSubmit={handleSubmit(onSubmit)}>
+          <h2>Activity Subtype: {formType}</h2>
           {/* Start of Geometry Fields */}
           <Fieldset label={'Geometry Information'}>
             <p>To modify or update, please draw a new shape on the Map</p>
@@ -195,6 +227,15 @@ const ActivityForm = () => {
             />
           </Fieldset>
 
+          <Fieldset label={'Related Records'}>
+            <CreatableSelect<FormSchema, { short_id: string; full: string }>
+              name="linked_activities"
+              label="Linked Record ID"
+              options={[]}
+              labelKey="short_id"
+              valueKey="full"
+            />
+          </Fieldset>
           {/* Start Basic Information Fields */}
           <Fieldset label={'Basic Information'}>
             <DateInput
@@ -226,9 +267,16 @@ const ActivityForm = () => {
             />
             <Spacer x={150} y={10} />
             {/* Start of Jurisdictions section */}
-            <ArrayField<FormSchema>
+            <ArrayField<FormSchema, 'jurisdictions'>
               name="jurisdictions"
               label="Jurisdictions"
+              rules={{
+                validate: {
+                  minimumItems: (val) => minArrayLength(val, 1),
+                  totalPercent: (val) => checkSum(val, 100, 'percent_covered'),
+                  noRepeatJurisdiction: (val) => noRepeatKey(val, 'jurisdiction')
+                }
+              }}
               width={Width.Half}
               emptyValue={{ jurisdiction: '', percent_covered: 0 }}
               renderRow={(index, remove) => (
@@ -250,27 +298,18 @@ const ActivityForm = () => {
                     {...register(`jurisdictions.${index}.percent_covered`, {
                       required: true,
                       valueAsNumber: true,
-                      onChange: trigger.bind(this, 'jurisdictions')
+                      validate: {
+                        min: (val) => minValue(val, 1),
+                        max: (val) => maxValue(val, 100)
+                      }
                     })}
                   />
-                  <DeleteControl
-                    onClick={() => {
-                      remove(index);
-                      setTimeout(() => {
-                        // TODO: Hacky, remove this
-                        unregister('jurisdictions');
-                        register('jurisdictions', {
-                          validate: (val) => checkSum(val, 100, 'percent_covered')
-                        });
-                        trigger('jurisdictions');
-                      }, 10);
-                    }}
-                  />
+                  <DeleteControl onClick={() => remove(index)} />
                 </>
               )}
             />
             {/* Start of Project Codes  */}
-            <ArrayField<FormSchema>
+            <ArrayField<FormSchema, 'projects'>
               name="projects"
               label="Project Codes"
               tooltip='Optional field that can be added to a record to enable searching/sorting for records with that project code entered later. Multiple project codes may be added eg. project areas, contract identifiers. Replaces "paper file ID" field used in IAPP'
@@ -293,28 +332,40 @@ const ActivityForm = () => {
             <TextArea
               label={'Location Description'}
               id="location_description"
+              error={errors?.location_description}
               tooltip="Text entry to provide location directions. Locations should start general and get more specific"
               width={Width.Third}
-              {...register('location_description')}
+              {...register('location_description', { required: true, validate: (val) => minValue(val, 10) })}
             />
             <TextArea
               width={Width.Third}
               label={'Access Description'}
+              error={errors?.access_description}
               required
               tooltip="Text entry to provide access directions."
-              {...register('access_description', { required: true, minLength: 10 })}
+              {...register('access_description')}
             />
             <TextArea
               label={'Comment'}
+              error={errors?.comment}
               tooltip="Plain text description of any supporting information about the observation that is not captured elsewhere"
               width={Width.Third}
               {...register('comment')}
             />
           </Fieldset>
           <SubtypeComposite />
+
           {/* Submit Button is tied to react-hook-form */}
-          <input type="submit" />
-          <pre style={{ display: 'flex', textWrap: 'wrap' }}>{JSON.stringify(formData, null, 2)}</pre>
+          <div className="control">
+            <input type="submit" value="Submit Form" />
+            <button onClick={saveToDraft}>Save to Draft</button>
+            <button onClick={handleClear}>Clear Form</button>
+          </div>
+          <Debug>
+            <Accordion title={'Form State (JSON)'}>
+              <pre style={{ display: 'flex', textWrap: 'wrap' }}>{JSON.stringify(formData, null, 2)}</pre>
+            </Accordion>
+          </Debug>
         </form>
       </FormProvider>
     </div>
