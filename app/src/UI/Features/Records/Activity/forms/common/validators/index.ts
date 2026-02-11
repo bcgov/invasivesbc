a065346a665bc87480d8c9926b5db39199fdebc3
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-02-11 11:49:44 -0800

    Add/Update Validators

--- app/src/UI/Features/Records/Activity/forms/common/validators/index.ts
+++ app/src/UI/Features/Records/Activity/forms/common/validators/index.ts
@@ -1,3 +1,6 @@
 export { default as noFutureDate } from './noFutureDate';
 export { default as checkSum } from './checkSum';
 export { default as minArrayLength } from './minArrayLength';
+export { default as noRepeatKey } from './noRepeatKey';
+export { default as minValue } from './minValue';
+export { default as maxValue } from './maxValue';
---
+++ app/src/UI/Features/Records/Activity/forms/common/validators/noRepeatKey.ts
@@ -0,0 +1,11 @@
+/**
+ * @desc Iterate an Array of Objects searching if any key matches another. e.g.: Two entries have the same invasive plant
+ * @param arr Array of entries
+ * @param key Key to compare e.g. 'invasive_plant'
+ * @param keyLabel Plain language representation of key to appear in error message e.g. 'Invasive Plant'
+ */
+const noRepeatKey = (arr: Record<PropertyKey, unknown>[], key: PropertyKey, keyLabel?: string): boolean | string =>
+  new Set(arr.map((o) => o[key])).size === arr.length ||
+  `The same ${String(keyLabel ?? key)} cannot appear in multiple entries.`;
+
+export default noRepeatKey;
