586347fb4b0158906be86075d7a6e0c1e39f8ead
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-02-12 11:36:34 -0800

    prevent react crashing error in multi-select from type mismatch

--- app/src/UI/Features/Records/Activity/forms/common/MultiSelect/MultiSelect.tsx
+++ app/src/UI/Features/Records/Activity/forms/common/MultiSelect/MultiSelect.tsx
@@ -56,7 +56,7 @@ export function MultiSelect<T extends FieldValues>({
             isSearchable={options?.length >= MIN_OPTIONS_TO_ENABLE_SEARCH}
             options={options?.map((o) => ({ label: o.full_name, value: o.code })) ?? []}
             value={options
-              ?.filter((o) => (valueKey ? value?.some((v) => v?.[valueKey] === o.code) : value?.includes(o.code)))
+              ?.filter((o) => (valueKey ? value?.some?.((v) => v?.[valueKey] === o.code) : value?.includes(o.code)))
               ?.map((o) => ({ label: o.full_name, value: o.code }))}
             onChange={(val) =>
               onChange(valueKey ? val?.map((c) => ({ [valueKey]: c.value })) : val?.map((c) => c.value))
