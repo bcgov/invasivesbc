515257bf7c4ce7f27297cf981f4b59ddfcfc79ab
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-02-24 14:23:52 -0800

    update noRepeatKey to not count empty fields

--- app/src/UI/Features/Records/Activity/forms/common/validators/noRepeatKey.ts
+++ app/src/UI/Features/Records/Activity/forms/common/validators/noRepeatKey.ts
@@ -4,8 +4,12 @@
  * @param key Key to compare e.g. 'invasive_plant'
  * @param keyLabel Plain language representation of key to appear in error message e.g. 'Invasive Plant'
  */
-const noRepeatKey = (arr: Record<PropertyKey, unknown>[], key: PropertyKey, keyLabel?: string): boolean | string =>
-  new Set(arr.map((o) => o[key])).size === arr.length ||
-  `The same ${String(keyLabel ?? key)} cannot appear in multiple entries.`;
+const noRepeatKey = (arr: Record<PropertyKey, unknown>[], key: PropertyKey, keyLabel?: string): boolean | string => {
+  const removedEmpty = arr.filter((entry) => !!entry?.[key]); // Filter out empty keys
+  return (
+    new Set(removedEmpty.map((o) => o?.[key])).size === removedEmpty.length ||
+    `The same ${String(keyLabel ?? key)} cannot appear in multiple entries.`
+  );
+};
 
 export default noRepeatKey;
