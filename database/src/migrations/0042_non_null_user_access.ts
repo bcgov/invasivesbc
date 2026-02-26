27bc209111f4907dad4158b250858fc365c5399c
Robert Johnstone <rob@plasticviking.com>
2026-02-26 09:29:43 +0800

    adjust migration

--- database/src/migrations/0042_non_null_user_access.ts
+++ database/src/migrations/0042_non_null_user_access.ts
@@ -1,22 +1,30 @@
 import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  // Drop all violations
-  await knex.raw(`
-    DELETE FROM user_access 
-    WHERE user_id IS NULL 
-      OR role_id IS NULL;
-  `);
-  // Enforce Constraints to prevent future violations
-  await knex.raw(`
-    ALTER TABLE user_access
-    ALTER COLUMN user_id SET NOT NULL,
-    ALTER COLUMN role_id SET NOT NULL;
-  `);
+  await knex.raw(
+    //language=PostgreSQL
+    `
+      set search_path = invasivesbc,public;
+      DELETE
+      FROM user_access
+      WHERE user_id IS NULL
+         OR role_id IS NULL; -- delete all violations
+
+      ALTER TABLE user_access
+        ALTER COLUMN user_id SET NOT NULL,
+        ALTER COLUMN role_id SET NOT NULL;
+    `
+  );
 }
 
 export async function down(knex: Knex): Promise<void> {
   // Drop Constraints
-  await knex.raw(`ALTER TABLE user_access ALTER COLUMN role_id DROP NOT NULL;`);
-  await knex.raw(`ALTER TABLE user_access ALTER COLUMN user_id DROP NOT NULL;`)
+  await knex.raw(
+    //language=PostgreSQL
+    `
+    set search_path = invasivesbc,public;
+    ALTER TABLE user_access ALTER COLUMN role_id DROP NOT NULL;
+    ALTER TABLE user_access ALTER COLUMN user_id DROP NOT NULL;
+    `
+  );
 }
