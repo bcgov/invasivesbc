b6e024fc8f436c7df2863c4c90f1526c41fbe991
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-03-11 13:54:22 -0700

    init radio input

---
+++ app/src/UI/Features/Records/Activity/forms/common/RadioInput/RadioInput.tsx
@@ -0,0 +1,93 @@
+import { Controller, FieldValues, Path, RegisterOptions, useFormContext } from 'react-hook-form';
+import { getInputWidth, Width } from '../utils';
+import RequiredField from '../RequiredField/RequiredField';
+import TooltipWithIcon from 'UI/Reusable/TooltipWithIcon/TooltipWithIcon';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import FormCode from 'interfaces/FormCode';
+import './radioInput.css';
+import AdvisoryMessage from '../AdvisoryMessage/AdvisoryMessage';
+
+interface PropTypes<T extends FieldValues> {
+  readonly label?: string;
+  readonly name: Path<T>;
+  readonly options: Array<FormCode>;
+  readonly required?: boolean;
+  readonly rules?: RegisterOptions<T, Path<T>>;
+  readonly tooltip?: string;
+  readonly advisoryText?: string;
+  readonly width?: Width;
+}
+
+export function RadioInput<T extends FieldValues>({
+  label,
+  name,
+  options,
+  required = false,
+  rules,
+  advisoryText,
+  tooltip,
+  width
+}: PropTypes<T>) {
+  const { control } = useFormContext<T>();
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      rules={{
+        ...rules,
+        required: required ? rules?.required || 'This field is required' : rules?.required
+      }}
+      render={({ field: { onChange, onBlur, value, ref }, fieldState: { error } }) => {
+        /**
+         * @desc Transform bool strings back into booleans (radio inputs don't support)
+         * @param val
+         * @returns
+         */
+        const handleValueChange = (val: string) => {
+          if (val === 'true') return onChange(true);
+          if (val === 'false') return onChange(false);
+          onChange(val);
+        };
+        return (
+          <div className={`form-radio-input ${getInputWidth(width)}`}>
+            {label && (
+              <div className="top">
+                <label>
+                  {label}
+                  {required && <RequiredField />}
+                </label>
+                {tooltip && <TooltipWithIcon tooltipText={tooltip} />}
+              </div>
+            )}
+            <ul className="options">
+              {options.map((o) => {
+                const optionValue = String(o.code);
+                const isChecked = String(value) === optionValue;
+                return (
+                  <li key={optionValue}>
+                    <input
+                      type="radio"
+                      id={`${name}-${optionValue}`}
+                      name={name}
+                      value={optionValue}
+                      checked={isChecked}
+                      onChange={() => handleValueChange(optionValue)}
+                      onBlur={onBlur}
+                      ref={ref}
+                    />
+                    <label htmlFor={`${name}-${optionValue}`}>{o.full_name}</label>
+                  </li>
+                );
+              })}
+            </ul>
+            <ErrorMessage error={error} label={label} />
+            {advisoryText && <AdvisoryMessage text={advisoryText} />}
+          </div>
+        );
+      }}
+    />
+  );
+}
+
+export default RadioInput;
