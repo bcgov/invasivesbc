a683a5e704f87a27284cd37c5aa1e8d41f2210fc
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-03-17 15:31:54 -0700

    constrain linked_ids to pre-defined buckets

--- api-rework/invasives/api/viewsets/ids_within_bounds.py
+++ api-rework/invasives/api/viewsets/ids_within_bounds.py
@@ -2,6 +2,7 @@ from django.contrib.gis.geos import GEOSGeometry
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from api.serializers.linked_record_query import LinkedRecordQuerySerializer
+from api.models.activity.activity_subtypes import ActivitySubtypes, SubtypePrimary
 from api.models.activity import Activity
 import json
 
@@ -9,6 +10,17 @@ class IdsWithinBoundsViewSet(viewsets.GenericViewSet):
     queryset = Activity.objects.all()
     serializer_class = LinkedRecordQuerySerializer
 
+    def map_subtype_to_type(self, subtype):
+        modelled_type: SubtypePrimary = ActivitySubtypes[subtype].typeOfActivity
+        match modelled_type:
+            case SubtypePrimary.Treatment:
+                return SubtypePrimary.Observation
+            case SubtypePrimary.Monitoring:
+                return SubtypePrimary.Treatment
+            case SubtypePrimary.Biocontrol:
+                return SubtypePrimary.Observation
+            case _:
+                pass
 
     def create(self, request):
         subtype = request.data.get('subtype')
@@ -23,9 +35,9 @@ class IdsWithinBoundsViewSet(viewsets.GenericViewSet):
         try:
             search_geometry = GEOSGeometry(json.dumps(bounds))
             queryset = Activity.objects.filter(shape__intersects=search_geometry)
-
-            if subtype: # apply additional filter
-                queryset = queryset.filter(subtype=subtype)
+            if subtype:
+                primary = self.map_subtype_to_type(subtype)
+                queryset = queryset.filter(type=primary)
 
             serializer = LinkedRecordQuerySerializer(queryset, many=True)
 
