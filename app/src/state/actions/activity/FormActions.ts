81e96da5a51ea69bf03923ff1f6fae5a550b723f
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-03-23 13:44:45 -0700

    Add online functionality to Linked ID Lookup

--- app/src/state/actions/activity/FormActions.ts
+++ app/src/state/actions/activity/FormActions.ts
@@ -11,6 +11,7 @@ import { IQueryParams } from 'utils/record-cache';
 import { RecordCacheServiceFactory } from 'utils/record-cache/context';
 import { AlertSeverity, AlertSubjects } from 'constants/alertEnums';
 import EFilterType from 'constants/EFilterType';
+import { getCurrentJWT } from 'state/sagas/auth/auth';
 
 interface FormSubmission {
   data: FormSchema;
@@ -60,34 +61,42 @@ class FormActions {
     `${this.PREFIX}/validateManualLinkedId`,
     async ({ id }: VerifyLinkedActivity, { getState, dispatch }) => {
       const state: RootState = getState() as RootState;
+      const BASE_API = state.Configuration.current.runtime.API_V2_BASE;
       const MOBILE = state.Configuration.current.build.MOBILE;
-      const OFFLINE = !state.Network.connected;
-      /*
-    TODO: implement
-    if Online:
-      Ping API using the shortID, if response comes back valid, return the full ID of the record
-    */
-      // if (MOBILE && OFFLINE) {
-      const service = await RecordCacheServiceFactory.getPlatformInstance();
-      const queryObj: IQueryParams = {
-        limit: 1,
-        tableFilters: [
-          {
-            field: 'short_id',
-            filter: id,
-            filterType: EFilterType.Table,
-            id: '1',
-            operator: 'CONTAINS',
-            operator2: 'AND'
-          }
-        ],
-        recordSetType: RecordSetType.Activity,
-        selectColumns: ['id', 'short_id']
-      };
-      const data = (await service.query(queryObj))[0];
-      // Confirm the match in case of partial string matching returns
-      if (data.short_id === id) return data.id;
-      // }
+      const ONLINE = state.Network.connected;
+      if (ONLINE) {
+        // Check online for records existence
+        const res = await fetch(`${BASE_API}/activities/resolve/${id}`, {
+          headers: { Authorization: await getCurrentJWT(), 'Content-Type': 'application/json' }
+        });
+        if (res.ok) {
+          const record = await res.json();
+          return record.id;
+        }
+      }
+      if (MOBILE) {
+        // Check Cache for records existence
+        const service = await RecordCacheServiceFactory.getPlatformInstance();
+        const queryObj: IQueryParams = {
+          limit: 1,
+          tableFilters: [
+            {
+              field: 'short_id',
+              filter: id,
+              filterType: EFilterType.Table,
+              id: '1',
+              operator: 'CONTAINS',
+              operator2: 'AND'
+            }
+          ],
+          recordSetType: RecordSetType.Activity,
+          selectColumns: ['id', 'short_id']
+        };
+        const data = (await service.query(queryObj))[0];
+        // Confirm the match in case of partial string matching returns
+        if (data.short_id === id) return data.id;
+      }
+      // No Record Found Matching ID.
       dispatch(
         Alerts.create({
           severity: AlertSeverity.Error,
