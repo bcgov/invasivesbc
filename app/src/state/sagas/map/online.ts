1c4357e149994ea64830b6e936b12e6b1c481a32
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-03-31 13:29:09 -0700

    convert tablerowsGetOnline to target Django API

--- app/src/state/sagas/map/online.ts
+++ app/src/state/sagas/map/online.ts
@@ -5,31 +5,37 @@ import { InvasivesAPI_Call } from 'hooks/useInvasivesApi';
 import IappActions, { IappTableRowGetRequest } from 'state/actions/activity/Iapp';
 import Activity, { ActivityTableRowGetRequest } from 'state/actions/activity/Activity';
 import { buildTimeConfig } from 'state/configuration/build-time-config';
+import { selectConfiguration } from 'state/reducers/configuration';
+import { getCurrentJWT } from 'state/sagas/auth/auth';
 
 export function* handle_ACTIVITIES_TABLE_ROWS_GET_ONLINE(action: PayloadAction<ActivityTableRowGetRequest>) {
-  let mapState = yield select((state) => state.Map);
+  const mapState = yield select((state) => state.Map);
+  const { API_V2_BASE, MOBILE } = yield select(selectConfiguration);
+  const body = JSON.stringify({ filterObjects: [action.payload.filterObj] });
 
-  const networkReturn = yield InvasivesAPI_Call('POST', `/api/v2/activities/`, {
-    filterObjects: [action.payload.filterObj]
+  const response = yield fetch(`${API_V2_BASE}/recordset-rows`, {
+    method: 'POST',
+    headers: { Authorization: yield getCurrentJWT(), 'Content-Type': 'application/json' },
+    body
   });
 
-  mapState = yield select((state) => state.Map);
   const tableFiltersHash = mapState?.recordTables[action.payload.recordSetID]?.tableFiltersHash;
   if (tableFiltersHash !== action.payload.tableFiltersHash) {
     return;
   }
 
-  if (networkReturn?.ok && networkReturn.data.result) {
+  if (response?.ok) {
+    const result = yield response.json();
     yield put(
       Activity.getRowsSuccess({
         recordSetID: action.payload.recordSetID,
-        rows: networkReturn.data.result,
+        rows: result,
         tableFiltersHash: action.payload.tableFiltersHash,
         page: action.payload.page,
         limit: action.payload.limit
       })
     );
-  } else if (buildTimeConfig.MOBILE) {
+  } else if (MOBILE) {
     // API Request Failed, see if we can rows from a cache
     yield getRowsFromCachedRecordset(action.payload);
   }
