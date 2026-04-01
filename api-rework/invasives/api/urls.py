ffc1cafa10d508a75c280905c4bc63c67dddb63e
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-04-01 14:22:47 -0700

    Stub endpoint for submissions

--- api-rework/invasives/api/urls.py
+++ api-rework/invasives/api/urls.py
@@ -6,6 +6,7 @@ from api.viewsets.code import CodeViewSet
 from api.viewsets.migration import MigrationStatusViewSet
 from api.viewsets.ids_within_bounds import IdsWithinBoundsViewSet
 from api.viewsets.recordset_rows import RecordsetRowsViewSet
+from api.viewsets.submission import SubmissionViewSet
 
 from api.protocol.activity.api import router as activity_router
 
@@ -18,6 +19,7 @@ ROUTER.register(r"codes", CodeViewSet, "code")
 ROUTER.register(r"migrations", MigrationStatusViewSet, "migration")
 ROUTER.register(r"ids-within-bounds", IdsWithinBoundsViewSet, "ids-within-bounds")
 ROUTER.register(r"recordset-rows", RecordsetRowsViewSet, "recordsets")
+ROUTER.register(r"entries", SubmissionViewSet, "entries")
 
 ninja_api = NinjaAPI()
 ninja_api.add_router("/activities", activity_router)
