23d397b5c065be7c45589db89408178a758c923e
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-04-07 14:33:50 -0700

    remove unused imports

--- api-rework/client/src/activities/subtypes/AquaticChemTreatment.tsx
+++ api-rework/client/src/activities/subtypes/AquaticChemTreatment.tsx
@@ -1,6 +1,4 @@
-import TextInput from 'common-components/inputs/TextInput';
 import { SubtypeData } from 'constants';
-import Fieldset from 'common-components/inputs/Fieldset';
 import ChemTreatmentInfo from './common/ChemTreatmentInfo';
 
 const AquaticChemTreatment = ({ subtypeData }: SubtypeData) => <ChemTreatmentInfo subtypeData={subtypeData} />;
