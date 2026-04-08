df335864f4f9f3a98baa0fb8a6e0ec61c6ea4f06
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-04-08 09:53:50 -0700

    Resize header on url change

--- app/src/UI/Layout/OverlayLayout/OverlayHeader.tsx
+++ app/src/UI/Layout/OverlayLayout/OverlayHeader.tsx
@@ -4,8 +4,9 @@ import 'UI/Layout/OverlayLayout/OverlayHeader.css';
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import DragHandleIcon from '@mui/icons-material/DragHandle';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import ContextualPopover from '../ContextualPopover/ContextualPopover';
+import { useSelector } from 'utils/use_selector';
 
 const maximize = () => {
   setOverlayStyle({ top: '0px' });
@@ -32,11 +33,8 @@ const getOverlayAnchorDimensions = () => {
   const appElement = document.getElementById('overlay-anchor');
 
   if (appElement !== null) {
-    const currentAppStyle = window.getComputedStyle(appElement);
-    return {
-      height: currentAppStyle.height,
-      top: currentAppStyle.top
-    };
+    const { height, top } = window.getComputedStyle(appElement);
+    return { height, top };
   }
 
   return {
@@ -55,11 +53,8 @@ const throttledRestyle = debounce(
 
 const drag = (e: PointerEvent) => {
   e.preventDefault();
-
   const correctedClientY = e.clientY - 60;
-
   const SNAP_TO_NEAREST = 1; // set to 1 for no snap
-
   throttledRestyle({ top: `${Math.floor(correctedClientY / SNAP_TO_NEAREST) * SNAP_TO_NEAREST}px` });
 };
 
@@ -78,9 +73,15 @@ const onClickDragButton = (e) => {
 };
 
 export const OverlayHeader = () => {
+  const url = useSelector((state) => state.AppMode.url);
+
   useEffect(() => {
-    defaultPosition();
-  }, []);
+    // On URL Change, if the Overlay Header is below 50%, resize it. If greater, do nothing.
+    const overlayY = document.getElementsByClassName('overlay-header')?.[0]?.getBoundingClientRect()?.y;
+    const screenHeight = parseInt(getOverlayAnchorDimensions().height);
+    if (overlayY == undefined || screenHeight == undefined) return;
+    if (overlayY - 90 > screenHeight / 2) defaultPosition();
+  }, [url]);
 
   return (
     <div className="overlay-header">
