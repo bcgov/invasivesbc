cc268127fcc3de237aca31c7f4b31794bf98eda5
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-04-09 11:22:39 -0700

    MVP Voucher Specimen

---
+++ api-rework/invasives/api/protocol/activity/plant_subtypes/common/voucher_specimen.py
@@ -0,0 +1,14 @@
+from ninja import Schema
+from pydantic import Field
+
+class VoucherSpecimenSchema(Schema):
+    voucher_sample_id: str
+    herbarium: str
+    accession_number: str
+    completed_by_person: str
+    completed_by_org: str
+    utm_zone: int = Field(...)
+    utm_easting: int = Field(...)
+    utm_northing: int = Field(...)
+    date_collected: str = Field(...)
+    date_verified: str
