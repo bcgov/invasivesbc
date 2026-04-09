4ae3af1b32b3eee3790a13cace2049048a3686a7
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-04-09 14:29:00 -0700

    Add code constraints to base_form_schema

--- api-rework/invasives/api/protocol/activity/plant_subtypes/base_form_schema.py
+++ api-rework/invasives/api/protocol/activity/plant_subtypes/base_form_schema.py
@@ -1,8 +1,17 @@
-from typing import List, Union, TypedDict, Optional
+from typing import List, Union, Optional
+from datetime import date
 from ninja import Schema
-from pydantic import Field, model_validator
+from pydantic import Field, model_validator, field_validator
 from pydantic_geojson import PointModel, FeatureModel, PolygonModel, MultiPolygonModel
 from api.models.activity import FormStatus
+from api.protocol.activity.validators.no_repeat_key import no_repeat_key
+from api.protocol.activity.validators.check_sum import check_sum
+from api.protocol.activity.validators.no_future_date import no_future_date
+from api.protocol.activity.validators.code_validation import (
+    EmployerCodeType,
+    JurisdictionCodeType,
+    FundingAgencyCodeType,
+)
 
 MAX_AREA_FOR_RECORD = 500000
 
@@ -22,56 +31,76 @@ class CleanSchema(Schema):
 
 
 class JurisdictionSchema(CleanSchema):
-    jurisdiction: str = Field(...)
+    jurisdiction: JurisdictionCodeType
     percent_covered: int = Field(..., ge=0, le=100)
 
 
 class Participant(CleanSchema):
-    name: str = Field(...)
+    name: str
     pac_number: Optional[int] = None
 
 
-class Employer(TypedDict):
-    employer: str
+class Employer(CleanSchema):
+    employer: EmployerCodeType
 
 
-class LinkedActivity(TypedDict):
+class LinkedActivity(CleanSchema):
     label: str
     full: str
 
 
-class FundingAgency(TypedDict):
-    invasive_species_agency_code: str
+class FundingAgency(CleanSchema):
+    invasive_species_agency_code: FundingAgencyCodeType
 
 
-class Media(TypedDict):
+class Media(CleanSchema):
     description: str
     encoded_file: str
 
 
-class ProjectCode(TypedDict):
+class ProjectCode(CleanSchema):
     description: str
 
 
 class BaseFormSchema(CleanSchema):
+    # Identifying Information (Subtype defined in Inherited Classes)
+    date: date
     id: Optional[str] = None  # Optional for first entries, required for updating.
-    access_description: Optional[str] = None
-    area_m: int = Field(..., gt=0, le=MAX_AREA_FOR_RECORD)
-    comment: Optional[str] = None
-    created_by: str = Field(...)
-    date: str = Field(...)
-    employer: List[Employer] = Field(..., min_length=1)
-    funding_agencies: List[FundingAgency] = Field(..., min_length=1)
-    form_status: Optional[FormStatus] = None
-    geom: Union[PointModel, FeatureModel, PolygonModel, MultiPolygonModel] = Field(...)
-    jurisdictions: List[JurisdictionSchema] = Field(..., min_length=1)
-    latitude: float = Field(...)
     linked_activities: List[LinkedActivity]
-    location_description: str = Field(..., min_length=10)
-    longitude: float = Field(...)
+    created_by: str
+    employer: List[Employer] = Field(..., min_length=1)
+    form_status: Optional[FormStatus] = None
+    funding_agencies: List[FundingAgency] = Field(..., min_length=1)
+    jurisdictions: List[JurisdictionSchema] = Field(..., min_length=1)
     media: List[Media]
     participants: List[Participant] = Field(..., min_length=1)
     projects: List[ProjectCode]
-    utm_easting: int = Field(...)
-    utm_northing: int = Field(...)
-    utm_zone: int = Field(...)
+
+    # Geometry Values
+    area_m: int = Field(..., gt=0, le=MAX_AREA_FOR_RECORD)
+    geom: Union[PointModel, FeatureModel, PolygonModel, MultiPolygonModel]
+    latitude: float
+    longitude: float
+    utm_easting: int
+    utm_northing: int
+    utm_zone: int
+
+    # Top-level Comments
+    access_description: Optional[str] = None
+    comment: Optional[str] = None
+    location_description: str = Field(..., min_length=10)
+
+    @field_validator("date")
+    @classmethod
+    def no_future_activity_date(cls, v):
+        return no_future_date(v)
+
+    @field_validator("jurisdictions")
+    @classmethod
+    def unique_jurisdictions(cls, v):
+        return no_repeat_key(v, key="jurisdiction", key_label="Jurisdiction")
+
+    @field_validator("jurisdictions")
+    @classmethod
+    def jurisdiction_sum(cls, v):
+        return check_sum(v, expected=100, key="percent_covered")
