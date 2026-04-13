3752564372288faeb873d8a7e60628bfa2f04d96
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-04-13 16:18:38 -0700

    implement biocontrol counts

---
+++ api-rework/invasives/api/protocol/activity/plant_subtypes/common/biocontrol_count.py
@@ -0,0 +1,16 @@
+from pydantic import Field
+from api.protocol.activity.validators.code_validation import (
+    BioAgentLifeStageCodeType,
+    PlantPositionCodeType,
+    AgentLocationFoundCodeType,
+)
+from api.protocol.activity.plant_subtypes.base_form_schema import CleanSchema
+
+class BiocontrolCountSimple(CleanSchema):
+    quantity: int = Field(..., gt=0)
+    stage: BioAgentLifeStageCodeType
+    is_estimate: bool = False
+
+class BiocontrolCountExtended(BiocontrolCountSimple):
+    plant_position: PlantPositionCodeType
+    agent_location: AgentLocationFoundCodeType
