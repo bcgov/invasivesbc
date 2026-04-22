7f6ad6799c5cb0a5f5a0bfbae2654ef7df5cb1b5
Robert Johnstone <rob@plasticviking.com>
2026-04-22 10:34:52 +0800

    specify schema in knexfile

--- database/src/knexfile.ts
+++ database/src/knexfile.ts
@@ -15,11 +15,13 @@ module.exports = {
     },
     migrations: {
       tableName: 'migration',
-      directory: './migrations'
+      directory: './migrations',
+      searchPath: 'public',
+      schemaName: 'public'
     },
     seeds: {
       tableName: 'seed',
-      directory: './seeds'
+      directory: './seeds',
     }
   },
   build: {
