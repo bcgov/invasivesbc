d4966f14fe6fccb9d26c9c55de838ce17e93b1c0
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-05-12 12:53:03 -0700

    Remove prefetch_related, solves nothing

--- api-rework/invasives/api/configs/exports/csv.py
+++ api-rework/invasives/api/configs/exports/csv.py
@@ -30,35 +30,14 @@ from . import (
     TREATMENT_CHEMICAL_ANNOTATIONS,
 )
 
-ADR_BASE = "activity_data_record__activity__activitydatarecord_set"
 CSV_SUBTYPE_CONFIG = {
     ActivitySubtypes.Observation_Plant_Terrestrial.name: {
         "entry_models": [TerrestrialPlantObservationEntries],
         "annotations": OBSERVATION_TERRESTRIAL_ANNOTATIONS,
-        "prefetch_related": [
-            f"{ADR_BASE}__specificuse_set__specific_use",
-        ],
-        "select_related": [],
     },
     ActivitySubtypes.Observation_Plant_Aquatic.name: {
         "entry_models": [AquaticPlantObservationEntry],
         "annotations": OBSERVATION_AQUATIC_ANNOTATIONS,
-        "prefetch_related": [
-            f"{ADR_BASE}__waterbodycontext_set__type",
-            f"{ADR_BASE}__waterbodyuse_set__waterbody_use",
-            f"{ADR_BASE}__waterbodylevelmanagement_set__waterlevel_management",
-            f"{ADR_BASE}__waterbodysubstratetype_set__substrate_type",
-            f"{ADR_BASE}__waterbodyadjacentlanduse_set__waterbody_adjacent_land_use",
-            f"{ADR_BASE}__aquaticplantobservationcontext_set",
-            f"{ADR_BASE}__pretreatmentobservation_set",
-            f"{ADR_BASE}__aquaticvoucherspecimen_set",
-            f"{ADR_BASE}__shorelinetypes_set__shoreline_type",
-            f"{ADR_BASE}__waterbodyinflowpermanent_set__flow_code",
-            f"{ADR_BASE}__waterbodyinflowseasonal_set__flow_code",
-            f"{ADR_BASE}__waterbodyoutflowpermanent_set__flow_code",
-            f"{ADR_BASE}__waterbodyoutflowseasonal_set__flow_code",
-        ],
-        "select_related": [],
     },
     ActivitySubtypes.Monitoring_Mechanical_Plant_Terrestrial_Aquatic.name: {
         "entry_models": [
@@ -66,7 +45,6 @@ CSV_SUBTYPE_CONFIG = {
             TerrestrialTreatmentMonitoringEntry,
         ],
         "annotations": MONITORING_ANNOTATIONS,
-        "prefetch_related": [],
     },
     ActivitySubtypes.Monitoring_Chemical_Plant_Terrestrial_Aquatic.name: {
         "entry_models": [
@@ -74,7 +52,6 @@ CSV_SUBTYPE_CONFIG = {
             TerrestrialTreatmentMonitoringEntry,
         ],
         "annotations": MONITORING_ANNOTATIONS,
-        "prefetch_related": [],
     },
     ActivitySubtypes.Monitoring_Biocontrol_Dispersal_Plant_Terrestrial.name: {
         "entry_models": [TerrestrialBiocontrolDispersalMonitoringEntry],
@@ -82,7 +59,6 @@ CSV_SUBTYPE_CONFIG = {
         + MONITORING_BIOCONTROL_DISPERSAL_RELEASE_ANNOTATIONS
         + EXTENDED_AGENT_COUNT_ANNOTATIONS
         + PLANT_PHENOLOGY_ANNOTATIONS,
-        "prefetch_related": [],
     },
     ActivitySubtypes.Biocontrol_Release.name: {
         "entry_models": [TerrestrialBiocontrolReleaseEntry],
@@ -90,7 +66,6 @@ CSV_SUBTYPE_CONFIG = {
         + BIOCONTROL_RELEASE_ANNOTATIONS
         + AGENT_COUNT_ANNOTATIONS
         + PLANT_PHENOLOGY_ANNOTATIONS,
-        "prefetch_related": [],
     },
     ActivitySubtypes.Monitoring_Biocontrol_Release_Plant_Terrestrial.name: {
         "entry_models": [TerrestrialBiocontrolDispersalMonitoringEntry],
@@ -99,7 +74,6 @@ CSV_SUBTYPE_CONFIG = {
         + EXTENDED_AGENT_COUNT_ANNOTATIONS
         + PLANT_PHENOLOGY_ANNOTATIONS
         + SPREAD_RESULTS_ANNOTATIONS,
-        "prefetch_related": [],
     },
     ActivitySubtypes.Biocontrol_Collection.name: {
         "entry_models": [TerrestrialBiocontrolCollectionEntry],
@@ -107,26 +81,21 @@ CSV_SUBTYPE_CONFIG = {
         + BIOCONTROL_COLLECTION_ANNOTATIONS
         + AGENT_COUNT_ANNOTATIONS
         + PLANT_PHENOLOGY_ANNOTATIONS,
-        "prefetch_related": [],
     },
     ActivitySubtypes.Treatment_Mechanical_Plant_Terrestrial.name: {
         "entry_models": [TerrestrialPlantMechanicalTreatmentEntry],
         "annotations": TREATMENT_MECHANICAL_TERRESTRIAL_ANNOTATIONS,
-        "prefetch_related": [],
     },
     ActivitySubtypes.Treatment_Mechanical_Plant_Aquatic.name: {
         "entry_models": [AquaticPlantMechanicalTreatmentEntry],
         "annotations": TREATMENT_MECHANICAL_AQUATIC_ANNOTATIONS,
-        "prefetch_related": [],
     },
     ActivitySubtypes.Treatment_Chemical_Plant_Aquatic.name: {
         "entry_models": [ChemicalTreatmentAquaticInvasivePlantRecord],
         "annotations": TREATMENT_CHEMICAL_ANNOTATIONS,
-        "prefetch_related": [],
     },
     ActivitySubtypes.Treatment_Chemical_Plant_Terrestrial.name: {
         "entry_models": [ChemicalTreatmentTerrestrialInvasivePlantRecord],
         "annotations": TREATMENT_CHEMICAL_ANNOTATIONS,
-        "prefetch_related": [],
     },
 }
