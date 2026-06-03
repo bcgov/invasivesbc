567755d57fc42f2dcdc28a365fa048e197c1fc5a
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-06-03 16:08:03 -0700

    return S3 files in URI format

--- api-rework/invasives/api/serializers/activity.py
+++ api-rework/invasives/api/serializers/activity.py
@@ -14,6 +14,7 @@ from api.models.activity import (
 from api.models.activity.activity import Activity
 from api.models.activity import UploadedImage
 from api.models.mixins.geometry import Geometry
+from api.utils.fetch_s3_activity_photos import FetchS3MediaFiles
 from api.serializers.type.subtype import (
     AquaticChemicalTreatmentSerializer,
     AquaticObservationSerializer,
@@ -48,7 +49,8 @@ class UploadedImageSerializer(serializers.ModelSerializer):
         )
 
     def get_encoded_file(self, obj):
-        return "TODO"
+        encoded_image = FetchS3MediaFiles().get_b64_encoded_image(obj.file_name)
+        return encoded_image
 
 
 class EmployerSerializer(serializers.ModelSerializer):
