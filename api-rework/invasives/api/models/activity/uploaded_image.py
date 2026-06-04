92dc77d1c3a17abcc9e5ea8dd587995dc5f89de3
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-06-04 13:15:51 -0700

    Implement get_or_create logic to avoid image reuploads

--- api-rework/invasives/api/models/activity/uploaded_image.py
+++ api-rework/invasives/api/models/activity/uploaded_image.py
@@ -1,4 +1,5 @@
 import uuid
+import hashlib
 from django.db import models
 from api.utils.fetch_s3_activity_photos import FetchS3MediaFiles
 
@@ -6,14 +7,37 @@ from api.models.activity import RepeatedFormData
 
 
 class UploadedImageManager(models.Manager):
-    def create(self, **kwargs):
-        encoded_file = kwargs.pop("encoded_file", None)
+    def get_or_create(self, **kwargs):
+        """
+        Idempotently gets or creates an image record.
+        Hashes file content and activity ID to block duplicate S3 uploads.
+        """
+
+        # Remove encoded_file, as is not property.
+        encoded_file: str = kwargs.pop("encoded_file", None)
         if not encoded_file:
             raise ValueError({"encoded_file": "No Encoded File Provided"})
 
         instance = self.model(**kwargs)
 
-        instance.file_name = instance._create_file_name(instance.file_name)
+        # Create a filename based on the hashed activity id + b64 image
+        activity_id = str(instance.activity_data_record.activity_id)
+        extension = instance.file_name.split(".")[-1]
+
+        hasher = hashlib.md5()
+        hasher.update(activity_id.encode("utf-8"))
+        hasher.update(encoded_file.encode("utf-8"))
+        hashed_string = hasher.hexdigest()
+
+        generated_file_name = f"IBC-{hashed_string}.{extension}"
+
+        # Check if this file already exists
+        existing_image = self.filter(file_name=generated_file_name).first()
+        if existing_image:
+            return existing_image, False
+
+        instance.file_name = generated_file_name
+
         success = FetchS3MediaFiles().upload_b64_encoded_image(
             b64_image=encoded_file, file_name=instance.file_name
         )
@@ -22,6 +46,11 @@ class UploadedImageManager(models.Manager):
             raise ValueError({"encoded_file": "An Error occurred during S3 upload."})
 
         instance.save(using=self._db)
+        return instance, True
+
+    def create(self, **kwargs):
+        """Use Deduplication logic in get_or_create, but only return the instance"""
+        instance, created = self.get_or_create(**kwargs)
         return instance
 
 
@@ -43,10 +72,3 @@ class UploadedImage(RepeatedFormData):
     class Meta:
         db_table = '"activity"."uploaded_image"'
         db_table_comment = "Image uploads for IBC Records"
-
-    def _create_file_name(self, provided_name) -> str:
-        """Convert users supplied file's name to provided name"""
-        ext = provided_name.split(".")[-1]
-        id = uuid.uuid4().hex
-        prescribed_name = f"IBC-{id}.{ext}"
-        return prescribed_name
