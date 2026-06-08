a117b62bf25482243bb29d01ab021185280c5e39
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-06-08 14:46:15 -0700

    update init

--- api-rework/invasives/api/tests/mock_frontend_submissions/__init__.py
+++ api-rework/invasives/api/tests/mock_frontend_submissions/__init__.py
@@ -2,3 +2,15 @@ from .observation_terrestrial import (
     MINIMAL_TERRESTRIAL_OBSERVATION,
     UPDATED_TERRESTRIAL_OBSERVATION,
 )
+from .observation_terrestrial import (
+    MINIMAL_TERRESTRIAL_OBSERVATION,
+    UPDATED_TERRESTRIAL_OBSERVATION,
+)
+from .chem_treatment_terrestrial import (
+    MINIMAL_CHEM_TREATMENT_TERRESTRIAL,
+    UPDATED_CHEM_TREATMENT_TERRESTRIAL,
+)
+from .chem_treatment_aquatic import (
+    MINIMAL_CHEM_TREATMENT_AQUATIC,
+    UPDATED_CHEM_TREATMENT_AQUATIC,
+)
