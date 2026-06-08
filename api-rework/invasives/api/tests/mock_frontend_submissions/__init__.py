73a22b89d74dd76b242ac00758776b1dd2ae8bc6
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-06-08 15:03:51 -0700

    Add mech treatment

--- api-rework/invasives/api/tests/mock_frontend_submissions/__init__.py
+++ api-rework/invasives/api/tests/mock_frontend_submissions/__init__.py
@@ -14,3 +14,11 @@ from .chem_treatment_aquatic import (
     MINIMAL_CHEM_TREATMENT_AQUATIC,
     UPDATED_CHEM_TREATMENT_AQUATIC,
 )
+from .mech_treatment_aquatic import (
+    MINIMAL_MECH_TREATMENT_AQUATIC,
+    UPDATED_MECH_TREATMENT_AQUATIC,
+)
+from .mech_treatment_terrestrial import (
+    MINIMAL_MECH_TREATMENT_TERRESTRIAL,
+    UPDATED_MECH_TREATMENT_TERRESTRIAL,
+)
---
+++ api-rework/invasives/api/tests/mock_frontend_submissions/mech_treatment_aquatic.py
@@ -0,0 +1,119 @@
+MINIMAL_MECH_TREATMENT_AQUATIC = {
+    "id": "bb3cf28f-812d-45c3-bcd7-f309c5377da4",
+    "short_id": "26PAMBB3CF28F",
+    "employer": [{"employer": "MOTI"}],
+    "subtype": "Treatment_Mechanical_Plant_Aquatic",
+    "funding_agencies": [{"invasive_species_agency_code": "MOTI"}],
+    "jurisdictions": [{"jurisdiction": "MOTI", "percent_covered": 100}],
+    "projects": [],
+    "location_description": "Location Description",
+    "access_description": "",
+    "form_status": "Draft",
+    "date": "2026-06-08",
+    "comment": "",
+    "area_m": 3,
+    "latitude": 48.412055,
+    "longitude": -123.362422,
+    "utm_zone": 10,
+    "utm_easting": 473181,
+    "utm_northing": 5362164,
+    "linked_activities": [],
+    "participants": [{"name": "John Smith"}],
+    "subtype_data": {
+        "entries": [
+            {
+                "disposed_material_amount": None,
+                "disposed_material_format": "",
+                "disposal_method": "II",
+                "invasive_plant": "JK",
+                "mechanical_method": "BRY",
+                "treated_area_msq": 21,
+            }
+        ],
+        "shoreline_types": [{"shoreline_type": "O", "percent_covered": 100}],
+        "authorization_info": "",
+    },
+    "type": "Treatment",
+    "media": [],
+    "created_by": "johnsmith@identifier",
+    "shape": {
+        "type": "Feature",
+        "properties": {"error": "false"},
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [-123.3624221506199, 48.41206448237686],
+                    [-123.36243201729307, 48.41206176961331],
+                    [-123.36243610420112, 48.41205522042333],
+                    [-123.36243201729052, 48.41204867123421],
+                    [-123.3624221506199, 48.41204595847149],
+                    [-123.36241228394927, 48.41204867123421],
+                    [-123.36240819703866, 48.41205522042333],
+                    [-123.36241228394671, 48.41206176961331],
+                    [-123.3624221506199, 48.41206448237686],
+                ]
+            ],
+        },
+    },
+}
+
+UPDATED_MECH_TREATMENT_AQUATIC = {
+    "id": "bb3cf28f-812d-45c3-bcd7-f309c5377da4",
+    "short_id": "26PAMBB3CF28F",
+    "employer": [{"employer": "MOTI"}],
+    "subtype": "Treatment_Mechanical_Plant_Aquatic",
+    "funding_agencies": [{"invasive_species_agency_code": "MOTI"}],
+    "jurisdictions": [{"jurisdiction": "MOTI", "percent_covered": 100}],
+    "projects": [{"description": "Project Code"}],
+    "location_description": "Location Description",
+    "access_description": "Access Description",
+    "form_status": "Draft",
+    "date": "2026-06-08",
+    "comment": "Comment Description",
+    "area_m": 3,
+    "latitude": 48.412055,
+    "longitude": -123.362422,
+    "utm_zone": 10,
+    "utm_easting": 473181,
+    "utm_northing": 5362164,
+    "linked_activities": [],
+    "participants": [{"name": "John Smith"}],
+    "subtype_data": {
+        "entries": [
+            {
+                "disposed_material_amount": 22,
+                "disposed_material_format": "plants",
+                "disposal_method": "II",
+                "invasive_plant": "JK",
+                "mechanical_method": "BRY",
+                "treated_area_msq": 21,
+            }
+        ],
+        "shoreline_types": [{"shoreline_type": "O", "percent_covered": 100}],
+        "authorization_info": "Waterbody Authorization Field",
+    },
+    "type": "Treatment",
+    "media": [],
+    "created_by": "johnsmith@identifier",
+    "shape": {
+        "type": "Feature",
+        "properties": {"error": "false"},
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [-123.3624221506199, 48.41206448237686],
+                    [-123.36243201729307, 48.41206176961331],
+                    [-123.36243610420112, 48.41205522042333],
+                    [-123.36243201729052, 48.41204867123421],
+                    [-123.3624221506199, 48.41204595847149],
+                    [-123.36241228394927, 48.41204867123421],
+                    [-123.36240819703866, 48.41205522042333],
+                    [-123.36241228394671, 48.41206176961331],
+                    [-123.3624221506199, 48.41206448237686],
+                ]
+            ],
+        },
+    },
+}
