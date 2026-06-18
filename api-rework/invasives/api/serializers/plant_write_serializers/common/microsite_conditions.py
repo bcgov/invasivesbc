bb24c76ee3aa77f67c3f1dd96a1c0a9ae88096ba
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-06-18 13:39:02 -0700

    delete previous write serializers

--- api-rework/invasives/api/serializers/plant_write_serializers/common/microsite_conditions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from api.models.activity import MicrositeCondition
-
-
-class MicrositeConditionsSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = MicrositeCondition
-        fields = ("mesoslope_position", "site_surface_shape")
