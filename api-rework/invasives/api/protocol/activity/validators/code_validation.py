c86c70e68a6e2d271723116c3306181981a5c58b
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-06-24 14:13:43 -0700

    Update MapCodeTable to handle self-serialization

--- api-rework/invasives/api/protocol/activity/validators/code_validation.py
+++ api-rework/invasives/api/protocol/activity/validators/code_validation.py
@@ -1,4 +1,4 @@
-from pydantic import BeforeValidator
+from pydantic import BeforeValidator, PlainSerializer
 from typing import Annotated, Type
 from django.db import models
 from api.models.codes import (
@@ -81,106 +81,267 @@ def MapCodeTable(model: Type[models.Model]):
     return BeforeValidator(validator)
 
 
+def SerializeCodeTable():
+    """
+    Serializes CodeTable instance back to its string code.
+    """
+    return PlainSerializer(
+        lambda v: v.code if isinstance(v, models.Model) else v,
+        return_type=str,
+        when_used="json",  # only fires during serialization output e.g. model_dump(mode='json')
+    )
+
+
 AdjacentLandUseCodeType = Annotated[
-    AdjacentLandUseCode, MapCodeTable(AdjacentLandUseCode)
+    AdjacentLandUseCode,
+    MapCodeTable(AdjacentLandUseCode),
+    SerializeCodeTable(),
 ]
 AgentLocationFoundCodeType = Annotated[
-    AgentLocationFoundCode, MapCodeTable(AgentLocationFoundCode)
+    AgentLocationFoundCode,
+    MapCodeTable(AgentLocationFoundCode),
+    SerializeCodeTable(),
 ]
+
 AgentLocationFoundTerrainCodeType = Annotated[
-    AgentLocationFoundTerrainCode, MapCodeTable(AgentLocationFoundTerrainCode)
+    AgentLocationFoundTerrainCode,
+    MapCodeTable(AgentLocationFoundTerrainCode),
+    SerializeCodeTable(),
 ]
-AquaticPlantCodeType = Annotated[AquaticPlantCode, MapCodeTable(AquaticPlantCode)]
-AspectCodeType = Annotated[AspectCode, MapCodeTable(AspectCode)]
-BaseCodeType = Annotated[BaseCode, MapCodeTable(BaseCode)]
+
+AquaticPlantCodeType = Annotated[
+    AquaticPlantCode,
+    MapCodeTable(AquaticPlantCode),
+    SerializeCodeTable(),
+]
+AspectCodeType = Annotated[
+    AspectCode,
+    MapCodeTable(AspectCode),
+    SerializeCodeTable(),
+]
+BaseCodeType = Annotated[
+    BaseCode,
+    MapCodeTable(BaseCode),
+    SerializeCodeTable(),
+]
 BioAgentCollectionMethodCodeType = Annotated[
-    BioAgentCollectionMethodCode, MapCodeTable(BioAgentCollectionMethodCode)
+    BioAgentCollectionMethodCode,
+    MapCodeTable(BioAgentCollectionMethodCode),
+    SerializeCodeTable(),
 ]
+
 BioAgentLifeStageCodeType = Annotated[
-    BioAgentLifeStageCode, MapCodeTable(BioAgentLifeStageCode)
+    BioAgentLifeStageCode,
+    MapCodeTable(BioAgentLifeStageCode),
+    SerializeCodeTable(),
 ]
 BiocontrolAgentCodeType = Annotated[
-    BiocontrolAgentCode, MapCodeTable(BiocontrolAgentCode)
+    BiocontrolAgentCode,
+    MapCodeTable(BiocontrolAgentCode),
+    SerializeCodeTable(),
 ]
 BiocontrolPresenceCodeType = Annotated[
-    BiocontrolPresenceCode, MapCodeTable(BiocontrolPresenceCode)
+    BiocontrolPresenceCode,
+    MapCodeTable(BiocontrolPresenceCode),
+    SerializeCodeTable(),
 ]
 ChemicalApplicationMethodDirectCodeType = Annotated[
     ChemicalApplicationMethodDirectCode,
     MapCodeTable(ChemicalApplicationMethodDirectCode),
+    SerializeCodeTable(),
 ]
 ChemicalApplicationMethodSprayCodeType = Annotated[
-    ChemicalApplicationMethodSprayCode, MapCodeTable(ChemicalApplicationMethodSprayCode)
+    ChemicalApplicationMethodSprayCode,
+    MapCodeTable(ChemicalApplicationMethodSprayCode),
+    SerializeCodeTable(),
 ]
 ChemicalPrecautionaryStatementType = Annotated[
-    ChemicalPrecautionaryStatement, MapCodeTable(ChemicalPrecautionaryStatement)
+    ChemicalPrecautionaryStatement,
+    MapCodeTable(ChemicalPrecautionaryStatement),
+    SerializeCodeTable(),
 ]
-CloudCoverCodeType = Annotated[CloudCoverCode, MapCodeTable(CloudCoverCode)]
-DensityCodeType = Annotated[DensityCode, MapCodeTable(DensityCode)]
-DisposalMethodCodeType = Annotated[DisposalMethodCode, MapCodeTable(DisposalMethodCode)]
-DistributionCodeType = Annotated[DistributionCode, MapCodeTable(DistributionCode)]
+CloudCoverCodeType = Annotated[
+    CloudCoverCode,
+    MapCodeTable(CloudCoverCode),
+    SerializeCodeTable(),
+]
+DensityCodeType = Annotated[
+    DensityCode,
+    MapCodeTable(DensityCode),
+    SerializeCodeTable(),
+]
+DisposalMethodCodeType = Annotated[
+    DisposalMethodCode,
+    MapCodeTable(DisposalMethodCode),
+    SerializeCodeTable(),
+]
+DistributionCodeType = Annotated[
+    DistributionCode,
+    MapCodeTable(DistributionCode),
+    SerializeCodeTable(),
+]
 EfficacyManagementRatingCodeType = Annotated[
-    EfficacyManagementRatingCode, MapCodeTable(EfficacyManagementRatingCode)
+    EfficacyManagementRatingCode,
+    MapCodeTable(EfficacyManagementRatingCode),
+    SerializeCodeTable(),
 ]
-EmployerCodeType = Annotated[EmployerCode, MapCodeTable(EmployerCode)]
-FundingAgencyCodeType = Annotated[FundingAgencyCode, MapCodeTable(FundingAgencyCode)]
+EmployerCodeType = Annotated[
+    EmployerCode,
+    MapCodeTable(EmployerCode),
+    SerializeCodeTable(),
+]
+FundingAgencyCodeType = Annotated[
+    FundingAgencyCode,
+    MapCodeTable(FundingAgencyCode),
+    SerializeCodeTable(),
+]
 GranularHerbicideCodeType = Annotated[
-    GranularHerbicideCode, MapCodeTable(GranularHerbicideCode)
+    GranularHerbicideCode,
+    MapCodeTable(GranularHerbicideCode),
+    SerializeCodeTable(),
 ]
 InvasivePlantsOnSiteCodeType = Annotated[
-    InvasivePlantsOnSiteCode, MapCodeTable(InvasivePlantsOnSiteCode)
+    InvasivePlantsOnSiteCode,
+    MapCodeTable(InvasivePlantsOnSiteCode),
+    SerializeCodeTable(),
 ]
-JurisdictionCodeType = Annotated[JurisdictionCode, MapCodeTable(JurisdictionCode)]
+JurisdictionCodeType = Annotated[
+    JurisdictionCode,
+    MapCodeTable(JurisdictionCode),
+    SerializeCodeTable(),
+]
 LiquidHerbicideCodeType = Annotated[
-    LiquidHerbicideCode, MapCodeTable(LiquidHerbicideCode)
+    LiquidHerbicideCode,
+    MapCodeTable(LiquidHerbicideCode),
+    SerializeCodeTable(),
 ]
 MesoslopePositionCodeType = Annotated[
-    MesoslopePositionCode, MapCodeTable(MesoslopePositionCode)
+    MesoslopePositionCode,
+    MapCodeTable(MesoslopePositionCode),
+    SerializeCodeTable(),
 ]
-PestManagementPlanType = Annotated[PestManagementPlan, MapCodeTable(PestManagementPlan)]
-PlantLifeStageCodeType = Annotated[PlantLifeStageCode, MapCodeTable(PlantLifeStageCode)]
+PestManagementPlanType = Annotated[
+    PestManagementPlan,
+    MapCodeTable(PestManagementPlan),
+    SerializeCodeTable(),
+]
+PlantLifeStageCodeType = Annotated[
+    PlantLifeStageCode,
+    MapCodeTable(PlantLifeStageCode),
+    SerializeCodeTable(),
+]
 PlantMechanicalTreatmentMethodCodeType = Annotated[
-    PlantMechanicalTreatmentMethodCode, MapCodeTable(PlantMechanicalTreatmentMethodCode)
+    PlantMechanicalTreatmentMethodCode,
+    MapCodeTable(PlantMechanicalTreatmentMethodCode),
+    SerializeCodeTable(),
 ]
-PlantPositionCodeType = Annotated[PlantPositionCode, MapCodeTable(PlantPositionCode)]
+PlantPositionCodeType = Annotated[
+    PlantPositionCode,
+    MapCodeTable(PlantPositionCode),
+    SerializeCodeTable(),
+]
 PlantsWithBiocontrolType = Annotated[
-    PlantsWithBiocontrol, MapCodeTable(PlantsWithBiocontrol)
+    PlantsWithBiocontrol,
+    MapCodeTable(PlantsWithBiocontrol),
+    SerializeCodeTable(),
 ]
-PrecipitationCodeType = Annotated[PrecipitationCode, MapCodeTable(PrecipitationCode)]
+PrecipitationCodeType = Annotated[
+    PrecipitationCode,
+    MapCodeTable(PrecipitationCode),
+    SerializeCodeTable(),
+]
 ServiceLicenseNumberAndCompanyType = Annotated[
-    ServiceLicenseNumberAndCompany, MapCodeTable(ServiceLicenseNumberAndCompany)
+    ServiceLicenseNumberAndCompany,
+    MapCodeTable(ServiceLicenseNumberAndCompany),
+    SerializeCodeTable(),
 ]
-ShorelineTypeCodeType = Annotated[ShorelineTypeCode, MapCodeTable(ShorelineTypeCode)]
+ShorelineTypeCodeType = Annotated[
+    ShorelineTypeCode,
+    MapCodeTable(ShorelineTypeCode),
+    SerializeCodeTable(),
+]
 SiteSurfaceShapeCodeType = Annotated[
-    SiteSurfaceShapeCode, MapCodeTable(SiteSurfaceShapeCode)
+    SiteSurfaceShapeCode,
+    MapCodeTable(SiteSurfaceShapeCode),
+    SerializeCodeTable(),
 ]
-SlopePercentCodeType = Annotated[SlopePercentCode, MapCodeTable(SlopePercentCode)]
-SoilTextureCodeType = Annotated[SoilTextureCode, MapCodeTable(SoilTextureCode)]
-SpecificUseCodeType = Annotated[SpecificUseCode, MapCodeTable(SpecificUseCode)]
-SubstrateCodeType = Annotated[SubstrateCode, MapCodeTable(SubstrateCode)]
+SlopePercentCodeType = Annotated[
+    SlopePercentCode,
+    MapCodeTable(SlopePercentCode),
+    SerializeCodeTable(),
+]
+SoilTextureCodeType = Annotated[
+    SoilTextureCode,
+    MapCodeTable(SoilTextureCode),
+    SerializeCodeTable(),
+]
+SpecificUseCodeType = Annotated[
+    SpecificUseCode,
+    MapCodeTable(SpecificUseCode),
+    SerializeCodeTable(),
+]
+SubstrateCodeType = Annotated[
+    SubstrateCode,
+    MapCodeTable(SubstrateCode),
+    SerializeCodeTable(),
+]
 TerrestrialPlantCodeType = Annotated[
-    TerrestrialPlantCode, MapCodeTable(TerrestrialPlantCode)
+    TerrestrialPlantCode,
+    MapCodeTable(TerrestrialPlantCode),
+    SerializeCodeTable(),
 ]
 TreatmentEfficacyRatingCodeType = Annotated[
-    TreatmentEfficacyRatingCode, MapCodeTable(TreatmentEfficacyRatingCode)
+    TreatmentEfficacyRatingCode,
+    MapCodeTable(TreatmentEfficacyRatingCode),
+    SerializeCodeTable(),
 ]
-WaterbodyFlowCodeType = Annotated[WaterbodyFlowCode, MapCodeTable(WaterbodyFlowCode)]
+WaterbodyFlowCodeType = Annotated[
+    WaterbodyFlowCode,
+    MapCodeTable(WaterbodyFlowCode),
+    SerializeCodeTable(),
+]
 WaterbodyFlowSeasonalCodeType = Annotated[
-    WaterbodyFlowSeasonalCode, MapCodeTable(WaterbodyFlowSeasonalCode)
+    WaterbodyFlowSeasonalCode,
+    MapCodeTable(WaterbodyFlowSeasonalCode),
+    SerializeCodeTable(),
 ]
-WaterbodyUseCodeType = Annotated[WaterbodyUseCode, MapCodeTable(WaterbodyUseCode)]
-WindDirectionCodeType = Annotated[WindDirectionCode, MapCodeTable(WindDirectionCode)]
+WaterbodyUseCodeType = Annotated[
+    WaterbodyUseCode,
+    MapCodeTable(WaterbodyUseCode),
+    SerializeCodeTable(),
+]
+WindDirectionCodeType = Annotated[
+    WindDirectionCode,
+    MapCodeTable(WindDirectionCode),
+    SerializeCodeTable(),
+]
 WaterLevelManagementType = Annotated[
-    WaterLevelManagement, MapCodeTable(WaterLevelManagement)
+    WaterLevelManagement,
+    MapCodeTable(WaterLevelManagement),
+    SerializeCodeTable(),
 ]
-WaterbodyTypeCodeType = Annotated[WaterbodyTypeCode, MapCodeTable(WaterbodyTypeCode)]
+WaterbodyTypeCodeType = Annotated[
+    WaterbodyTypeCode,
+    MapCodeTable(WaterbodyTypeCode),
+    SerializeCodeTable(),
+]
 WaterbodySubstrateCodeType = Annotated[
-    WaterbodySubstrateCode, MapCodeTable(WaterbodySubstrateCode)
+    WaterbodySubstrateCode,
+    MapCodeTable(WaterbodySubstrateCode),
+    SerializeCodeTable(),
 ]
 BioAgentMonitoringMethodCodeType = Annotated[
-    BioAgentMonitoringMethodCode, MapCodeTable(BioAgentMonitoringMethodCode)
+    BioAgentMonitoringMethodCode,
+    MapCodeTable(BioAgentMonitoringMethodCode),
+    SerializeCodeTable(),
 ]
-HerbicideTypeCodeType = Annotated[HerbicideTypeCode, MapCodeTable(HerbicideTypeCode)]
+HerbicideTypeCodeType = Annotated[
+    HerbicideTypeCode,
+    MapCodeTable(HerbicideTypeCode),
+    SerializeCodeTable(),
+]
 HerbicideApplicationMethodCodeType = Annotated[
-    HerbicideApplicationMethodCode, MapCodeTable(HerbicideApplicationMethodCode)
+    HerbicideApplicationMethodCode,
+    MapCodeTable(HerbicideApplicationMethodCode),
+    SerializeCodeTable(),
 ]
