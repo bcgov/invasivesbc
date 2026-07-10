f81fc7161a2d9666a7ed2c1f6c20aea3b81b5690
Robert Johnstone <rob@plasticviking.com>
2026-07-10 09:39:57 +0800

    Issue #4608 -- use Point On Surface for imported records (#4612)

--- api-rework/invasives/api/serializers/activity_migration_status.py
+++ api-rework/invasives/api/serializers/activity_migration_status.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from api.models.activity import Activity
 from api.models.migrator import MigrationError
 from api.models.migrator.activity_migration_status import ActivityMigrationStatus
 
@@ -16,14 +17,14 @@ class ActivityMigrationErrorSerializer(serializers.ModelSerializer):
 class ActivityMigrationStatusSerializer(serializers.ModelSerializer):
     detail = ActivityMigrationErrorSerializer(source="migration_error_set", many=True)
 
-    # def get_detail(self, obj):
-    #     return MigrationError.objects.filter(migration_status__id=obj.id)
+    remarks = serializers.SerializerMethodField()
+
+    def get_remarks(self, obj):
+        found = Activity.objects.filter(id=obj.activity_id).first()
+        if not found:
+            return None
+        return found.migration_remarks
 
     class Meta:
         model = ActivityMigrationStatus
-        fields = (
-            "activity_id",
-            "timestamp",
-            "success",
-            "detail",
-        )
+        fields = ("activity_id", "timestamp", "success", "detail", "remarks")
