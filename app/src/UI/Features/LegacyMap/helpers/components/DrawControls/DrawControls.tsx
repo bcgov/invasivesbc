50026d62e9b1f882d739b7db1468e482a7a45fe2
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-07-20 10:22:46 -0700

    move layer styles array to own file

--- app/src/UI/Features/LegacyMap/helpers/components/DrawControls/DrawControls.tsx
+++ app/src/UI/Features/LegacyMap/helpers/components/DrawControls/DrawControls.tsx
@@ -19,7 +19,7 @@ import GeoShapes from 'constants/geoShapes';
 import GeoTracking from 'state/actions/geotracking/GeoTracking';
 import { TargetMode } from 'constants/targetModes';
 import { GEO_TRACKING_FEATURE, SUBMITTED_ACTIVITY_SHAPE } from 'UI/Features/LegacyMap/helpers/functional/constants';
-
+import drawControlLayerStyles from './drawControlLayers';
 import { DrawModeDisplay, EditControls } from 'UI/Features/LegacyMap/helpers/components/MapCustomControls';
 import Alerts from 'state/actions/alerts/Alerts';
 import mappingAlertMessages from 'constants/alerts/mappingAlerts';
@@ -487,85 +487,7 @@ const DrawControls = () => {
         whats_here_box_mode: WhatsHereBoxMode,
         geo_tracking_mode: GeoTrackingMode
       },
-      styles: [
-        {
-          id: 'gl-edited-line.hot',
-          type: 'line',
-          layout: {
-            'line-cap': 'round',
-            'line-join': 'round'
-          },
-          filter: ['all', ['==', 'active', 'true']],
-          paint: {
-            'line-color': '#FCBA19',
-            'line-dasharray': [0.2, 2],
-            'line-width': 3
-          },
-          slot: LAYER_Z_FOREGROUND
-        },
-        {
-          id: 'gl-drawn-line.hot',
-          type: 'line',
-          layout: {
-            'line-cap': 'round',
-            'line-join': 'round'
-          },
-          filter: ['all', ['==', 'active', 'false'], ['!=', 'user_error', 'true']],
-          paint: {
-            'line-color': '#FCBA19',
-            'line-width': 3
-          },
-          slot: LAYER_Z_FOREGROUND
-        },
-        {
-          id: 'gl-drawn-fill.hot',
-          type: 'fill',
-          layout: {},
-          filter: ['all', ['==', 'active', 'false'], ['!=', 'user_error', 'true'], ['==', '$type', 'Polygon']],
-          paint: {
-            'fill-color': 'white',
-            'fill-opacity': 0.002
-          },
-          slot: LAYER_Z_FOREGROUND
-        },
-        {
-          id: 'gl-error-line.hot',
-          type: 'line',
-          layout: {
-            'line-cap': 'round',
-            'line-join': 'round'
-          },
-          paint: {
-            'line-color': ['match', ['get', 'user_error'], 'true', '#B00020', 'false', '#FCBA19', '#FCBA19'],
-            'line-dasharray': [1, 2],
-            'line-width': 3
-          },
-          slot: LAYER_Z_FOREGROUND
-        },
-        {
-          id: 'gl-draw-polygon-point.hot',
-          type: 'circle',
-          paint: {
-            'circle-radius': 3,
-            'circle-color': ['match', ['get', 'user_error'], 'true', '#B00020', 'false', '#FCBA19', '#FCBA19'],
-            'circle-stroke-width': 1,
-            'circle-stroke-color': '#fff'
-          },
-          slot: LAYER_Z_FOREGROUND
-        },
-        {
-          id: 'whats-here-box-start-point-marker',
-          filter: ['all', ['==', 'mode', 'whats_here_box_mode'], ['==', 'meta:type', 'Point']],
-          type: 'circle',
-          paint: {
-            'circle-radius': 4,
-            'circle-color': '#FCBA19',
-            'circle-stroke-width': 1,
-            'circle-stroke-color': '#fff'
-          },
-          slot: LAYER_Z_FOREGROUND
-        }
-      ]
+      styles: drawControlLayerStyles
     });
     drawModeDisplay.current = new DrawModeDisplay(mode);
     editControls.current = new EditControls(handleEdit, handleSave, isEditDisabled);
