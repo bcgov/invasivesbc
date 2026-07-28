a03ded0c0e93da0015aac7c2a73874b247c739fd
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-07-28 15:10:44 -0700

    Add Chemical Treatment Fields

--- api-rework/invasives/api/configs/exports/treatment_chemical.py
+++ api-rework/invasives/api/configs/exports/treatment_chemical.py
@@ -1,9 +1,22 @@
-from django.db.models import F, Value, CharField, Min, Func
-from django.db.models.functions import Cast
+from django.db.models import (
+    F,
+    Value,
+    CharField,
+    Min,
+    Func,
+    Case,
+    When,
+    Value,
+    CharField,
+    Value,
+)
+from django.db.models.functions import Cast, Concat
+from django.contrib.postgres.aggregates import StringAgg
 from .helpers import agg
 
 ROOT = f"root_activity__activitydatarecord"
 CTX = f"{ROOT}__chemicaltreatmentcontext"
+CHEM_CTX = f"{ROOT}__chemtreatmentcontext"
 
 
 TREATMENT_CHEMICAL_ANNOTATIONS = [
@@ -12,11 +25,11 @@ TREATMENT_CHEMICAL_ANNOTATIONS = [
         "key": "well_proximity_display",
         "annotation": Min(f"{ROOT}__wellentry__distance"),
     },
-    # {
-    #     "header": "Service License",
-    #     "key": "service_license_display",
-    #     "annotation": agg(f"{CTX}__pesticide_employer_code__full"),
-    # },
+    {
+        "header": "Service License",
+        "key": "service_license_display",
+        "annotation": agg(f"{CTX}__pesticide_employer_code__full"),
+    },
     {
         "header": "Pesticide Use Permit",
         "key": "pesticide_use_permit_display",
@@ -27,11 +40,11 @@ TREATMENT_CHEMICAL_ANNOTATIONS = [
         "key": "pest_management_plan_display",
         "annotation": agg(f"{CTX}__pest_management_plan__full"),
     },
-    # {
-    #     "header": "Pest Management Plan (Manual Entry)",
-    #     "key": "pmp_not_in_dropdown_display",
-    #     "annotation": F(f"pmp_not_in_dropdown"),
-    # },
+    {
+        "header": "Pest Management Plan (Manual Entry)",
+        "key": "pmp_not_in_dropdown_display",
+        "annotation": agg(f"{CTX}__pest_management_plan_manual"),
+    },
     {
         "header": "Temperature (C)",
         "key": "temperature_celsius_display",
@@ -75,54 +88,72 @@ TREATMENT_CHEMICAL_ANNOTATIONS = [
     {
         "header": "Invasive Plant Percent Covered",
         "key": "invasive_plant_percent_display",
-        "annotation": F(f"percent_area_covered"),
+        "annotation": F("invasive_plant_percent"),
     },
-    # {"header": "Tank Mix Used", "key": "tank_mix_display", "annotation": F(f"tank_mix")},
-    # {
-    #     "header": "Chemical Application Method",
-    #     "key": "chemical_application_method_display",
-    #     "annotation": F(f"chemical_application_method"),
-    # },
-    # {
-    #     "header": "Herbicide Type",
-    #     "key": "herbicide_type_display",
-    #     "annotation": F(f"herbicide_type"),
-    # },
-    # {"header": "Herbicide", "key": "herbicide_display", "annotation": F(f"herbicide")},
-    # {
-    #     "header": "Calculation Type",
-    #     "key": "calculation_type_display",
-    #     "annotation": F(f"calculation_type"),
-    # },
-    # {
-    #     "header": "Delivery Rate of Mix",
-    #     "key": "delivery_rate_of_mix_display",
-    #     "annotation": F(f"delivery_rate_of_mix"),
-    # },
-    # {
-    #     "header": "Product Application Rate",
-    #     "key": "product_application_rate_display",
-    #     "annotation": F(f"product_application_rate"),
-    # },
-    # {"header": "Dilution", "key": "dilution_display", "annotation": F(f"dilution")},
-    # {
-    #     "header": "Amount of Undiluted Herbicide Used",
-    #     "key": "amount_of_undiluted_herbicide_used_liters_display",
-    #     "annotation": F(f"amount_of_undiluted_herbicide_used_liters"),
-    # },
-    # {
-    #     "header": "Area_Treated (sqm)",
-    #     "key": "area_treated_sqm_display",
-    #     "annotation": F(f"area_treated_sqm"),
-    # },
-    # {
-    #     "header": "Amount of Mix Used (L)",
-    #     "key": "amount_of_mix_used_display",
-    #     "annotation": F(f"amount_of_mix_used"),
-    # },
-    # {
-    #     "header": "Percent Area Covered",
-    #     "key": "percent_area_covered_display",
-    #     "annotation": F(f"percent_area_covered"),
-    # },
+    {
+        "header": "Tank Mix Used",
+        "key": "tank_mix_display",
+        "annotation": agg(
+            Case(
+                When(**{f"{CHEM_CTX}__tank_mix": True}, then=Value("Yes")),
+                default=Value("No"),
+                output_field=CharField(),
+            )
+        ),
+    },
+    {
+        "header": "Chemical Application Method",
+        "key": "chemical_application_method_display",
+        "annotation": agg(f"{CHEM_CTX}__application_method__full"),
+    },
+    {
+        "header": "Herbicide Type",
+        "key": "herbicide_type_display",
+        "annotation": F(f"herbicide_type__full"),
+    },
+    {
+        "header": "Herbicide",
+        "key": "herbicide_display",
+        "annotation": F(f"herbicide_name__full"),
+    },
+    {
+        "header": "Calculation Type",
+        "key": "calculation_type_display",
+        "annotation": agg(f"{CHEM_CTX}__calculation_type"),  # TODO: CASE FOR FULL
+    },
+    {
+        "header": "Delivery Rate of Mix",
+        "key": "delivery_rate_of_mix_display",
+        "annotation": agg(Cast(f"{CHEM_CTX}__delivery_rate", CharField())),
+    },
+    {
+        "header": "Product Application Rate",
+        "key": "product_application_rate_display",
+        "annotation": F(f"product_application_rate"),
+    },
+    {
+        "header": "Dilution",
+        "key": "dilution_display",
+        "annotation": F(f"dilution"),
+    },
+    {
+        "header": "Amount of Undiluted Herbicide Used",
+        "key": "amount_of_undiluted_herbicide_used_l_display",
+        "annotation": F(f"undiluted_herbicide_used_l"),
+    },
+    {
+        "header": "Area Treated (sqm)",
+        "key": "area_treated_sqm_display",
+        "annotation": F(f"area_treated_sqm"),
+    },
+    {
+        "header": "Amount of Mix Used (L)",
+        "key": "amount_of_mix_used_display",
+        "annotation": F(f"amount_of_mix_used"),
+    },
+    {
+        "header": "Percent Area Covered",
+        "key": "percent_area_covered_display",
+        "annotation": F(f"percentage_area_covered"),
+    },
 ]
