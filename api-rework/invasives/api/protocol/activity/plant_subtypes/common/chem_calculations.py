dc14e2404a664feba3b30649806490a11943b4f7
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-07-30 10:34:11 -0700

    update backend treatments for exports

--- api-rework/invasives/api/protocol/activity/plant_subtypes/common/chem_calculations.py
+++ api-rework/invasives/api/protocol/activity/plant_subtypes/common/chem_calculations.py
@@ -1,3 +1,12 @@
+"""
+Chemical Calculations used for the Exporting of Chemical Treatment records.
+Creates a row for every combination of:
+    - Jurisdiction
+    - Plant
+    - Herbicide
+In the event of tank mix calculations, some fields are divided evenly by the number of herbicides in the mix to ensure sums are accurate.
+"""
+
 from pydantic import TypeAdapter
 from typing import List, TypedDict
 from api.models.activity import Jurisdiction
@@ -163,29 +172,31 @@ def get_chem_calculation_results(
 def mSpecie_sLHerb_spray_usingProdAppRate(
     params: ApplicationRatePayload,
 ) -> List[CalculationResponseValues]:
+    # Calculation scenario: 1-2
     area_m = params.get("area_m")
     product_application_rate_lha = params.get("product_application_rate_lha")
     amount_mix_used_l = params.get("amount_mix_used_l")
     delivery_rate_of_mix = params.get("delivery_rate_of_mix")
-    plants_treated = params.get("plants_treated", [])
-    herbicide_name = params.get("herbicide_name")
-    herbicide_type = params.get("herbicide_type")
-    dilution = (product_application_rate_lha / delivery_rate_of_mix) * 100
+    dilution_percent = product_application_rate_lha / delivery_rate_of_mix
 
-    results = []
-    for j in params.get("jurisdictions", []):
-        multiplier = j.percent_covered / 100
-        for plant in plants_treated:
-            percent_covered = plant.percent_covered
-            amount_of_mix_used = trunc(amount_mix_used_l * (percent_covered / 100))
-            area_treated_hectares = (
-                (amount_mix_used_l / delivery_rate_of_mix) * percent_covered
-            ) / 100
-            area_treated_by_plant = area_treated_hectares * HECTARE_TO_SQM
-            percentage_area_covered = trunc((area_treated_by_plant / area_m) * 100)
-            undiluted_herbicide_used_l = trunc(
-                (dilution / 100) * amount_mix_used_l * (percent_covered / 100)
-            )
+    results: List[CalculationResponseValues] = []
+    for plant in params.get("plants_treated", []):
+        plant_percentage = plant.percent_covered / 100
+        amount_of_mix_used = trunc(amount_mix_used_l * plant_percentage)
+        area_treated_sqm = (
+            (amount_mix_used_l / delivery_rate_of_mix)
+            * plant_percentage
+            * HECTARE_TO_SQM
+        )
+        undiluted_herbicide_used_l = trunc(
+            dilution_percent * amount_mix_used_l * plant_percentage
+        )
+        percentage_area_covered = area_treated_sqm / area_m * 100
+
+        for j in params.get("jurisdictions", []):
+            multiplier = j.percent_covered / 100
+            herbicide_name = params.get("herbicide_name")
+            herbicide_type = params.get("herbicide_type")
             results.append(
                 {
                     "jurisdiction": j.jurisdiction,
@@ -194,14 +205,14 @@ def mSpecie_sLHerb_spray_usingProdAppRate(
                     "invasive_plant_percent": plant.percent_covered,
                     "herbicide_name": herbicide_name,
                     "herbicide_type": herbicide_type,
-                    "amount_of_mix_used": amount_of_mix_used * multiplier,
-                    "dilution": trunc(dilution),
-                    "area_treated_sqm": trunc(
-                        (area_treated_hectares * HECTARE_TO_SQM) * multiplier
+                    "product_application_rate": product_application_rate_lha,
+                    "amount_of_mix_used": trunc(amount_of_mix_used * multiplier),
+                    "dilution": dilution_percent * 100,
+                    "area_treated_sqm": trunc(area_treated_sqm * multiplier),
+                    "undiluted_herbicide_used_l": undiluted_herbicide_used_l,
+                    "percentage_area_covered": trunc(
+                        percentage_area_covered * multiplier
                     ),
-                    "undiluted_herbicide_used_l": undiluted_herbicide_used_l
-                    * multiplier,
-                    "percentage_area_covered": percentage_area_covered * multiplier,
                 }
             )
 
@@ -211,36 +222,37 @@ def mSpecie_sLHerb_spray_usingProdAppRate(
 def mSpecie_sLHerb_spray_usingDilutionPercent(
     params: DilutionPayload,
 ) -> List[CalculationResponseValues]:
-    dilution_percent = params.get("dilution_percent")
+    # Calculation scenario: 4
     area_treated_sqm = params.get("area_treated_sqm")
     area_m = params.get("area_m", 0)
     amount_mix_used_l = params.get("amount_mix_used_l")
-    herbicide_name = params.get("herbicide_name")
-    herbicide_type = params.get("herbicide_type")
-    results = []
+    dilution_percent = params.get("dilution_percent") / 100
 
-    for j in params.get("jurisdictions", []):
-        multiplier = j.percent_covered / 100
-        for plant in params.get("plants_treated", []):
-            plant_area_treated_sqm = area_treated_sqm * plant.percent_covered / 100
-            percentage_area_covered = trunc((plant_area_treated_sqm / area_m) * 100)
-            undiluted_herbicide_used_l = trunc(
-                (dilution_percent / 100)
-                * amount_mix_used_l
-                * (plant.percent_covered / 100)
-            )
+    results: List[CalculationResponseValues] = []
+    for plant in params.get("plants_treated", []):
+        plant_percentage = plant.percent_covered / 100
+        plant_area_treated_sqm = area_treated_sqm * plant_percentage
+        undiluted_herbicide_used_l = (
+            amount_mix_used_l * dilution_percent * plant_percentage
+        )
+        percentage_area_covered = (plant_area_treated_sqm / area_m) * 100
+        for j in params.get("jurisdictions", []):
+            multiplier = j.percent_covered / 100
             results.append(
                 {
+                    "herbicide_type": params.get("herbicide_type"),
+                    "herbicide_name": params.get("herbicide_name"),
                     "jurisdiction": j.jurisdiction,
                     "jurisdiction_percent": j.percent_covered,
                     "invasive_plant": plant.invasive_plant,
                     "invasive_plant_percent": plant.percent_covered,
-                    "herbicide_name": herbicide_name,
-                    "herbicide_type": herbicide_type,
-                    "area_treated_sqm": plant_area_treated_sqm * multiplier,
-                    "undiluted_herbicide_used_l": undiluted_herbicide_used_l
-                    * multiplier,
-                    "percentage_area_covered": percentage_area_covered * multiplier,
+                    "amount_of_mix_used": trunc(
+                        (amount_mix_used_l * plant_percentage * multiplier)
+                    ),
+                    "dilution": trunc(dilution_percent * 100),
+                    "area_treated_sqm": trunc(plant_area_treated_sqm),
+                    "undiluted_herbicide_used_l": trunc(undiluted_herbicide_used_l),
+                    "percentage_area_covered": trunc(percentage_area_covered),
                 }
             )
 
@@ -250,26 +262,31 @@ def mSpecie_sLHerb_spray_usingDilutionPercent(
 def mSpecie_sGHerb_spray_usingProdAppRate(
     p: ApplicationRatePayload,
 ) -> List[CalculationResponseValues]:
+    # Calculation scenario: 5
     product_application_rate_lha = p.get("product_application_rate_lha")
     delivery_rate_of_mix = p.get("delivery_rate_of_mix")
-    dilution = (product_application_rate_lha / 1000 / delivery_rate_of_mix) * 100
+    amount_of_mix_used_l = p.get("amount_mix_used_l")
     area_m = p.get("area_m")
-    amount_mix_used_l = p.get("amount_mix_used_l")
-    herbicide_name = p.get("herbicide_name")
-    herbicide_type = p.get("herbicide_type")
 
-    results = []
-    for j in p.get("jurisdictions", []):
-        multiplier = j.percent_covered / 100
-        for plant in p.get("plants_treated", []):
-            amount_of_mix_used = amount_mix_used_l * (plant.percent_covered / 100)
-            area_treated_sqm = (
-                (amount_mix_used_l / delivery_rate_of_mix) * plant.percent_covered / 100
-            ) * HECTARE_TO_SQM
-            percentage_area_covered = trunc((area_treated_sqm / area_m) * 100)
-            undiluted_herbicide_used_l = trunc(
-                (dilution / 100) * amount_mix_used_l * (plant.percent_covered / 100)
-            )
+    dilution_percent = product_application_rate_lha / 1000 / delivery_rate_of_mix
+
+    results: List[CalculationResponseValues] = []
+    for plant in p.get("plants_treated", []):
+        plant_percent = plant.percent_covered / 100
+        amount_of_mix_used = amount_of_mix_used_l * plant_percent
+        area_treated_sqm = (
+            (amount_of_mix_used_l / delivery_rate_of_mix)
+            * plant_percent
+            * HECTARE_TO_SQM
+        )
+        percentage_area_covered = (area_treated_sqm / area_m) * 100
+        undiluted_herbicide_used_l = (
+            amount_of_mix_used_l * dilution_percent * plant_percent
+        )
+        for j in p.get("jurisdictions", []):
+            herbicide_name = p.get("herbicide_name")
+            herbicide_type = p.get("herbicide_type")
+            multiplier = j.percent_covered / 100
             results.append(
                 {
                     "jurisdiction": j.jurisdiction,
@@ -278,13 +295,16 @@ def mSpecie_sGHerb_spray_usingProdAppRate(
                     "invasive_plant_percent": plant.percent_covered,
                     "herbicide_name": herbicide_name,
                     "herbicide_type": herbicide_type,
-                    "amount_of_mix_used": trunc(amount_of_mix_used * multiplier),
                     "product_application_rate": product_application_rate_lha,
-                    "dilution": trunc(dilution),
+                    "amount_of_mix_used": trunc(amount_of_mix_used * multiplier),
+                    "dilution": trunc(dilution_percent * 100),
                     "area_treated_sqm": trunc(area_treated_sqm * multiplier),
-                    "undiluted_herbicide_used_l": undiluted_herbicide_used_l
-                    * multiplier,
-                    "percentage_area_covered": percentage_area_covered * multiplier,
+                    "undiluted_herbicide_used_l": trunc(
+                        undiluted_herbicide_used_l * multiplier
+                    ),
+                    "percentage_area_covered": trunc(
+                        percentage_area_covered * multiplier
+                    ),
                 }
             )
     return results
@@ -295,21 +315,21 @@ def mSpecie_sGHerb_spray_usingDilutionPercent(
 ) -> List[CalculationResponseValues]:
     area_m = p.get("area_m")
     amount_mix_used_l = p.get("amount_mix_used_l")
-    dilution_percent = p.get("dilution_percent")
+    dilution_percent = p.get("dilution_percent") / 100
     area_treated_sqm = p.get("area_treated_sqm")
     herbicide_name = p.get("herbicide_name")
     herbicide_type = p.get("herbicide_type")
-    results = []
-    for j in p.get("jurisdictions", []):
-        multiplier = j.percent_covered / 100
-        for plant in p.get("plants_treated"):
-            area_treated_by_plant = area_treated_sqm * (plant.percent_covered / 100)
-            percentage_area_covered = trunc((area_treated_by_plant / area_m) * 100)
-            undiluted_herbicide_used_l = trunc(
-                (dilution_percent / 100)
-                * amount_mix_used_l
-                * (plant.percent_covered / 100)
-            )
+    results: List[CalculationResponseValues] = []
+
+    for plant in p.get("plants_treated"):
+        plant_percentage = plant.percent_covered / 100
+        amount_mix_used_on_plant = amount_mix_used_l * plant_percentage
+        area_treated_by_plant = area_treated_sqm * plant_percentage
+        percentage_area_covered = (area_treated_by_plant / area_m) * 100
+        undiluted_herbicide_used_l = dilution_percent * amount_mix_used_on_plant
+
+        for j in p.get("jurisdictions", []):
+            multiplier = j.percent_covered / 100
             results.append(
                 {
                     "jurisdiction": j.jurisdiction,
@@ -318,10 +338,15 @@ def mSpecie_sGHerb_spray_usingDilutionPercent(
                     "invasive_plant_percent": plant.percent_covered,
                     "herbicide_name": herbicide_name,
                     "herbicide_type": herbicide_type,
+                    "dilution": dilution_percent * 100,
                     "area_treated_sqm": trunc(area_treated_by_plant * multiplier),
-                    "undiluted_herbicide_used_l": undiluted_herbicide_used_l
-                    * multiplier,
-                    "percentage_area_covered": percentage_area_covered * multiplier,
+                    "amount_of_mix_used": trunc(amount_mix_used_on_plant * multiplier),
+                    "undiluted_herbicide_used_l": trunc(
+                        undiluted_herbicide_used_l * multiplier
+                    ),
+                    "percentage_area_covered": trunc(
+                        percentage_area_covered * multiplier
+                    ),
                 }
             )
     return results
@@ -330,50 +355,58 @@ def mSpecie_sGHerb_spray_usingDilutionPercent(
 def mSpecie_mLGHerb_spray_usingProdAppRate(
     p: TankMixPayload,
 ) -> List[CalculationResponseValues]:
+    # Calculation scenario: 8-10 (11)
     area_m = p.get("area_m")
+    herbicides = p.get("herbicide", [])
+    num_herbicides = len(herbicides)
     amount_mix_used_l = p.get("amount_mix_used_l")
     delivery_rate = p.get("delivery_rate")
+    results: List[CalculationResponseValues] = []
 
-    results = []
-    for j in p.get("jurisdictions", []):
-        multiplier = j.percent_covered / 100
-        for plant in p.get("plants_treated"):
-            area_treated_sqm = (
-                (amount_mix_used_l / delivery_rate)
-                * (plant.percent_covered / 100)
-                * HECTARE_TO_SQM
-            )
-            area_covered_pct = (area_treated_sqm / area_m) * 100
+    for plant in p.get("plants_treated"):
+        plant_percent = plant.percent_covered / 100
+        plant_name = plant.invasive_plant
+        area_treated_sqm = (
+            (amount_mix_used_l / delivery_rate) * plant_percent * HECTARE_TO_SQM
+        )
+        area_covered_pct = (area_treated_sqm / area_m) * 100
 
-            for h in p.get("herbicide"):
-                if h.type.code == "G":
-                    dilution = (h.application_rate / 1000 / delivery_rate) * 100
-                elif h.type.code == "L":
-                    dilution = (h.application_rate / delivery_rate) * 100
-                else:
-                    raise ValueError(
-                        "Herbicide has an undefined type associated with it."
-                    )
+        for herbicide in herbicides:
+            if herbicide.type.code == "G":
+                dilution = herbicide.application_rate / 1000 / delivery_rate
+            elif herbicide.type.code == "L":
+                dilution = herbicide.application_rate / delivery_rate
+            else:
+                raise ValueError("Herbicide has an undefined type associated with it.")
 
-                undiluted_herbicide_used_l = (
-                    (dilution / 100) * amount_mix_used_l * plant.percent_covered
-                ) / 100
+            undiluted_herbicide_used_l = dilution * amount_mix_used_l * plant_percent
+
+            for j in p.get("jurisdictions", []):
+                multiplier = j.percent_covered / 100
+                # Sum fields are divided by the number of herbicides to assure the sum matches correctly
+                area_treated_for_result = area_treated_sqm / num_herbicides
+                amount_mix_used_for_result = amount_mix_used_l / num_herbicides
+                percent_area_covered_for_result = area_covered_pct / num_herbicides
                 results.append(
                     {
                         "jurisdiction": j.jurisdiction,
                         "jurisdiction_percent": j.percent_covered,
-                        "invasive_plant": plant.invasive_plant,
+                        "invasive_plant": plant_name,
                         "invasive_plant_percent": plant.percent_covered,
-                        "herbicide_name": h.name,
-                        "herbicide_type": h.type,
-                        "product_application_rate": h.application_rate,
-                        "amount_of_mix_used": trunc(amount_mix_used_l * multiplier),
-                        "dilution": trunc(dilution),
-                        "area_treated_sqm": trunc(area_treated_sqm * multiplier),
+                        "herbicide_name": herbicide.name,
+                        "herbicide_type": herbicide.type,
+                        "product_application_rate": herbicide.application_rate,
+                        "amount_of_mix_used": trunc(
+                            amount_mix_used_for_result * plant_percent * multiplier
+                        ),
+                        "dilution": trunc(dilution * 100),
+                        "area_treated_sqm": trunc(area_treated_for_result * multiplier),
                         "undiluted_herbicide_used_l": trunc(
                             undiluted_herbicide_used_l * multiplier
                         ),
-                        "percentage_area_covered": trunc(area_covered_pct * multiplier),
+                        "percentage_area_covered": trunc(
+                            percent_area_covered_for_result * multiplier
+                        ),
                     }
                 )
     return results
