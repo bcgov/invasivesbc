982804d78c83c3d1be95df029628b88b8055deb7
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-08-05 08:24:04 -0700

    Check subtype before assigning PAC to record

--- api-rework/invasives/api/legacy_db/mappings/participants.py
+++ api-rework/invasives/api/legacy_db/mappings/participants.py
@@ -1,5 +1,10 @@
 from api.legacy_db.model_serializer import LegacyActivity
-from api.models.activity import Activity, Participant, ActivityDataRecord
+from api.models.activity import (
+    Activity,
+    Participant,
+    ActivityDataRecord,
+    ActivitySubtypes,
+)
 
 
 def add_persons(
@@ -9,8 +14,12 @@ def add_persons(
     adr = ActivityDataRecord.objects.create(activity=new)
 
     for person in old.activity_payload.form_data.activity_type_data.activity_persons:
+        is_chem_treatment = old.activity_subtype in [
+            ActivitySubtypes.Treatment_Chemical_Plant_Terrestrial,
+            ActivitySubtypes.Treatment_Chemical_Plant_Aquatic,
+        ]
         Participant.objects.create(
             activity_data_record=adr,
             name=person.person_name,
-            pac_number=str(person.applicator_license),
+            pac_number=str(person.applicator_license) if is_chem_treatment else None,
         )
