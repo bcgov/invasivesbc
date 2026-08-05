9b13c442ecf5db871d745d9c440a7c52c9f53fcb
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-08-05 15:49:14 -0700

    Add linking records to activity serializer

--- api-rework/invasives/api/serializers/activity.py
+++ api-rework/invasives/api/serializers/activity.py
@@ -68,6 +68,7 @@ class BaseSerializer(serializers.ModelSerializer):
     subtype_data = serializers.SerializerMethodField()
     participants = serializers.SerializerMethodField()
     linked_activities = serializers.SerializerMethodField()
+    linking_activities = serializers.SerializerMethodField()
 
     shape = serializers.SerializerMethodField()
     centroid = serializers.SerializerMethodField()
@@ -82,6 +83,7 @@ class BaseSerializer(serializers.ModelSerializer):
             "created_by",
             "form_status",
             "linked_activities",
+            "linking_activities",
             # Record type details
             "type",
             "subtype",
@@ -156,6 +158,13 @@ class ActivitySerializer(BaseSerializer):
                 }
             )
         return arr
+
+    def get_linking_activities(self, obj):
+        from api.serializers import ActivityRecordsetRowSerializer
+
+        return ActivityRecordsetRowSerializer(
+            obj.activity_set.all().order_by("-date"), many=True
+        ).data
 
     def get_shape(self, obj: Activity):
         geojson_py_object = json.loads(obj.shape.json)
@@ -260,6 +269,9 @@ class DraftActivitySerializer(BaseSerializer):
                 }
             )
         return arr
+
+    def get_linking_activities(self, obj):
+        return []
 
     def get_shape(self, obj: DraftActivity):
         if not obj.shape:  # Shouldn't happen outside of Draft Records.
