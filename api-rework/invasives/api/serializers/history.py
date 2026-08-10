e1b13f1506323d22e89ea7d9a698679bec668d9d
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-08-10 08:11:47 -0700

    Create history serializer

---
+++ api-rework/invasives/api/serializers/history.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from api.models.audits import ActivityModificationRecord
+
+
+class HistorySerializer(serializers.ModelSerializer):
+    user = serializers.CharField(source="user.display_name")
+    activity = serializers.CharField(source="activity.short_id")
+
+    class Meta:
+        model = ActivityModificationRecord
+        fields = (
+            "user",
+            "version",
+            "activity",
+            "platform",
+            "diff",
+        )
