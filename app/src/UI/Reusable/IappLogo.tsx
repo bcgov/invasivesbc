29174fee2cb9e30f8fd600a98946a42597931d00
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-08-12 15:44:22 -0700

    Add IAPP Logo as component

---
+++ app/src/UI/Reusable/IappLogo.tsx
@@ -0,0 +1,11 @@
+import iappLogo from '/assets/iapp-logo.png';
+
+const IappLogo = () => (
+  <img
+    src={iappLogo}
+    alt="Invasive Alien Plant Program Logo"
+    style={{ objectFit: 'contain', maxWidth: '1.75rem', width: '100%' }}
+  />
+);
+
+export default IappLogo;
