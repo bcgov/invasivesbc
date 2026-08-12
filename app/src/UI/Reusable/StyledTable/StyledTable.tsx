5931e44a7473a611edcf2021c800bf603b39f7e8
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-08-12 10:16:19 -0700

    Update StyledTable to accept known table props

--- app/src/UI/Reusable/StyledTable/StyledTable.tsx
+++ app/src/UI/Reusable/StyledTable/StyledTable.tsx
@@ -1,10 +1,12 @@
-import { PropsWithChildren } from 'react';
+import { ComponentPropsWithoutRef, ComponentPropsWithRef } from 'react';
 import './styledTable.css';
 
-const StyledTable = ({ children }: PropsWithChildren) => {
+const StyledTable = ({ children, ...props }: ComponentPropsWithoutRef<'table'> | ComponentPropsWithRef<'table'>) => {
   return (
     <div className="table-wrapper">
-      <table className="table-inner">{children}</table>
+      <table className="table-inner" {...props}>
+        {children}
+      </table>
     </div>
   );
 };
