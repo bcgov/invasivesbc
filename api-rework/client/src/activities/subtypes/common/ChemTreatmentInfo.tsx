998bd38e855e08aa94e84970078ce60e971af714
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-08-12 13:40:29 -0700

    update client chemtreatmentinfo to new params

--- api-rework/client/src/activities/subtypes/common/ChemTreatmentInfo.tsx
+++ api-rework/client/src/activities/subtypes/common/ChemTreatmentInfo.tsx
@@ -4,124 +4,53 @@ import TextInput from 'common-components/inputs/TextInput';
 import Fieldset from 'common-components/inputs/Fieldset';
 
 const ChemTreatmentInfo = ({ subtypeData }: SubtypeData) => {
+  const { context, treatment_context } = subtypeData;
+
   return (
     <>
       <NearestWells data={subtypeData?.well_entries} />
       <Fieldset label={'Treatment Context'}>
-        <TextInput label={'service license number'} value={subtypeData?.context?.pesticide_employer_code} />
-        <TextInput label={'pesticide use permit'} value={subtypeData?.context?.pesticide_use_permit} />
-        <TextInput label={'pest management plan'} value={subtypeData?.context?.pest_management_plan} />
-        <TextInput label={'pest management plan manual'} value={subtypeData?.context?.pest_management_plan_manual} />
-        <TextInput label={'temperature (c)'} value={subtypeData?.context?.temperature_c} />
-        <TextInput label={'wind speed kmh'} value={subtypeData?.context?.wind_speed_kmh} />
-        <TextInput label={'application start time'} value={subtypeData?.context?.application_start_time} />
-        <TextInput label={'wind direction'} value={subtypeData?.context?.wind_direction} />
-        <TextInput label={'humidity'} value={subtypeData?.context?.humidity} />
-        <TextInput label={'treatment notice signs'} value={subtypeData?.context?.treatment_notice_signs} />
-        <TextInput label={'precautionary statement'} value={subtypeData?.context?.precautionary_statement} />
-        <TextInput label={'ntz reduction'} value={subtypeData?.context?.ntz_reduction ? 'Yes' : 'No'} />
-        <TextInput label={'rationale for ntz reduction'} value={subtypeData?.context?.rationale_for_ntz_reduction} />
+        <TextInput label={'service license number'} value={context?.pesticide_employer_code} />
+        <TextInput label={'pesticide use permit'} value={context?.pesticide_use_permit} />
+        <TextInput label={'pest management plan'} value={context?.pest_management_plan} />
+        <TextInput label={'pest management plan manual'} value={context?.pest_management_plan_manual} />
+        <TextInput label={'temperature (c)'} value={context?.temperature_c} />
+        <TextInput label={'wind speed kmh'} value={context?.wind_speed_kmh} />
+        <TextInput label={'application start time'} value={context?.application_start_time} />
+        <TextInput label={'wind direction'} value={context?.wind_direction} />
+        <TextInput label={'humidity'} value={context?.humidity} />
+        <TextInput label={'treatment notice signs'} value={context?.treatment_notice_signs} />
+        <TextInput label={'precautionary statement'} value={context?.precautionary_statement} />
+        <TextInput label={'ntz reduction'} value={context?.ntz_reduction ? 'Yes' : 'No'} />
+        <TextInput label={'rationale for ntz reduction'} value={context?.rationale_for_ntz_reduction} />
         <TextInput
           label={'additional unmapped well water'}
           value={subtypeData?.additional_unmapped_well_water ? 'Yes' : 'No'}
         />
-      </Fieldset>
-      <Fieldset label={'Pest injury threshold Determination'}>
-        <TextInput value={subtypeData?.pest_injury_threshold_determination ? 'Yes' : 'No'} />
-      </Fieldset>
-      <Fieldset label={'Chem Treatment Details'}>
-        <TextInput label={'Tank Mix'} value={subtypeData.detail?.tank_mix ? 'Yes' : 'No'} />
-        <TextInput label={'Chemical Application Method'} value={subtypeData.detail?.chemical_application_method} />
-        <TextInput label={'calculation type'} value={subtypeData.detail?.calculation_result?.calculation_type} />
-        <TextInput label={'area treated sqm'} value={subtypeData.detail?.calculation_result?.area_treated_sqm} />
-        <TextInput
-          label={'percent area covered'}
-          value={subtypeData.detail?.calculation_result?.percent_area_covered}
-        />
-        <TextInput
-          label={'amount of undiluted herbicide used (l)'}
-          value={subtypeData.detail?.calculation_result?.amount_of_undiluted_herbicide_used_liters}
-        />
-        <TextInput label={'Dilution'} value={subtypeData.detail?.calculation_result?.dilution} />
-        <TextInput
-          label={'skip application rate validation'}
-          value={subtypeData?.detail?.skip_application_rate_validation ? 'Yes' : 'No'}
-        />
-      </Fieldset>
-      <Fieldset label={'Calculation Results'}>
-        <TextInput label={'tank mix details'} value={subtypeData?.detail?.tank_mix_details} />
-        <TextInput
-          label={'legacy object had error flag set?'}
-          value={subtypeData?.detail?.legacy_object_had_error_flag_set ? 'Yes' : 'No'}
-        />
-        {subtypeData.detail?.calculation_result?.per_plant_calculations?.length > 0 && (
-          <Fieldset label={'Per Plant calculations'}>
-            <table>
-              <thead>
-                <tr>
-                  {Object.keys(subtypeData.detail.calculation_result.per_plant_calculations[0]).map((k) => (
-                    <th>{k.replaceAll('_', ' ')}</th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {subtypeData.detail?.calculation_result?.per_plant_calculations.map((r) => (
-                  <tr>
-                    {Object.entries(r).map(([k, v]) => (
-                      <td key={k}>{v}</td>
-                    ))}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </Fieldset>
-        )}
-        <Fieldset label={'Invasive Plants'} small>
-          {[
-            ...(subtypeData?.detail?.terrestrial_invasive_plants ?? []),
-            ...(subtypeData?.detail?.aquatic_invasive_plants ?? [])
-          ].map((p) => (
-            <div className="group-wrap" key={p.index}>
-              <TextInput label={'Index'} value={p.index} />
-              <TextInput label={'Invasive Plant'} value={p.invasive_plant} />
-              <TextInput label={'Area Covered %'} value={p.percent_area_covered} />
+        <Fieldset label="Invasive Plants" small>
+          {treatment_context.plants_treated.map((pt) => (
+            <div className="group-wrap">
+              <TextInput label="invasive plant" value={pt?.invasive_plant} />
+              <TextInput label="Percent Covered" value={pt.percent_covered} />
             </div>
           ))}
         </Fieldset>
-        <Fieldset label={'Herbicides'} small>
-          {subtypeData?.detail?.herbicides?.map((h) => (
-            <div className="group-wrap" key={h.index}>
-              <TextInput label={'index'} value={h.index} />
-              <TextInput label={'dilution'} value={h.dilution} />
-              <TextInput label={'amount of mix'} value={h.amount_of_mix} />
-              <TextInput label={'herbicide type'} value={h.herbicide_type} />
-              <TextInput label={'liquid herbicide'} value={h.liquid_herbicide} />
-              <TextInput label={'granular herbicide'} value={h.granular_herbicide} />
+        <Fieldset label="Herbicides" small>
+          {treatment_context.herbicide.map(({ type, name, application_rate }) => (
+            <div className="group-wrap">
+              <TextInput label="Herbicide Type" value={type} />
+              <TextInput label="Herbicide Name" value={name} />
+              <TextInput label="Application Rate" value={application_rate} />
             </div>
           ))}
         </Fieldset>
-        {subtypeData.detail?.calculation_result?.per_herbicide_calculations?.length > 0 && (
-          <Fieldset label={'Per Herbicide Calculations'}>
-            <table>
-              <thead>
-                <tr>
-                  {Object.keys(subtypeData.detail.calculation_result.per_herbicide_calculations[0]).map((k) => (
-                    <th key={k}>{k.replaceAll('_', ' ')}</th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {subtypeData.detail?.calculation_result?.per_herbicide_calculations.map((r) => (
-                  <tr>
-                    {Object.entries(r).map(([k, v]) => (
-                      <td key={k}>{v}</td>
-                    ))}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </Fieldset>
-        )}
+        <TextInput label="Amount of Mix Used L" value={treatment_context?.amount_of_mix_used_l} />
+        <TextInput label="Application Method" value={treatment_context?.application_method} />
+        <TextInput label="Area Treated SQM" value={treatment_context?.area_treated_sqm} />
+        <TextInput label="Calculation Type" value={treatment_context?.calculation_type} />
+        <TextInput label="Delivery Rate" value={treatment_context?.delivery_rate} />
+        <TextInput label="Dilution Percent" value={treatment_context?.dilution_percent} />
+        <TextInput label="Tank Mix?" value={treatment_context?.tank_mix ? 'Yes' : 'No'} />
       </Fieldset>
     </>
   );
