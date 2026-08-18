1acb09ae4cfe483991d213f55572cc99f4c9f42b
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-08-18 11:19:05 -0700

    apply distinct entries to mechanical treatments
    Signed-off-by: LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>

--- api-rework/invasives/api/protocol/activity/plant_subtypes/treatment_mechanical_terrestrial.py
+++ api-rework/invasives/api/protocol/activity/plant_subtypes/treatment_mechanical_terrestrial.py
@@ -1,6 +1,6 @@
 from typing import List, Literal, Optional
 from pydantic import Field, field_validator
-from api.protocol.activity.validators.no_repeat_key import no_repeat_key
+from api.protocol.activity.validators.distinct_entries import distinct_entries
 from api.protocol.activity.plant_subtypes.base_form_schema import (
     BaseFormSchema,
     CleanSchema,
@@ -40,8 +40,12 @@ class SubtypeData(DraftSubtypeData):
 
     @field_validator("entries")
     @classmethod
-    def unique_plants(cls, v):
-        return no_repeat_key(v, key="invasive_plant", key_label="Invasive Plant")
+    def unique_entries(cls, v):
+        return distinct_entries(
+            v,
+            unique_keys=["invasive_plant", "mechanical_method"],
+            error_message="Entries must contain a unique Invasive Plant / Mechanical Method combination",
+        )
 
 
 class DraftTreatmentMechanicalTerrestrial(DraftBaseFormSchema):
