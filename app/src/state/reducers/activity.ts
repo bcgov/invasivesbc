3b8558538fae756d2e1481ef8a86d423e57c9e5f
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-08-19 13:23:15 -0700

    force persistence of created_by in clearState action

--- app/src/state/reducers/activity.ts
+++ app/src/state/reducers/activity.ts
@@ -243,10 +243,9 @@ function createActivityReducer() {
       } else if (FormActions.clearFormState.match(action) && draftState.formState) {
         delete draftState.geometry_details;
         delete draftState.wellsInRecordArea;
-
         if (draftState.formType) {
           Object.assign(draftState.formState, {
-            ...getDefaultFormState(draftState.formType),
+            ...getDefaultFormState(draftState.formType, draftState.formState.created_by),
             id: draftState.formId,
             subtype: draftState.formType
           });
