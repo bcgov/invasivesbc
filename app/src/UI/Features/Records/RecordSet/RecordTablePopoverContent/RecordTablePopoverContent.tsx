bdf2ce733f8d04774c34dc47980f75a5ce3966e8
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-08-19 09:31:11 -0700

    Change button type from mui, apply hover css for contained buttons

--- app/src/UI/Features/Records/RecordSet/RecordTablePopoverContent/RecordTablePopoverContent.tsx
+++ app/src/UI/Features/Records/RecordSet/RecordTablePopoverContent/RecordTablePopoverContent.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@mui/material';
 import './RecordTablePopoverContent.css';
 import { RecordSetType } from 'interfaces/UserRecordSet';
 import { useDispatch } from 'utils/use_selector';
@@ -6,8 +5,8 @@ import UserSettings from 'state/actions/userSettings/UserSettings';
 import { GeoJSON } from 'geojson';
 import { MobileOnly } from 'UI/Reusable/Predicates/MobileOnly';
 import { useNavigate } from 'react-router';
-import { Debug } from 'UI/Reusable/Predicates/Debug';
-import { BugReport } from '@mui/icons-material';
+import DebugButton from '../../Activity/forms/debug/DebugButton';
+import Button from 'UI/Reusable/Button/Button';
 
 /**
  * @property { string } recordDisplayId Short ID / Site ID for a Record, displayed in the Popover
@@ -68,11 +67,9 @@ const RecordTablePopoverContent = ({ recordDisplayId: id, recordLookupId, record
         Open
       </Button>
       {/* // TODO: Remove RJSF Option */}
-      <Debug>
-        <Button onClick={handleOpenRecordInRJSF} variant="contained">
-          <BugReport /> Open <Debug>- RJSF Form</Debug>
-        </Button>
-      </Debug>
+      <DebugButton onClick={handleOpenRecordInRJSF} variant="contained">
+        Open - RJSF Form
+      </DebugButton>
       {!!geom && (
         <Button onClick={handleMarkGeometryOnMap.bind(this, true)} variant="contained">
           Pan to record
