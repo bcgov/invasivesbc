ad30cfcebe846c373381e74ecc80a407a1e93646
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-09-01 08:30:45 -0700

    filteredServiceLicenseCodes check Edit permission over role

--- app/src/UI/Features/Records/Activity/forms/plant/hooks/useFilteredServiceLicenseCodes.ts
+++ app/src/UI/Features/Records/Activity/forms/plant/hooks/useFilteredServiceLicenseCodes.ts
@@ -1,17 +1,13 @@
 import { useMemo } from 'react';
-import { Role } from 'constants/roles';
 import { useSelector } from 'utils/use_selector';
 
 const useFilteredServiceLicenseCodes = (disabled: boolean) => {
   const NOT_REQUIRED = 'NRQ';
   const codes = useSelector((state) => state.ActivityPage.formCodes?.ServiceLicenseNumberAndCompany);
   const user = useSelector((state) => state.Auth?.extendedInfo);
-  // TODO: Link to Permissions instead of role specific.
-  const isUserMasterAdmin = useSelector((state) =>
-    state.Auth?.roles.some((r) => r.role_name === Role.MASTER_ADMINISTRATOR)
-  );
+  const userCanEdit = useSelector((state) => state.ActivityPage.recordActions?.includes('EDIT'));
 
-  // Extract Servuce License numbers from user info
+  // Extract Service License numbers from user info
   const userServiceLicenses = useMemo(() => {
     const licenses: Array<string> = [];
     if (user.pac_service_number_1) licenses.push(user.pac_service_number_1 as string);
@@ -21,9 +17,9 @@ const useFilteredServiceLicenseCodes = (disabled: boolean) => {
 
   // If a non-administrator is creating a form. limit options to only fields they have assigned to their profile.
   const filteredCodes = useMemo(() => {
-    if (disabled || isUserMasterAdmin) return codes;
+    if (disabled || !userCanEdit) return codes;
     return codes.filter(({ code }) => userServiceLicenses.includes(code as string) || code === NOT_REQUIRED);
-  }, [disabled, codes, isUserMasterAdmin, userServiceLicenses]);
+  }, [disabled, codes, userCanEdit, userServiceLicenses]);
 
   return { serviceLicenseCodes: filteredCodes };
 };
