7df9874c1618bb09a5240a60888e16cd98481895
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-09-22 11:35:51 -0700

    remove old csv annotations

--- api-rework/invasives/api/configs/exports/helpers.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.postgres.aggregates import StringAgg
-
-
-def agg(path, **kwargs):
-    """
-    Shorthand StringAgg to reduce repeating delimiter and distinct arguments
-    """
-    return StringAgg(path, delimiter=", ", distinct=True, **kwargs)
