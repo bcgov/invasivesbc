f8f812c87e45ac9c483a473c39094846ffe6470d
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2026-09-24 14:48:57 -0700

    simplify CreateTeamSchema to array of strings

--- api-rework/invasives/api/protocol/teams/create_team.py
+++ api-rework/invasives/api/protocol/teams/create_team.py
@@ -4,15 +4,8 @@ from pydantic import Field, ConfigDict
 from api.protocol.activity.validators.code_validation import FundingAgencyCodeType
 
 
-class FundingAgency(Schema):
-    model_config = ConfigDict(arbitrary_types_allowed=True)
-    """Single Funding Agency Entry, Casts alias to match the incoming payload to the db column"""
-
-    agency: FundingAgencyCodeType
-
-
 class CreateTeamSchema(Schema):
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
     name: str
-    agencies: List[FundingAgency] = Field(min_length=1)
+    agencies: List[FundingAgencyCodeType] = Field(min_length=1)
